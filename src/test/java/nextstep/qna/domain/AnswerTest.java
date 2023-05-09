76ca9f2f70b7011dcc8d1b8bdc673d816cded5c0
saerang <lsg10119@gamail.com>
2023-05-09 22:33:34 +0900

    refactor: fixture 사용

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,9 @@
 package nextstep.qna.domain;
 
+import nextstep.Fixtures;
 import nextstep.users.domain.NsUserTest;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, Fixtures.createQuestion(NsUserTest.JAVAJIGI), "Answers Contents1");
+    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, Fixtures.createQuestion(NsUserTest.JAVAJIGI), "Answers Contents2");
 }
