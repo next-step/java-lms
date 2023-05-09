04aaca07f48caeea0f2f767512c6314fd8ced6a4
saerang <lsg10119@gamail.com>
2023-05-09 23:22:48 +0900

    refactor: answer 에 delete 메서드 추가

--- src/main/java/nextstep/qna/domain/Answer.java
+++ src/main/java/nextstep/qna/domain/Answer.java
@@ -72,6 +72,10 @@ public class Answer {
         this.question = question;
     }
 
+    public void delete() {
+        this.deleted = true;
+    }
+
     @Override
     public String toString() {
         return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -2,8 +2,23 @@ package nextstep.qna.domain;
 
 import nextstep.Fixtures;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static nextstep.Fixtures.createAnswer;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, Fixtures.createQuestion(NsUserTest.JAVAJIGI), "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, Fixtures.createQuestion(NsUserTest.JAVAJIGI), "Answers Contents2");
+
+    @Test
+    @DisplayName("answer 를 삭제한다.")
+    void test01() {
+        Answer answer = createAnswer(NsUserTest.JAVAJIGI);
+
+        answer.delete();
+
+        assertThat(answer.isDeleted()).isEqualTo(true);
+    }
 }
