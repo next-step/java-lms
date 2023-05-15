34dea9c50331747795f18b0174e68985acc01187
right1203 <right1203@gmail.com>
2023-05-15 21:10:17 +0900

    refactor: 중간에 삭제된 경우, 원래의 Answer 상태를 되살리도록 개선

--- src/main/java/nextstep/qna/domain/Answers.java
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -6,30 +6,69 @@ import nextstep.users.domain.NsUser;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+
 public class Answers {
-    private List<Answer> answers;
+    private final List<Answer> answers;
+
+    public Answers() {
+        this(new ArrayList<>());
+    }
 
     public Answers(List<Answer> answers) {
         this.answers = answers;
     }
 
+    public Answers(Answer... answers) {
+        this.answers = List.of(answers);
+    }
 
     public void add(Answer answer) {
         answers.add(answer);
     }
 
-    public List<Answer> getAnswers() {
-        return answers;
-    }
-
-    public List<DeleteHistory> deleteAll(NsUser requestUser) throws CannotDeleteException {
-        List<DeleteHistory> deleteHistories = new ArrayList<>();
-        for (Answer answer : answers) {
-            DeleteHistory delete = answer.delete(requestUser);
-            deleteHistories.add(delete);
+    public DeleteHistories deleteAll(NsUser requestUser) throws CannotDeleteException {
+        DeleteHistories deleteHistories = new DeleteHistories();
+        List<Answer> aliveAnswers = getAliveAnswers();
+        for (Answer answer : aliveAnswers) {
+            deleteHistories.add(tryDelete(requestUser, answer, aliveAnswers));
         }
         return deleteHistories;
+    }
+
+    private DeleteHistory tryDelete(NsUser requestUser, Answer answer, List<Answer> restoreTarget) throws CannotDeleteException {
+        try {
+            return answer.delete(requestUser);
+        } catch (CannotDeleteException e) {
+            restoreAnswers(restoreTarget);
+            throw new CannotDeleteException(e.getMessage());
+        }
+    }
+
+    private List<Answer> getAliveAnswers() {
+        return answers.stream()
+                .filter(answer -> !answer.isDeleted())
+                .collect(Collectors.toList());
+    }
+
+    private void restoreAnswers(List<Answer> aliveAnswers) {
+        for (Answer aliveAnswer : aliveAnswers) {
+            aliveAnswer.setDeleted(false);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answers answers1 = (Answers) o;
+        return Objects.equals(answers, answers1.answers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(answers);
     }
 }
---
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -0,0 +1,82 @@
+package nextstep.qna.domain;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnswersTest {
+    @AfterEach
+    void tearDown() {
+        AnswerTest.A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    }
+
+    @Test
+    @DisplayName("답변 추가")
+    void add() {
+        // given
+        Answers answers = new Answers();
+        Answer answer = new Answer();
+
+        // when
+        answers.add(answer);
+
+        // then
+        assertThat(answers).isEqualTo(new Answers(answer));
+    }
+
+    @Test
+    @DisplayName("답변 삭제")
+    void delete() throws CannotDeleteException {
+        // given
+        Answers answers = new Answers();
+        Answer answer = AnswerTest.A1;
+        answers.add(answer);
+
+        // when
+        DeleteHistories deleteHistories = answers.deleteAll(answer.getWriter());
+
+        // then
+        assertThat(deleteHistories).isEqualTo(new DeleteHistories(
+                new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now())
+        ));
+    }
+
+    @Test
+    @DisplayName("답변 삭제 - 답변 중 이미 삭제된 답변이 있는 경우")
+    void delete1() throws CannotDeleteException {
+        // given
+        Answer answer = AnswerTest.A1;
+        Answer answer2 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents2");
+        Answers answers = new Answers(answer, answer2);
+
+        // when
+        answer.delete(answer.getWriter());
+
+        // then
+        assertThat(answers.deleteAll(answer.getWriter())).isEqualTo(new DeleteHistories(
+                new DeleteHistory(ContentType.ANSWER, answer2.getId(), answer2.getWriter(), LocalDateTime.now())
+        ));
+    }
+
+    @Test
+    @DisplayName("답변 삭제 - 삭제 중 실패")
+    void delete2() throws CannotDeleteException {
+        // given
+        Answer answer = AnswerTest.A1;
+        Answer answer2 = AnswerTest.A2;
+        Answers answers = new Answers(answer, answer2);
+
+        // when
+        assertThatThrownBy(() -> answers.deleteAll(answer.getWriter()))
+                .isInstanceOf(CannotDeleteException.class);
+        assertThat(answer.isDeleted()).isFalse();
+    }
+}
