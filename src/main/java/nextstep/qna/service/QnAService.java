a49c8b9d0bb14a5263d692be3c5a75974ca72f38
right1203 <right1203@gmail.com>
2023-05-15 21:10:43 +0900

    feat: DeleteHistory의 일급 컬렉션

--- src/main/java/nextstep/qna/service/QnAService.java
+++ src/main/java/nextstep/qna/service/QnAService.java
@@ -26,7 +26,7 @@ public class QnAService {
     @Transactional
     public void deleteQuestion(NsUser loginUser, long questionId) throws CannotDeleteException {
         Question question = questionRepository.findById(questionId).orElseThrow(NotFoundException::new);
-        List<DeleteHistory> deleteHistories = question.delete(loginUser);
-        deleteHistoryService.saveAll(deleteHistories);
+        DeleteHistories deleteHistories = question.delete(loginUser);
+        deleteHistoryService.saveAll(deleteHistories.histories());
     }
 }
