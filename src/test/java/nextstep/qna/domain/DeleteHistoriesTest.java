d74ab79791dc4bb8bfa0f0dd993bee0ed47e7b88
right1203 <right1203@gmail.com>
2023-05-15 21:10:35 +0900

    feat: DeleteHistory의 일급 컬렉션

---
+++ src/test/java/nextstep/qna/domain/DeleteHistoriesTest.java
@@ -0,0 +1,53 @@
+package nextstep.qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteHistoriesTest {
+    @Test
+    @DisplayName("DeleteHistories 생성")
+    void create() {
+        // given
+        DeleteHistories deleteHistories = new DeleteHistories(
+                new DeleteHistory(), new DeleteHistory()
+        );
+
+        // when
+        assertThat(deleteHistories.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("DeleteHistory 추가")
+    void add() {
+        // given
+        DeleteHistories deleteHistories = new DeleteHistories();
+        DeleteHistory deleteHistory = new DeleteHistory();
+
+        // when
+        deleteHistories.add(deleteHistory);
+
+        // then
+        assertThat(deleteHistories).isEqualTo(new DeleteHistories(deleteHistory));
+    }
+
+    @Test
+    @DisplayName("DeleteHistories 병합")
+    void addAll() {
+        // given
+        DeleteHistories deleteHistories = new DeleteHistories();
+        DeleteHistory deleteHistory = new DeleteHistory();
+        DeleteHistory deleteHistory2 = new DeleteHistory();
+
+        // when
+        deleteHistories.addAll(new DeleteHistories(deleteHistory, deleteHistory2));
+
+        // then
+        assertThat(deleteHistories).isEqualTo(new DeleteHistories(deleteHistory, deleteHistory2));
+    }
+
+}
