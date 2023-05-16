2ca5455a09df8bef645d6e4b48e59505d4ea662a
donggada <koem0425@naver.com>
2023-05-16 23:24:06 +0900

    step1
    answrs for 문 돌리는 로직도 도메인안에서 구현 해야하나요?
    
    excption throw 하는 메서드도 도메인 안에서 구현해야하나요?
    만약 구현한다면 stream 으로 구현하고 싶은데 어떻게하는게 좋을까요?

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,18 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    public void deleteAnswer() {
+        Assertions.assertThat(A1.deleteAnswers()).isEqualTo(new DeleteHistory(ContentType.ANSWER, null, A1.getWriter(), LocalDateTime.now()));
+        Assertions.assertThat(A2.deleteAnswers()).isEqualTo(new DeleteHistory(ContentType.ANSWER, null, A2.getWriter(), LocalDateTime.now()));
+    }
 }
