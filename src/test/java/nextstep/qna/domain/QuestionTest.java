07458bfc141e69569f1b69c422b2db5c0f93aeca
didrlgus <rlgusdid@naver.com>
2023-05-17 09:12:58 +0900

    refactor: 도메인으로 비즈니스 로직 이동
    - 작성자와 삭제시도 유저가 다른 경우 예외 로직 Question 도메인으로 이동
    - QuestionTest에서 TC 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("질문 작성자와 삭제시도 유저가 다르다.")
+    @Test
+    void test1() {
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
 }
