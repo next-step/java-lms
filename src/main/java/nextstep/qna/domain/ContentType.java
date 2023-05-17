f555efbbfdb3b9d914bd567e638ca5aa7e30c6ff
진우 <nice7677@naver.com>
2023-05-17 12:40:14 +0900

    feat: ContentType 찾기 추가
    (cherry picked from commit 65750b9c5015bcb874a77cf669a3f40aa0dc6e58)

--- src/main/java/nextstep/qna/domain/ContentType.java
+++ src/main/java/nextstep/qna/domain/ContentType.java
@@ -2,4 +2,12 @@ package nextstep.qna.domain;
 
 public enum ContentType {
     QUESTION, ANSWER;
+
+    public static ContentType findByContent(Object object) {
+        if (object instanceof Question) {
+            return QUESTION;
+        }
+        return ANSWER;
+    }
+
 }
