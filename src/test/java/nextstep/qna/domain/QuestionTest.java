f7b3d0ee06dc39961f460023aa2b9953e00a64e4
dkswnkk <jjoy9810@gmail.com>
2023-05-17 23:57:32 +0900

    test: 테스트 코드 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,24 @@
 package nextstep.qna.domain;
 
-import nextstep.users.domain.NsUserTest;
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.User;
+import nextstep.users.domain.UserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static final Question QUESTION_BY_JAVAJIGI = new Question(UserTest.JAVAJIGI, "title1", "contents1");
+    public static final Question QUESTION_BY_SANJIGI = new Question(UserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("답변이 없는 경우 질문 삭제가 가능하다.")
+    @Test
+    void question_can_be_deleted_if_no_answers_exist() throws CannotDeleteException {
+        User writer = UserTest.JAVAJIGI;
+        Question question = QUESTION_BY_JAVAJIGI;
+
+        question.delete(writer);
+
+        Assertions.assertThat(question.isDeleted()).isTrue();
+    }
 }
