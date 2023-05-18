0ef7bcba6585149835200cbf257dd7c797b081c7
dkswnkk <jjoy9810@gmail.com>
2023-05-19 00:18:33 +0900

    feat: 강의 시간 검증 Error 추가

---
+++ src/main/java/nextstep/courses/exception/InvalidSessionDateTimeException.java
@@ -0,0 +1,7 @@
+package nextstep.courses.exception;
+
+public class InvalidSessionDateTimeException extends Exception {
+    public InvalidSessionDateTimeException(String message) {
+        super(message);
+    }
+}
