1b387e7f9e538cbd74d8612a992dba0380174613
dkswnkk <jjoy9810@gmail.com>
2023-05-19 00:18:24 +0900

    feat: 강의 등록 Error 추가

---
+++ src/main/java/nextstep/courses/exception/SessionEnrollmentException.java
@@ -0,0 +1,7 @@
+package nextstep.courses.exception;
+
+public class SessionEnrollmentException extends Exception {
+    public SessionEnrollmentException(String message) {
+        super(message);
+    }
+}
