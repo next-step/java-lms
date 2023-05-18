bcb536090302d15850240f78356e87e704960ed2
dkswnkk <jjoy9810@gmail.com>
2023-05-19 00:19:04 +0900

    feat: Image 엔티티 추가

---
+++ src/main/java/nextstep/courses/domain/Image.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain;
+
+import nextstep.courses.domain.enums.ImageType;
+
+public class Image extends BaseEntity {
+    private Long id;
+
+    private String name;
+
+    private String uri;
+
+    private Long size;
+
+    private ImageType type;
+}
