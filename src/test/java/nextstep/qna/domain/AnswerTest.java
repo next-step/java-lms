492e594e2d3e5120656479a896d5b76712126e6d
hoyeoon <hoyeon.backend@gmail.com>
2023-05-19 01:45:15 +0900

    test: 다른 사람이 쓴 답변이 있어 삭제할 수 없습니다 예외처리

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,23 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static nextstep.qna.domain.QuestionTest.Q1;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, Q1, "Answers Contents2");
+
+    @Test
+    void 다른_사람이_쓴_답변이_있어_삭제할수_없습니다_예외() {
+        Q1.addAnswer(A1);
+        Q1.addAnswer(A2);
+
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
