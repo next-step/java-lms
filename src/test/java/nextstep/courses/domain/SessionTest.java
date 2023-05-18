5a1523578e6f352745ff1f8a25a8a663068b3ab2
dkswnkk <jjoy9810@gmail.com>
2023-05-19 00:20:46 +0900

    test: 테스트 코드 작성

---
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -0,0 +1,53 @@
+package nextstep.courses.domain;
+
+import nextstep.courses.domain.enums.SessionStatus;
+import nextstep.courses.domain.enums.SessionType;
+import nextstep.courses.exception.InvalidSessionDateTimeException;
+import nextstep.courses.exception.SessionEnrollmentException;
+import nextstep.users.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionTest {
+
+    @Test
+    @DisplayName("강의 시작일이 종요일보다 늦으면 에외를 던진다.")
+    void create_session_with_invalid_date_time_should_throw_exception() {
+        LocalDateTime openingDateTime = LocalDateTime.of(2023, 5, 1, 0, 0);
+        LocalDateTime closingDateTime = LocalDateTime.of(2023, 4, 1, 0, 0);
+
+        assertThrows(InvalidSessionDateTimeException.class, () ->
+                Session.create("1st", new Image(), openingDateTime, closingDateTime,
+                        SessionType.FREE, SessionStatus.RECRUITING, 100));
+    }
+
+    @DisplayName("강의가 모집중이 아닌데 수강 등록을 하면 예외를 던진다.")
+    @Test
+    void enroll_when_session_is_not_recruiting_should_throw_exception() throws InvalidSessionDateTimeException {
+        LocalDateTime openingDateTime = LocalDateTime.of(2023, 5, 1, 0, 0);
+        LocalDateTime closingDateTime = LocalDateTime.of(2023, 6, 1, 0, 0);
+        Session session = Session.create("1st", new Image(), openingDateTime, closingDateTime,
+                SessionType.FREE, SessionStatus.ENDED, 100);
+        User user = new User(1L, "username", "password", "name", "email");
+
+        assertThrows(SessionEnrollmentException.class, () -> session.enroll(user));
+    }
+
+    @DisplayName("강의 최대 수강 인원을 넘어선 수강신청을 하면 예외를 던진다.")
+    @Test
+    void enroll_when_session_is_full_should_throw_exception() throws InvalidSessionDateTimeException, SessionEnrollmentException {
+        LocalDateTime openingDateTime = LocalDateTime.of(2023, 5, 1, 0, 0);
+        LocalDateTime closingDateTime = LocalDateTime.of(2023, 6, 1, 0, 0);
+        Session session = Session.create("1st", new Image(), openingDateTime, closingDateTime,
+                SessionType.FREE, SessionStatus.RECRUITING, 0);
+        User user = new User(1L, "username1", "password1", "name1", "email1");
+
+
+        assertThrows(SessionEnrollmentException.class, () -> session.enroll(user));
+    }
+
+}
