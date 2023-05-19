9832de7f3d39a0e91e43def27c1c8f25f9ab48a3
soochan <tncksdl05@gmail.com>
2023-05-20 02:20:58 +0900

    test : 질문 삭제 시, 질문 삭제 히스토록 목록과 답변 삭제 히스토록 목록을 합친 결과를 확인하는 테스트 코드 추가

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -3,11 +3,18 @@ package nextstep.qna.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static Answer A1 = new Answer(0L, NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static Answer A2 = new Answer(1L, NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @BeforeEach
+    void tearDown() {
+        A1 = new Answer(0L, NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        A2 = new Answer(1L, NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    }
 
 
     @Test
@@ -30,7 +37,7 @@ public class AnswerTest {
     void 답변_삭제시_history_생성_검증() {
         DeleteHistory deleted = A1.deleted();
 
-        DeleteHistory expected = new DeleteHistory(ContentType.ANSWER,null, NsUserTest.JAVAJIGI);
+        DeleteHistory expected = new DeleteHistory(ContentType.ANSWER,0L, NsUserTest.JAVAJIGI);
 
         assertThat(A1.isDeleted()).isTrue();
         assertThat(deleted).isEqualTo(expected);
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -10,16 +10,15 @@ import org.junit.jupiter.api.Test;
 
 
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+    public static Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
 
     @BeforeEach
-    void setUp() {
-        Q1.addAnswer(AnswerTest.A1);
-
-        Q2.addAnswer(AnswerTest.A2);
-        Q2.addAnswer(AnswerTest.A1);
+    void tearDown() {
+        Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
     }
+
 
     @Test
     void 로그인_사용자와_질문한_사람이_다른경우_예외_확인() {
@@ -42,6 +41,9 @@ public class QuestionTest {
 
     @Test
     void 해당_질문에_다른_사람이_작성한_답변이_있으면_예외_확인() {
+        Q2.addAnswer(AnswerTest.A2);
+        Q2.addAnswer(AnswerTest.A1);
+
         assertThatThrownBy(Q2::validateHasAnswerByOtherUser).isInstanceOf(CannotDeleteException.class);
     }
 
@@ -68,6 +70,9 @@ public class QuestionTest {
 
     @Test
     void 질문_작성자와_로그인한_사람이_같고_다른사람_답변이_있는_경우_삭제_불가_검증() {
+        Q2.addAnswer(AnswerTest.A2);
+        Q2.addAnswer(AnswerTest.A1);
+
         assertThatThrownBy(() -> {
             Q2.deleteBy(NsUserTest.SANJIGI);
         }).isInstanceOf(CannotDeleteException.class);
@@ -81,7 +86,7 @@ public class QuestionTest {
     }
 
     @Test
-    void 질문을_삭제할_경우_히스토리_생성_확인() {
+    void 질문을_삭제할_경우_히스토리_생성_검증() {
         DeleteHistory deleted = Q1.deleted();
 
         DeleteHistory expected = new DeleteHistory(ContentType.QUESTION, 0L, NsUserTest.JAVAJIGI);
@@ -89,6 +94,33 @@ public class QuestionTest {
         assertThat(Q1.isDeleted()).isTrue();
         assertThat(deleted).isEqualTo(expected);
     }
+
+    @Test
+    void 답변이_있는_질문을_삭제_성공_경우_질문과_답변_히스토리_목록_생성_검증() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A1);
+        List<DeleteHistory> deleted = Q1.deleteBy(NsUserTest.JAVAJIGI);
+
+
+        DeleteHistory expectedQuestion = new DeleteHistory(ContentType.QUESTION, 0L, NsUserTest.JAVAJIGI);
+        DeleteHistory expectedAnswer1 = new DeleteHistory(ContentType.ANSWER, 0L, NsUserTest.JAVAJIGI);
+
+        assertThat(Q1.isDeleted()).isTrue();
+        assertThat(deleted).hasSize(2);
+        assertThat(deleted).contains(expectedQuestion, expectedAnswer1);
+    }
+
+    @Test
+    void 답변이_없는_질문을_삭제_성공_경우_질문_히스토리_목록_생성_검증() throws CannotDeleteException {
+        List<DeleteHistory> deleted = Q1.deleteBy(NsUserTest.JAVAJIGI);
+
+        DeleteHistory expectedQuestion = new DeleteHistory(ContentType.QUESTION, 0L, NsUserTest.JAVAJIGI);
+
+        assertThat(Q1.isDeleted()).isTrue();
+        assertThat(deleted).hasSize(1);
+        assertThat(deleted).contains(expectedQuestion);
+    }
+
+
 
 
 
