128a07aebc8b1ac010f082cc6a8cb468dab227fe
juno-junho <ssmm0205@naver.com>
2023-05-19 22:52:09 +0900

    test: test 픽스처 수정

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,19 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
+@DisplayName("답변 관련 기능")
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    private Answer A1;
+    private Answer A2;
+    private Question Q1;
+
+    @BeforeEach
+    void setUp() {
+        this.Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        this.A1 = new Answer(NsUserTest.JAVAJIGI, Q1, "Answers Contents1");
+        this.A2 = new Answer(NsUserTest.SANJIGI, Q1, "Answers Contents2");
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,17 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
+@DisplayName("질문 관련 기능")
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    private Question Q1;
+    private Question Q2;
+
+    @BeforeEach
+    void setUp() {
+        this.Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        this.Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    }
 }
