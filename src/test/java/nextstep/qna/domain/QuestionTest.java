80bd44e1f7277ea2397c9671dc330339f93051fb
seriouskang <scdean110@gmail.com>
2023-05-19 22:20:32 +0900

    feat(Question): delete 메소드 신규
    변경 사항:
    - 로그인 사용자와 질문자 동일 여부 검증 책임 변경(서비스 -> 도메인)

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("질문한 유저가 아닌 유저가 질문을 삭제하려고 시도할 경우, CannotDeleteException 예외 발생")
+    void user_who_is_not_the_owner_of_question_try_to_delete_question_then_throw_throw_CannotDeleteException() {
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
 }
