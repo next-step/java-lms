8c01c7d1346ce72a46489407a67e5b55523c4c1c
dkswnkk <jjoy9810@gmail.com>
2023-05-20 13:15:51 +0900

    refactor: Exception 상속 클래스 변경

--- src/main/java/nextstep/courses/exception/InvalidSessionDateTimeException.java
+++ src/main/java/nextstep/courses/exception/InvalidSessionDateTimeException.java
@@ -1,6 +1,6 @@
 package nextstep.courses.exception;
 
-public class InvalidSessionDateTimeException extends Exception {
+public class InvalidSessionDateTimeException extends RuntimeException {
     public InvalidSessionDateTimeException(String message) {
         super(message);
     }
--- src/main/java/nextstep/courses/exception/SessionEnrollmentException.java
+++ src/main/java/nextstep/courses/exception/SessionEnrollmentException.java
@@ -1,6 +1,6 @@
 package nextstep.courses.exception;
 
-public class SessionEnrollmentException extends Exception {
+public class SessionEnrollmentException extends RuntimeException {
     public SessionEnrollmentException(String message) {
         super(message);
     }
