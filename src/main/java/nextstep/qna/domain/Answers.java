31cf3ba639f685f6b9dd8148516cdeb14a20d5a8
nayoon-kim <belloyv@gmail.com>
2023-05-20 16:08:03 +0900

    answer에 delete validate 추가

--- src/main/java/nextstep/qna/domain/Answers.java
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -27,9 +27,7 @@ public class Answers {
     }
     private void validate(NsUser loginUser) throws CannotDeleteException {
         for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
+            answer.validateDelete(loginUser);
         }
     }
 }
--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("로그인 사용자와 답변자 체크")
+    void validateDelete_exception() {
+        assertThatThrownBy(() -> {
+            A1.validateDelete(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class).hasMessageContaining("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
