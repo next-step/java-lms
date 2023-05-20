010a9189850c3425e00d765f43215f16825cab7f
jerry shin <shiney798@gmail.com>
2023-05-20 11:16:39 +0900

    docs : 기능요구사항 상세하게 보완

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
+
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void 질문삭제여부검증() {
+        @BeforeEach
+        public void setUp() throws Exception {
+            question = new Question(1L, NsUserTest.JAVAJIGI, "title1", "contents1");
+            answer = new Answer(11L, NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+            question.addAnswer(answer);
+        }
+    }
 }
