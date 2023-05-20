2128b9f3e5c125e694992c2e73a40ecf6efc1c64
oneny <gjwodnjs1996@gmail.com>
2023-05-20 13:49:23 +0900

    refactor: Session에서 maxUserEnrollment 상태 NextStepUsers로 책임 분리

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -10,8 +10,7 @@ public class Session {
   private final Long id;
   private final SessionPayment sessionPayment;
   private SessionStatus sessionStatus;
-  private final MaxUserEnrollment maxUserEnrollment;
-  private final NextStepUsers nextStepUsers = new NextStepUsers();
+  private final NextStepUsers nextStepUsers;
   private final SessionDate startDate;
   private final SessionDate closedDate;
   private final SessionCoverUrl sessionCoverUrl;
@@ -23,7 +22,7 @@ public class Session {
     this.id = id;
     this.sessionPayment = sessionPayment;
     this.sessionStatus = sessionStatus;
-    this.maxUserEnrollment = new MaxUserEnrollment(maxUserEnrollment);
+    this.nextStepUsers = new NextStepUsers(maxUserEnrollment);
     this.closedDate = new SessionDate(closedDate);
     this.sessionCoverUrl = new SessionCoverUrl(sessionCoverUrl);
   }
@@ -38,7 +37,6 @@ public class Session {
 
   public void processEnrollment(NextStepUser nextStepUser) {
     sessionStatus.validateAcceptingStatus();
-    maxUserEnrollment.validateUserEnrollment(nextStepUsers);
 
     nextStepUsers.enroll(nextStepUser);
   }
--- src/main/java/nextstep/users/domain/NextStepUsers.java
+++ src/main/java/nextstep/users/domain/NextStepUsers.java
@@ -4,21 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NextStepUsers {
+
+  private static final String MAX_ENROLLMENT_MESSAGE = "해당 세션의 수강 인원이 만석되었습니다.";
+  private final int maxUserEnrollment;
   private final List<NextStepUser> nextStepUsers;
 
-  public NextStepUsers() {
+  public NextStepUsers(int maxUserEnrollment) {
     this.nextStepUsers = new ArrayList<>();
+    this.maxUserEnrollment = maxUserEnrollment;
   }
 
-  public NextStepUsers(List<NextStepUser> nextStepUsers) {
-    this.nextStepUsers = nextStepUsers;
+  public void enroll(NextStepUser nextStepUser) {
+    validateUserEnrollment();
+
+    nextStepUsers.add(nextStepUser);
   }
 
   public int currentEnrollment() {
     return nextStepUsers.size();
   }
 
-  public void enroll(NextStepUser nextStepUser) {
-    nextStepUsers.add(nextStepUser);
+  private void validateUserEnrollment() {
+    if (nextStepUsers.size() >= maxUserEnrollment) {
+      throw new IllegalArgumentException(MAX_ENROLLMENT_MESSAGE);
+    }
   }
 }
