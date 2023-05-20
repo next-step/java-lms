ef482821bdc70c43e667b929e6883b412d0523af
ryan <wsdf25867@gmail.com>
2023-05-20 17:32:37 +0900

    feat : 답변 일급 컬렉션 추가

---
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -0,0 +1,33 @@
+package nextstep.qna.domain;
+
+import nextstep.users.domain.NsUser;
+import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class AnswersTest {
+
+    public static final Answers AS1 = Answers.from(List.of(AnswerTest.A1, AnswerTest.A2));
+    public static final Answers AS2 = Answers.from(List.of(AnswerTest.A2, AnswerTest.A2));
+    @Test
+    void from() {
+        assertThat(AS1).isEqualTo(new Answers(List.of(AnswerTest.A1, AnswerTest.A2)));
+    }
+
+    @Test
+    void isNotOnlyWriter() {
+        assertThat(AS1.isNotOnlyWriter(NsUserTest.JAVAJIGI)).isTrue();
+    }
+
+    @Test
+    void delete() {
+        List<DeleteHistory> delete = AS2.delete(NsUserTest.SANJIGI);
+
+        assertThat(delete).hasSize(2);
+    }
+}
