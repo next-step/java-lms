8974a9083aad16bced79fe7abd7074b9e8242fd7
oneny <gjwodnjs1996@gmail.com>
2023-05-20 13:52:50 +0900

    refactor: SessionPayment 유료 및 무료강의 필드값 추가

--- src/main/java/nextstep/courses/domain/SessionPayment.java
+++ src/main/java/nextstep/courses/domain/SessionPayment.java
@@ -1,5 +1,12 @@
 package nextstep.courses.domain;
 
 public enum SessionPayment {
-  FREE, PAID
+  FREE("무료"),
+  PAID("유료");
+
+  private final String paymentType;
+
+  SessionPayment(String paymentType) {
+    this.paymentType = paymentType;
+  }
 }
