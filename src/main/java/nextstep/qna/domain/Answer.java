63b225aad1823184399e88f2dfb90811b0d0d5ff
jaden.1230 <jaden.1230@kakaocorp.com>
2023-05-21 15:50:52 +0900

    refactor: delete answers

--- src/main/java/nextstep/qna/domain/Answer.java
+++ src/main/java/nextstep/qna/domain/Answer.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.qna.NotFoundException;
 import nextstep.qna.UnAuthorizedException;
 import nextstep.users.domain.NsUser;
@@ -66,6 +67,14 @@ public class Answer {
 
     public String getContents() {
         return contents;
+    }
+
+    public void delete(NsUser writer) throws CannotDeleteException {
+        if (!isOwner(writer)) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+
+        deleted = true;
     }
 
     public void toQuestion(Question question) {
