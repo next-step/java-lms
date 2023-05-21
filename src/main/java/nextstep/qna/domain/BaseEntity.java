5c0e43b111dd6d26e4e694ba2067640500b6dcd9
jaewon0913 <jaewon9937@gmail.com>
2023-05-21 16:06:00 +0900

    Feat(lms) : Step1 : 레거시 코드 리팩토링
    - LocalDateTime 엔티티 클래스로 분기

---
+++ src/main/java/nextstep/qna/domain/BaseEntity.java
@@ -0,0 +1,9 @@
+package nextstep.qna.domain;
+
+import java.time.LocalDateTime;
+
+public abstract class BaseEntity {
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    private LocalDateTime updatedDate;
+}
