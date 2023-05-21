3238265852c3d2b09f3335c6798cf899115b69a4
juno-junho <ssmm0205@naver.com>
2023-05-21 23:05:42 +0900

    test: 기능 목록에 따른 Question 클래스에 해당하는 테스트 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,13 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("질문 관련 기능")
 public class QuestionTest {
@@ -13,5 +18,21 @@ public class QuestionTest {
     void setUp() {
         this.Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
         this.Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    }
+
+    @DisplayName("질문 삭제시 로그인 사용자와 질문한 사람이 같을 경우 예외를 발생하지 않는다.")
+    @Test
+    void when_LoginUserIsNotEqualToQuestionId_Expects_ThrowException() {
+        assertThatNoException()
+                .isThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI));
+    }
+
+    @DisplayName("질문 삭제시 로그인 사용자와 질문한 사람이 같지 않을 경우 예외를 발생한다.")
+    @Test
+    void when_LoginUserIsNotEqualToQuestionId_Expects_ThrowException() {
+
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+
     }
 }
