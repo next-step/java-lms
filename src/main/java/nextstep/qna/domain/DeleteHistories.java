e3e172a17b64e926a4fd6cde005dc8c3ef0a31a8
juno-junho <ssmm0205@naver.com>
2023-05-22 01:03:47 +0900

    feat(domain): DeleteHistories 일급 컬렉션 구현

---
+++ src/main/java/nextstep/qna/domain/DeleteHistories.java
@@ -0,0 +1,25 @@
+package nextstep.qna.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteHistories {
+
+    private final List<DeleteHistory> deleteHistories;
+
+    public DeleteHistories() {
+        this.deleteHistories = new ArrayList<>();
+    }
+
+    public void add(Question question) {
+        this.deleteHistories.add(new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter()));
+    }
+
+    public void add(Answer answer) {
+        this.deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter()));
+    }
+
+    public List<DeleteHistory> getDeleteHistories() {
+        return deleteHistories;
+    }
+}
