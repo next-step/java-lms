1cf5c96b22b8ef93abec516b538150d84dba9d55
DeveloperKGH <developerkgh@gmail.com>
2023-05-21 23:45:54 +0900

    :recycle: refactor : QuestionDetail 객체 정상 생성 관련 테스트 추가

--- src/test/java/nextstep/qna/domain/vo/QuestionDetailTest.java
+++ src/test/java/nextstep/qna/domain/vo/QuestionDetailTest.java
@@ -3,6 +3,7 @@ package nextstep.qna.domain.vo;
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,6 +19,12 @@ public class QuestionDetailTest {
                 Arguments.of(NsUserTest.JAVAJIGI, true),
                 Arguments.of(NsUserTest.SANJIGI, false)
         );
+    }
+
+    @DisplayName("QuestionDetail 객체가 잘 생성되는지 확인")
+    @Test
+    void 객체가_정상적으로_생성되는지_확인() {
+        assertThat(QuestionDetail.of("제목1", "내용1", NsUserTest.JAVAJIGI)).isInstanceOf(QuestionDetail.class);
     }
 
     @DisplayName("로그인 사용자와 질문한 사람이 동일한 사람인지 확인")
