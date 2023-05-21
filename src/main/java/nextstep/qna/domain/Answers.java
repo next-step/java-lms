95868336ac611fa222659c424cc8c2ae3088a706
juno-junho <ssmm0205@naver.com>
2023-05-22 00:28:48 +0900

    feat(domain): Answers 일급컬렉션 구현

---
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -0,0 +1,35 @@
+package nextstep.qna.domain;
+
+import nextstep.qna.CannotDeleteException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Answers {
+
+    private final List<Answer> answers;
+
+    public Answers() {
+        this.answers = new ArrayList<>();
+    }
+
+    public void add(Answer answer) {
+        if (Objects.isNull(answer)) {
+            throw new NullPointerException();
+        }
+        this.answers.add(answer);
+    }
+
+    public List<Answer> getAnswers() {
+        return this.answers;
+    }
+    public void deleteAnswers() throws CannotDeleteException {
+        if (answers.isEmpty()) {
+            return;
+        }
+        for (Answer answer : this.answers) {
+            answer.delete();
+        }
+    }
+}
