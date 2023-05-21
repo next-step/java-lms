c3870ddb5776373bb99e13c7b862d38822d63202
ShinYeahChan <syeahc@gmail.com>
2023-05-22 00:33:27 +0900

    refactor(test) : 테스트 메서드별 Answers 개별적 사용을 위해 @BeforeEach 처리

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -2,6 +2,7 @@ package nextstep.qna.domain;
 
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -14,33 +15,38 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class AnswersTest {
 
-    private static final Answers ANSWERS = new Answers() {{
-        add(AnswerTest.A1);
-        add(AnswerTest.A2);
-    }};
+    private static Answers answers;
+
+    @BeforeEach
+    public void setUp(){
+        answers = new Answers() {{
+            add(AnswerTest.A1);
+            add(AnswerTest.A2);
+        }};
+    }
 
     @Test
     void 답변추가() {
-        ANSWERS.add(AnswerTest.YEAHCHAN_A1);
+        answers.add(AnswerTest.YEAHCHAN_A1);
         assertAll(
-                () -> assertThat(ANSWERS.size()).isEqualTo(3),
-                () -> assertThat(ANSWERS.contains(AnswerTest.YEAHCHAN_A1)).isTrue()
+                () -> assertThat(answers.size()).isEqualTo(3),
+                () -> assertThat(answers.contains(AnswerTest.YEAHCHAN_A1)).isTrue()
         );
     }
 
     @Test
     void 다른유저답변_삭제불가() {
-        assertThatThrownBy(() -> ANSWERS.delete(NsUserTest.YEAHCHAN)).isInstanceOf(CannotDeleteException.class);
+        assertThatThrownBy(() -> answers.delete(NsUserTest.YEAHCHAN)).isInstanceOf(CannotDeleteException.class);
     }
 
     @Test
     void 답변모두_삭제가능() throws CannotDeleteException {
-        Answers answers = new Answers() {{
+        Answers newAnswers = new Answers() {{
             add(AnswerTest.YEAHCHAN_A1);
             add(AnswerTest.YEAHCHAN_A2);
         }};
         List<DeleteHistory> deleteHistories = new ArrayList<>();
-        answers.delete(NsUserTest.YEAHCHAN);
+        newAnswers.delete(NsUserTest.YEAHCHAN);
         assertAll(
                 () -> assertThat(AnswerTest.YEAHCHAN_A1.isDeleted()).isTrue(),
                 () -> assertThat(AnswerTest.YEAHCHAN_A2.isDeleted()).isTrue()
