56dc6830841fc685d61fc0ac347462d939721225
dkswnkk <jjoy9810@gmail.com>
2023-05-23 02:19:19 +0900

    refactor: BaseEntity 추상 클래스로 변경

--- src/main/java/nextstep/courses/domain/BaseEntity.java
+++ src/main/java/nextstep/courses/domain/BaseEntity.java
@@ -2,8 +2,16 @@ package nextstep.courses.domain;
 
 import java.time.LocalDateTime;
 
-public class BaseEntity {
+public abstract class BaseEntity {
     public LocalDateTime createdAt = LocalDateTime.now();
 
     public LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
--- src/main/java/nextstep/qna/domain/BaseEntity.java
+++ src/main/java/nextstep/qna/domain/BaseEntity.java
@@ -2,7 +2,8 @@ package nextstep.qna.domain;
 
 import java.time.LocalDateTime;
 
-public class BaseEntity {
+public abstract class BaseEntity {
     private LocalDateTime createdDate = LocalDateTime.now();
+
     private LocalDateTime updatedDate;
 }
