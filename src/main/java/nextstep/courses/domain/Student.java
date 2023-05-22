0a007c72ecb969e43c388c82ad2421f27c2acb6a
javajigi <javajigi@gmail.com>
2023-05-22 16:15:19 +0900

    refactor(course): remove unused import

--- src/main/java/nextstep/courses/domain/Student.java
+++ src/main/java/nextstep/courses/domain/Student.java
@@ -1,14 +1,6 @@
 package nextstep.courses.domain;
 
-import java.math.BigDecimal;
-import java.nio.file.Path;
 import java.util.Objects;
-import java.util.function.DoublePredicate;
-import java.util.function.IntPredicate;
-import java.util.function.LongPredicate;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class Student {
     private final Long nsUserId;
