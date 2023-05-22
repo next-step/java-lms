a6a51bcc3aa4209b6306dd75b8affbc310255462
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-05-22 23:10:06 +0900

    feature - 학생 강의 수강 신청 기능

--- src/test/java/lms/domain/Session.java
+++ src/test/java/lms/domain/Session.java
@@ -10,18 +10,21 @@ public class Session {
     private LocalDate endDate;
     private Image imageCover;
     private SessionState sessionState = READY;
+    private int studentCapacity;
+    private int registeredStudent = 0;
 
-    public Session(LocalDate startDate, LocalDate endDate, Image imageCover) {
+    public Session(LocalDate startDate, LocalDate endDate, Image imageCover, int studentCapacity) {
         if (endDate.isBefore(startDate)) {
             throw new IllegalArgumentException("종료 날짜가 시작 날짜보다 앞일 수 없습니다.");
         }
         this.startDate = startDate;
         this.endDate = endDate;
         this.imageCover = imageCover;
+        this.studentCapacity = studentCapacity;
     }
 
     public Session(LocalDate startDate, LocalDate endDate) {
-        this(startDate, endDate, null);
+        this(startDate, endDate, null, 0);
     }
 
     public Image changeImageCover(Image changeCover) {
@@ -39,5 +42,26 @@ public class Session {
             sessionState = FINISH;
         }
         return sessionState;
+    }
+
+    public int register() {
+        ++registeredStudent;
+
+        validateSessionState();
+        validateCapacity();
+
+        return registeredStudent;
+    }
+
+    private void validateSessionState() {
+        if (!sessionState.equals(RECRUITING)) {
+            throw new IllegalArgumentException("모집 중이 아닙니다.");
+        }
+    }
+
+    private void validateCapacity() {
+        if (registeredStudent > studentCapacity) {
+            throw new IllegalArgumentException("수강 인원이 다 찼습니다.");
+        }
     }
 }
--- src/test/java/lms/domain/SessionTest.java
+++ src/test/java/lms/domain/SessionTest.java
@@ -10,6 +10,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SessionTest {
 
+    private Session session;
+
+    void setUp(int studentCapacity) {
+        LocalDate startDate = LocalDate.of(2023, 5, 22);
+        LocalDate endDate = LocalDate.of(2023, 5, 23);
+        Image imageCover = new Image();
+
+        session = new Session(startDate, endDate, imageCover, studentCapacity);
+    }
+
     @Test
     @DisplayName("강의 시작일과 종료일 유효성 체크")
     void validateSessionDateTest() {
@@ -24,12 +34,7 @@ public class SessionTest {
     @Test
     @DisplayName("강의 이미지 커버 수정 기능")
     void changeImageCoverTest() {
-        LocalDate startDate = LocalDate.of(2023, 5, 22);
-        LocalDate endDate = LocalDate.of(2023, 5, 23);
-        Image imageCover = new Image();
-
-        Session session = new Session(startDate, endDate, imageCover);
-
+        setUp(0);
         Image changeCover = new Image();
 
         assertThat(session.changeImageCover(changeCover))
@@ -39,11 +44,7 @@ public class SessionTest {
     @Test
     @DisplayName("강의 모집중으로 변경")
     void recruitStudentsTest() {
-        LocalDate startDate = LocalDate.of(2023, 5, 22);
-        LocalDate endDate = LocalDate.of(2023, 5, 23);
-        Image imageCover = new Image();
-
-        Session session = new Session(startDate, endDate, imageCover);
+        setUp(0);
 
         assertThat(session.recruitStudents())
                 .isEqualTo(SessionState.RECRUITING);
@@ -52,11 +53,7 @@ public class SessionTest {
     @Test
     @DisplayName("종료일이 지났을 때, 강의 상태 종료로 변경")
     void finishSessionTest() {
-        LocalDate startDate = LocalDate.of(2023, 5, 22);
-        LocalDate endDate = LocalDate.of(2023, 5, 23);
-        Image imageCover = new Image();
-
-        Session session = new Session(startDate, endDate, imageCover);
+        setUp(0);
 
         LocalDate now = LocalDate.of(2023, 5, 24);
 
@@ -67,16 +64,48 @@ public class SessionTest {
     @Test
     @DisplayName("종료일이 지나지 않았으면 강의 상태가 종료로 바뀌지 않음")
     void notFinishSessionTest() {
-        LocalDate startDate = LocalDate.of(2023, 5, 22);
-        LocalDate endDate = LocalDate.of(2023, 5, 23);
-        Image imageCover = new Image();
-
-        Session session = new Session(startDate, endDate, imageCover);
+        setUp(0);
 
         LocalDate now = LocalDate.of(2023, 5, 23);
 
         assertThat(session.finishSessionState(now))
                 .isEqualTo(SessionState.READY);
     }
+
+    @Test
+    @DisplayName("모집 중이 아닐 때 수강 신청 에러")
+    void notRecruitingErrorTest() {
+        int studentCapacity = 5;
+        setUp(studentCapacity);
+
+        assertThatThrownBy(() -> session.register())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("수강 신청 인원 초과 시 수강 신청 에러")
+    void overStudentCapacityErrorTest() {
+        int studentCapacity = 2;
+        setUp(studentCapacity);
+
+        session.recruitStudents();
+        session.register();
+        session.register();
+
+        assertThatThrownBy(() -> session.register())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("학생 강의 수강 신청 기능")
+    void registerTest() {
+        int studentCapacity = 5;
+        setUp(studentCapacity);
+        session.recruitStudents();
+
+        assertThat(session.register())
+                .isEqualTo(1);
+    }
+
 
 }
