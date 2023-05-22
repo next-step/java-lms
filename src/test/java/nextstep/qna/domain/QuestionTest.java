126b3b821cdd895ce63f9fabd5ae21411a19cd1b
juno-junho <ssmm0205@naver.com>
2023-05-22 17:35:24 +0900

    refactor(service): DeleteHistories 생성 위치 domain으로 변경

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -12,11 +12,9 @@ import static org.assertj.core.api.Assertions.*;
 public class QuestionTest {
     private Question Q1;
     private Question Q2;
-    private DeleteHistories deleteHistories;
 
     @BeforeEach
     void setUp() {
-        deleteHistories = new DeleteHistories();
         this.Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
         this.Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
     }
@@ -25,28 +23,28 @@ public class QuestionTest {
     @Test
     void when_LoginUserIsEqualToQuestionWriter_Expects_DoesNotThrowException() {
         assertThatNoException()
-                .isThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI, deleteHistories));
+                .isThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI));
 
         assertThatNoException()
-                .isThrownBy(() -> Q2.delete(NsUserTest.SANJIGI, deleteHistories));
+                .isThrownBy(() -> Q2.delete(NsUserTest.SANJIGI));
     }
 
     @DisplayName("질문 삭제시 로그인 사용자와 질문한 사람이 같지 않을 경우 예외를 발생한다.")
     @Test
     void when_LoginUserIsNotEqualToQuestionWriter_Expects_ThrowException() {
 
-        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI, deleteHistories))
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class);
 
-        assertThatThrownBy(() -> Q2.delete(NsUserTest.JAVAJIGI, deleteHistories))
+        assertThatThrownBy(() -> Q2.delete(NsUserTest.JAVAJIGI))
                 .isInstanceOf(CannotDeleteException.class);
     }
 
     @DisplayName("삭제가능 할 경우, 삭제시 deleted 필드 값을 true로 변경한다.")
     @Test
     void when_deletingQuestionIfPossible_Expects_returnTrue() {
-        Q1.delete(NsUserTest.JAVAJIGI, deleteHistories);
-        Q2.delete(NsUserTest.SANJIGI, deleteHistories);
+        Q1.delete(NsUserTest.JAVAJIGI);
+        Q2.delete(NsUserTest.SANJIGI);
 
         assertThat(Q1.isDeleted()).isTrue();
         assertThat(Q2.isDeleted()).isTrue();
