8fcab0fa6cf173f0e1410596135e235434cb0db1
dkswnkk <jjoy9810@gmail.com>
2023-05-23 02:27:48 +0900

    refactor: JdbcUserRepository 수정

--- src/main/java/nextstep/users/infrastructure/JdbcUserRepository.java
+++ src/main/java/nextstep/users/infrastructure/JdbcUserRepository.java
@@ -22,11 +22,11 @@ public class JdbcUserRepository implements UserRepository {
     public Optional<User> findByUserId(String userId) {
         String sql = "select id, user_id, password, name, email, created_at, updated_at from next_step_user where user_id = ?";
         RowMapper<User> rowMapper = (rs, rowNum) -> new User(
-                rs.getLong("id"),
-                rs.getString("user_id"),
-                rs.getString("password"),
-                rs.getString("name"),
-                rs.getString("email"),
+                rs.getLong(1),
+                rs.getString(2),
+                rs.getString(3),
+                rs.getString(4),
+                rs.getString(5),
                 toLocalDateTime(rs.getTimestamp(6)),
                 toLocalDateTime(rs.getTimestamp(7)));
         return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, userId));
