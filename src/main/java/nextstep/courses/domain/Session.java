d784b52b4978a2985f626ad677053bffdc9be738
ryan <wsdf25867@gmail.com>
2023-05-22 23:42:20 +0900

    feat : 강의는 시작일을 가진다

---
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -0,0 +1,17 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDateTime;
+
+public class Session {
+    private final LocalDateTime startAt;
+    private final LocalDateTime endAt;
+
+    public Session(LocalDateTime startAt, LocalDateTime endAt) {
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+
+    public LocalDateTime getStartAt() {
+        return startAt;
+    }
+}
---
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -0,0 +1,21 @@
+package nextstep.courses.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SessionTest {
+
+    @Test
+    @DisplayName("강의는 시작일을 가진다.")
+    void startAt() {
+        LocalDateTime startAt = LocalDateTime.MIN;
+        LocalDateTime endAt = LocalDateTime.MAX;
+        Session session = new Session(startAt, endAt);
+        assertThat(session.getStartAt()).isEqualTo(startAt);
+    }
+}
