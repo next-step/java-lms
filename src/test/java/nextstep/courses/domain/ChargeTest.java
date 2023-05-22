3b56a34781f4ce3e5a1d9597333a8d43582ec7ac
javajigi <javajigi@gmail.com>
2023-05-22 11:57:03 +0900

    feat(course): add Charge

---
+++ src/test/java/nextstep/courses/domain/ChargeTest.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ChargeTest {
+    @Test
+    void 생성() {
+        new Charge(ChargeStatus.FREE);
+        new Charge(ChargeStatus.CHARGE, 500_000);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Charge(ChargeStatus.FREE, 100_000));
+    }
+}
