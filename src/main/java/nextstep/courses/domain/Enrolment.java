d49e5d1818f4d30a2c590b36c60a074da1e42d3a
dong <ultraraptor@naver.com>
2023-05-23 17:39:47 +0900

    arch :  기능에 대한 기본 클래스 설계

--- src/main/java/nextstep/courses/domain/Enrolment.java
+++ src/main/java/nextstep/courses/domain/Enrolment.java
@@ -1,5 +1,9 @@
 package nextstep.courses.domain;
 
+import nextstep.users.domain.NsUser;
+
 public class Enrolment {
     private EnrolmentId enrolmentId;
+    private Session session;
+    private NsUser nsUser;
 }
---
+++ src/test/java/nextstep/courses/domain/EnrolmentTest.java
@@ -0,0 +1,10 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.Disabled;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("클래스는 필요한데, 요구사항이 없어 테스트할 것이 없음")
+class EnrolmentTest {
+
+}
