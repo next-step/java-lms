db8343e285dc96f55aa8c5dce109fa98b3e09676
munjeongho <96mjh00@gmail.com>
2023-05-23 17:57:50 +0900

    refactor : 테스트코드 리팩터링

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,35 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static nextstep.SetUp.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("답변삭제")
+    @Test
+    void answerDelete() {
+        Answer answer = createAnswer1(NsUserTest.JAVAJIGI);
+
+        LocalDateTime now = LocalDateTime.now();
+        DeleteHistory deleteHistory = answer.delete(NsUserTest.JAVAJIGI, now);
+
+        assertThat(deleteHistory).isEqualTo(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), now));
+        assertThat(answer.isDeleted()).isEqualTo(true);
+    }
+
+    @DisplayName("답변 입력한 사람 다르면 에러")
+    @Test
+    void answerWriterDiffException() {
+        Answer answer = createAnswer1(NsUserTest.JAVAJIGI);
+
+        assertThatThrownBy(() -> answer.delete(NsUserTest.SANJIGI, LocalDateTime.now())).isInstanceOf(CannotDeleteException.class);
+    }
 }
