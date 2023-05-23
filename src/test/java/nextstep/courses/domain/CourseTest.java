101a77a5e197ee47474821d15f28115a48a9f802
henry.5 <henry.5@kakaoenterprise.com>
2023-05-24 00:35:17 +0900

    step2 - 과정 <> 강의 추가

--- src/test/java/nextstep/courses/domain/CourseTest.java
+++ src/test/java/nextstep/courses/domain/CourseTest.java
@@ -1,6 +1,29 @@
 package nextstep.courses.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CourseTest {
+
+    private Course course;
+
+    @BeforeEach
+    void setup(){
+        course = new Course();
+    }
+    @Test
+    @DisplayName("과정 객체 생성 case 1 - 기수 필드 여부")
+    void course_has_generation_field(){
+        assertThat(course)
+                .hasFieldOrProperty("generation");
+    }
+
+
+
 
 
 }
