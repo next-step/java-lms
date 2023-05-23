86b0aaadbe79f0434a34fe324dafe85d92f3e935
soochan <tncksdl05@gmail.com>
2023-05-23 14:28:00 +0900

    feat : 강의 시작일, 종료일에 대한 validate 코드 추가

--- src/main/java/nextstep/courses/domain/SessionPeriod.java
+++ src/main/java/nextstep/courses/domain/SessionPeriod.java
@@ -8,8 +8,15 @@ public class SessionPeriod {
   private final LocalDateTime endDateTime;
 
   public SessionPeriod(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+    validateTime(startDateTime, endDateTime);
     this.startDateTime = startDateTime;
     this.endDateTime = endDateTime;
+  }
+
+  private void validateTime(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+    if (startDateTime.isAfter(endDateTime)) {
+      throw new IllegalArgumentException("시작 시간이 종료 시간보다 늦을 수 없습니다.");
+    }
   }
 
 }
