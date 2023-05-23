93e28ffe5ef2ac574129883227939e19fcd6e445
woony-kim <woony.kim@balancehero.com>
2023-05-23 21:42:42 +0900

    refactor question 삭제 권한 확인 로직 -> Question 이관

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,24 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    public void shouldThrowException_ifOwnerIsNotLoginUser() {
+        //given
+        NsUser wrongUser = NsUserTest.SANJIGI;
+        //when & then
+        assertThatThrownBy(() -> {
+            Q1.hasAuthorityToDelete(wrongUser);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
