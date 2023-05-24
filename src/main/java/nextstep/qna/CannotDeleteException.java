b62b2edb296b1f97e68fb4227376503d83a09b3c
In-wook Hwang <inwook9@gmail.com>
2023-05-24 23:23:15 +0900

    feat: Question 객체 삭제 검증로직 추가
    feat: 로그인 사용자와 질문한 사람이 같을 경우 삭제가 가능

--- src/main/java/nextstep/qna/CannotDeleteException.java
+++ src/main/java/nextstep/qna/CannotDeleteException.java
@@ -1,9 +1,15 @@
 package nextstep.qna;
 
-public class CannotDeleteException extends Exception {
+public class CannotDeleteException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     public CannotDeleteException(String message) {
         super(message);
     }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+
 }
