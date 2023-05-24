5b7f60d1141e4606a77cd5dbfa917fb81a973f77
In-wook Hwang <inwook9@gmail.com>
2023-05-24 23:49:24 +0900

    test: 테스트 객체 사용 변경

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -2,27 +2,35 @@ package nextstep.qna.domain;
 
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+class QuestionTest {
+
+    private Question javajigiQuestion;
+    private Question sanjigiQuestion;
+
+    @BeforeEach
+    void setUp() {
+        javajigiQuestion = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        sanjigiQuestion = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    }
 
     @Test
     @DisplayName("로그인 사용자와 질문한 사람이 같을 경우 삭제가 가능하다.")
     void test01() {
-        Q1.delete(NsUserTest.JAVAJIGI);
-        assertThat(Q1.isDeleted()).isTrue();
+        javajigiQuestion.delete(NsUserTest.JAVAJIGI);
+        assertThat(javajigiQuestion.isDeleted()).isTrue();
     }
 
     @Test
     @DisplayName("로그인 사용자와 질문한 사람이 다를 경우 삭제가 불가능하다.")
     void test02() {
-        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
+        assertThatThrownBy(() -> sanjigiQuestion.delete(NsUserTest.JAVAJIGI))
                 .isInstanceOf(CannotDeleteException.class);
     }
 
