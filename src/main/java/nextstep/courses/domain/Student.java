442d7137d7213675943d0303e79e51cad88a97c5
juno-junho <ssmm0205@naver.com>
2023-05-25 06:23:27 +0900

    feat(domain): 학생 식별 할 수 있는 id 추가

--- src/main/java/nextstep/courses/domain/Student.java
+++ src/main/java/nextstep/courses/domain/Student.java
@@ -1,17 +1,34 @@
 package nextstep.courses.domain;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Student {
+    private final Long studentId;
 
-    private final Set<Session> sessions = new HashSet<>();
+    private final Set<Session> sessions;
 
-    public Student() {
+    public Student(Long studentId) {
+        this.studentId = studentId;
+        this.sessions = new HashSet<>();
     }
 
     public void enroll(Session session) {
         session.add(this);
         sessions.add(session);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(studentId, student.studentId) && Objects.equals(sessions, student.sessions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, sessions);
     }
 }
