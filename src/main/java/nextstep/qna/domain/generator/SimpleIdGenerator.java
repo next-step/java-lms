7c9cc690d25e49f1a8a3e83e6e01849189eb3ced
Lee Byoung Seong <dev.bslee@gmail.com>
2023-05-24 21:03:00 +0900

    feat: SimpleIdGenerator 테스트코드

--- src/main/java/nextstep/qna/domain/generator/SimpleIdGenerator.java
+++ src/main/java/nextstep/qna/domain/generator/SimpleIdGenerator.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleIdGenerator {
-    private static final long INITIAL_SEQUENCE = 1;
+    private static final long INITIAL_SEQUENCE = 0;
     private static final long INCREMENTAL_VALUE = 1;
     private static final Map<Class<?>, Long> cache = new HashMap<>();
 
@@ -12,27 +12,17 @@ public class SimpleIdGenerator {
 
         long id = cache.getOrDefault(clazz, INITIAL_SEQUENCE);
 
-        if (id == INITIAL_SEQUENCE) {
-            return put(clazz, INCREMENTAL_VALUE, false);
-        }
-
-        return put(clazz, id, true);
-
+        return put(clazz, id);
     }
 
-    private static long put(Class<?> clazz, long value, boolean isIncrease) {
-        long putVal = getPutVal(value, isIncrease);
+    private static long put(Class<?> clazz, long value) {
+        long putVal = increase(value);
 
         cache.put(clazz, putVal);
         return putVal;
     }
 
-    private static long getPutVal(long value, boolean isIncrease) {
-
-        if (isIncrease) {
-            return value + INITIAL_SEQUENCE;
-        }
-
-        return value;
+    private static long increase(long value) {
+        return value + INCREMENTAL_VALUE;
     }
 }
