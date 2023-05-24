4c493516d964afea1bb6897e037875c04ef3105f
eohyemin <eohyemin@eohyemin-ui-MacBookPro.local>
2023-05-25 04:36:31 +0900

    refactor(Question): setter 메소드 삭제

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -69,18 +69,8 @@ public class Question {
         return title;
     }
 
-    public Question setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
     public String getContents() {
         return contents;
-    }
-
-    public Question setContents(String contents) {
-        this.contents = contents;
-        return this;
     }
 
     public NsUser getWriter() {
