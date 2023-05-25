18a616dab28edb7b78adefac17f68b49a4863eca
woony-kim <woony.kim@balancehero.com>
2023-05-25 20:53:18 +0900

    refactor AnswersTest ensureAllAnswersOwnedByUser 테스트 추가

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -1,9 +1,14 @@
 package nextstep.qna.domain;
 
 import static nextstep.qna.domain.AnswerTest.A1;
+import static nextstep.qna.domain.AnswerTest.A2;
+import static nextstep.users.domain.NsUserTest.SANJIGI;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
 
 public class AnswersTest {
     @Test
@@ -12,5 +17,16 @@ public class AnswersTest {
         answers.add(A1);
 
         assertThat(answers.contains(A1)).isTrue();
+    }
+
+    @Test
+    void shouldEnsureAllAnswersOwnedByUser() {
+        Answers answers = new Answers();
+        answers.add(A1);
+        answers.add(A2);
+
+        assertThatThrownBy(() -> {
+            answers.ensureAllAnswersOwnedByUser(SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
     }
 }
