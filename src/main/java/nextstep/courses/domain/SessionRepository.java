f24b5001af21f3a0378f6d7cded722d776f53920
javajigi <javajigi@gmail.com>
2023-05-25 10:52:02 +0900

    refactor(course): remove Service, Repository

--- src/main/java/nextstep/courses/domain/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package nextstep.courses.domain;
-
-public interface SessionRepository {
-    int save(Session session);
-
-    Session findById(Long id);
-}
--- src/main/java/nextstep/courses/domain/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package nextstep.courses.domain;
-
-import java.util.List;
-
-public interface StudentRepository {
-    int save(Student student);
-
-    List<Student> findBySessionId(Long sessionId);
-}
