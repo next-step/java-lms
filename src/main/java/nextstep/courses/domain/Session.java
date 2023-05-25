397d79b2be1543b0d257670470e12be474acbf67
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-25 16:04:37 +0900

    feat: 수강신청 메서드 및 exception 클래스 구현

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,5 +1,7 @@
 package nextstep.courses.domain;
 
+import nextstep.courses.exception.CannotRegisterException;
+
 public class Session {
     private String coverImageUrl;
     private SessionPeriod period;
@@ -8,4 +10,13 @@ public class Session {
     private int capacity;
 
 
+    public void register() {
+        if (status != SessionStatus.RECRUITING) {
+            throw new CannotRegisterException("현재 모집중인 강의가 아닙니다");
+        }
+        if (currentUsers <= capacity) {
+            throw new CannotRegisterException("강의 최대 수강 인원을 초과했습니다");
+        }
+        currentUsers++;
+    }
 }
