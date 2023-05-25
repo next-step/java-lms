0499b48de0822e52dbbcdf214f8c505481da9bdb
highjune <highjune37@gmail.com>
2023-05-25 19:14:58 +0900

    test(AnswersTest): 일급 컬렉션 테스트 추가

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -18,6 +18,7 @@ public class AnswersTest {
     @BeforeEach
     void setUp() {
         answers = new Answers();
+
         this.Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
         this.A1 = new Answer(NsUserTest.JAVAJIGI, Q1, "Answers Contents1");
         this.A2 = new Answer(NsUserTest.SANJIGI, Q1, "Answers Contents2");
@@ -39,5 +40,12 @@ public class AnswersTest {
     void enroll_AddNull_ThrowException() {
         Assertions.assertThatThrownBy(() -> answers.add(null))
                 .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("답변이 없는 리스트를 삭제해도 예외가 발생하지 않는다.")
+    void delete_EmptyAnswersDelete_NotThrowException() {
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> answers.deleteAnswers());
     }
 }
