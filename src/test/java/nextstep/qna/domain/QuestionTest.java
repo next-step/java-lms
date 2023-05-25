6eb0d4836a8a68572469ffb69139edbcc96cbac2
Gangbean <xbox2537@gmail.com>
2023-05-25 09:29:17 +0900

    (feat) Question loginUser verify implementation

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,19 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void 질문삭제권한존재() {
+        assertThatNoException().isThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI));
+    }
+
 }
