cb1afeb3428cb0d5ff968733e59e2eb9ef936c2e
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-25 15:25:29 +0900

    refactor: `SessionDateRange` -> `SessionPeriod`

--- src/main/java/nextstep/courses/domain/SessionPeriod.java
+++ src/main/java/nextstep/courses/domain/SessionPeriod.java
@@ -2,11 +2,11 @@ package nextstep.courses.domain;
 
 import java.time.LocalDate;
 
-public class SessionDateRange {
+public class SessionPeriod {
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public SessionDateRange(LocalDate startDate, LocalDate endDate) {
+    public SessionPeriod(LocalDate startDate, LocalDate endDate) {
         if (endDate.isBefore(startDate)) {
             throw new IllegalArgumentException("강의 종료일이 강의 시작일보다 빠릅니다");
         }
