b00c0d472e84d23c300576254043089808055c6b
PiHyoJeong <shson4007@gmail.com>
2023-05-26 00:12:05 +0900

    Test: 테스트코드 작성

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,11 +1,15 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.qna.NotFoundException;
 import nextstep.qna.UnAuthorizedException;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
@@ -26,5 +30,31 @@ public class AnswerTest {
         assertThatThrownBy(() -> {
             new Answer(null, NsUserTest.JAVAJIGI, null, "Answers Contents1");
         }).isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    @DisplayName(value = "답변 삭제 후 삭제 정보 검사")
+    void test3() {
+        DeleteHistory delete = A1.delete(NsUserTest.JAVAJIGI);
+
+        assertThat(delete).isEqualTo(new DeleteHistory(ContentType.ANSWER, null, NsUserTest.JAVAJIGI, LocalDateTime.now()));
+    }
+
+    @Test
+    @DisplayName(value = "답변 삭제 후 삭제 상태 참으로 변환 검사")
+    void test4() {
+        A1.delete(NsUserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName(value = "작성자와 다른 사용자가 답변 작성 시 삭제 불가")
+    void test5() {
+        A1.delete(NsUserTest.JAVAJIGI);
+
+        assertThatThrownBy(() -> {
+            A1.delete(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
     }
 }
