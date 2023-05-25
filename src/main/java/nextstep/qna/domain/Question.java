028bb48a32c08635875eaa6c6b3f562875f4d934
highjune <highjune37@gmail.com>
2023-05-25 16:08:48 +0900

    test(QuestionTest): TDD의 테스트 먼저 작성
    - 질문 삭제시 로그인 사용자와 질문한 사람이 같을 경우 예외를 던지지 않는다.
    - 질문 삭제시 로그인 사용자와 질문한 사람이 다를 경우 예외를 던진다.

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -89,4 +89,9 @@ public class Question {
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
+
+    public List<DeleteHistory> delete(NsUser loginUser) {
+
+        return null;
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,19 +1,32 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("질문 기능 관련 테스트")
 public class QuestionTest {
     public Question Q1;
     public Question Q2;
-//    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-//    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
 
     @BeforeEach
     void setUp() {
         Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
         Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    }
+
+    @Test
+    @DisplayName("질문 삭제시 로그인 사용자와 질문한 사람이 같을 경우 예외를 던지지 않는다.")
+    void delete_LoginUserEqualToQuestionUser_NotThrowException() {
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI));
+    }
+
+    @Test
+    @DisplayName("질문 삭제시 로그인 사용자와 질문한 사람이 다를 경우 예외를 던진다.")
+    void delete_LoginUserNotEqualToQuestionUser_ThrowException() {
+        Assertions.assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI));
     }
 }
