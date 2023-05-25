11a87a038c76e27aedccaa425e093ba3cf17268f
Gangbean <xbox2537@gmail.com>
2023-05-25 09:40:52 +0900

    (feat) Question verify answers

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -5,12 +5,31 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void 답변삭제불가_다른작성자의답변존재() {
+        Question actual = new Question(NsUser.GUEST_USER, "title3", "contents3");
+        actual.addAnswer(new Answer(NsUserTest.JAVAJIGI, actual, "question1"));
+
+        assertThatExceptionOfType(CannotDeleteException.class)
+            .isThrownBy(() -> actual.delete(NsUser.GUEST_USER))
+            .withMessageMatching("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @Test
+    void 답변삭제가능() {
+        Question actual = new Question(NsUser.GUEST_USER, "title3", "contents3");
+        actual.addAnswer(new Answer(NsUser.GUEST_USER, actual, "question1"));
+
+        assertThatNoException().isThrownBy(() -> actual.delete(NsUser.GUEST_USER));
+    }
 
     @Test
     void 질문삭제권한없음() {
