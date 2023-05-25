218e2888920b8eae0e5ba8ca072eccb46310f2ba
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-25 15:17:03 +0900

    feat: 강의 시작일, 종료일을 `SessionDateRange`로 묶음

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,12 +1,12 @@
 package nextstep.courses.domain;
 
-import java.time.LocalDateTime;
-
 public class Session {
     private String coverImageUrl;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+
+    private SessionDateRange range;
     private SessionType type;
     private SessionStatus status;
     private int capacity;
+
+
 }
