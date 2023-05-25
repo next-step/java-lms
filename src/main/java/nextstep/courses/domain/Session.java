73214d5a10f49e30757924708cb8f9f22f834d79
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-25 15:11:30 +0900

    feat: 요구사항에 따라 `Session` 모델 초기버전 구현

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,34 +1,12 @@
 package nextstep.courses.domain;
 
-import nextstep.courses.exception.CannotRegisterException;
+import java.time.LocalDateTime;
 
 public class Session {
-    private final int capacity;
-    private ImageInfo imageInfo;
-    private SessionPeriod period;
-    private SessionStatus status;
+    private String coverImageUrl;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private SessionType type;
-    private int currentUsers;
-
-    public Session(ImageInfo imageInfo, SessionPeriod period, SessionType type, SessionStatus status, int capacity) {
-        if (capacity <= 0) {
-            throw new IllegalArgumentException("총 수용인원은 1명 이상이어야 합니다");
-        }
-
-        this.imageInfo = imageInfo;
-        this.period = period;
-        this.type = type;
-        this.status = status;
-        this.capacity = capacity;
-    }
-
-    public void register() {
-        if (status != SessionStatus.RECRUITING) {
-            throw new CannotRegisterException("현재 모집중인 강의가 아닙니다");
-        }
-        if (currentUsers >= capacity) {
-            throw new CannotRegisterException("강의 최대 수강 인원을 초과했습니다");
-        }
-        currentUsers++;
-    }
+    private SessionStatus status;
+    private int capacity;
 }
