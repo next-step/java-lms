a77b510c31f273aec800f78fd429f7a3e50ca345
nayoon-kim <belloyv@gmail.com>
2023-05-26 07:42:36 +0900

    강의 및 학생

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,5 +1,12 @@
 package nextstep.courses.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class SessionTest {
     public static Session s1 = Session.of("lotto1", "image1", Cost.FREE, State.READY, 30);
+
+    @Test
+    @DisplayName("수강 신청")
+    public
 }
