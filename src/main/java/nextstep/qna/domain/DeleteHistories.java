d1def5aabf5138f210376c9501344a7391cb7a44
highjune <highjune37@gmail.com>
2023-05-25 19:51:19 +0900

    feat(domain): DeleteHistories 일급 컬렉션 구현

---
+++ src/main/java/nextstep/qna/domain/DeleteHistories.java
@@ -0,0 +1,28 @@
+package nextstep.qna.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteHistories {
+
+    private final List<DeleteHistory> deleteHistories;
+
+    public DeleteHistories() {
+        this.deleteHistories = new ArrayList<>();
+    }
+
+    public void add(Question question) {
+        this.deleteHistories.add(new DeleteHistory(
+                ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now()));
+    }
+
+    public void add(Answer answer) {
+        this.deleteHistories.add(new DeleteHistory(
+                ContentType.QUESTION, answer.getId(), answer.getWriter(), LocalDateTime.now()));
+    }
+
+    public List<DeleteHistory> getDeleteHistories() {
+        return deleteHistories;
+    }
+}
