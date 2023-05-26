700bdb1d493ea797b3ec0edf5bf3ec723946e99a
ryan <wsdf25867@gmail.com>
2023-05-27 00:06:28 +0900

    fix : 테스트 전체 실행과 단독 실행 같은 결과 반환하도록 수정

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -2,6 +2,7 @@ package nextstep.qna.domain;
 
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +11,14 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static Question Q1;
+    public static Question Q2;
+
+    @BeforeEach
+    void setUp() {
+       Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+       Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    }
 
     @Test
     @DisplayName("작성자가 질문을 삭제하면 deleted 상태는 TRUE")
