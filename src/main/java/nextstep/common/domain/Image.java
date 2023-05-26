3c882fbec9539b31874903b96b856de1a7fa5d53
dong <ultraraptor@naver.com>
2023-05-27 00:13:55 +0900

    refactor : 컬럼순서변경, 게터메서드 추가

--- src/main/java/nextstep/common/domain/Image.java
+++ src/main/java/nextstep/common/domain/Image.java
@@ -3,12 +3,21 @@ package nextstep.common.domain;
 import javax.validation.constraints.NotBlank;
 
 public class Image {
-    @NotBlank(message = "이미지링크는 공백이 허용되지 않습니다")
-    private final String imageLink;
     private Long imageId;
+    @NotBlank(message = "이미지링크는 공백이 허용되지 않습니다")
+    private String imageLink;
 
-    public Image(String imageLink) {
+    public Image(Long imageId,String imageLink) {
+        this.imageId = imageId;
         this.imageLink = imageLink;
+    }
+
+    public static Image of(String imageLink) {
+        return new Image(null, imageLink);
+    }
+
+    public Long getImageId() {
+        return imageId;
     }
 
     public String getImageUrl() {
