5371e908270239dcc5f0b693576c83487e1f8b76
player-geun <player_geun@naver.com>
2023-05-26 17:39:21 +0900

    refactor : SQL 매핑을 위해 정적 팩토리 메서드 구현

--- src/main/java/nextstep/courses/domain/SessionStatus.java
+++ src/main/java/nextstep/courses/domain/SessionStatus.java
@@ -1,7 +1,19 @@
 package nextstep.courses.domain;
 
+import nextstep.courses.exception.NoSuchSessionException;
+
 public enum SessionStatus {
     READY, OPEN, CLOSE;
+
+    public static SessionStatus from(String value) {
+        try {
+            return valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new NoSuchSessionException(
+                    String.format("%s는 존재하지 않는 강의 상태입니다.", value)
+            );
+        }
+    }
 
     public boolean isReady() {
         return this.equals(SessionStatus.READY);
--- src/main/java/nextstep/courses/domain/SessionType.java
+++ src/main/java/nextstep/courses/domain/SessionType.java
@@ -1,5 +1,17 @@
 package nextstep.courses.domain;
 
+import nextstep.courses.exception.NoSuchSessionException;
+
 public enum SessionType {
-    FREE, PAY
+    FREE, PAY;
+
+    public static SessionType from(String value) {
+        try {
+            return valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new NoSuchSessionException(
+                    String.format("%s는 존재하지 않는 강의 타입입니다.", value)
+            );
+        }
+    }
 }
