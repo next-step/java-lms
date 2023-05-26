8f630a8f6a71a5e7a3fc74767a951aa1b9272f1a
dhkimneer <kimds989@naver.com>
2023-05-27 02:16:02 +0900

    feature: Add DeleteHistories.java for making first class collection and having addDeleteHistory logic

---
+++ src/main/java/nextstep/qna/domain/DeleteHistories.java
@@ -0,0 +1,17 @@
+package nextstep.qna.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteHistories {
+
+    private final List<DeleteHistory> deleteHistories;
+
+    public DeleteHistories() {
+        this.deleteHistories = new ArrayList<>();
+    }
+
+    public void addDeleteHistory(DeleteHistory deleteHistory) {
+        deleteHistories.add(deleteHistory);
+    }
+}
