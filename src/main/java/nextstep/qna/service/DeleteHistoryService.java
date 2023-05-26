e713eddfd1797256d399ed188e63e6c9c9ba3c85
dhkimneer <kimds989@naver.com>
2023-05-27 02:27:50 +0900

    chore: Update DeleteHistoryService.java for adding deleteHistories

--- src/main/java/nextstep/qna/service/DeleteHistoryService.java
+++ src/main/java/nextstep/qna/service/DeleteHistoryService.java
@@ -1,5 +1,6 @@
 package nextstep.qna.service;
 
+import nextstep.qna.domain.DeleteHistories;
 import nextstep.qna.domain.DeleteHistory;
 import nextstep.qna.domain.DeleteHistoryRepository;
 import org.springframework.stereotype.Service;
@@ -7,15 +8,24 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Service("deleteHistoryService")
 public class DeleteHistoryService {
     @Resource(name = "deleteHistoryRepository")
     private DeleteHistoryRepository deleteHistoryRepository;
 
+    private final DeleteHistories deleteHistories;
+
+    public DeleteHistoryService() {
+        this.deleteHistories = new DeleteHistories();
+    }
+
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void saveAll(List<DeleteHistory> deleteHistories) {
+    public void saveAll() {
         deleteHistoryRepository.saveAll(deleteHistories);
+    }
+
+    public void addDeleteHistory(DeleteHistory deleteHistory) {
+        deleteHistories.addDeleteHistory(deleteHistory);
     }
 }
