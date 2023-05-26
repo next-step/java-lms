45f789833bc93748068c522e6d52865f68953f57
soochan <tncksdl05@gmail.com>
2023-05-26 16:18:05 +0900

    test : SessionEntity Test 코드 추가

---
+++ src/main/java/nextstep/courses/infrastructure/persistence/dao/SessionEntityRepository.java
@@ -0,0 +1,43 @@
+package nextstep.courses.infrastructure.persistence.dao;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import nextstep.courses.infrastructure.persistence.entity.SessionEntity;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository("sessionEntityRepository")
+public class SessionEntityRepository {
+
+  private final JdbcOperations jdbcTemplate;
+
+  public SessionEntityRepository(JdbcOperations jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public Optional<SessionEntity> findById(Long sessionId) {
+    String sql = "select id, course_id, title, description, cover_image_id, session_type, session_status,"
+        + " max_enrollment_size, start_date_time, end_date_time, created_at, updated_at from session where id = ?";
+    return jdbcTemplate.queryForObject(sql, rowMapper(), sessionId);
+  }
+
+  private RowMapper<Optional<SessionEntity>> rowMapper() {
+    return (rs, rowNum) -> {
+      Long id = rs.getLong("id");
+      Long courseId = rs.getLong("course_id");
+      String title = rs.getString("title");
+      String description = rs.getString("description");
+      Long coverImageId = rs.getLong("cover_image_id");
+      String sessionType = rs.getString("session_type");
+      String sessionStatus = rs.getString("session_status");
+      int maxEnrollmentSize = rs.getInt("max_enrollment_size");
+      LocalDateTime startDateTime = rs.getTimestamp("start_date_time").toLocalDateTime();
+      LocalDateTime endDateTime = rs.getTimestamp("end_date_time").toLocalDateTime();
+
+      return Optional.of(
+          new SessionEntity(id, courseId, title, description, coverImageId, sessionType,
+              sessionStatus, maxEnrollmentSize, startDateTime, endDateTime));
+    };
+  }
+}
---
+++ src/test/java/nextstep/courses/infrastructure/persistence/dao/SessionEntityRepositoryTest.java
@@ -0,0 +1,38 @@
+package nextstep.courses.infrastructure.persistence.dao;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+import nextstep.courses.infrastructure.persistence.entity.SessionEntity;
+import nextstep.users.infrastructure.UserRepositoryTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcOperations;
+
+@JdbcTest
+class SessionEntityRepositoryTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryTest.class);
+
+
+  @Autowired
+  private JdbcOperations jdbcOperations;
+
+  private SessionEntityRepository sessionEntityRepository;
+
+  @BeforeEach
+  void setUp() {
+    sessionEntityRepository = new SessionEntityRepository(jdbcOperations);
+  }
+
+  @Test
+  void findById() {
+    Optional<SessionEntity> sessionEntity = sessionEntityRepository.findById(1L);
+    assertThat(sessionEntity).isNotNull();
+    LOGGER.debug("SessionEntity: {}", sessionEntity);
+  }
+}
