42040c60a40c79d46b91145ce26d7fe923a38744
soochan <tncksdl05@gmail.com>
2023-05-27 03:38:51 +0900

    refactor : session id를 통해서 수강 신청한 학생 고유 숫자 id 값을 return 하도록 변경

--- src/main/java/nextstep/courses/infrastructure/persistence/dao/SessionEnrollmentEntityRepository.java
+++ src/main/java/nextstep/courses/infrastructure/persistence/dao/SessionEnrollmentEntityRepository.java
@@ -14,8 +14,8 @@ public class SessionEnrollmentEntityRepository {
   }
 
 
-  public List<String> findUserIdsBySessionId(Long sessionId) {
+  public List<Long> findUserIdsBySessionId(Long sessionId) {
     String sql = "select user_id from session_enrollment where session_id = ?";
-    return jdbcTemplate.queryForList(sql, String.class, sessionId);
+    return jdbcTemplate.queryForList(sql, Long.class, sessionId);
   }
 }
