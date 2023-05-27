8cb2fba344e94247c4e8f0335e5c90b3434a40dd
KimJuHyun <rlawngus0910@naver.com>
2023-05-27 22:36:24 +0900

    test: 수강신청 성공 테스트 추가

--- src/test/java/nextstep/session/domain/SessionTest.java
+++ src/test/java/nextstep/session/domain/SessionTest.java
@@ -5,6 +5,7 @@ import nextstep.session.StudentNumberExceededException;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SessionTest {
@@ -31,5 +32,18 @@ public class SessionTest {
         assertThatThrownBy(
                 () -> session.signUp(NsUserTest.JAVAJIGI))
                 .isInstanceOf(StudentNumberExceededException.class);
+    }
+
+    @Test
+    void 수강신청_성공() throws StudentNumberExceededException, NotRecruitException {
+
+        // given
+        Session session = new Session(1L, 2L, SessionStatus.RECRUITING);
+
+        // when
+        session.signUp(NsUserTest.JAVAJIGI);
+
+        // then
+        assertThat(session.getStudents()).contains(NsUserTest.JAVAJIGI);
     }
 }
