d5a9d49ab29607ab42f429b7ac2de28d63674007
munjeongho <96mjh00@gmail.com>
2023-05-28 08:23:01 +0900

    refactor :  피드백 이후 리팩토링

--- src/main/java/nextstep/courses/domain/SessionBillingType.java
+++ src/main/java/nextstep/courses/domain/SessionBillingType.java
@@ -1,16 +1,6 @@
 package nextstep.courses.domain;
 
 public enum SessionBillingType {
-    FREE("무료"),
-    PAID("유료");
-
-    private final String type;
-
-    SessionBillingType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
+    FREE,
+    PAID;
 }
