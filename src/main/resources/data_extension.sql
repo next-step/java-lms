65a514e28e6a9ccbe57d6dd6a6f5f015330bd2b7
dong <ultraraptor@naver.com>
2023-05-27 14:10:20 +0900

    refactor : 여러번 반복적으로 들어갈수 있는 sql 파일과 아닌 파일의 분리

---
+++ src/main/resources/data_extension.sql
@@ -0,0 +1,20 @@
+-- NsUser 도메인
+INSERT INTO ns_user ( user_id, password, name, email, created_at)
+values ('javajigi-sql', 'test', '자바지기 SQL', 'javajigi@slipp.net', CURRENT_TIMESTAMP()),
+       ('sanjigi-sql', 'test', '산지기 SQL', 'sanjigi@slipp.net', CURRENT_TIMESTAMP());
+
+-- Question 도메인
+INSERT INTO question ( writer_id, title, contents, created_at, deleted)
+VALUES ( 1, 'Ruby on Rails와 Play가 활성화되기 힘든 이유', 'Ruby on Rails',CURRENT_TIMESTAMP(), false),
+       ( 2, 'reflect 발동 주체','설계를 희한하게 하는 바람',CURRENT_TIMESTAMP(), false);
+
+-- Answer 도메인
+INSERT INTO answer ( writer_id, contents, created_at, question_id, deleted)
+VALUES (1,'자바스크립트의 언어 기본 API 보완',CURRENT_TIMESTAMP(), 1, false),
+       (2,'최신 버전을 공부',CURRENT_TIMESTAMP(), 1, false);
+
+-- Image
+INSERT INTO IMAGE (IMAGE_URL)
+VALUES ('www'),
+       ('sdsdf'),
+       ('asdfsdf');
