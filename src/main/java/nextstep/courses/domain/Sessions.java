34d99743ad9261c3622cb2817d0f42560f9e7f52
seriouskang <scdean110@gmail.com>
2023-05-27 20:30:50 +0900

    feat(Sessions): 생성자 추가

--- src/main/java/nextstep/courses/domain/Sessions.java
+++ src/main/java/nextstep/courses/domain/Sessions.java
@@ -4,7 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Sessions {
-    private final Set<Session> sessions = new HashSet<>();
+    private final Set<Session> sessions;
+
+    public Sessions() {
+        this(new HashSet<>());
+    }
+
+    public Sessions(Set<Session> sessions) {
+        this.sessions = sessions;
+    }
 
     public void add(Session session) {
         if(sessions.contains(session)) {
