35ff9b9580c35cdcb460c5332ec2a3ed7d6cb9f8
patrickYoo <yhy1045@naver.com>
2023-05-28 01:23:05 +0900

    test: Answer 누락된 테스트 추가

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -1,7 +1,12 @@
 package nextstep.qna.domain;
 
+import static java.time.LocalDateTime.now;
+import static nextstep.qna.domain.ContentType.ANSWER;
+import static nextstep.users.domain.NsUserTest.JAVAJIGI;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -16,9 +21,6 @@ public class AnswersTest {
 
     public static final Answers SINGLE_JAVAJIGI_ANSWERS = new Answers(
             List.of(AnswerTest.JAVAJIGI_ANSWER));
-
-    public static final Answers SINGLE_SANJIGI_ANSWERS = new Answers(
-            List.of(AnswerTest.SANJIGI_ANSWER));
 
     @Test
     @DisplayName("질문 목록중 로그인 유저와 다른 유저가 배열에 존재할 경우 true 를 리턴한다.")
@@ -41,4 +43,20 @@ public class AnswersTest {
                 .isTrue();
     }
 
+    @Test
+    @DisplayName("삭제되지 않은 답변에 대해 서는 비어있는 내역을 반환한다.")
+    void historiesNotYetDeletedTest() {
+        assertThat(SINGLE_JAVAJIGI_ANSWERS.deleteHistories().isEmpty())
+                .isTrue();
+    }
+
+    @Test
+    @DisplayName("답변에 대해 삭제 내역을 리턴한다.")
+    void deleteHistoriesTest() {
+        SINGLE_JAVAJIGI_ANSWERS.deleteAll();
+        assertThat(SINGLE_JAVAJIGI_ANSWERS.deleteHistories())
+                .isEqualTo(new ArrayList<>(List.of(
+                        new DeleteHistory(ANSWER,null,JAVAJIGI, now())
+                )));
+    }
 }
