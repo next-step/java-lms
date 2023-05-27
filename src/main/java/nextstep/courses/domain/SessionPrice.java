bdce9c6d0349728f0d0eede6da010abdd10fb8a1
seriouskang <scdean110@gmail.com>
2023-05-27 12:33:03 +0900

    feat(SessionPrice): 생성자 추가

--- src/main/java/nextstep/courses/domain/SessionPrice.java
+++ src/main/java/nextstep/courses/domain/SessionPrice.java
@@ -7,11 +7,15 @@ public class SessionPrice {
     private final SessionChargeType type;
 
     public SessionPrice(int price) {
+        this(price, SessionChargeType.of(price));
+    }
+
+    public SessionPrice(int price, SessionChargeType type) {
         if(price < FREE_SESSION_PRICE) {
             throw new IllegalArgumentException("강의 최소 수강료는 " + FREE_SESSION_PRICE + "원입니다: " + price);
         }
         this.price = price;
-        this.type = SessionChargeType.of(price);
+        this.type = type;
     }
 
     public SessionChargeType type() {
