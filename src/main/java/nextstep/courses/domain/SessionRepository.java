aeb9b5f06e7d57bc5ddda2085bcadfe93ab1c290
juno-junho <ssmm0205@naver.com>
2023-05-28 05:26:17 +0900

    feat(domain): SessionRepository course로부터 session 조회 기능 추가

--- src/main/java/nextstep/courses/domain/SessionRepository.java
+++ src/main/java/nextstep/courses/domain/SessionRepository.java
@@ -1,7 +1,11 @@
 package nextstep.courses.domain;
 
+import java.util.List;
+
 public interface SessionRepository {
     int save(Session session);
 
     Session findById(Long id);
+
+    List<Session> findByCourseId(Long courseId);
 }
