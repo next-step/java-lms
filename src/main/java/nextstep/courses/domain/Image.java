f7e05b988b33830f9a7978acc9892c95f369549b
Lee Byoung Seong <dev.bslee@gmail.com>
2023-05-27 17:39:59 +0900

    feat: Image 객체 생성

---
+++ src/main/java/nextstep/courses/domain/Image.java
@@ -0,0 +1,33 @@
+package nextstep.courses.domain;
+
+import nextstep.qna.UnAuthorizedException;
+import nextstep.qna.domain.generator.SimpleIdGenerator;
+
+public class Image {
+    private final long id;
+    private String path;
+
+    private Image(long id, String path) {
+
+        if (id == 0L) {
+            throw new IllegalArgumentException("유효하지 않는 아이디에요 :( [입력 값 : " + id + "]");
+        }
+
+        if (path == null || path.isEmpty()) {
+            throw new UnAuthorizedException("이미지 경로가 입력되질 않았어요 :(");
+        }
+
+        this.id = id;
+        this.path = path;
+    }
+
+    public static Image of(long id, String path) {
+        return new Image(id, path);
+    }
+
+    public static Image from(String path) {
+        long id = SimpleIdGenerator.getAndIncrement(Image.class);
+        return new Image(id, path);
+    }
+
+}
