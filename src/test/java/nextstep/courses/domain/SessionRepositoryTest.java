12e10f42f129173551311b2e5fecc7daf57e75ba
진우 <nice7677@naver.com>
2023-05-27 14:05:43 +0900

    test: SessionRepositoryTest 테스트

---
+++ src/test/java/nextstep/courses/domain/SessionRepositoryTest.java
@@ -0,0 +1,92 @@
+package nextstep.courses.domain;
+
+import nextstep.courses.code.SessionStatus;
+import nextstep.courses.code.SessionType;
+import nextstep.courses.infrastructure.CourseRepositoryTest;
+import nextstep.courses.infrastructure.JdbcCourseRepository;
+import nextstep.courses.infrastructure.JdbcSessionRepository;
+import nextstep.users.domain.NextStepUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@JdbcTest
+public class SessionRepositoryTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessionRepositoryTest.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private SessionRepository sessionRepository;
+
+    private CourseRepository courseRepository;
+
+    private Course course = new Course("넥스트스텝", 1L);
+    private Session session = new Session(course, 1, "우하하하하", SessionType.FREE, SessionStatus.WAITING, 0, LocalDateTime.now(), LocalDateTime.now().plusDays(5));
+    private NextStepUser JAVAJIGI = new NextStepUser(1L, "javajigi", "password", "name", "javajigi@slipp.net");
+
+    @BeforeEach
+    void setUp() {
+        sessionRepository = new JdbcSessionRepository(jdbcTemplate);
+        courseRepository = new JdbcCourseRepository(jdbcTemplate);
+    }
+
+    @Test
+    void create() {
+        assertAll(
+                () -> assertThat(session).isNotNull(),
+                () -> assertThat(session.getCourse().getTitle()).isEqualTo("넥스트스텝"),
+                () -> assertThat(session.getCoverImage()).isEqualTo("우하하하하"),
+                () -> assertThat(session.getType()).isEqualTo(SessionType.FREE),
+                () -> assertThat(session.getStatus()).isEqualTo(SessionStatus.WAITING),
+                () -> assertThat(session.getStatus()).isNotEqualTo(SessionStatus.RECRUIT)
+                ); ;
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "{a}")
+    void addSignUpHistory(String input) {
+
+        assertThatThrownBy(() -> {
+            session.addSignUpHistory(new SignUpHistory(session, JAVAJIGI));
+        }).hasMessageContaining("강의 수강신청은 강의 상태가 모집중일 때만 가능합니다.");
+
+        session = new Session(course, 1, "우하하하하", SessionType.FREE, SessionStatus.RECRUIT, 0, LocalDateTime.now(), LocalDateTime.now().plusDays(5));
+
+        assertThatThrownBy(() -> {
+            session.addSignUpHistory(new SignUpHistory(session, JAVAJIGI));
+        }).hasMessageContaining("최대 수강 인원을 초과할 수 없습니다.");
+
+    }
+
+    @Test
+    void crud() {
+
+        Course course = new Course("TDD, 클린 코드 with Java", 1L);
+        courseRepository.save(course);
+        Course savedCourse = courseRepository.findById(1L);
+
+        Session session = new Session(savedCourse, 1, "우하하하하", SessionType.FREE, SessionStatus.WAITING, 10, LocalDateTime.now(), LocalDateTime.now().plusDays(5));
+        int count = sessionRepository.save(session);
+        assertThat(count).isEqualTo(1);
+
+        Session saveSession = sessionRepository.findById(1L);
+        assertThat(saveSession.getCoverImage()).isEqualTo(session.getCoverImage());
+        LOGGER.debug("saveSession : {}", saveSession);
+
+    }
+
+}
