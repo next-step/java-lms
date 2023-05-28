6a684ead5ab5eb9293357059b8fb4e337368b544
highjune <highjune37@gmail.com>
2023-05-29 01:56:12 +0900

    test(domain): StudentTest 에서 등록시 1 증가하는 테스트 먼저 생성

--- src/test/java/nextstep/courses/domain/StudentTest.java
+++ src/test/java/nextstep/courses/domain/StudentTest.java
@@ -1,14 +1,30 @@
 package nextstep.courses.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StudentTest {
 
+    Student june1;
+    Student june2;
+    Student june3;
+    Session session;
+
     @BeforeEach
     void setUp() {
-        Student june1 = new Student(0L, 10L);
-        Student june2 = new Student(0L, 10L);
-        Student june3 = new Student(0L, 10L);
-        Session session = SessionCreator.create(2L, SessionStatus.OPENED);
+        june1 = new Student(0L, 10L);
+        june2 = new Student(0L, 10L);
+        june3 = new Student(0L, 10L);
+        session = SessionCreator.create(2L, SessionStatus.OPENED);
     }
+
+    @Test
+    @DisplayName("학생은 수강신청시 강의의 현재 수강인원이 1이 증가한다.")
+    void enroll_SessionMaxNumberOfValue_PlusOne() {
+        june1.enroll(session);
+        Assertions.assertThat(session.totalStudentNum()).isEqualTo(1);
+    }
+
 }
