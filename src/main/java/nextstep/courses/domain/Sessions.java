79df9d18b515d0d59262021ac1ba632b6e8604c2
seongjo24 <kangsj24@naver.com>
2023-05-28 21:29:04 +0900

    Course Sesseions 추가

---
+++ src/main/java/nextstep/courses/domain/Sessions.java
@@ -0,0 +1,13 @@
+package nextstep.courses.domain;
+
+import java.util.List;
+
+public class Sessions {
+    private List<Session> sessions;
+
+    public Sessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+
+}
