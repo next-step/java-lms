6d708a895dc7cea84a1cb845b7ceaed8fed91ec0
jaewon0913 <jaewon9937@gmail.com>
2023-05-28 17:46:20 +0900

    Feat(lms) : Step2 : 수강신청(도메인 모델)
    - PriceTest 테스트 코드 추가

---
+++ src/test/java/nextstep/courses/domain/PriceTest.java
@@ -0,0 +1,21 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class PriceTest {
+
+    @DisplayName("가격(무료=0/유료!=0) 테스트")
+    @Test
+    public void 가격_테스트() {
+        assertAll(
+                () -> assertThat(new Price(0).getPrice())
+                        .isEqualTo(PriceType.FREE),
+                () -> assertThat(new Price(2).getPrice())
+                        .isEqualTo(PriceType.PAID)
+        );
+    }
+}
