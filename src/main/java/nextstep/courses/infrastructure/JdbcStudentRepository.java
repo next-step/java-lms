1b05637acc491085930d0ab87104877c1793bddf
juno-junho <ssmm0205@naver.com>
2023-05-29 04:49:57 +0900

    feat(infra): JdbcStudentRepository 작성

---
+++ src/main/java/nextstep/courses/infrastructure/JdbcStudentRepository.java
@@ -0,0 +1,39 @@
+package nextstep.courses.infrastructure;
+
+import nextstep.courses.domain.*;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("studentRepository")
+public class JdbcStudentRepository implements StudentRepository {
+
+    private final JdbcOperations jdbcTemplate;
+
+    public JdbcStudentRepository(JdbcOperations jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int save(Student student) {
+        String sql = "insert into student (session_id)" +
+                " values(?)";
+        return jdbcTemplate.update(sql, student.getSessionId());
+    }
+
+    @Override
+    public List<Student> findBySessionId(Long sessionId) {
+        String sql = "select id, session_id" +
+                " from student" +
+                " where session_id = ?";
+
+        RowMapper<Student> rowMapper = (rs, rowNum) -> new Student(
+                rs.getLong(1),
+                rs.getLong(2)
+        );
+
+        return jdbcTemplate.queryForList(sql, Student.class, rowMapper, sessionId);
+    }
+}
