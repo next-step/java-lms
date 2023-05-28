64350b60be92472a7f523eda32c2e1571b847eb9
jaewon0913 <jaewon9937@gmail.com>
2023-05-28 17:53:58 +0900

    Feat(lms) : Step2 : 수강신청(도메인 모델)
    - SessionsTest 테스트 코드 추가

---
+++ src/test/java/nextstep/courses/domain/SessionsTest.java
@@ -0,0 +1,33 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SessionsTest {
+
+    @DisplayName("강의 신청 시 해당하는 강의 id 가 없을 경우에 에러를 응답한다.")
+    @Test
+    public void 강의_신청_해당_강의_없는경우_테스트() {
+        Session session = new Session(1L, LocalDateTime.now(), null,
+                new SessionInfo(10, 0, SessionStatusType.RECRUITING),
+                new CoverImage("url"),
+                new SessionDate(LocalDateTime.now(), LocalDateTime.now()),
+                new Price(1000));
+
+        Session session2 = new Session(2L, LocalDateTime.now(), null,
+                new SessionInfo(10, 5, SessionStatusType.RECRUITING),
+                new CoverImage("url2"),
+                new SessionDate(LocalDateTime.now(), LocalDateTime.now()),
+                new Price(0));
+
+        Sessions sessions = new Sessions(List.of(session, session2));
+
+        assertThatThrownBy(() -> sessions.registerSession(3L, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
