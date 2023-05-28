46d5a26ae224baa08f7c72ae58ae668c5e2c6e39
MoonHKLee <mf311110@gmail.com>
2023-05-29 01:23:14 +0900

    refactor(testCourse) : 공통사용 객체 리팩터링.

--- src/test/java/nextstep/courses/domain/CourseTest.java
+++ src/test/java/nextstep/courses/domain/CourseTest.java
@@ -1,16 +1,22 @@
 package nextstep.courses.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CourseTest {
 
+    private Course course;
+
+    @BeforeEach
+    void setUp() {
+        //given
+        course = new Course("객체지향 프로그래밍", 1L);
+    }
+
     @Test
     void test_과정을_만들면_강의리스트가_생성된다(){
-        // given
-        Course course = new Course("객체지향 프로그래밍", 1L);
-
         // when
         Sessions sessions = course.getSessions();
 
@@ -20,9 +26,6 @@ public class CourseTest {
 
     @Test
     void test_과정을_만들면_강의리스트가_비어있다(){
-        // given
-        Course course = new Course("객체지향 프로그래밍", 1L);
-
         // when
         Sessions sessions = course.getSessions();
 
@@ -33,7 +36,6 @@ public class CourseTest {
     @Test
     void test_과정에_강의를_등록할_수_있다() {
         // given
-        Course course = new Course("객체지향 프로그래밍", 1L);
         Session session = new Session(1L);
 
         // when
@@ -46,14 +48,13 @@ public class CourseTest {
     @Test
     void test_과정에_있는_강의를_확인할_수_있다() {
         // given
-        Course course = new Course("객체지향 프로그래밍", 1L);
         Session session = new Session(1L);
         course.addSession(session);
 
         // when
-        Session targetSession = course.getSessions().getSession(1L);
+        Session readSession = course.getSessions().getSession(1L);
 
         // then
-        assertThat(targetSession).isEqualTo(session);
+        assertThat(readSession).isEqualTo(session);
     }
 }
