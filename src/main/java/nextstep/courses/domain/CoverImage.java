00c0c046ac54df7595cbfd1b600fed30671d0d2c
ryan <wsdf25867@gmail.com>
2023-05-28 10:49:46 +0900

    chore : import 제거 및 equals and hashcode 구현

--- src/main/java/nextstep/courses/domain/CoverImage.java
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -1,5 +1,7 @@
 package nextstep.courses.domain;
 
+import java.util.Objects;
+
 public class CoverImage {
     private String path;
     private String name;
@@ -7,5 +9,18 @@ public class CoverImage {
     public CoverImage(String path, String name) {
         this.path = path;
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CoverImage that = (CoverImage) o;
+        return Objects.equals(path, that.path) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path, name);
     }
 }
--- src/test/java/nextstep/courses/domain/CoverImageTest.java
+++ src/test/java/nextstep/courses/domain/CoverImageTest.java
@@ -1,7 +1,5 @@
 package nextstep.courses.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CoverImageTest {
 
     public static final CoverImage C1 = new CoverImage("/", "커버이미지");
