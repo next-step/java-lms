5dafd3930aba8048a4b41050d2fb05b4f184b6aa
patrickYoo <yhy1045@naver.com>
2023-05-28 17:53:33 +0900

    chor: 불필요한 Qualifier 삭제

--- src/main/java/nextstep/qna/infrastructure/JdbcDeleteHistoryRepository.java
+++ src/main/java/nextstep/qna/infrastructure/JdbcDeleteHistoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository("deleteHistoryRepository")
+@Repository
 public class JdbcDeleteHistoryRepository implements DeleteHistoryRepository {
     @Override
     public void saveAll(List<DeleteHistory> deleteHistories) {
