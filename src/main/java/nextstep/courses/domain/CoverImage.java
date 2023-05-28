78f34903c47e1c435662812893b600d808a46210
jaewon0913 <jaewon9937@gmail.com>
2023-05-28 16:46:57 +0900

    Feat(lms) : Step2 : 수강신청(도메인 모델)
    - CoverImage 추가(커버 이미지 정보)

---
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -0,0 +1,13 @@
+package nextstep.courses.domain;
+
+public class CoverImage {
+    private final String imageUrl;
+
+    public CoverImage(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
