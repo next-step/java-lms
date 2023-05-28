275582093cfef920b2a86ef10fbafc830eb2942b
dkswnkk <jjoy9810@gmail.com>
2023-05-28 14:08:16 +0900

    feat: Image 생성자 추가

--- src/main/java/nextstep/courses/domain/Image.java
+++ src/main/java/nextstep/courses/domain/Image.java
@@ -6,6 +6,7 @@ import nextstep.courses.exception.InvalidImageException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Objects;
 
 public class Image extends BaseEntity {
     private static final Long MAX_SIZE = 5L * 1024 * 1024; // 5MB
@@ -23,6 +24,14 @@ public class Image extends BaseEntity {
     protected Image() {
     }
 
+    public Image(Long id, String name, URI uri, Long size, ImageType type) {
+        this.id = id;
+        this.name = name;
+        this.uri = uri;
+        this.size = size;
+        this.type = type;
+    }
+
     public static Image create(String name, String uri, Long size, String typeString) throws InvalidImageException, URISyntaxException {
         validateUri(uri);
         validateSize(size);
@@ -34,6 +43,10 @@ public class Image extends BaseEntity {
         image.type = ImageType.of(typeString);
 
         return image;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
@@ -70,4 +83,16 @@ public class Image extends BaseEntity {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return Objects.equals(id, image.id) && Objects.equals(name, image.name) && Objects.equals(uri, image.uri) && Objects.equals(size, image.size) && type == image.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, uri, size, type);
+    }
 }
