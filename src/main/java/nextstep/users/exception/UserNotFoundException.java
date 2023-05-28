70927d4bf6487e09165f396f3d0024eb96ed2a44
dkswnkk <jjoy9810@gmail.com>
2023-05-28 14:07:23 +0900

    feat: exception 추가

---
+++ src/main/java/nextstep/users/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package nextstep.users.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
