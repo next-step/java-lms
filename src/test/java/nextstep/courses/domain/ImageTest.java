b6b22014eeee08b696fde6ad029947385db53b6a
MoonHKLee <mf311110@gmail.com>
2023-05-29 02:18:36 +0900

    test(session) : 강의 이미지 정보 테스트

---
+++ src/test/java/nextstep/courses/domain/ImageTest.java
@@ -0,0 +1,6 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class ImageTest {
+}
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,22 +1,42 @@
 package nextstep.courses.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SessionTest {
 
+    private Session session;
+    @BeforeEach
+    void setUp() {
+        session = new Session(1L,"2021-01-01", "2021-01-02", Image.DEFAULT_IMAGE_URL);
+    }
+
+    @Test
+    void test_강의는_기간을_갖는다() {
+        //when
+        Period period = session.getPeriod();
+
+        //then
+        assertThat(period).isNotNull();
+    }
+
     @Test
     void test_강의의_시작일과_종료일_수정() {
-        // given
-        Session session = new Session(1L);
-
         // when
         session.updatePeriod("2021-01-01", "2021-01-02");
 
         // then
-        assertThat(session.getStartDate()).isEqualTo(LocalDate.parse("2021-01-01"));
+        assertThat(session.getPeriod()).isEqualTo(new Period("2021-01-01", "2021-01-02"));
+    }
+
+    @Test
+    void test_강의_생성시_이미지_정보를_갖는다() {
+        // when
+        Image image = session.getImage();
+
+        // then
+        assertThat(image).isNotNull();
     }
 }
