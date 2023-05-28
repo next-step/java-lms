52c5ab384c746f380f74d5685327605dd9964b00
jaewon0913 <jaewon9937@gmail.com>
2023-05-28 16:47:54 +0900

    Feat(lms) : Step2 : 수강신청(도메인 모델)
    - SessionStatusType Enum 추가

---
+++ src/main/java/nextstep/courses/domain/SessionStatusType.java
@@ -0,0 +1,16 @@
+package nextstep.courses.domain;
+
+public enum SessionStatusType {
+    PREPARE("준비중"),
+    RECRUITING("모집중"),
+    END("종료");
+
+    String status;
+
+    SessionStatusType(String status) {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
