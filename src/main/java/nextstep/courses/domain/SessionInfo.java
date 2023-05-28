3ce973160a002fc9298d972f50a07f1a81ace991
jaewon0913 <jaewon9937@gmail.com>
2023-05-28 17:15:54 +0900

    Feat(lms) : Step2 : 수강신청(도메인 모델)
    - SessionInfo 추가
    - 강의 상태 및 최대 수강 인원 체크

---
+++ src/main/java/nextstep/courses/domain/SessionInfo.java
@@ -0,0 +1,36 @@
+package nextstep.courses.domain;
+
+public class SessionInfo {
+
+    private static final String REGISTER_ERROR_MESSAGE = "수강 신청은 모집중 일때만 신청 가능합니다.";
+    private static final String MAX_STUDENTS_COUNT_ERROR_MESSAGE = "최대 수강 신청 인원을 초과할 수 없습니다.";
+
+    private final int maxStudentCount;
+    private int currentStudentCount;
+    private SessionStatusType sessionStatusType;
+
+    public SessionInfo(int maxStudentCount, int currentStudentCount, SessionStatusType sessionStatusType) {
+        this.maxStudentCount = maxStudentCount;
+        this.currentStudentCount = currentStudentCount;
+        this.sessionStatusType = sessionStatusType;
+    }
+
+    public void registerSession(int studentCount) {
+        this.validationSessionStatus();
+        this.validationSessionMaxStudentCount(studentCount);
+
+        this.currentStudentCount += studentCount;
+    }
+
+    private void validationSessionStatus() {
+        if (this.sessionStatusType != SessionStatusType.RECRUITING) {
+            throw new IllegalArgumentException(REGISTER_ERROR_MESSAGE);
+        }
+    }
+
+    private void validationSessionMaxStudentCount(int studentCount) {
+        if (this.currentStudentCount + studentCount > this.maxStudentCount) {
+            throw new IllegalArgumentException(MAX_STUDENTS_COUNT_ERROR_MESSAGE);
+        }
+    }
+}
