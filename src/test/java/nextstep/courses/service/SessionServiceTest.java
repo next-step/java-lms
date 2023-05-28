e8d0fde5260ef1dae0e1d84416ea68c7f7384cea
eohyemin <eohyemin@eohyemin-ui-MacBookPro.local>
2023-05-28 17:31:57 +0900

    test(SessionServiceTest): 강의 수강신청 할 수 없는 경우 테스트 추가

--- src/test/java/nextstep/courses/service/SessionServiceTest.java
+++ src/test/java/nextstep/courses/service/SessionServiceTest.java
@@ -5,6 +5,7 @@ import nextstep.courses.domain.SessionStatus;
 import nextstep.courses.fixture.SessionFixture;
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @SpringBootTest
 public class SessionServiceTest {
@@ -46,5 +48,25 @@ public class SessionServiceTest {
 
         assertThat(sessionUsers).hasSize(1);
         assertThat(sessionUsers.get(0).getUserId()).isEqualTo(NsUserTest.JAVAJIGI.getUserId());
+    }
+
+    @Test
+    @DisplayName("강의 최대 인원이 초과된 경우 수강신청할 수 없다")
+    void saveSessionUser_Fail1() {
+        Session session = sessionService.save(SessionFixture.create(SessionStatus.RECRUITING, 1), 1L);
+        sessionService.saveSessionUser(session, NsUserTest.JAVAJIGI);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sessionService.saveSessionUser(session, NsUserTest.SANJIGI))
+                .withMessageMatching("강의 최대 수강 인원이 초과되었습니다.");
+    }
+
+    @Test
+    @DisplayName("강의가 모집중이 아닌 경우 수강신청할 수 없다")
+    void saveSessionUser_Fail2() {
+        Session session = sessionService.save(SessionFixture.create(SessionStatus.PREPARING, 1), 1L);
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sessionService.saveSessionUser(session, NsUserTest.JAVAJIGI))
+                .withMessageMatching("모집중인 강의가 아닙니다.");
     }
 }
