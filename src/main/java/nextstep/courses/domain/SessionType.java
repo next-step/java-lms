1dc809f9b8d1e059675b6826cf0b2e8513cbd9ec
juno-junho <ssmm0205@naver.com>
2023-05-29 04:38:53 +0900

    feat(domain): string으로 enum 값 찾는 메서드 추가

--- src/main/java/nextstep/courses/domain/SessionType.java
+++ src/main/java/nextstep/courses/domain/SessionType.java
@@ -1,5 +1,14 @@
 package nextstep.courses.domain;
 
+import java.util.Arrays;
+
 public enum SessionType {
-    FREE, CHARGED
+    FREE, CHARGED;
+
+    public static SessionType find(String sessionType) {
+        return Arrays.stream(values())
+                .filter(type -> type.name().equalsIgnoreCase(sessionType))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 강의 타입 입니다."));
+    }
 }
