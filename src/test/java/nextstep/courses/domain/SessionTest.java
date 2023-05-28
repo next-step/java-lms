8188fa20b51342f20c9135820d566a58f8efea6f
MoonHKLee <mf311110@gmail.com>
2023-05-29 02:57:06 +0900

    test(session) : 수강신청 테스트 추가.

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,16 +1,20 @@
 package nextstep.courses.domain;
 
+import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class SessionTest {
 
     private Session session;
     @BeforeEach
     void setUp() {
-        session = new Session(1L,"2021-01-01", "2021-01-02", Image.DEFAULT_IMAGE_URL,Cost.FREE, Status.READY);
+        session = new Session(1L,"2021-01-01", "2021-01-02", Image.DEFAULT_IMAGE_URL,Cost.FREE, Status.OPENED);
     }
 
     @Test
@@ -47,5 +51,23 @@ public class SessionTest {
 
         // then
         assertThat(status).isNotNull();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Status.class, names = {"READY", "CLOSED"})
+    void test_수강신청_강의상태_모집중_아닐시_실패(Status status) {
+        // given
+        Session session = new Session(1L,"2021-01-01", "2021-01-02", Image.DEFAULT_IMAGE_URL, Cost.FREE, status);
+
+        // then
+        assertThatCode(() -> session.register(NsUserTest.JAVAJIGI))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("수강신청은 강의 모집 중에만 가능합니다.");
+    }
+
+    @Test
+    void test_수강신청_강의상태_모집중_성공() {
+        // then
+        assertThatCode(() -> session.register(NsUserTest.JAVAJIGI)).doesNotThrowAnyException();
     }
 }
