fab114530b0a14229ee098034d2b12f558d7598c
KangHo-dev <tiger452003@gmail.com>
2023-05-28 18:37:12 +0900

    feat : 삭제 가능 여부 체크 (답변)

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,20 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("질문자와 답변자가 다른 경우, 답변 삭제 불가능")
+    @Test
+    void 답변_삭제_권한_테스트() {
+        Assertions.assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> A1.delete(NsUserTest.SANJIGI))
+                .withMessageMatching("삭제할 권한이 없습니다.");
+    }
 }
