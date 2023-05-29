eb86f59c7b8e860bb46da98a5d9106e1534cb704
eohyemin <eohyemin@eohyemin-ui-MacBookPro.local>
2023-05-29 16:22:34 +0900

    feat: 수강 신청자 상태 구현

---
+++ src/main/java/nextstep/courses/domain/SessionUserStatus.java
@@ -0,0 +1,23 @@
+package nextstep.courses.domain;
+
+public enum SessionUserStatus implements EnumModel {
+    WAIT("대기"),
+    APPROVAL("승인"),
+    REJECT("거절");
+
+    private String status;
+
+    SessionUserStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return status;
+    }
+}
