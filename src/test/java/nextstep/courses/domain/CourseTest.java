fa0de891f6a438013a9f609027d51c202a292934
PiHyoJeong <shson4007@gmail.com>
2023-05-30 00:09:19 +0900

    Feat: 2단계 구현

---
+++ src/test/java/nextstep/courses/domain/CourseTest.java
@@ -0,0 +1,8 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDateTime;
+
+public class CourseTest {
+
+    public static final Course C1 = new Course(0L, "title1", 0L, LocalDateTime.now(), LocalDateTime.now());
+}
