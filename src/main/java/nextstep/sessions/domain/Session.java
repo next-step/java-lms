19c5cf320648afe92423727d8d00963e3a913d79
DeveloperKGH <developerkgh@gmail.com>
2023-05-30 01:51:28 +0900

    :sparkles: feat : 강의는 시작일과 종료일을 가진다.

--- src/main/java/nextstep/sessions/domain/Session.java
+++ src/main/java/nextstep/sessions/domain/Session.java
@@ -2,21 +2,44 @@ package nextstep.sessions.domain;
 
 import nextstep.courses.domain.Course;
 
+import java.time.LocalDateTime;
+
 public class Session {
     private Long id;
 
     private Course course;
 
+    private LocalDateTime from;
+
+    private LocalDateTime to;
+
     public static Session of(Long id, Course course) {
-        return new Session(id, course);
+        return new Session(id, course, LocalDateTime.MIN, LocalDateTime.MAX);
     }
 
-    private Session(Long id, Course course) {
+    public static Session of(Long id, Course course, LocalDateTime from, LocalDateTime to) {
+        return new Session(id, course, from, to);
+    }
+
+    private Session(Long id, Course course, LocalDateTime from, LocalDateTime to) {
+        validatePeriod(from, to);
         this.id = id;
         this.course = course;
+        this.from = from;
+        this.to = to;
     }
 
     public void toCourse(Course course) {
         this.course = course;
+    }
+
+    private static void validatePeriod(LocalDateTime from, LocalDateTime to) {
+        if(from == null || to == null) {
+            throw new IllegalArgumentException("시작일 또는 종료일은 null 일 수 없습니다.");
+        }
+
+        if (from.isEqual(to) || from.isAfter(to)) {
+            throw new IllegalArgumentException("시작일이 종료일과 같거나 이후일 수 없습니다.");
+        }
     }
 }
--- src/test/java/nextstep/sessions/domain/SessionTest.java
+++ src/test/java/nextstep/sessions/domain/SessionTest.java
@@ -3,8 +3,15 @@ package nextstep.sessions.domain;
 import nextstep.courses.domain.CourseTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class SessionTest {
     public static final Session S1 = Session.of(1L, CourseTest.C);
@@ -14,5 +21,32 @@ public class SessionTest {
     @Test
     void Session_객체가_정상적으로_생성되는지_확인() {
         assertThat(Session.of(1L, CourseTest.C)).isInstanceOf(Session.class);
+    }
+
+    static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(null, LocalDateTime.now()),
+                Arguments.of(LocalDateTime.now(), null),
+                Arguments.of(null, null)
+        );
+    }
+
+    @DisplayName("시작일 또는 종료일의 값이 유효하지 않은경우 IllegalArgumentException 발생하는지 확인")
+    @ParameterizedTest(name = "from : {0}, to : {1}")
+    @MethodSource("provideArguments")
+    void 시작일_또는_종료일의_값이_유효하지_않은경우_IllegalArgumentException(LocalDateTime from, LocalDateTime to) {
+        assertThatThrownBy(() -> Session.of(1L, CourseTest.C, from, to))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시작일 또는 종료일은 null 일 수 없습니다.");
+    }
+
+    @DisplayName("시작일이 종료일과 같거나 늦는 경우 IllegalArgumentException 발생하는지 확인")
+    @Test
+    void 시작일이_종료일과_같거나_늦는_경우_IllegalArgumentException() {
+        LocalDateTime now = LocalDateTime.now();
+
+        assertThatThrownBy(() -> Session.of(1L, CourseTest.C, now, now))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시작일이 종료일과 같거나 이후일 수 없습니다.");
     }
 }
