be762d17f3c7d0de1594def754c05bb2c0f87357
dkswnkk <jjoy9810@gmail.com>
2023-05-30 02:53:07 +0900

    feat: SessionService 기능 추가

--- src/main/java/nextstep/courses/service/SessionService.java
+++ src/main/java/nextstep/courses/service/SessionService.java
@@ -2,11 +2,14 @@ package nextstep.courses.service;
 
 import nextstep.courses.domain.Session;
 import nextstep.courses.domain.SessionRepository;
+import nextstep.courses.domain.enums.ApprovalStatus;
 import nextstep.courses.exception.SessionEnrollmentException;
 import nextstep.courses.exception.SessionNotFoundException;
 import nextstep.users.domain.User;
 import nextstep.users.domain.UserRepository;
+import nextstep.users.exception.NotInstructorException;
 import nextstep.users.exception.UserNotFoundException;
+import nextstep.users.exception.UserNotSelectedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,13 +39,52 @@ public class SessionService {
     @Transactional
     public void enrollUserToSession(long sessionId, long userId) {
         Session session = findSessionById(sessionId);
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException("존재하지 않는 유저입니다."));
+        User user = findUserById(userId);
 
         checkUserAlreadyEnrolled(sessionId, userId);
 
         session.enroll(user);
         sessionRepository.enrollUser(session);
+    }
+
+    @Transactional
+    public void approveUserToSession(long adminId, long userId, long sessionId) {
+        validateInstructor(adminId);
+        Session session = findSessionById(sessionId);
+        User user = findUserById(userId);
+
+        if (!user.isSelected()) {
+            throw new UserNotSelectedException("선발되지 않은 인원입니다.");
+        }
+
+        session.approveUser(user);
+        sessionRepository.updateApprovalStatus(sessionId, userId, ApprovalStatus.APPROVED);
+    }
+
+    @Transactional
+    public void disApproveUserToSession(long adminId, long userId, long sessionId) {
+        validateInstructor(adminId);
+        Session session = findSessionById(sessionId);
+        User user = findUserById(userId);
+
+        if (user.isSelected()) {
+            throw new UserNotSelectedException("선발된 인원입니다.");
+        }
+
+        session.disApproveUser(user);
+        sessionRepository.updateApprovalStatus(sessionId, userId, ApprovalStatus.CANCELED);
+    }
+
+    private User findUserById(long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("존재하지 않는 유저입니다."));
+    }
+
+    private void validateInstructor(long adminId) {
+        User admin = findUserById(adminId);
+        if (!admin.isInstructor()) {
+            throw new NotInstructorException("강사가 아닙니다");
+        }
     }
 
     private void checkUserAlreadyEnrolled(long sessionId, long userId) {
