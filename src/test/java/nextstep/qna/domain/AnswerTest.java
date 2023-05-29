46d2cfe6a5b666292fc4e58847f3550682d91e3b
wotjd4305 <wotjd4305@naver.com>
2023-05-29 13:40:41 +0900

    [step1] test. 질문 삭제하기 비즈니스 로직 -> 도메인 모델

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,37 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    public static Answer answer1;
+    public static Answer answer2;
+
+    @BeforeEach
+    void init(){
+        answer1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.QUESTION_1, "Answers Contents1");
+        answer2 = new Answer(NsUserTest.SANJIGI, QuestionTest.QUESTION_2, "Answers Contents2");
+    }
+
+    @Test
+    void isOwnerTest() {
+        assertThat(answer1.isOwner(NsUserTest.JAVAJIGI)).isTrue();
+        assertThat(answer1.isOwner(NsUserTest.SANJIGI)).isFalse();
+        assertThat(answer2.isOwner(NsUserTest.JAVAJIGI)).isFalse();
+        assertThat(answer2.isOwner(NsUserTest.SANJIGI)).isTrue();
+    }
+
+    @Test
+    void deleteTest() {
+        assertThat(answer1.delete())
+                .isInstanceOf(DeleteHistory.class)
+                .isEqualTo(new DeleteHistory(ContentType.ANSWER, answer1.getId(), NsUserTest.JAVAJIGI, LocalDateTime.now()));
+        assertThat(answer1.isDeleted()).isTrue();
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,37 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    public static final Question QUESTION_1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+    public static final Question QUESTION_2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static Answer answer1;
+    public static Answer answer2;
+
+    @BeforeEach
+    void init(){
+        answer1 = new Answer(NsUserTest.JAVAJIGI, QUESTION_1, "Answers Contents1");
+        answer2 = new Answer(NsUserTest.SANJIGI, QUESTION_2, "Answers Contents1");
+    }
+
+    @Test
+    void deleteTest() throws CannotDeleteException {
+        var deleteHistories = QUESTION_1.delete(NsUserTest.JAVAJIGI);
+
+        System.out.println(deleteHistories);
+    }
+
+    @Test
+    void deleteAnswersTest() throws CannotDeleteException {
+        QUESTION_1.addAnswer(answer1);
+        QUESTION_1.deleteAnswers(NsUserTest.JAVAJIGI);
+
+        assertThat(QUESTION_1.isDeleted()).isTrue();
+    }
 }
