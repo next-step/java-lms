d0c1eaf6bfb657a1c8ad109a3919978f970f2b13
yoonjaej3 <yoonjaej3@gmail.com>
2023-05-29 22:45:35 +0900

    refact : toString() 메서드 위치 변경

--- src/main/java/nextstep/qna/domain/Answer.java
+++ src/main/java/nextstep/qna/domain/Answer.java
@@ -73,16 +73,16 @@ public class Answer {
         this.question = question;
     }
 
-    @Override
-    public String toString() {
-        return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
-    }
-
     public DeleteHistory delete(NsUser loginUser) throws CannotDeleteException {
         if (!this.isOwner(loginUser)) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
         this.deleted = true;
         return new DeleteHistory(ContentType.ANSWER, this.id, this.writer, LocalDateTime.now());
+    }
+
+    @Override
+    public String toString() {
+        return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
     }
 }
