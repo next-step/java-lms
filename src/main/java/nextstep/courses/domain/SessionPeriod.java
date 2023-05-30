398d8fc408fe85d81f6fd4c401cb77ee740f0349
ryan <wsdf25867@gmail.com>
2023-05-30 23:13:18 +0900

    feat : SessionPeriod 클래스 추가

---
+++ src/main/java/nextstep/courses/domain/SessionPeriod.java
@@ -0,0 +1,20 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDateTime;
+
+public class SessionPeriod {
+    private final LocalDateTime startAt;
+    private final LocalDateTime endAt;
+
+    private SessionPeriod(LocalDateTime startAt, LocalDateTime endAt) {
+        if (startAt.isAfter(endAt)) {
+            throw new IllegalArgumentException("시작일은 종료일 이후 일 수 없습니다.");
+        }
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+
+    public static SessionPeriod between(LocalDateTime startAt, LocalDateTime endAt) {
+        return new SessionPeriod(startAt, endAt);
+    }
+}
