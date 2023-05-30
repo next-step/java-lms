7fb3f3f178a1ce3f84605b60fb7fed13e49a4db1
qkrxodud00 <qkrxodud00@gmail.com>
2023-05-31 01:45:47 +0900

    * Test : 테스트 코드 추가 및 수정

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,26 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 다른_사용자_삭제시() {
+        assertThatThrownBy(() -> {A1.delete(NsUserTest.SANJIGI);})
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+
+    @Test
+    void 나의_질문_삭제시() throws CannotDeleteException {
+        A1.delete(NsUserTest.JAVAJIGI);
+        assertThat(A1.isDeleted()).isEqualTo(true);
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,27 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void 다른_사용자_삭제시() {
+        assertThatThrownBy(() -> {Q1.delete(NsUserTest.SANJIGI);})
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    void 나의_질문_삭제시() throws CannotDeleteException {
+        Q1.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isEqualTo(true);
+    }
 }
