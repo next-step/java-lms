9967473a74aed8230d1752c17fad7b9e3d1b3475
dong <ultraraptor@naver.com>
2023-05-30 09:57:06 +0900

    feat : 변경된 요구사항 설계

--- src/main/java/nextstep/courses/domain/SessionStatus.java
+++ src/main/java/nextstep/courses/domain/SessionStatus.java
@@ -1,7 +1,15 @@
 package nextstep.courses.domain;
 
 public enum SessionStatus {
-    PREPARING,
-    RECRUITING,
-    CLOSED
+
+    PREPARING(SessionRecruitStatus.OPEN),
+    RECRUITING(SessionRecruitStatus.OPEN),
+    IN_PROGRESS(SessionRecruitStatus.OPEN),
+    CLOSED(SessionRecruitStatus.CLOSED);
+
+    private final SessionRecruitStatus sessionProgressStatus;
+
+    SessionStatus(SessionRecruitStatus sessionProgressStatus) {
+        this.sessionProgressStatus = sessionProgressStatus;
+    }
 }
