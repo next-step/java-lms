ee3e7112f5449d0e68a64c368b4c3a1a87f113e9
dong <ultraraptor@naver.com>
2023-05-30 16:08:11 +0900

    test : 테스트코드 구현 완료

--- src/test/java/nextstep/courses/domain/SessionRepositoryTest.java
+++ src/test/java/nextstep/courses/domain/SessionRepositoryTest.java
@@ -12,6 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @JdbcTest
@@ -75,8 +80,29 @@ public class SessionRepositoryTest {
     @Test
     public void crud() {
         //given
+        Session session1 = TestFixture.LEMON_SESSION;
+        Session session2 = TestFixture.LIME_SESSION;
         //when
+        Session save1 = sessionRepository.save(session1);
+        Session save2 = sessionRepository.save(session2);
+        Session find1 = sessionRepository.findBySessionId(save1.getSessionId()).orElseThrow();
+        Session find2 = sessionRepository.findBySessionId(save2.getSessionId()).orElseThrow();
+        List<SessionId> findAll = sessionRepository.findAll()
+                .stream()
+                .map(Session::getSessionId)
+                .collect(Collectors.toList());
         //then
-        fail();
+        assertAll("CRUD 과정을 검증한다",
+                () -> assertThat(find1.getSessionId())
+                        .as("저장과 조회의 데이터가 동일함을 검증한다")
+                        .isEqualTo(save1.getSessionId()),
+                () -> assertThat(find2.getSessionId())
+                        .as("저장과 조회의 데이터가 동일함을 검증한다")
+                        .isEqualTo(save2.getSessionId()),
+                () -> assertThat(findAll)
+                        .as("findAll 조회 메서드의 동작을 검증한다")
+                        .contains(find1.getSessionId(), find2.getSessionId())
+        );
+
     }
 }
