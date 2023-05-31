565cde3d76bbfb1844925732be47ef3b370a0eee
nayoon-kim <belloyv@gmail.com>
2023-05-31 18:33:54 +0900

    rename Test DisplayName

--- src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
@@ -29,14 +29,24 @@ public class SessionRepositoryTest {
     }
 
     @Test
-    @DisplayName("crud")
-    void crud() {
+    @DisplayName("save")
+    void save() {
         Session session = Session.of(1L, "lms1", "img://cover.com/lms", 1
                 , SessionCostType.FREE, State.READY, 30
                 , LocalDateTime.of(2023, 6, 1, 14, 0, 0)
                 , LocalDateTime.of(2023, 6, 30, 14, 0, 0));
         int count = sessionRepository.save(session);
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("select")
+    void select() {
+        Session session = Session.of(1L, "lms1", "img://cover.com/lms", 1
+                , SessionCostType.FREE, State.READY, 30
+                , LocalDateTime.of(2023, 6, 1, 14, 0, 0)
+                , LocalDateTime.of(2023, 6, 30, 14, 0, 0));
+        sessionRepository.save(session);
 
         Session savedSession = sessionRepository.findById(1L).orElseThrow();
         assertThat(savedSession.getTitle()).isEqualTo(session.getTitle());
