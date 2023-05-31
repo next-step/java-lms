172e9e8c838c357a647214cb78dbe44819f5f7c8
wotjd4305 <wotjd4305@naver.com>
2023-05-31 22:31:43 +0900

    [step2] refactor. 세션 테스트 시에 객체 생성 빌더/부모 객체 혼용사용

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,37 +1,31 @@
 package nextstep.courses.domain;
 
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import nextstep.users.domain.NsUser;
 
+@Builder
+@AllArgsConstructor
 public class Session {
 
     private Long id;
     private String title;
-    private final LocalDateTime startDate;
-    private final LocalDateTime endDate;
+    private Period period;
     private String imageUrl;
-    private SessionState sessionState;
     private SessionType sessionType;
-    private SessionHeadCount sessionHeadCount;
+    private SessionValidator sessionValidator;
 
-    public Session(Long id, String title, int maxCount, LocalDateTime startDate,
-            LocalDateTime endDate, String imageUrl, SessionState sessionState,
+    public Session(Long id, String title, int maxCount, Period period, String imageUrl, SessionState sessionState,
             SessionType sessionType) {
         this.id = id;
         this.title = title;
-        this.sessionHeadCount = new SessionHeadCount(maxCount);
-        this.startDate = startDate;
-        this.endDate = endDate;
+        this.sessionValidator = new SessionValidator(maxCount, sessionState);
+        this.period = period;
         this.imageUrl = imageUrl;
-        this.sessionState = sessionState;
         this.sessionType = sessionType;
     }
 
     public void apply(NsUser loginUser) {
-        if (!SessionState.isRecruitable(sessionState)) {
-            throw new IllegalArgumentException("해당 강의는 수강신청중이 아닙니다.");
-        }
-
-        sessionHeadCount.addPerson(loginUser);
+        sessionValidator.addPerson(loginUser);
     }
 }
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -3,18 +3,34 @@ package nextstep.courses.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDateTime;
+
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
 class SessionTest {
 
-    public static final Session SESSION1 = new Session(1L, "JPA와 함께", 3, LocalDateTime.now(),
-            LocalDateTime.now(), "http://image1.com", SessionState.RECRUITING, SessionType.PAY);
-    public static final Session SESSION2 = new Session(2L, "마이바티스와 함께..", 3, LocalDateTime.now(),
-            LocalDateTime.now(), "http://image2.com", SessionState.CLOSE, SessionType.FREE);
+    static Session.SessionBuilder sessionBuilder = Session.builder()
+            .imageUrl("http://image.com")
+            .period(new Period(LocalDateTime.now().plusHours(1)));
+
+    public static final Session SESSION1 = sessionBuilder.id(1L)
+            .sessionType(SessionType.PAY)
+            .sessionValidator(new SessionValidator(3, SessionState.RECRUITING))
+            .title("JPA와 함께")
+            .build();
+
+    public static final Session SESSION2 = sessionBuilder.id(2L)
+            .sessionType(SessionType.FREE)
+            .sessionValidator(new SessionValidator(3, SessionState.CLOSE))
+            .title("마이바티스와 함께..")
+            .build();
 
     @Test
-    void applyTest_실패_모집중이_아님(){
+    void applyTest_실패_모집중이_아님() {
+
+        System.out.println(SESSION1);
+        System.out.println(SESSION2);
+
         assertThatThrownBy(() -> SESSION2.apply(NsUserTest.JAVAJIGI))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 강의는 수강신청중이 아닙니다.");
