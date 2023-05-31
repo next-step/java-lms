32aa4f100627c10c2ea8183d704239df6b846915
seongjo24 <kangsj24@naver.com>
2023-06-01 01:14:45 +0900

    maxUserCount Enrollment로 이동

--- src/main/java/nextstep/courses/domain/Enrollment.java
+++ src/main/java/nextstep/courses/domain/Enrollment.java
@@ -7,21 +7,35 @@ public class Enrollment {
     private SessionStatus sessionStatus;
     private NsUsers nsUsers;
 
-    public Enrollment(SessionStatus sessionStatus, NsUsers nsUsers) {
+    private int maxUserCount;
+
+
+    public Enrollment(SessionStatus sessionStatus, NsUsers nsUsers, int maxUserCount) {
         this.sessionStatus = sessionStatus;
         this.nsUsers = nsUsers;
+        this.maxUserCount = maxUserCount;
     }
 
     public Enrollment enrollNsUser(NsUser nsUser) {
         if (sessionStatus != SessionStatus.RECRUITING) {
             throw new IllegalArgumentException("강의가 모집중 일때만 등록 가능합니다.");
         }
+        if (nsUsers.count() == maxUserCount) {
+            throw new IllegalArgumentException("강의 최대 수강 인원이 초과되었습니다.");
+        }
         nsUsers.enroll(nsUser);
         return this;
     }
 
+    public SessionStatus getSessionStatus() {
+        return sessionStatus;
+    }
 
-    public int countNsUsers() {
-        return nsUsers.count();
+    public NsUsers getNsUsers() {
+        return nsUsers;
+    }
+
+    public int getMaxUserCount() {
+        return maxUserCount;
     }
 }
