ed54dffde5f3c003b9415bcda667fff7784b346f
Hwang Sangwook <goodbyeyo@naver.com>
2023-06-01 10:08:07 +0900

    refactor: 결제 전략 메서드명 변경 및 테스트 추가

---
+++ src/test/java/nextstep/courses/domain/FreePaymentStrategyTest.java
@@ -0,0 +1,16 @@
+package nextstep.courses.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("무료 강의 테스트")
+class FreePaymentStrategyTest {
+
+    @DisplayName("무료 강의는 결제 금액이 0원이다")
+    @Test
+    void freePaymentPrice() {
+        FreePaymentStrategy freePaymentStrategy = new FreePaymentStrategy();
+        Assertions.assertThat(freePaymentStrategy.chargeValue()).isEqualTo(0);
+    }
+}
