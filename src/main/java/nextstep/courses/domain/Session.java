cb5e07c6b6aeabf5e498e6535f5f03ef0941eb11
javajigi <javajigi@gmail.com>
2023-06-01 10:55:45 +0900

    refactor: remove courseId from Session

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -7,8 +7,6 @@ import java.time.LocalDate;
 public class Session {
     private final Long id;
 
-    private final Long courseId;
-
     private final  SessionInfo sessionInfo;
 
     private final  Charge charge;
@@ -17,31 +15,30 @@ public class Session {
 
     private final  SessionPeriod sessionPeriod;
 
-    public Session(Long courseId, String title, Long creatorId, String coverImage,
+    public Session(String title, Long creatorId, String coverImage,
                    ChargeStatus chargeStatus, int price,
                    int capacity, SessionStatus sessionStatus,
                    LocalDate startDate, LocalDate endDate) {
-        this(0L, courseId,
+        this(0L,
                 new SessionInfo(title, creatorId, coverImage),
                 new Charge(chargeStatus, price),
                 new Enrollment(capacity, sessionStatus),
                 new SessionPeriod(startDate, endDate));
     }
 
-    public Session(Long id, Long courseId, String title, Long creatorId, String coverImage,
+    public Session(Long id, String title, Long creatorId, String coverImage,
                    ChargeStatus chargeStatus, int price,
                    int capacity, SessionStatus sessionStatus,
                    LocalDate startDate, LocalDate endDate) {
-        this(id, courseId,
+        this(id,
                 new SessionInfo(title, creatorId, coverImage),
                 new Charge(chargeStatus, price),
                 new Enrollment(capacity, sessionStatus),
                 new SessionPeriod(startDate, endDate));
     }
 
-    public Session(Long id, Long courseId, SessionInfo sessionInfo, Charge charge, Enrollment enrollment, SessionPeriod sessionPeriod) {
+    public Session(Long id, SessionInfo sessionInfo, Charge charge, Enrollment enrollment, SessionPeriod sessionPeriod) {
         this.id = id;
-        this.courseId = courseId;
         this.sessionInfo = sessionInfo;
         this.charge = charge;
         this.enrollment = enrollment;
