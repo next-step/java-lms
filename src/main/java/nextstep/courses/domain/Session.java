32198ffc6140f83711d615b00fc9236e5585de3a
Baek Jiyeon <100.paperkite@gmail.com>
2023-06-01 13:46:50 +0900

    refactor: `Session` 인자들의 `null` 체크

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -3,18 +3,20 @@ package nextstep.courses.domain;
 import nextstep.courses.exception.CannotRegisterException;
 import nextstep.users.domain.NsUser;
 
+import java.util.Objects;
+
 public class Session {
-    private SessionPeriod period;
+    private final SessionPeriod period;
+    private final SessionUsers users;
     private ImageUrl imageUrl;
     private SessionStatus status;
     private SessionType type;
-    private SessionUsers users;
 
-        this.period = period;
-        this.type = type;
-        this.status = status;
     public Session(ImageUrl imageUrl, SessionPeriod period, SessionType type, SessionStatus status, int capacity) {
         this.imageUrl = Objects.requireNonNull(imageUrl);
+        this.period = Objects.requireNonNull(period);
+        this.type = Objects.requireNonNull(type);
+        this.status = Objects.requireNonNull(status);
         this.users = new SessionUsers(capacity);
     }
 
