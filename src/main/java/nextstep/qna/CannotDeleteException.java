80e30c00e0ef9c42e36792faec8c6231d90b3127
dhkimneer <kimds989@naver.com>
2023-06-05 01:27:24 +0900

    chore: Move Exception files to exception folder

--- src/main/java/nextstep/qna/CannotDeleteException.java
+++
@@ -1,9 +0,0 @@
-package nextstep.qna;
-
-public class CannotDeleteException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public CannotDeleteException(String message) {
-        super(message);
-    }
-}
