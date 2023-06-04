b7d738319ec8002df28e456a67ebf9d76c5d615e
dhkimneer <kimds989@naver.com>
2023-06-05 01:12:40 +0900

    feature: Add AnswersContents.java for reducing state variables

---
+++ src/main/java/nextstep/qna/domain/AnswerContents.java
@@ -0,0 +1,10 @@
+package nextstep.qna.domain;
+
+public class AnswerContents {
+
+    private final String contents;
+
+    public AnswerContents(String contents) {
+        this.contents = contents;
+    }
+}
