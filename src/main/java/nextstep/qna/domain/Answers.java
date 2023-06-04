40116d457e37bda49e71ca1ffff13add38c592a6
dhkimneer <kimds989@naver.com>
2023-06-05 01:16:59 +0900

    chore: Update Answers.java for changing deleteAll function, and adding toHistories function

--- src/main/java/nextstep/qna/domain/Answers.java
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -1,16 +1,43 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.exception.CannotDeleteException;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Answers {
 
     private final Question question;
-    private final List<Answer> answers;
+    private final List<Answer> answers = new ArrayList<>();
 
     public Answers(Question question) {
         this.question = question;
-        this.answers = new ArrayList<>();
+    }
+
+    public void deleteAll() throws CannotDeleteException {
+        validateIfAllRespondersAreSame();
+        deleteAllAnswers();
+    }
+
+    private void deleteAllAnswers() throws CannotDeleteException {
+        for (Answer answer : answers) {
+            answer.delete();
+        }
+    }
+
+    private void validateIfAllRespondersAreSame() throws CannotDeleteException {
+        if (!areAllRespondersSame()) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+    }
+
+    private boolean areAllRespondersSame() {
+        return answers.stream().allMatch(answer -> answer.isOwner(question.getWriterId()));
+    }
+
+    public List<DeleteHistory> toHistories() {
+        return answers.stream().map(Answer::toHistory).collect(Collectors.toList());
     }
 
     public void addAnswer(Answer answer) {
@@ -18,7 +45,7 @@ public class Answers {
         answers.add(answer);
     }
 
-    public List<Answer> getAnswers() {
-        return answers;
+    public boolean isAllDeleted() {
+        return answers.stream().allMatch(Answer::isDeleted);
     }
 }
