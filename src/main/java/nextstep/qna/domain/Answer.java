f7402daf329bddc2f5f1a8a74327272574ff1299
dhkimneer <kimds989@naver.com>
2023-06-05 01:14:31 +0900

    chore: Update Answer.java for reducing state variables, changing delete function, and adding toHistory function

--- src/main/java/nextstep/qna/domain/Answer.java
+++ src/main/java/nextstep/qna/domain/Answer.java
@@ -1,74 +1,77 @@
 package nextstep.qna.domain;
 
-import nextstep.qna.NotFoundException;
-import nextstep.qna.UnAuthorizedException;
+import nextstep.common.BaseEntity;
+import nextstep.qna.exception.CannotDeleteException;
+import nextstep.qna.exception.NotFoundException;
+import nextstep.qna.exception.UnAuthorizedException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
 
-public class Answer {
-    private Long id;
+public class Answer extends BaseEntity {
+    private final Long id;
 
-    private NsUser writer;
+    private final NsUser writer;
 
     private Question question;
 
-    private String contents;
+    private AnswerContents answerContents;
 
     private boolean deleted = false;
 
-    private LocalDateTime createdDate = LocalDateTime.now();
-
-    private LocalDateTime updatedDate;
-
-    public Answer() {
-    }
-
-    public Answer(NsUser writer, Question question, String contents) {
-        this(null, writer, question, contents);
-    }
-
-    public Answer(Long id, NsUser writer, Question question, String contents) {
+    public Answer(Long id, NsUser writer, Question question, AnswerContents answerContents) {
+        validateWriter(writer);
+        validateQuestion(question);
         this.id = id;
-        if(writer == null) {
-            throw new UnAuthorizedException();
-        }
+        this.writer = writer;
+        this.question = question;
+        this.answerContents = answerContents;
+    }
 
+    private void validateQuestion(Question question) {
         if(question == null) {
             throw new NotFoundException();
         }
-
-        this.writer = writer;
-        this.question = question;
-        this.contents = contents;
     }
 
-    public Long getId() {
-        return id;
+    private void validateWriter(NsUser writer) {
+        if(writer == null) {
+            throw new UnAuthorizedException();
+        }
     }
 
-    public void delete(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public boolean isOwner(NsUser writer) {
-        return this.writer.equals(writer);
-    }
-
-    public NsUser getWriter() {
-        return writer;
+    public DeleteHistory toHistory() {
+        return new DeleteHistory(ContentType.ANSWER, id, writer, LocalDateTime.now());
     }
 
     public void toQuestion(Question question) {
         this.question = question;
     }
 
-    @Override
-    public String toString() {
-        return "Answer [id=" + getId() + ", writer=" + writer + ", contents=" + contents + "]";
+    public void delete() throws CannotDeleteException {
+        validateIfQuestionerIsSameAsAnswerer();
+        this.deleted = true;
+    }
+
+    private void validateIfQuestionerIsSameAsAnswerer() throws CannotDeleteException {
+        if (!isOwner(question.getWriterId())) {
+            throw new CannotDeleteException("질문자와 답변자가 다른 경우 답변을 삭제할 수 없습니다.");
+        }
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public boolean isOwner(Long writerId) {
+        return this.writer.getId().equals(writerId);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public NsUser getWriter() {
+        return writer;
     }
 }
