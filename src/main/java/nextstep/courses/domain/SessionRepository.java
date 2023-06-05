725b60fa88a3429c9da5391dbdfb1d46dea3961a
KangHo-dev <tiger452003@gmail.com>
2023-06-05 21:23:44 +0900

    refactor : Student 객체 생성으로 SessionRepository 미사용 메소드 삭제

--- src/main/java/nextstep/courses/domain/SessionRepository.java
+++ src/main/java/nextstep/courses/domain/SessionRepository.java
@@ -1,6 +1,7 @@
 package nextstep.courses.domain;
 
 import nextstep.courses.domain.registration.SessionRegistration;
+import nextstep.courses.domain.registration.Student;
 import nextstep.users.domain.NsUser;
 
 import java.util.List;
@@ -8,9 +9,6 @@ import java.util.List;
 public interface SessionRepository {
     long save(Session session);
 
-    long saveSessionUser(Session session, NsUser nsUser);
-
     Session findById(Long sessionId);
 
-    List<Long> findSessionUserIdsBySessionId(Long sessionId);
 }
--- src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
@@ -2,18 +2,12 @@ package nextstep.courses.infrastructure;
 
 import nextstep.courses.domain.Session;
 import nextstep.courses.domain.SessionRepository;
-import nextstep.users.domain.NsUser;
-import nextstep.users.domain.NsUserTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.List;
-import java.util.Set;
 
 import static nextstep.Fixtures.aSession;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -49,29 +43,6 @@ public class SessionRepositoryTest {
         Session foundSession = sessionRepository.findById(sessionId);
         assertThat(foundSession).isNotNull();
         assertThat(foundSession.getId()).isEqualTo(sessionId);
-    }
-
-    @DisplayName("Session User 저장")
-    @Test
-    void saveSessionUser() {
-        long sessionId = sessionRepository.save(session);
-        Session foundSession = sessionRepository.findById(sessionId);
-        long sessionUsersId = sessionRepository.saveSessionUser(foundSession, NsUserTest.JAVAJIGI);
-
-        assertThat(sessionUsersId).isNotNull();
-    }
-
-    @DisplayName("Session UserIds 조회")
-    @Test
-    void findSessionUserIdsBySessionId() {
-        long sessionId = sessionRepository.save(session);
-        Session foundSession = sessionRepository.findById(sessionId);
-
-        sessionRepository.saveSessionUser(foundSession, NsUserTest.JAVAJIGI);
-        sessionRepository.saveSessionUser(foundSession, NsUserTest.SANJIGI);
-
-        List<Long> userIds = sessionRepository.findSessionUserIdsBySessionId(sessionId);
-        assertThat(userIds).hasSize(2);
     }
 
 }
