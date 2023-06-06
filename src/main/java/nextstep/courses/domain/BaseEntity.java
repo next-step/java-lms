12184fbea035a83ace00c45ff7c70fd98b33cbb2
Baek Jiyeon <100.paperkite@gmail.com>
2023-06-06 22:31:19 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/nextstep/courses/domain/BaseEntity.java
@@ -0,0 +1,36 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+    private final Long id;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public BaseEntity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -3,21 +3,65 @@ package nextstep.courses.domain;
 import nextstep.courses.exception.CannotRegisterException;
 import nextstep.users.domain.NsUser;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
-public class Session {
+public class Session extends BaseEntity {
+    private final Long courseId;
+    private final CoverImage coverImage;
     private final SessionPeriod period;
     private final SessionUsers users;
-    private ImageUrl imageUrl;
-    private SessionStatus status;
-    private SessionType type;
+    private final String title;
+    private final SessionStatus status;
+    private final SessionType type;
 
-    public Session(ImageUrl imageUrl, SessionPeriod period, SessionType type, SessionStatus status, int capacity) {
-        this.imageUrl = Objects.requireNonNull(imageUrl);
+    public Session(Long courseId, String title, CoverImage coverImage, SessionPeriod period, SessionType type,
+                   SessionStatus status, int capacity) {
+        this(0L, courseId, title, coverImage, period, type, status, capacity, LocalDateTime.now(), null);
+    }
+
+    public Session(Long id, Long courseId, String title, CoverImage coverImage, SessionPeriod period, SessionType type,
+                   SessionStatus status, int capacity, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        super(id, createdAt, updatedAt);
+        this.courseId = Objects.requireNonNull(courseId);
+        this.title = Objects.requireNonNull(title);
+        this.coverImage = Objects.requireNonNull(coverImage);
         this.period = Objects.requireNonNull(period);
         this.type = Objects.requireNonNull(type);
         this.status = Objects.requireNonNull(status);
         this.users = new SessionUsers(capacity);
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public CoverImage getCoverImage() {
+        return coverImage;
+    }
+
+    public SessionPeriod getPeriod() {
+        return period;
+    }
+
+    public SessionUsers getUsers() {
+        return users;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public SessionStatus getStatus() {
+        return status;
+    }
+
+    public SessionType getType() {
+        return type;
+    }
+
+    public int getCapacity() {
+        return users.capacity();
     }
 
     public void register(NsUser user) {
@@ -25,5 +69,19 @@ public class Session {
             throw new CannotRegisterException("현재 모집중인 강의가 아닙니다");
         }
         users.add(user);
+    }
+
+    @Override
+    public String toString() {
+        return "Session{" +
+                "courseId=" + courseId +
+                ", coverImage=" + coverImage +
+                ", period=" + period +
+                ", users=" + users +
+                ", title='" + title + '\'' +
+                ", status=" + status +
+                ", type=" + type +
+                '}'
+                + super.toString();
     }
 }
