1bbde482c8b4f5a3c1c75b8b9b67dcebf3c6f751
munjeongho <96mjh00@gmail.com>
2023-06-07 14:01:53 +0900

    refactor : 코드리뷰 피드백 이후 수정

--- src/main/java/nextstep/courses/app/DefaultSessionService.java
+++ src/main/java/nextstep/courses/app/DefaultSessionService.java
@@ -2,10 +2,13 @@ package nextstep.courses.app;
 
 import nextstep.courses.domain.Session;
 import nextstep.courses.domain.SessionRepository;
+import nextstep.qna.NotFoundException;
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -33,7 +36,13 @@ public class DefaultSessionService implements SessionService {
     @Override
     public long register(long sessionId, NsUser nsUser) {
         Session session = sessionRepository.findById(sessionId);
-        session.register(nsUser);
+        Optional<NsUser> nsUserOptional = userRepository.findByUserId(nsUser.getUserId());
+
+        if (nsUserOptional.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 ID 입니다.");
+        }
+
+        session.register(nsUserOptional.get());
 
         return sessionRepository.saveSessionJoin(session);
     }
