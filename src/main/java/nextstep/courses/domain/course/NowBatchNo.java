cd5655ce71b8d4a7faf1beb426c2168ef8d44046
hoa0217 <gjwjdghk123@naver.com>
2023-06-11 18:50:57 +0900

    refactor: NowBatchNo immutable로 변환

--- src/main/java/nextstep/courses/domain/course/NowBatchNo.java
+++ src/main/java/nextstep/courses/domain/course/NowBatchNo.java
@@ -2,15 +2,14 @@ package nextstep.courses.domain.course;
 
 public class NowBatchNo {
 
-  private int nowBatchNo;
+  private final int nowBatchNo;
 
   public NowBatchNo(int nowBatchNo) {
     this.nowBatchNo = nowBatchNo;
   }
 
-  public int createdBatch() {
-    ++nowBatchNo;
-    return nowBatchNo;
+  public NowBatchNo createdBatch() {
+    return new NowBatchNo(nowBatchNo + 1);
   }
 
   public int getNowBatchNo() {
