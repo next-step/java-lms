06d02c8327a1deecf2daca464cf75023e2655cf7
SeokyungKim <kskyung0624@gmail.com>
2023-06-12 20:04:15 +0900

    test : SessionRepository test를 작성한다

---
+++ src/test/java/nextstep/sessions/infrastructure/SessionRepositoryTest.java
@@ -0,0 +1,86 @@
+package nextstep.sessions.infrastructure;
+
+import static nextstep.sessions.testFixture.SessionBuilder.aSession;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.time.LocalDateTime;
+import nextstep.sessions.domain.Session;
+import nextstep.sessions.domain.SessionRepository;
+import nextstep.sessions.domain.SessionStatus;
+import nextstep.users.domain.NsUser;
+import nextstep.users.domain.UserRepository;
+import nextstep.users.infrastructure.JdbcUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+class SessionRepositoryTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SessionRepositoryTest.class);
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  private SessionRepository sessionRepository;
+  private UserRepository userRepository;
+
+  @BeforeEach
+  void setUp() {
+    sessionRepository = new JdbcSessionRepository(jdbcTemplate);
+    userRepository = new JdbcUserRepository(jdbcTemplate);
+  }
+
+  @Test
+  void saveAndFind() {
+    Session session = aSession().build();
+    int count = sessionRepository.save(session);
+    assertThat(count).isEqualTo(1);
+    Session savedSession = sessionRepository.findById(2L);
+    assertThat(savedSession.getTitle()).isEqualTo(session.getTitle());
+    LOGGER.debug("Session: {}", savedSession);
+  }
+
+  // TODO: autoIncrement라서 rollback시 pk가 날아가는 것 때문에 테스트하기 어려움
+  // -> PK에 종속적이지 않게 테스트할 수 없을까?
+  @Test
+  void sessionStatusUpdateAndFind() {
+    Session session = aSession().build();
+    int count = sessionRepository.save(session);
+    assertThat(count).isEqualTo(1);
+
+    Session savedSession = sessionRepository.findById(3L);
+    assertThat(savedSession.getTitle()).isEqualTo(session.getTitle());
+
+    savedSession.recruitStart();
+    sessionRepository.update(savedSession);
+    Session updatedSession = sessionRepository.findById(3L);
+    assertThat(updatedSession.getStatus()).isEqualTo(SessionStatus.RECRUITING);
+  }
+
+
+  // TODO: 시나리오 테스트이기 때문에 단위테스트가 아니라고 느껴짐
+  // -> 레포지토리 테스트를 단위테스트처럼 만들 수 있는 방법은?
+  @Test
+  void userUpdateAndFind() {
+    Session session = aSession().build();
+    int count = sessionRepository.save(session);
+    assertThat(count).isEqualTo(1);
+
+    Session savedSession = sessionRepository.findById(1L);
+    savedSession.recruitStart();
+    // data.sql에 의존되어 있음. 괜찮은가?
+    NsUser user = userRepository.findByUserId("javajigi").orElseThrow();
+    savedSession.enrollment(user, LocalDateTime.of(2023, 6, 2, 13, 0));
+    sessionRepository.update(savedSession);
+
+    Session updatedSession = sessionRepository.findById(1L);
+    assertThat(updatedSession.getTitle()).isEqualTo(session.getTitle());
+    NsUser enrolledUser = updatedSession.getUsers().stream().findFirst().orElseThrow();
+    assertThat(enrolledUser.getUserId()).isEqualTo(user.getUserId());
+  }
+}
