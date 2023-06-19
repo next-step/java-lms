00102b0f12e0f8b81dfcdb6e989914964835594b
김태민 <59005171+jsungmin6@users.noreply.github.com>
2023-06-19 17:04:24 +0900

    step1 질문 삭제하기 리팩토링 (#202)
    Co-authored-by: chess.min <chess.min@kakaocorp.com>

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+
+
+    @Test
+    void isSameWriter() {
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        Answer answer = new Answer(NsUserTest.JAVAJIGI, question, "Answers Contents1");
+
+        Assertions.assertThat(answer.isSameWriter(NsUserTest.JAVAJIGI)).isTrue();
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,54 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+
+    @Test
+    public void delete_성공_내가_쓴_글() throws Exception {
+        //given
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        question.delete(NsUserTest.JAVAJIGI);
+
+        //then
+        Assertions.assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_실패_다른_사람이_쓴_글() throws Exception {
+        //given
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+
+        //then
+        Assertions.assertThatThrownBy(()->question.delete(NsUserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
+
+    }
+
+    @Test
+    public void delete_성공_질문자_답변자_같음() throws Exception {
+        //given
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        question.addAnswer(new Answer(NsUserTest.JAVAJIGI,question,"test"));
+
+        question.delete(NsUserTest.JAVAJIGI);
+
+        //then
+        Assertions.assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_실패_답변_중_다른_사람이_쓴_글() throws Exception {
+        //given
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        question.addAnswer(new Answer(NsUserTest.SANJIGI,question,"test"));
+
+        //then
+        Assertions.assertThatThrownBy(()->question.delete(NsUserTest.JAVAJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
 }
