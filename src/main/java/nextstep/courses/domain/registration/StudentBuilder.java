b45eb6572ba44ba216a70a6201702a5ae5f666e9
highjune <highjune37@gmail.com>
2023-06-23 20:08:59 +0900

    feat(domain): StudentBuilder 추가

---
+++ src/main/java/nextstep/courses/domain/registration/StudentBuilder.java
@@ -0,0 +1,36 @@
+package nextstep.courses.domain.registration;
+
+public class StudentBuilder {
+
+    private Long nsUserId;
+    private Long sessionId;
+
+    private boolean approved;
+
+    private StudentBuilder() {
+    }
+
+    public static StudentBuilder aStudentBuilder() {
+        return new StudentBuilder();
+    }
+
+    public StudentBuilder withUserId(Long nsUserId) {
+        this.nsUserId = nsUserId;
+        return this;
+    }
+
+    public StudentBuilder withSessionId(Long sessionId) {
+        this.sessionId = sessionId;
+        return this;
+    }
+
+    public StudentBuilder withStatus(boolean approved) {
+        this.approved = approved;
+        return this;
+    }
+
+    public Student build() {
+        Student student = new Student(nsUserId, sessionId, approved);
+        return student;
+    }
+}
