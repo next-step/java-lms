8ce35a982e391e4572213d188e6565820161fb52
Lee Byoung Seong <dev.bslee@gmail.com>
2023-06-24 13:57:58 +0900

    refactor: 모집 상태 도메인 분리중

--- src/main/java/nextstep/courses/domain/RecruitmentState.java
+++ src/main/java/nextstep/courses/domain/RecruitmentState.java
@@ -22,4 +22,11 @@ public enum RecruitmentState {
         return this.availableRecruitment;
     }
 
+    // 추가모집 상태변경 가능 -> 모집상태가 종료이면서, 강의 시작후 7일
+//    public boolean isAllowAdditionalRecruitment(RecruitmentState recruitmentState, SessionPeriod sessionPeriod) {
+//
+//
+//    }
+
+
 }
