234a53577b80ef3f9c9ecc5fbed6933357070a49
Lee Byoung Seong <dev.bslee@gmail.com>
2023-06-24 14:53:21 +0900

    refactor: 강의 상태는 각 값간의 검증 로직이 달라 추상 클래스로 구현한다

--- src/main/java/nextstep/courses/domain/SessionState.java
+++ src/main/java/nextstep/courses/domain/SessionState.java
@@ -3,21 +3,75 @@ package nextstep.courses.domain;
 import java.time.LocalDate;
 
 public enum SessionState {
+    PREPARING {
+        @Override
+        public String desc() {
+            return "준비중";
+        }
 
-    PREPARING("준비중", true),
-    PROGRESSING("진행중", false),
-    FINISH("종료", false);
+        @Override
+        public void validate(SessionPeriod sessionPeriod, SessionState sessionState) {
 
-    private final String desc;
-    private final boolean availableManualChangeSession;
+            if (sessionState == SessionState.PROGRESSING) {
+                validateProgressing(sessionPeriod);
+            }
 
+            if (sessionState == SessionState.FINISH) {
+                validateFinish(sessionPeriod);
+            }
+        }
 
-    SessionState(
-            String desc,
-            boolean availableManualChangeSession
-    ) {
-        this.desc = desc;
-        this.availableManualChangeSession = availableManualChangeSession;
+        private void validateProgressing(SessionPeriod sessionPeriod) {
+            if (sessionPeriod.isBeforeStartDate(LocalDate.now())) {
+                throw new IllegalArgumentException("강의 시작 일시 전이에요, '진행중' 상태로 변경할수 없어요 :(");
+            }
+        }
+
+        private void validateFinish(SessionPeriod sessionPeriod) {
+            if (sessionPeriod.isAfterEndDate(LocalDate.now())) {
+                throw new IllegalArgumentException("강의 종료 일시 전이에요, '종료' 상태로 변경할수 없어요 :(");
+            }
+        }
+
+        @Override
+        public boolean isAvailableManualChangeSession() {
+            return true;
+        }
+    },
+    PROGRESSING {
+        @Override
+        public String desc() {
+            return "진행중";
+        }
+
+        @Override
+        public void validate(SessionPeriod sessionPeriod, SessionState sessionState) {
+
+        }
+
+        @Override
+        public boolean isAvailableManualChangeSession() {
+            return false;
+        }
+    },
+    FINISH {
+        @Override
+        public String desc() {
+            return "종료";
+        }
+
+        @Override
+        public void validate(SessionPeriod sessionPeriod, SessionState sessionState) {
+
+        }
+
+        @Override
+        public boolean isAvailableManualChangeSession() {
+            return false;
+        }
+    };
+
+    SessionState() {
     }
 
     public SessionState syncSessionStateAccordingTo(LocalDate now, SessionPeriod sessionPeriod) {
@@ -33,9 +87,10 @@ public enum SessionState {
         return SessionState.PROGRESSING;
     }
 
+    public abstract String desc();
 
-    public boolean isAvailableManualChangeSession() {
-        return availableManualChangeSession;
-    }
+    public abstract void validate(SessionPeriod sessionPeriod, SessionState sessionState);
+
+    public abstract boolean isAvailableManualChangeSession();
 
 }
