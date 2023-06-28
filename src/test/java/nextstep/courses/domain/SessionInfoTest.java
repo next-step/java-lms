b36201ba81f96683e54b42b559b53430d41fceb0
lehdqlsl <lehdqlsl@naver.com>
2023-06-28 14:38:18 +0900

    refactor: 빌더 패턴 적용

--- src/test/java/nextstep/courses/domain/SessionInfoTest.java
+++ src/test/java/nextstep/courses/domain/SessionInfoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
 import java.util.List;
 
+import static nextstep.courses.domain.SessionInfoBuilder.sessionInfo;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,52 +16,31 @@ public class SessionInfoTest {
 
     @Test
     void 수강신청_정보() {
-        SessionInfo sessionInfo = new SessionInfo(
-                LectureStatus.PREPARING,
-                List.of(
-                        NsUser.GUEST_USER
-                ),
-                100);
+        SessionInfo sessionInfo = sessionInfo().build();
 
-        assertThat(sessionInfo.getMaxUser()).isEqualTo(100);
-        assertThat(sessionInfo.getLectureStatus()).isEqualTo(LectureStatus.PREPARING);
-        assertThat(sessionInfo.getUsers()).hasSize(1);
-    }
-
-    @Test
-    void 수강신청_정보_준비중() {
-        SessionInfo sessionInfo = new SessionInfo(
-                LectureStatus.PREPARING,
-                List.of(
-                        NsUser.GUEST_USER
-                ),
-                100);
-
-        assertThat(sessionInfo.getMaxUser()).isEqualTo(100);
-        assertThat(sessionInfo.getLectureStatus()).isEqualTo(LectureStatus.PREPARING);
-        assertThat(sessionInfo.getUsers()).hasSize(1);
+        assertThat(sessionInfo.getMaxUser()).isNotNull();
+        assertThat(sessionInfo.getLectureStatus()).isNotNull();
+        assertThat(sessionInfo.getUsers()).isNotNull();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"PREPARING", "RECRUITING", "COMPLETED"})
     void 수강신청_상태(String input) {
         LectureStatus lectureStatus = LectureStatus.valueOf(input);
-        SessionInfo sessionInfo = new SessionInfo(
-                lectureStatus,
-                List.of(
-                        NsUser.GUEST_USER
-                ),
-                100);
+
+        SessionInfo sessionInfo = sessionInfo()
+                .lectureStatus(lectureStatus)
+                .build();
 
         assertThat(sessionInfo.getLectureStatus()).isEqualTo(lectureStatus);
     }
 
     @Test
     void 모집중일때_수강신청() {
-        SessionInfo sessionInfo = new SessionInfo(
-                LectureStatus.RECRUITING,
-                new ArrayList<>(),
-                100);
+        SessionInfo sessionInfo = sessionInfo()
+                .lectureStatus(LectureStatus.RECRUITING)
+                .build();
+
 
         assertThatCode(() -> sessionInfo.register(NsUser.GUEST_USER))
                 .doesNotThrowAnyException();
@@ -68,10 +48,9 @@ public class SessionInfoTest {
 
     @Test
     void 준비중일때_수강신청() {
-        SessionInfo sessionInfo = new SessionInfo(
-                LectureStatus.PREPARING,
-                new ArrayList<>(),
-                100);
+        SessionInfo sessionInfo = sessionInfo()
+                .lectureStatus(LectureStatus.PREPARING)
+                .build();
 
         assertThatThrownBy(() -> sessionInfo.register(NsUser.GUEST_USER))
                 .isInstanceOf(RuntimeException.class)
@@ -80,10 +59,9 @@ public class SessionInfoTest {
 
     @Test
     void 최대수강인원_초과불가() {
-        SessionInfo sessionInfo = new SessionInfo(
-                LectureStatus.RECRUITING,
-                new ArrayList<>(),
-                2);
+        SessionInfo sessionInfo = sessionInfo()
+                .maxUser(2)
+                .build();
 
         sessionInfo.register(NsUser.GUEST_USER);
         sessionInfo.register(NsUser.GUEST_USER);
