e6543465644efff5abe03ba0928a56a82ee61cbd
ryan <wsdf25867@gmail.com>
2023-07-28 23:43:18 +0900

    feat(SessionUsersTest.java, SessionUsers.java) : SessionUsers 클래스 추가

---
+++ src/main/java/nextstep/courses/domain/session/SessionUsers.java
@@ -0,0 +1,26 @@
+package nextstep.courses.domain.session;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessionUsers {
+
+    private List<SessionUser> sessionUsers;
+    private int maxUserSize;
+    public SessionUsers(int maxUserSize) {
+        this(new ArrayList<>(), maxUserSize);
+    }
+
+    private SessionUsers(List<SessionUser> sessionUsers, int maxUserSize) {
+        this.sessionUsers = sessionUsers;
+        this.maxUserSize = maxUserSize;
+    }
+
+
+    public void enroll(SessionUser sessionUser) {
+        if (sessionUsers.size() == maxUserSize) {
+            throw new IllegalStateException("수강 인원이 가득 찼습니다.");
+        }
+        sessionUsers.add(sessionUser);
+    }
+}
