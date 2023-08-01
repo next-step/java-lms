d0a014b3dfb196b7343756a39d72afd7c9ee628b
rkd5656meta <rkd8307@naver.com>
2023-08-02 01:17:45 +0900

    refactor: SessionTime, SessionManagement 객체 추가 및 그에 따른 필드 삭제.

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,20 +1,12 @@
 package nextstep.courses.domain;
 
-import nextstep.courses.NotChangeStatusException;
 import nextstep.users.domain.NsUser;
-
-import java.time.LocalDateTime;
 
 public class Session {
     private final long id;
+    private final SessionManagement sessionManagement;
     private String title;
-    private int maxNumberOfStudent;
     private CoverImage coverImage;
-    private LocalDateTime startDt = null;
-    private LocalDateTime endDt = null;
-    private boolean free;
-    private SessionStatus sessionStatus = SessionStatus.PREPARING;
-    private Students students = new Students();
 
     public Session(long id, String title, int maxNumberOfStudent, boolean free) {
         this(id, title, maxNumberOfStudent, free, null);
@@ -23,53 +15,24 @@ public class Session {
     public Session(long id, String title, int maxNumberOfStudent, boolean free, CoverImage coverImage) {
         this.id = id;
         this.title = title;
-        this.maxNumberOfStudent = maxNumberOfStudent;
-        this.free = free;
+        this.sessionManagement = new SessionManagement(free, maxNumberOfStudent);
         this.coverImage = coverImage;
     }
 
     public void enrolment(NsUser user) throws IllegalArgumentException {
-        checkedProceeding();
-
-        checkedMaxNumberOfStudent();
-
-        students.addStudent(user);
-    }
-
-    private void checkedMaxNumberOfStudent() throws IllegalArgumentException {
-        if (!students.isPossibleAdd(maxNumberOfStudent)) {
-            throw new IllegalArgumentException("수강 인원이 가득 찼습니다.");
-        }
-    }
-
-    private void checkedProceeding() throws IllegalArgumentException {
-        if (sessionStatus != SessionStatus.RECRUITING) {
-            throw new IllegalArgumentException("모집 중이 아닙니다.");
-        }
+        sessionManagement.enrolment(user);
     }
 
     public SessionStatus startRecruiting() {
-        return changeSessionStatus(SessionStatus.PREPARING, SessionStatus.RECRUITING);
+        return sessionManagement.startRecruiting();
     }
 
     public SessionStatus startSession() {
-        SessionStatus sessionStatus = changeSessionStatus(SessionStatus.RECRUITING, SessionStatus.PROCEEDING);
-        this.startDt = LocalDateTime.now();
-        return sessionStatus;
+        return sessionManagement.startSession();
     }
 
     public SessionStatus endSession() {
-        SessionStatus sessionStatus = changeSessionStatus(SessionStatus.PROCEEDING, SessionStatus.END);
-        this.endDt = LocalDateTime.now();
-        return sessionStatus;
-    }
-
-    private SessionStatus changeSessionStatus(SessionStatus compareStatus, SessionStatus changeStatus) {
-        if (this.sessionStatus != compareStatus) {
-            throw new NotChangeStatusException();
-        }
-
-        return this.sessionStatus = changeStatus;
+        return sessionManagement.endSession();
     }
 
     public void addCoverImage(CoverImage coverImage){
