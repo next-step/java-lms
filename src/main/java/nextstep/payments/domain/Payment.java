de587b9b414b8ec9ce8f32cf255ec1be8a8670a2
pobi <pobi@woowahan.local>
2023-10-12 15:04:26 +0900

    feat(payment): create Payment object and find Payment by id

---
+++ src/main/java/nextstep/payments/domain/Payment.java
@@ -0,0 +1,29 @@
+package nextstep.payments.domain;
+
+import java.time.LocalDateTime;
+
+public class Payment {
+    private String id;
+
+    // 결제한 강의 아이디
+    private Long sessionId;
+
+    // 결제한 사용자 아이디
+    private Long nsUserId;
+
+    // 결제 금액
+    private Long amount;
+
+    private LocalDateTime createdAt;
+
+    public Payment() {
+    }
+
+    public Payment(String id, Long sessionId, Long nsUserId, Long amount) {
+        this.id = id;
+        this.sessionId = sessionId;
+        this.nsUserId = nsUserId;
+        this.amount = amount;
+        this.createdAt = LocalDateTime.now();
+    }
+}
