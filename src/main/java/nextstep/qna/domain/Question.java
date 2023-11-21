18fd777eef29a350e780a518003b1ae46cddfe70
g0rani <pjw0405@naver.com>
2023-11-21 20:58:12 +0900

    refactor: Question 에 delete 메소드 생성
    - 작성자 일치 비지니스 로직 위임

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -68,6 +69,16 @@ public class Question {
         answers.add(answer);
     }
 
+    public void delete(NsUser loginUser) throws CannotDeleteException {
+        validateOwner(loginUser);
+    }
+
+    private void validateOwner(NsUser loginUser) throws CannotDeleteException {
+        if (!this.isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+    }
+
     public boolean isOwner(NsUser loginUser) {
         return writer.equals(loginUser);
     }
@@ -89,4 +100,5 @@ public class Question {
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
+
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,18 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("질문의 작성자와 삭제를 요청한 대상이 다르면 예외 처리를 한다")
+    void deleteNotCorrectUser() {
+        Assertions.assertThrows(CannotDeleteException.class, () -> Q1.delete(NsUserTest.SANJIGI), "질문을 삭제할 권한이 없습니다.");
+    }
 }
