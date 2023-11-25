4181f48326d34c294d9198f60f2f891296c176eb
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-26 00:25:14 +0900

    feat: 강의 상태는 준비중, 모집중, 종료 3가지 상태를 가진다

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,15 +1,19 @@
 package nextstep.courses.domain;
 
+import nextstep.courses.domain.type.SessionStatus;
+
 import java.time.LocalDate;
 
 public class Session {
 
     private Duration duration;
     private Image image;
+    private SessionStatus status;
 
-    public Session(LocalDate start, LocalDate end, Image image) {
+    public Session(LocalDate start, LocalDate end, Image image, SessionStatus status) {
         this.duration = new Duration(start, end);
         this.image = image;
+        this.status = status;
     }
 
     public Image image() {
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,5 +1,6 @@
 package nextstep.courses.domain;
 
+import nextstep.courses.domain.type.SessionStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ public class SessionTest {
     @DisplayName("강의는 강의 커버 이미지 정보를 가진다")
     public void session_image() {
         Image image = new Image(1, "JPG", 300, 200);
-        assertThat(new Session(LocalDate.now(), LocalDate.now(), image)).extracting(Session::image).isEqualTo(image);
+        assertThat(new Session(LocalDate.now(), LocalDate.now(), image, SessionStatus.READY)).extracting(Session::image).isEqualTo(image);
     }
 
 }
