7d53902bda9a576bbe17c8604eb63f45811753f9
g0rani <pjw0405@naver.com>
2023-11-26 14:50:38 +0900

    feat: ThumbnailRepository 작성

--- src/main/java/nextstep/courses/domain/Thumbnail.java
+++ src/main/java/nextstep/courses/domain/Thumbnail.java
@@ -2,6 +2,8 @@ package nextstep.courses.domain;
 
 public class Thumbnail {
 
+    private final long id;
+    private final long sessionId;
     private final String name;
     private final String uri;
     private final FileSize fileSize;
@@ -11,6 +13,27 @@ public class Thumbnail {
                      String uri,
                      FileSize fileSize,
                      ImageSize imageSize) {
+        this(0L, 0L, name, uri, fileSize, imageSize);
+    }
+
+    public Thumbnail(long id,
+                     long sessionId,
+                     String name,
+                     String uri,
+                     long fileSize,
+                     long widthPixel,
+                     long heightPixel) {
+        this(id, sessionId, name, uri, new FileSize(fileSize), new ImageSize(widthPixel, heightPixel));
+    }
+
+    public Thumbnail(long id,
+                     long sessionId,
+                     String name,
+                     String uri,
+                     FileSize fileSize,
+                     ImageSize imageSize) {
+        this.id = id;
+        this.sessionId = sessionId;
         this.name = name;
         this.uri = uri;
         this.fileSize = fileSize;
