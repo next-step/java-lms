89a8637bb85b98690eaa5c066cec38fc982e7c08
KimGwangHoon <khkimykk@naver.com>
2023-11-27 00:15:07 +0900

    test: answers에 추가 및 삭제용 테스트 코드 작성

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -20,4 +20,28 @@ public class AnswersTest {
         //then
         assertThat(answers.size()).isEqualTo(1);
     }
+
+    @DisplayName("답변목록에 답변을 추가한다.")
+    @Test
+    void 답변목록에_답변을_추가한다() {
+        //given
+        List<Answer> answer = List.of(new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"));
+        Answers answers = new Answers(answer);
+        //when
+        answers.add(new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2"));
+        //then
+        assertThat(answers.size()).isEqualTo(2);
+    }
+
+    @DisplayName("답변을 삭제한다.")
+    @Test
+    void 답변을_삭제한다() {
+        //given
+        List<Answer> answer = List.of(new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"));
+        Answers answers = new Answers(answer);
+        //when
+        List<DeleteHistory> histories = answers.delete(new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"));
+        //then
+        assertThat(histories.size()).isEqualTo(1);
+    }
 }
