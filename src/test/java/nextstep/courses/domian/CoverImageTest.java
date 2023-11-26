1722c2cc241c8ea57ac5bbc56402c27794be7eba
엄태권 <akfls221@gmail.com>
2023-11-27 02:54:52 +0900

    feat : 불필요 클래스 삭제

--- src/test/java/nextstep/courses/domian/CoverImageTest.java
+++
@@ -1,8 +0,0 @@
-package nextstep.courses.domian;
-
-import nextstep.courses.domain.CoverImage;
-
-class CoverImageTest {
-
-    public static final CoverImage COVER_IMAGE = new CoverImage(CoverImageFileNameTest.CF, CoverImageSizeTest.CS, CoverImagePixelTest.CP);
-}
