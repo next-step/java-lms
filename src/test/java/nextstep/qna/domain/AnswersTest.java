127614111d1098c5eb9492f38eb4a37dc56b4851
KimGwangHoon <khkimykk@naver.com>
2023-11-26 23:54:09 +0900

    test: answer의 일급컬렉션 생성 테스트작성

---
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -0,0 +1,24 @@
+package nextstep.qna.domain;
+
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnswersTest {
+
+    @DisplayName("답변목록을 생성한다.")
+    @Test
+    void 답변목록을_생성한다() {
+        //given
+        List<Answer> answer = List.of(new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1"));
+        Answers answers = new Answers(answer);
+        //when
+        //then
+        assertThat(answers.size()).isEqualTo(1);
+    }
+}
