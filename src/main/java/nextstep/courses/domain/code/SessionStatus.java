9ab2834796fc54944da0b266c26f6e301b683d53
g0rani <pjw0405@naver.com>
2023-11-26 11:37:46 +0900

    refactor: enum의 함수 인터페이스 제거
    - 함수 인터페이스 대신 boolean으로 변경

--- src/main/java/nextstep/courses/domain/code/SessionStatus.java
+++ src/main/java/nextstep/courses/domain/code/SessionStatus.java
@@ -2,19 +2,17 @@ package nextstep.courses.domain.code;
 
 import nextstep.courses.exception.CanNotApplySessionStatusException;
 
-import java.util.function.Supplier;
-
 public enum SessionStatus {
 
-    PREPARING("준비중", () -> false),
-    RECRUITING("모집중", () -> true),
-    END("종료", () -> false);
+    PREPARING("준비중", false),
+    RECRUITING("모집중", true),
+    END("종료", false);
 
     private final String description;
-    private final Supplier<Boolean> canApply;
+    private final boolean canApply;
 
     SessionStatus(String description,
-                  Supplier<Boolean> canApply) {
+                  boolean canApply) {
         this.description = description;
         this.canApply = canApply;
     }
@@ -24,13 +22,9 @@ public enum SessionStatus {
     }
 
     public void validateApply() {
-        if (!canApply()) {
+        if (!this.canApply) {
             throw new CanNotApplySessionStatusException("수강 신청이 가능한 상태가 아닙니다.");
         }
-    }
-
-    private boolean canApply() {
-        return this.canApply.get();
     }
 
 }
