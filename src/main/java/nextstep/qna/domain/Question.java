190323ea1e9c72b7b0b8f5bbd36f23ba101604e0
qwe5507 <apvmffkdls@gmail.com>
2023-11-26 19:21:49 +0900

    refactor: 질문을 삭제할때, 로그인 사용자와 다르면 삭제 할수 없다.

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -68,10 +69,6 @@ public class Question {
         answers.add(answer);
     }
 
-    public boolean isOwner(NsUser loginUser) {
-        return writer.equals(loginUser);
-    }
-
     public Question setDeleted(boolean deleted) {
         this.deleted = deleted;
         return this;
@@ -85,6 +82,14 @@ public class Question {
         return answers;
     }
 
+    public void delete(NsUser loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+    }
+    private boolean isOwner(NsUser loginUser) {
+        return writer.equals(loginUser);
+    }
     @Override
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,28 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("질문을 삭제할 때, 로그인한 사용자와 다르면 삭제 할 수 없다.")
+    @Test
+    void deleteTest() {
+        final NsUser qwe5507 = new NsUser(1L, "qwe5507", "1234", "simpson", "qwe5507@gmail.com");
+        final NsUser pobi = new NsUser(2L, "zxc5507", "1234", "pobi", "pobi@gmail.com");
+        final Question question = new Question(qwe5507, "title", "contents");
+
+        assertThatThrownBy(() -> {
+            question.delete(pobi);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
