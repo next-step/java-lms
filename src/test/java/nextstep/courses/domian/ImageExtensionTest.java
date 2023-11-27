ecb1aa99d8f3de3b955c073479fd6a563bea56d8
엄태권 <akfls221@gmail.com>
2023-11-27 22:45:22 +0900

    fix : 경로수정

--- src/test/java/nextstep/courses/domian/ImageExtensionTest.java
+++ src/test/java/nextstep/courses/domian/ImageExtensionTest.java
@@ -1,5 +1,7 @@
-package nextstep.courses.domain;
+package nextstep.courses.domian;
 
+import nextstep.courses.domain.ImageExtension;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,6 +12,6 @@ class ImageExtensionTest {
     @ParameterizedTest(name = "확장자 {0}는 이미지 확장자 이다 {1}")
     @CsvSource(value = {"jpg,true","jpeg,true","txt,false","docs,false"})
     void isPossible(String extension, boolean expected) {
-        assertThat(ImageExtension.isPossible(extension)).isEqualTo(expected);
+        Assertions.assertThat(ImageExtension.isPossible(extension)).isEqualTo(expected);
     }
 }
