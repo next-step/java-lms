71bb113a5557299ac4f50b912148d70058f82127
jikimee64 <jikimee64@gmail.com>
2023-11-27 22:48:41 +0900

    feat: 로그인 사용자와 질문한 사람이 같은 경우 삭제 가능하다.

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -68,8 +69,16 @@ public class Question {
         answers.add(answer);
     }
 
+    //AS-IS
     public boolean isOwner(NsUser loginUser) {
         return writer.equals(loginUser);
+    }
+
+    //TO-BE
+    public void delete(NsUser loginUser) throws CannotDeleteException {
+        if (!writer.equals(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
     }
 
     public Question setDeleted(boolean deleted) {
@@ -89,4 +98,6 @@ public class Question {
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
+
+
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,29 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static nextstep.users.domain.NsUserTest.SANJIGI;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static final Question Q2 = new Question(SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("성공 - 로그인 사용자와 질문한 사람이 같은 경우 삭제가 가능하다.")
+    void success_delete_qna_condition() throws Exception {
+        Q1.delete(NsUserTest.JAVAJIGI);
+    }
+
+    @Test
+    @DisplayName("실패 - 로그인 사용자와 질문한 사람이 다른 경우 삭제가 불가능하다.")
+    void fail_delete_qna_condition() throws Exception {
+        Assertions.assertThatThrownBy(() -> Q1.delete(SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
 }
