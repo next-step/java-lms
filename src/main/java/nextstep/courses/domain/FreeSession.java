eec4700b7530c85dbbc6b5d1a4e17d0ad866b9b9
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-28 01:05:59 +0900

    feat: free session repository 추가

--- src/main/java/nextstep/courses/domain/FreeSession.java
+++ src/main/java/nextstep/courses/domain/FreeSession.java
@@ -4,6 +4,8 @@ import nextstep.courses.domain.type.SessionStatus;
 import nextstep.payments.domain.Payment;
 import nextstep.users.domain.NsUser;
 
+import java.time.LocalDateTime;
+
 public class FreeSession extends Session {
 
     public FreeSession(Duration duration, Image image) {
@@ -12,6 +14,10 @@ public class FreeSession extends Session {
 
     public FreeSession(Duration duration, Image image, SessionStatus status) {
         super(duration, image, status);
+    }
+
+    public FreeSession(Long id, Duration duration, Image image, SessionStatus status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        super(id, duration, image, status, createdAt, updatedAt);
     }
 
     public void apply(Payment payment, NsUser nsUser) {
---
+++ src/test/java/nextstep/courses/infrastructure/FreeSessionRepositoryTest.java
@@ -0,0 +1,72 @@
+package nextstep.courses.infrastructure;
+
+import nextstep.courses.domain.Course;
+import nextstep.courses.domain.Duration;
+import nextstep.courses.domain.FreeSession;
+import nextstep.courses.domain.Image;
+import nextstep.courses.domain.type.SessionStatus;
+import nextstep.courses.repository.CourseRepository;
+import nextstep.courses.repository.FreeSessionRepository;
+import nextstep.courses.repository.ImageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+public class FreeSessionRepositoryTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FreeSessionRepositoryTest.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private FreeSessionRepository freeSessionRepository;
+    private CourseRepository courseRepository;
+    private ImageRepository imageRepository;
+
+    @BeforeEach
+    void setUp() {
+        imageRepository = new JdbcImageRepository(jdbcTemplate);
+        courseRepository = new JdbcCourseRepository(jdbcTemplate);
+        freeSessionRepository = new JdbcFreeSessionRepository(jdbcTemplate, imageRepository);
+    }
+
+    @Test
+    void crud() {
+        FreeSession session = session(savedImage());
+        int count = freeSessionRepository.save(session, savedCourse());
+        assertThat(count).isEqualTo(1);
+        FreeSession savedFreeSession = freeSessionRepository.findById(1L);
+        assertThat(session).isEqualTo(savedFreeSession);
+        LOGGER.debug("FreeSession: {}", savedFreeSession);
+    }
+
+    private Course savedCourse() {
+        Course course = new Course(1L, "TDD, 클린 코드 with Java", 1L, LocalDateTime.now(), null);
+        courseRepository.save(course);
+        return course;
+    }
+
+    private Image savedImage() {
+        Image image = new Image(1L, 1, "JPG", 300, 200, LocalDateTime.now(), null);
+        imageRepository.save(image);
+        return image;
+    }
+
+    private FreeSession session(Image image) {
+        return new FreeSession(1L, duration(), image, SessionStatus.RECRUITING, LocalDateTime.now(), null);
+    }
+
+    private Duration duration() {
+        return new Duration(LocalDate.now(), LocalDate.now());
+    }
+
+}
