b092ac6562e5ea65952c638988016f2c0b6f7302
sukhyunmoon <sukhyunmoon@sk.com>
2023-11-27 15:00:46 +0900

    refactor: 강의에 신청한 인원을 관리하는 부분을 우선 하드코딩으로 구현, 추후에 repository이용한 db관계로 개선예정

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -3,6 +3,7 @@ package nextstep.courses.domain;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Session {
@@ -14,18 +15,28 @@ public class Session {
     private SessionStatus sessionStatus;
     private int maximumEnrollment = 0;
     private int fee;
-    private List<NsUser> participants;
+    private List<NsUser> participants = new ArrayList<>();
 
+    public Session(LocalDateTime startDate, LocalDateTime endDate, SessionType sessionType, Image coverImage, SessionStatus sessionStatus, int maximumEnrollment, int fee) {
 
-    public Session() {
-    }
+        inputValidation(startDate, endDate, sessionType, coverImage, sessionStatus, maximumEnrollment, fee);
 
-    public Session(LocalDateTime startDate, LocalDateTime endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
+        this.sessionType = sessionType;
+        this.coverImage = coverImage;
+        this.sessionStatus = sessionStatus;
+        this.maximumEnrollment = maximumEnrollment;
+        this.fee = fee;
     }
 
-    public boolean canEnroll(int paidFee){
+    private void inputValidation(LocalDateTime startDate, LocalDateTime endDate, SessionType sessionType, Image coverImage, SessionStatus sessionStatus, int maximumEnrollment, int fee) {
+        if(sessionType == SessionType.FREE && maximumEnrollment != 0){
+            throw new IllegalArgumentException("무료 세션은 최대 수강 인원이 없습니다.");
+        }
+    }
+
+    private boolean canEnroll(int paidFee){
         return isOpened() && !isFullEnrollment() && possibleFee(paidFee);
     }
 
@@ -41,4 +52,10 @@ public class Session {
         return sessionType == SessionType.PAID && fee == paidFee;
     }
 
+    public boolean enroll(NsUser nsUser, int paidFee) {
+        if (!canEnroll(paidFee)) {
+            throw new IllegalArgumentException("수강 신청이 불가능합니다.");
+        }
+        return participants.add(nsUser);
+    }
 }
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,20 +1,41 @@
 package nextstep.courses.domain;
 
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class SessionTest {
 
+    private static final LocalDateTime START_DATE = LocalDateTime.of(2020, 1, 1, 0, 0);
+    private static final LocalDateTime END_DATE = LocalDateTime.of(2020, 2, 1, 0, 0);
+    private final Session defaultSession = new Session(
+            START_DATE,
+            END_DATE,
+            SessionType.PAID,
+            new Image(ImageTest.IMAGE_URL, 1024, 1024, ImageType.PNG),
+            SessionStatus.OPENED,
+            10,
+            1000
+    );
 
     @Test
     @DisplayName("강의는 시작일과 종료일을 가진다.")
     void sessionHasStartAndEndDate() {
-        Session session = new Session(LocalDateTime.of(2020, 1, 1, 0, 0)
-                , LocalDateTime.of(2020, 2, 1, 0, 0));
+        Session session = new Session(
+                START_DATE,
+                END_DATE,
+                SessionType.PAID,
+                new Image(ImageTest.IMAGE_URL, 1024, 1024, ImageType.PNG),
+                SessionStatus.OPENED,
+                10,
+                1000
+        );
         assertThat(session)
                 .hasFieldOrProperty("startDate")
                 .hasFieldOrProperty("endDate");
@@ -24,8 +45,38 @@ public class SessionTest {
     @Test
     @DisplayName("강의는 강의 커버 이미지 정보를 가진다.")
     void sessionHasCoverUrl() {
-        Session session = new Session();
-        assertThat(session).hasFieldOrProperty("coverImage");
+        assertThat(defaultSession).hasFieldOrProperty("coverImage");
+    }
+
+    @Test
+    @DisplayName("무료 강의는 최대 수강 인원 제한이 없다.")
+    void sessionHasSessionType() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Session(
+                    START_DATE,
+                    END_DATE,
+                    SessionType.FREE,
+                    new Image(ImageTest.IMAGE_URL, 1024, 1024, ImageType.PNG),
+                    SessionStatus.OPENED,
+                    10,
+                    1000
+            );
+        });
+    }
+
+    @Test
+    @DisplayName("유료 강의는 수강생이 결제한 금액과 수강료가 일치할 때 수강 신청이 가능하다.")
+    void sessionHasFee() {
+        Session session = new Session(
+                START_DATE,
+                END_DATE,
+                SessionType.PAID,
+                new Image(ImageTest.IMAGE_URL, 1024, 1024, ImageType.PNG),
+                SessionStatus.OPENED,
+                10,
+                1000
+        );
+        assertThat(session.enroll(NsUserTest.JAVAJIGI, 1000)).isTrue();
     }
 
 }
