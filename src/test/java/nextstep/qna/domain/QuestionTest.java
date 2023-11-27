b5d309ad4dfb146433a5d49ab35e8b5665230a7c
이창섭 <ventulus95@gmail.com>
2023-11-27 23:28:04 +0900

    refact: 질문은 게시한 사람만 삭제할 수 있다.

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,20 +1,27 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
 
     @Test
-    void 질문은_삭제하면_삭제상태가_변경된다() {
+    void 질문은_삭제하면_삭제상태가_변경된다() throws CannotDeleteException {
         Question question = new Question(NsUser.GUEST_USER, "title1", "content");
-        question.delete();
+        question.delete(NsUser.GUEST_USER);
         assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test
+    void 질문은_올린사람만_삭제_가능하다() {
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI)).isInstanceOf(CannotDeleteException.class);
     }
 
 }
