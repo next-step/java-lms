a0df4aef1de4fabe70e97d3e0b148b2d46d02ff2
kyunghyun-park <qqqq_o@naver.com>
2023-11-29 00:15:33 +0900

    refactor: 답변을 삭제한다.

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,25 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("답변 생성 시 삭제 여부 메서드에 false가 반환된다.")
+    void answer_초기() {
+        assertThat(new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents3").isDeleted()).isFalse();
+    }
+
+    @Test
+    @DisplayName("답변을 삭제하면 삭제 여부 메서드에 true가 반환된다.")
+    void answer_삭제() {
+        A1.delete();
+        assertThat(A1.isDeleted()).isTrue();
+    }
 }
