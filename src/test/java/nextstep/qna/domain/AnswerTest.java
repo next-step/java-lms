bb2d2f72774cd313bf98fd9a6bc4815522211391
solchan98 <dev.sol0427@gmail.com>
2023-11-28 15:07:16 +0900

    feat: 작성자가 아닌 경우 질문을 삭제할 수 없는 기능 실패 테스트 작성

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,20 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 작성자와_다른_유저는_삭제할_수_없다() {
+        Answer answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        assertThatThrownBy(() -> answer.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("작성자가 아닌경우 삭제할 수 없습니다.");
+    }
 }
