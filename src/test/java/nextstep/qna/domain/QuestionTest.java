ac2e29eb13c351fb44a42bd773e8f7ecfa544cad
solchan98 <dev.sol0427@gmail.com>
2023-11-28 14:50:58 +0900

    feat: 질문 삭제 시, 답변이 있는경우 삭제 불가에 대한 실패 테스트 케이스 구현

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,10 +1,12 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.qna.UnAuthorizedException;
-import nextstep.qna.service.QnAService;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.qna.domain.AnswerTest.A1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -24,7 +26,16 @@ public class QuestionTest {
     @Test
     void 로그인한_사용자와_질문한_사용자가_다른경우_삭제할_수_없다() {
         assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
-                .isInstanceOf(UnAuthorizedException.class)
+                .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("삭제 권한이 존재하지 않습니다.");
+    }
+
+    @Test
+    void 답변이_있는_경우_삭제할_수_없다() {
+        Q1.addAnswer(A1);
+
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("답변이 존재합니다.");
     }
 }
