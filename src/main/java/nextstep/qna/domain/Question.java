8501c4e87346b10bd865da04e47263026d3366e5
ByungChan Lee <lbc2415@gmail.com>
2023-11-29 00:31:20 +0900

    refactor: deleted setDeleted method in Domain class

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -73,11 +73,6 @@ public class Question {
         return writer.equals(loginUser);
     }
 
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
-
     public boolean isDeleted() {
         return deleted;
     }
