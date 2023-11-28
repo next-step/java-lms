452c7f01b428c002d55c1e7f41b5d27b3f203115
ByungChan Lee <lbc2415@gmail.com>
2023-11-29 00:10:51 +0900

    refactor: refactored validateForDelete to be in Question domain class

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,46 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    private Question question;
+    private Answer answer;
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        question = new Question(1L, NsUserTest.JAVAJIGI, "title1", "contents1");
+        answer = new Answer(11L, NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents1");
+        question.addAnswer(answer);
+    }
+
+    @Test
+    public void assertValidateForDeleteCase1() {
+        Exception exception = assertThrows(CannotDeleteException.class, () -> {
+            question.validateForDelete(NsUserTest.SANJIGI);
+        });
+        String expectedMessage = "질문을 삭제할 권한이 없습니다.";
+
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+    @Test
+    public void assertValidateForDeleteCase2() {
+        Exception exception = assertThrows(CannotDeleteException.class, () -> {
+            question.validateForDelete(NsUserTest.JAVAJIGI);
+        });
+
+        String expectedMessage = "다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.";
+
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
 }
