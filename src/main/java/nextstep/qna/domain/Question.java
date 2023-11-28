09101693c578497d1d8d6e1d60b70735677523e9
ByungChan Lee <lbc2415@gmail.com>
2023-11-29 00:15:47 +0900

    refactor: refactored to add validateForDelete in Answer class

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -97,9 +97,7 @@ public class Question {
         }
 
         for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
+            answer.validateForDelete(loginUser);
         }
     }
 }
