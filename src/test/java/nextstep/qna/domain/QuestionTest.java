3330f4ba38df8814873e966446c5cdc74c749e81
solchan98 <dev.sol0427@gmail.com>
2023-11-28 14:43:18 +0900

    feat: 질문 삭제 시, 질문 작성자만 삭제할 수 있는 기능 실패 테스트 케이스 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,30 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.UnAuthorizedException;
+import nextstep.qna.service.QnAService;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void 로그인한_사용자와_질문한_사용자가_같은경우_삭제_가능하다() {
+        // when
+        Q1.delete(NsUserTest.JAVAJIGI);
+
+        // when
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    void 로그인한_사용자와_질문한_사용자가_다른경우_삭제할_수_없다() {
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(UnAuthorizedException.class)
+                .hasMessage("삭제 권한이 존재하지 않습니다.");
+    }
 }
