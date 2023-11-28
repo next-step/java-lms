4bbdcba0999f17256476be85ec8c3ffcb307e754
namuhuchutong <namuhuchutong@gmail.com>
2023-11-28 19:35:29 +0900

    refactor: 다른 사람이 쓴 답변 검증 로직을 Answer 객체로 옮김

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,20 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("다른 사람이 쓴 답변이 있다면 예외가 발생한다..")
+    @Test
+    void if_user_is_not_same_as_login_user_then_can_not_delete_question() {
+        assertThatThrownBy(() -> A1.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
