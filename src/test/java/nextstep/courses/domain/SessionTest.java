613e90f767feab18737bb2948af6306a4df2347a
sukhyunmoon <sukhyunmoon@sk.com>
2023-11-28 09:18:42 +0900

    test: 강의생성 중복코드 메소드로 추출

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,7 +1,6 @@
 package nextstep.courses.domain;
 
 import nextstep.users.domain.NsUserTest;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,15 +26,7 @@ public class SessionTest {
     @Test
     @DisplayName("강의는 시작일과 종료일을 가진다.")
     void sessionHasStartAndEndDate() {
-        Session session = new Session(
-                START_DATE,
-                END_DATE,
-                SessionType.PAID,
-                new Image(ImageTest.IMAGE_URL, 1024, 1024, ImageType.PNG),
-                SessionStatus.OPENED,
-                10,
-                1000
-        );
+        Session session = SESSION_모집중_10명_1000원();
         assertThat(session)
                 .hasFieldOrProperty("startDate")
                 .hasFieldOrProperty("endDate");
@@ -67,41 +58,35 @@ public class SessionTest {
     @Test
     @DisplayName("유료 강의는 수강생이 결제한 금액과 수강료가 일치할 때 수강 신청이 가능하다.")
     void sessionHasFee() {
-        Session session = new Session(
-                START_DATE,
-                END_DATE,
-                SessionType.PAID,
-                new Image(ImageTest.IMAGE_URL, 1024, 1024, ImageType.PNG),
-                SessionStatus.OPENED,
-                10,
-                1000
-        );
+        Session session = SESSION_모집중_10명_1000원();
         assertThat(session.enroll(NsUserTest.JAVAJIGI, 1000)).isTrue();
     }
 
     @Test
     @DisplayName("유료 강의는 강의 최대 수강 인원을 초과할 수 없다.")
     void sessionHasMaximumEnrollment() {
-        Session session = new Session(
-                START_DATE,
-                END_DATE,
-                SessionType.PAID,
-                new Image(ImageTest.IMAGE_URL, 1024, 1024, ImageType.PNG),
-                SessionStatus.OPENED,
-                10,
-                1000
-        );
-        for (int i = 0; i < 10; i++) {
-            session.enroll(NsUserTest.JAVAJIGI, 1000);
-        }
+        Session session = SESSION_최대인원으로_수강신청됨();
         assertThatIllegalArgumentException().isThrownBy(() -> {
             session.enroll(NsUserTest.SANJIGI, 1000);
         });
     }
 
+    private static Session SESSION_최대인원으로_수강신청됨() {
+        Session session = SESSION_모집중_10명_1000원();
+        for (int i = 0; i < 10; i++) {
+            session.enroll(NsUserTest.JAVAJIGI, 1000);
+        }
+        return session;
+    }
+
     @Test
     @DisplayName("강의 수강신청은 강의 상태가 모집중일 때만 가능하다.")
     void sessionHasSessionStatus() {
+        Session session = SESSION_모집중_10명_1000원();
+        assertThat(session.enroll(NsUserTest.JAVAJIGI, 1000)).isTrue();
+    }
+
+    private static Session SESSION_모집중_10명_1000원() {
         Session session = new Session(
                 START_DATE,
                 END_DATE,
@@ -111,7 +96,7 @@ public class SessionTest {
                 10,
                 1000
         );
-        assertThat(session.enroll(NsUserTest.JAVAJIGI, 1000)).isTrue();
+        return session;
     }
 
 }
