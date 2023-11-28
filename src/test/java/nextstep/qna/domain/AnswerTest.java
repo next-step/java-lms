f18130b60d49a5ea111d4713ad9eac8c1fca3ea0
solchan98 <dev.sol0427@gmail.com>
2023-11-28 15:15:51 +0900

    feat: 질문이 삭제된 경우, 삭제 히스토리를 남기는 기능 실패 테스트 케이스 작성

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -4,7 +4,11 @@ import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
@@ -16,5 +20,15 @@ public class AnswerTest {
         assertThatThrownBy(() -> answer.delete(NsUserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("작성자가 아닌경우 삭제할 수 없습니다.");
+    }
+
+    @Test
+    void 질문이_삭제되면_DeleteHistory를_반환한다() {
+        Answer answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+        DeleteHistory expectedDeleteHistory = new DeleteHistory(ContentType.ANSWER, answer.getId(),
+                NsUserTest.JAVAJIGI, LocalDateTime.now());
+
+        DeleteHistory deleteHistory = answer.delete(NsUserTest.JAVAJIGI);
+        assertThat(deleteHistory).isEqualTo(expectedDeleteHistory);
     }
 }
