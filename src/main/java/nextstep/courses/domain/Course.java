aa0c1dc4efc3e7af2d59a17f183a2ce93ff8053c
HyunggiPark <phg9587@naver.com>
2023-11-30 00:23:38 +0900

    feat: Course는 기수(generation)를 가진다.
    - 기수 단위로 운영된다.

--- src/main/java/nextstep/courses/domain/Course.java
+++ src/main/java/nextstep/courses/domain/Course.java
@@ -1,9 +1,14 @@
 package nextstep.courses.domain;
 
+import org.springframework.util.Assert;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Course {
     private Long id;
+
+    private Integer generation;
 
     private String title;
 
@@ -13,19 +18,37 @@ public class Course {
 
     private LocalDateTime updatedAt;
 
-    public Course() {
-    }
-
     public Course(String title, Long creatorId) {
         this(0L, title, creatorId, LocalDateTime.now(), null);
     }
 
     public Course(Long id, String title, Long creatorId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        validateCourse(id, title, creatorId, createdAt);
+
         this.id = id;
+        this.generation = 1;
         this.title = title;
         this.creatorId = creatorId;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+    }
+
+    public Course(final String title, final int generation, final long creatorId) {
+        this(title, creatorId);
+
+        validateGeneration(generation);
+        this.generation = generation;
+    }
+
+    private void validateCourse(final Long id, final String title, final Long creatorId, final LocalDateTime createdAt) {
+        Assert.notNull(id, "id must not be null");
+        Assert.hasText(title, "title must not be empty");
+        Assert.isTrue(creatorId > 0, "creatorId must be greater than 0");
+        Assert.notNull(createdAt, "createdAt must not be null");
+    }
+
+    private void validateGeneration(final int generation) {
+        Assert.isTrue(generation > 0, "generation must be greater than 0");
     }
 
     public String getTitle() {
@@ -41,9 +64,25 @@ public class Course {
     }
 
     @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Course course = (Course) o;
+        return Objects.equals(generation, course.generation)
+                && Objects.equals(getTitle(), course.getTitle())
+                && Objects.equals(getCreatorId(), course.getCreatorId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(generation, getTitle(), getCreatorId());
+    }
+
+    @Override
     public String toString() {
         return "Course{" +
                 "id=" + id +
+                ", generation=" + generation +
                 ", title='" + title + '\'' +
                 ", creatorId=" + creatorId +
                 ", createdAt=" + createdAt +
---
+++ src/test/java/nextstep/courses/domain/CourseTest.java
@@ -0,0 +1,40 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CourseTest {
+    @Test
+    @DisplayName("같은 title, 같은 creatorId를 가지더라도, 기수가 다르면 다른 과정이다. (Course는 기수 단위로 운영)")
+    void testCourseDistinctByGeneration() {
+        //given
+        final String title = "TDD, 클린 코드 with Java";
+        final long creatorId = 1L;
+        Course course1 = new Course(title, 1, creatorId);
+        Course course2 = new Course(title, 2, creatorId);
+
+        //when
+        final boolean isCourseEquals = course1.equals(course2);
+
+        //then
+        assertThat(isCourseEquals).isFalse();
+    }
+
+    @Test
+    @DisplayName("같은 title, 같은 generation ,같은 creatorId를 가지면, 같은 과정이다.")
+    void testCourseEqualsByTitleAndGenerationAndCreator() {
+        //given
+        final String title = "TDD, 클린 코드 with Java";
+        final long creatorId = 1L;
+        Course course1 = new Course(title, 1, creatorId);
+        Course course2 = new Course(title, 1, creatorId);
+
+        //when
+        final boolean isCourseEquals = course1.equals(course2);
+
+        //then
+        assertThat(isCourseEquals).isTrue();
+    }
+}
