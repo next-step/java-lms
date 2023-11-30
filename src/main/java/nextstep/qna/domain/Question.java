ba3e263b28d8faf186f7649cad2f2b0b0983c8d9
hoho282 <76132534+hoho282@users.noreply.github.com>
2023-11-30 17:27:12 +0900

    feat : 질문(답변) 삭제하기 기능 구현 (#266)

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,10 +1,12 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Question {
     private Long id;
@@ -88,5 +90,36 @@ public class Question {
     @Override
     public String toString() {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+    }
+
+    public DeleteHistory deleteQuestion(NsUser userId) throws CannotDeleteException {
+        if (!isOwner(userId)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+        setDeleted(true);
+        return new DeleteHistory(ContentType.QUESTION, this.id, this.writer, LocalDateTime.now());
+    }
+
+    public List<DeleteHistory> delete(NsUser userId) throws CannotDeleteException {
+        List<DeleteHistory> deleteHistories = deleteAnswers(userId);
+        DeleteHistory questionDeleteHistory = deleteQuestion(userId);
+        deleteHistories.add(questionDeleteHistory);
+        return deleteHistories;
+    }
+
+    public List<DeleteHistory> deleteAnswers(NsUser userId) throws CannotDeleteException {
+        if (!ableDeleteAnswer(userId)) {
+            throw new CannotDeleteException("다른 사람이 작성한 답변이 존재해 삭제할 수 없습니다.");
+        }
+        return answers.stream()
+                .map(answer -> answer.delete())
+                .collect(Collectors.toList());
+    }
+
+    private boolean ableDeleteAnswer(NsUser userId) {
+        return answers.stream().filter(answer -> !answer.isOwner(userId))
+                .findAny()
+                .map(i -> false)
+                .orElse(true);
     }
 }
