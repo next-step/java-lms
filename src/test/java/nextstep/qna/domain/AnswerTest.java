174e80be498bed933f9808c903479bfa5eeef7d7
이승준 <skp.seungjun@sk.com>
2023-11-30 16:54:41 +0900

    주어진 사용자와 답변자가 다를 경우에 삭제를 요청하면 -> 거부한다.

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -19,4 +19,13 @@ public class AnswerTest {
 
         assertThat(answer.isDeleted()).isTrue();
     }
+
+    @Test
+    @DisplayName("[Answer.deleteIfWriter()] 주어진 사용자와 답변자가 다르다면 -> 삭제 거부")
+    public void deleteByWrongWriterTest() {
+        Answer answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "hello");
+        answer.deleteIfWriter(NsUserTest.SANJIGI);
+
+        assertThat(answer.isDeleted()).isFalse();
+    }
 }
