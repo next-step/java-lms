b6853a18918eca38c8be92a2a02c623a79ef74b7
gilbertlim <9ilbert.app@gmail.com>
2023-11-30 11:30:36 +0900

    feat: 이미지 파일 크기 제한 검증 로직 추가

--- src/main/java/nextstep/sessions/domain/CoverImage.java
+++ src/main/java/nextstep/sessions/domain/CoverImage.java
@@ -1,6 +1,10 @@
 package nextstep.sessions.domain;
 
+import nextstep.sessions.domain.exception.SessionsException;
+
 public class CoverImage {
+
+    private static final int IMAGE_FILE_SIZE_LIMIT = 1024 * 1024;
 
     private String name;
     private ImageType type;
@@ -10,12 +14,19 @@ public class CoverImage {
     private String path;
 
     public CoverImage(String name, int size, int width, int height, String path) {
-        this.name = name;
+        validateFileSize(size);
         this.type = ImageType.valueOfName(name);
+        this.name = name;
         this.size = size;
         this.width = width;
         this.height = height;
         this.path = path;
+    }
+
+    private void validateFileSize(int size) {
+        if (size > IMAGE_FILE_SIZE_LIMIT) {
+            throw new SessionsException("이미지 파일 크기가 초과했습니다.");
+        }
     }
 
 }
--- src/test/java/nextstep/sessions/domain/CoverImageTest.java
+++ src/test/java/nextstep/sessions/domain/CoverImageTest.java
@@ -9,9 +9,16 @@ public class CoverImageTest {
 
     @Test
     void 이미지_타입_제한() {
-        assertThatThrownBy(() -> new CoverImage("image.heif", 1024, 300, 200, "/images/"))
+        assertThatThrownBy(() -> new CoverImage("image.heif", 1048576, 300, 200, "/images/"))
             .isInstanceOf(SessionsException.class)
             .hasMessage("지원하지 않는 이미지 타입입니다.");
+    }
+
+    @Test
+    void 이미지_파일_크기_제한() {
+        assertThatThrownBy(() -> new CoverImage("image.png", 1048577, 300, 200, "/images/"))
+            .isInstanceOf(SessionsException.class)
+            .hasMessage("이미지 파일 크기가 초과했습니다.");
     }
 
 
