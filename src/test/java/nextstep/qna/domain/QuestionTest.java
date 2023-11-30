258d81fce5a8aee746d4103bb3d9b794612f93de
Hanna Choi <44824602+gkssk4163@users.noreply.github.com>
2023-11-30 20:45:20 +0900

    feat: Question 삭제권한 체크 구현

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,31 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    private static final NsUser Q1Writer = NsUserTest.JAVAJIGI;
+    private static final NsUser Q2Writer = NsUserTest.SANJIGI;
+    public static final Question Q1 = new Question(Q1Writer, "title1", "contents1");
+    public static final Question Q2 = new Question(Q2Writer, "title2", "contents2");
+
+    @Test
+    void 로그인사용자가_질문작성자가_아닌_경우_CannotDeleteException() {
+        assertThatThrownBy(() -> {
+            Q1.delete(Q2Writer);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    void 로그인사용자가_질문작성자인_경우_삭제상태변경() throws CannotDeleteException {
+        Q1.delete(Q1Writer);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
 }
