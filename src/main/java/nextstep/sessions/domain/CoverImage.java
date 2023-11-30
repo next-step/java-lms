e3f683ed95afda1a6fb7a5ede6251748a15bf266
gilbertlim <9ilbert.app@gmail.com>
2023-11-30 14:05:51 +0900

    feat: 이미지 가로/세로 크기 제한 검증 로직 추가

--- src/main/java/nextstep/sessions/domain/CoverImage.java
+++ src/main/java/nextstep/sessions/domain/CoverImage.java
@@ -1,32 +1,17 @@
 package nextstep.sessions.domain;
 
-import nextstep.sessions.domain.exception.SessionsException;
-
 public class CoverImage {
 
-    private static final int IMAGE_FILE_SIZE_LIMIT = 1024 * 1024;
-
-    private String name;
-    private ImageType type;
-    private int size;
-    private int width;
-    private int height;
-    private String path;
+    private final String name;
+    private final ImageType type;
+    private final ImageSize size;
+    private final String path;
 
     public CoverImage(String name, int size, int width, int height, String path) {
-        validateFileSize(size);
         this.type = ImageType.valueOfName(name);
         this.name = name;
-        this.size = size;
-        this.width = width;
-        this.height = height;
+        this.size = new ImageSize(size, width, height);
         this.path = path;
-    }
-
-    private void validateFileSize(int size) {
-        if (size > IMAGE_FILE_SIZE_LIMIT) {
-            throw new SessionsException("이미지 파일 크기가 초과했습니다.");
-        }
     }
 
 }
--- src/test/java/nextstep/sessions/domain/CoverImageTest.java
+++ src/test/java/nextstep/sessions/domain/CoverImageTest.java
@@ -2,6 +2,8 @@ package nextstep.sessions.domain;
 
 import nextstep.sessions.domain.exception.SessionsException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -21,5 +23,23 @@ public class CoverImageTest {
             .hasMessage("이미지 파일 크기가 초과했습니다.");
     }
 
+    @ParameterizedTest
+    @CsvSource(delimiter = ':',
+        value = {
+            "299:200",
+            "300:199",
+        })
+    void 이미지_가로세로_크기_미달(int width, int height) {
+        assertThatThrownBy(() -> new CoverImage("image.png", 1048576, width, height, "/images/"))
+            .isInstanceOf(SessionsException.class)
+            .hasMessage(ImageSize.IMAGE_SIZE_VALIDATION_MESSAGE);
+    }
+
+    @Test
+    void 이미지_비율_불일치() {
+        assertThatThrownBy(() -> new CoverImage("image.png", 1048576, 301, 200, "/images/"))
+            .isInstanceOf(SessionsException.class)
+            .hasMessage(ImageSize.IMAGE_RATIO_VALIDATION_MESSAGE);
+    }
 
 }
