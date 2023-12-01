53fa7184962d498ac138b1ffa5e28e7f768bb0ee
cjsrhd94 <cjsrhd94@gmail.com>
2023-12-01 17:33:30 +0900

    test: StudentsTest 구현

--- src/test/java/nextstep/courses/domain/PeriodTest.java
+++ src/test/java/nextstep/courses/domain/PeriodTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class PeriodTest {
 	@DisplayName("강의 시작일이 강의 종료일 이후 일 수 없습니다.")
 	@Test
-	void valid_is_greater_than_zero() {
+	void valid_start_is_before_end() {
 		assertThatIllegalArgumentException()
 			.isThrownBy(() -> new Period(
 				LocalDate.of(2023,12,1),
