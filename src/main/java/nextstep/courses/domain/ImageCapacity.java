c3a4e7c01595d89caad37e366bd324ec31335d96
cjsrhd94 <cjsrhd94@gmail.com>
2023-12-01 17:08:37 +0900

    test: ImageCapacityTest 구현

--- src/main/java/nextstep/courses/domain/ImageCapacity.java
+++ src/main/java/nextstep/courses/domain/ImageCapacity.java
@@ -6,12 +6,22 @@ public class ImageCapacity {
 
 	public ImageCapacity(Double capacity) {
 		this.capacity = capacity;
-		isOverCapacity();
+		valid();
 	}
 
+	private void valid() {
+		isGreaterThanZero();
+		isOverCapacity();
+	}
 	private void isOverCapacity() {
 		if (capacity > MAX_CAPACITY) {
 			throw new IllegalArgumentException("이미지 용량 제한을 초과하였습니다.");
+		}
+	}
+
+	private void isGreaterThanZero() {
+		if (capacity < 0) {
+			throw new IllegalArgumentException("이미지 용량은 0보다 작을 수 없습니다.");
 		}
 	}
 }
