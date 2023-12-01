5ca06d18b1f33557578f4b8bf2af4799b4ac5be5
kimgwanghoon <khkimykk@naver.com>
2023-12-01 15:26:28 +0900

    test: 유료강의 구매시 참여자에대한 테스트

--- src/test/java/nextstep/courses/domain/session/PriceTest.java
+++ src/test/java/nextstep/courses/domain/session/PriceTest.java
@@ -2,8 +2,10 @@ package nextstep.courses.domain.session;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PriceTest {
 
@@ -13,8 +15,9 @@ public class PriceTest {
         // given
         boolean isFree = false;
         int money = 10000;
+        ParticipantCount participantCount = new ParticipantCount(10);
         // when
-        Price price = new Price(isFree, money);
+        Price price = new Price(isFree, money, participantCount);
         // then
         assertThat(price.isFree()).isFalse();
     }
@@ -29,5 +32,47 @@ public class PriceTest {
         Price price = new Price(isFree, money);
         // then
         assertThat(price.isFree()).isTrue();
+    }
+
+    @DisplayName("강의 유료 결제 한다.")
+    @Test
+    void 강의유료결제한다() {
+        // given
+        boolean isFree = false;
+        int money = 10000;
+        ParticipantCount participantCount = new ParticipantCount(10);
+        // when
+        Price price = new Price(isFree, money, participantCount);
+        price.pay(money);
+        // then
+        assertThat(price.nowParticipants()).isEqualTo(1);
+    }
+
+    @DisplayName("강의 참여가 최대를 넘기면 예외가 발생한다.")
+    @Test
+    void 강의_참여가_최대를_넘기면_예외가_발생한다() {
+        // given
+        boolean isFree = false;
+        int money = 10000;
+        ParticipantCount participantCount = new ParticipantCount(10, 10);
+        // when
+        Price price = new Price(isFree, money, participantCount);
+        // then
+        assertThatThrownBy(() -> price.addParticipant())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("결제할 금액이 강의가격과 다르면 예외가 발생한다.")
+    @Test
+    void 결제할_금액이_강의가격과_다르면_예외가_발생한다() {
+        // given
+        boolean isFree = false;
+        int money = 10000;
+        ParticipantCount participantCount = new ParticipantCount(10);
+        // when
+        Price price = new Price(isFree, money, participantCount);
+        // then
+        assertThatThrownBy(() -> price.pay(1000))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
