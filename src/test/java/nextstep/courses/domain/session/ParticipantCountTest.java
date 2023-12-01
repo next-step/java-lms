4ee9dd04b23a2a9e3cc3fa3dd460bee8ee96734a
kimgwanghoon <khkimykk@naver.com>
2023-12-01 14:56:09 +0900

    test: 강의 참여자에 대한 테스트 추가

---
+++ src/test/java/nextstep/courses/domain/session/ParticipantCountTest.java
@@ -0,0 +1,53 @@
+package nextstep.courses.domain.session;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ParticipantCountTest {
+
+    @DisplayName("최대 참가자 수 생성하고 참여자는 0명이다.")
+    @Test
+    void 최대참가자수_생성하고_참여자는_0명이다() {
+        // given
+        int maxCount = 10;
+        // when
+        ParticipantCount participantCount = new ParticipantCount(maxCount);
+        // then
+        assertThat(participantCount.max()).isEqualTo(0);
+    }
+
+    @DisplayName("최대 참가자 수 생성하고 기본참여자는 2명이다.")
+    @Test
+    void 최대참가자수_생성하고_참여자는_0명이다() {
+        // given
+        int maxCount = 10;
+        int defaultCount = 2;
+        // when
+        ParticipantCount participantCount = new ParticipantCount(maxCount,defaultCount);
+        // then
+        assertThat(participantCount.nowCount()).isEqualTo(2);
+    }
+
+    @DisplayName("참가자를 추가한다.")
+    @Test
+    void 참가자를_추가한다() {
+        // given
+        int maxCount = 10;
+        ParticipantCount participantCount = new ParticipantCount(maxCount);
+        // when
+        participantCount.add();
+        // then
+        assertThat(participantCount.nowCount()).isEqualTo(1);
+    }
+
+    @DisplayName("최대 참가자 수를 초과하면 예외가 발생한다.")
+    @Test
+    void 최대참가자수를_초과하면_예외가_발생한다() {
+        // given
+        int maxCount = 10;
+        // when
+        // then
+        assertThatThrownBy(() -> new ParticipantCount(maxCount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
