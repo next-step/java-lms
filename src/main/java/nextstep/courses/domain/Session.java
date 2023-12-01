a1550eaec77f5ddc0e8baea123524994f35d988f
kimgwanghoon <khkimykk@naver.com>
2023-12-01 10:29:39 +0900

    feat: 강의 생성 구현

---
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -0,0 +1,32 @@
+package nextstep.courses.domain;
+
+import nextstep.courses.type.SessionStatus;
+
+public class Session {
+
+    private Long id;
+    private String title;
+    private SessionPeriod sessionPeriod;
+    private SessionStatus sessionStatus;
+    private boolean isFree;
+    private Integer price;
+    private Integer maxParticipants;
+
+    public Session(String title, SessionPeriod sessionPeriod, boolean isFree, int price, int maxParticipants, SessionStatus sessionStatus) {
+        this(null, title, sessionPeriod, isFree, price, maxParticipants, sessionStatus);
+    }
+
+    public Session(Long id, String title, SessionPeriod sessionPeriod, boolean isFree, int price, int maxParticipants, SessionStatus sessionStatus) {
+        this.id = id;
+        this.title = title;
+        this.sessionPeriod = sessionPeriod;
+        this.isFree = isFree;
+        this.price = price;
+        this.maxParticipants = maxParticipants;
+        this.sessionStatus = sessionStatus;
+    }
+
+    public String title() {
+        return title;
+    }
+}
