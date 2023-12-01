8bdefe19574df2cbca588062e2cfdad2096f87f3
kimgwanghoon <khkimykk@naver.com>
2023-12-01 18:03:13 +0900

    feat: 결제추가

--- src/main/java/nextstep/courses/domain/session/Price.java
+++ src/main/java/nextstep/courses/domain/session/Price.java
@@ -5,12 +5,39 @@ public class Price {
     private boolean isFree;
     private int money;
 
+    private final ParticipantCount participantCount;
+
+    public Price(boolean isFree, int money, ParticipantCount participantCount) {
+        this.isFree = isFree;
+        this.money = money;
+        this.participantCount = participantCount;
+    }
+
     public Price(boolean isFree, int money) {
         this.isFree = isFree;
         this.money = money;
+        this.participantCount = null;
     }
 
     public boolean isFree() {
         return isFree;
+    }
+
+    public void addParticipant(int money) {
+        pay(money);
+        participantCount.add();
+    }
+
+    private void pay(int money) {
+        if (isFree) {
+            throw new IllegalArgumentException("무료 강의는 결제할 수 없습니다.");
+        }
+        if(this.money != money) {
+            throw new IllegalArgumentException("결제 금액이 다릅니다.");
+        }
+    }
+
+    public int nowParticipants() {
+        return participantCount.nowCount();
     }
 }
