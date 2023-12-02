797ab27f986ced6c573e75d52e0a1b3bbeae2b66
choomi1217 <whdudal1217@naver.com>
2023-12-02 17:20:26 +0900

    refactor: `saveAll(Question question, List<Answer> answers)` 제거

--- src/main/java/nextstep/qna/service/DeleteHistoryService.java
+++ src/main/java/nextstep/qna/service/DeleteHistoryService.java
@@ -1,21 +1,16 @@
 package nextstep.qna.service;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import nextstep.qna.domain.Answer;
-import nextstep.qna.domain.ContentType;
+import java.util.List;
+import javax.annotation.Resource;
 import nextstep.qna.domain.DeleteHistory;
 import nextstep.qna.domain.DeleteHistoryRepository;
-import nextstep.qna.domain.Question;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
-import java.util.List;
-
 @Service("deleteHistoryService")
 public class DeleteHistoryService {
+
     @Resource(name = "deleteHistoryRepository")
     private DeleteHistoryRepository deleteHistoryRepository;
 
@@ -24,16 +19,4 @@ public class DeleteHistoryService {
         deleteHistoryRepository.saveAll(deleteHistories);
     }
 
-    public void saveAll(Question question, List<Answer> answers) {
-        List<DeleteHistory> deleteHistories = new ArrayList<>();
-        deleteHistories.add(
-            new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(),
-                LocalDateTime.now()));
-        for (Answer answer : answers) {
-            deleteHistories.add(
-                new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(),
-                    LocalDateTime.now()));
-        }
-        this.saveAll(deleteHistories);
-    }
 }
