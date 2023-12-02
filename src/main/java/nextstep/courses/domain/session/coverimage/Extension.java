8b0d15b9597e18b9dd5e4013a79180442c08dd4a
jonghunbaek <qor6028@naver.com>
2023-12-02 17:26:24 +0900

    feat : 이미지 파일의 확장자를 검증하는 enum 추가

---
+++ src/main/java/nextstep/courses/domain/session/coverimage/Extension.java
@@ -0,0 +1,27 @@
+package nextstep.courses.domain.session.coverimage;
+
+import nextstep.courses.exception.NotFindExtensionException;
+
+import java.util.Arrays;
+
+public enum Extension {
+
+    GIF("gif"),
+    JPG("jpg"),
+    JPEG("jpeg"),
+    PNG("png"),
+    SVG("svg");
+
+    private final String val;
+
+    Extension(String val) {
+        this.val = val;
+    }
+
+    public static Extension extension(String extension) throws NotFindExtensionException {
+        return Arrays.stream(values())
+            .filter(ext -> ext.val.equals(extension))
+            .findFirst()
+            .orElseThrow(() -> new NotFindExtensionException(String.format("해당 확장자는 사용할 수 없는 이미지 파일입니다. 현재 확장자 :: %s", extension)));
+    }
+}
---
+++ src/main/java/nextstep/courses/exception/NotFindExtensionException.java
@@ -0,0 +1,8 @@
+package nextstep.courses.exception;
+
+public class NotFindExtensionException extends Exception {
+
+    public NotFindExtensionException(String message) {
+        super(message);
+    }
+}
