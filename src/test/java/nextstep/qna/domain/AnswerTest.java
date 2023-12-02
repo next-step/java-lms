d984872f45bcd8a06f1fdcfea449f92fd12c2b23
김태현 <thxogusrla@gmail.com>
2023-12-02 14:42:34 +0900

    test: 도메인 수정 후 테스트 추가

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,27 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("Answers 작성자 오류 테스트")
+    @Test
+    void 질문자가_다름() {
+        Answers answers = new Answers();
+        answers.add(A1);
+        answers.add(A1);
+        answers.add(A1);
+        answers.add(A2);
+
+        Assertions.assertThatThrownBy(() -> {
+            answers.isValidLoginUser(NsUserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("로그인유저와 질문작성자가 다른경우 오류")
+    @Test
+    void 질문작성자_테스트() {
+        Assertions.assertThatThrownBy(() -> {
+           Q1.isValidLoginUser(NsUser.GUEST_USER);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
