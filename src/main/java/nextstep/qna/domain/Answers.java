d78c218a8ba8cf17d56cd6ab53b5e65119f64c04
김태현 <thxogusrla@gmail.com>
2023-12-02 14:42:52 +0900

    feat: 일급컬렉션 기능 추가

---
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -0,0 +1,41 @@
+package nextstep.qna.domain;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answers {
+    private final List<Answer> answers;
+
+    public Answers() {
+        this(new ArrayList<>());
+    }
+
+    public Answers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public void add(Answer answer) {
+        this.answers.add(answer);
+    }
+
+    public List<Answer> allAnswers() {
+        return this.answers;
+    }
+
+    public void isValidLoginUser(NsUser loginUser) throws CannotDeleteException {
+        for (Answer answer : this.answers) {
+            if (!answer.isOwner(loginUser)) {
+                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            }
+        }
+    }
+
+    public void allDeleted() {
+        for(Answer answer : this.answers) {
+            answer.deleted();
+        }
+    }
+}
