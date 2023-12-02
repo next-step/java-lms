1acb739eb48634bcabef71874a5f2757a400868a
kyunghyun-park <qqqq_o@naver.com>
2023-12-02 16:59:00 +0900

    feat: 이미지 width, height 검증 로직 추가

--- src/test/java/nextstep/courses/domain/CoverImage.java
+++ src/test/java/nextstep/courses/domain/CoverImage.java
@@ -2,14 +2,26 @@ package nextstep.courses.domain;
 
 import nextstep.courses.exception.ImageSizeOverException;
 import nextstep.courses.exception.InvalidImageExtensionException;
+import nextstep.courses.exception.InvalidImageWidthAndHeightException;
 
 public class CoverImage {
 
     private static final int IMAGE_SIZE = 1;
+    private static final int WIDTH_PIXEL = 300;
+    private static final int HEIGHT_PIXEL = 200;
+    private static final int WIDTH_RATIO = 2;
+    private static final int HEIGHT_RATIO = 3;
 
-    public CoverImage(long size, String extension) {
+    public CoverImage(long size, String extension, int width, int height) {
         validateImageSize(size);
         validateExtension(extension);
+        validateWidthAndHeight(width, height);
+    }
+
+    private void validateImageSize(long size) {
+        if (size > IMAGE_SIZE) {
+            throw new ImageSizeOverException(size);
+        }
     }
 
     private void validateExtension(String extension) {
@@ -18,9 +30,17 @@ public class CoverImage {
         }
     }
 
-    private void validateImageSize(long size) {
-        if (size > IMAGE_SIZE) {
-            throw new ImageSizeOverException(size);
+    private void validateWidthAndHeight(int width, int height) {
+        if (!checkPixel(width, height) || !checkRatio(width, height)) {
+            throw new InvalidImageWidthAndHeightException();
         }
+    }
+
+    private static boolean checkPixel(int width, int height) {
+        return width >= WIDTH_PIXEL && height >= HEIGHT_PIXEL;
+    }
+
+    private static boolean checkRatio(int width, int height) {
+        return width * WIDTH_RATIO == height * HEIGHT_RATIO;
     }
 }
--- src/test/java/nextstep/courses/domain/CoverImageTest.java
+++ src/test/java/nextstep/courses/domain/CoverImageTest.java
@@ -2,18 +2,18 @@ package nextstep.courses.domain;
 
 import nextstep.courses.exception.ImageSizeOverException;
 import nextstep.courses.exception.InvalidImageExtensionException;
+import nextstep.courses.exception.InvalidImageWidthAndHeightException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class CoverImageTest {
 
     @Test
     @DisplayName("이미지 크기가 1(MB)이상일 경우 예외를 던진다.")
     void size_test() {
-        assertThatThrownBy(() -> new CoverImage(2, "gif"))
+        assertThatThrownBy(() -> new CoverImage(2, "gif", 300, 200))
                 .isInstanceOf(ImageSizeOverException.class)
                 .hasMessageContaining("이미지 크기는 1MB를 초과할 수 없습니다.");
     }
@@ -21,14 +21,22 @@ public class CoverImageTest {
     @Test
     @DisplayName("이미지 타입이 gif, jpg(jpeg 포함), png, svg이 아니면 예외를 던진다.")
     void extension_test_invalid() {
-        assertThatThrownBy(() -> new CoverImage(1, "hwp"))
+        assertThatThrownBy(() -> new CoverImage(1, "hwp", 300, 200))
                 .isInstanceOf(InvalidImageExtensionException.class)
                 .hasMessageContaining("gif, jpg(jpeg 포함), png, svg 확장자만 허용됩니다.");
     }
 
     @Test
-    @DisplayName("이미지 타입이 gif, jpg(jpeg 포함), png, svg이면 예외를 던지지 않는다.")
+    @DisplayName("이미지 width는 300픽셀, height는 200픽셀 이상이 아니면 예외를 던진다")
+    void extension_width_height_size() {
+        assertThatThrownBy(() -> new CoverImage(1, "jpg", 290, 200))
+                .isInstanceOf(InvalidImageWidthAndHeightException.class);
+    }
+
+    @Test
+    @DisplayName("width와 height의 비율이 3:2이 아니면 예외를 던진다.")
     void extension_test_valid() {
-        assertDoesNotThrow(() -> new CoverImage(1, "jpg"));
+        assertThatThrownBy(() -> new CoverImage(1, "jpg", 350, 200))
+                .isInstanceOf(InvalidImageWidthAndHeightException.class);
     }
 }
