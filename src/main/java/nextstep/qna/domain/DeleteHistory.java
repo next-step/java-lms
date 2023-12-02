6c28948b417534c99479c261252ee544398b9f07
박현우 <62179139+qweadzs@users.noreply.github.com>
2023-12-03 08:03:53 +0900

    feat: 1단계 (#290)

--- src/main/java/nextstep/qna/domain/DeleteHistory.java
+++ src/main/java/nextstep/qna/domain/DeleteHistory.java
@@ -1,8 +1,11 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class DeleteHistory {
