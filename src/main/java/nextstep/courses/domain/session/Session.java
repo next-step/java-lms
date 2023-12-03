50245656101a185488a63b7ad69326d0b8442c19
qwe5507 <apvmffkdls@gmail.com>
2023-12-03 16:45:47 +0900

    feat: 강의 상태는 준비중, 모집중, 종료 3가지 상태를 가진다.

--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -3,9 +3,11 @@ package nextstep.courses.domain.session;
 public class Session {
     private final Long id;
     private final SessionPeriod sessionPeriod;
+    private final SessionStatus sessionStatus;
 
-    public Session(final Long id, final SessionPeriod sessionPeriod) {
+    public Session(final Long id, final SessionPeriod sessionPeriod, final SessionStatus sessionStatus) {
         this.id = id;
         this.sessionPeriod = sessionPeriod;
+        this.sessionStatus = sessionStatus;
     }
 }
