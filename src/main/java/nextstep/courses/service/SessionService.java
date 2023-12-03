13e1bad1a21776ecbe3aac0abc1b1a2464e04ad1
엄태권 <akfls221@gmail.com>
2023-12-04 01:18:48 +0900

    fix : 불필요 import 제거

--- src/main/java/nextstep/courses/service/SessionService.java
+++ src/main/java/nextstep/courses/service/SessionService.java
@@ -9,7 +9,6 @@ import nextstep.users.domain.NsUser;
 import nextstep.users.domain.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
 import javax.annotation.Resource;
 
