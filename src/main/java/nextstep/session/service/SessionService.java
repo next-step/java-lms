37d7e246053d68e2d80f8b008e9fd138f07df62a
hoon25 <cchoon95@hanmail.net>
2023-12-04 00:56:51 +0900

    feat(session): 강의 등록 기능 구현

--- src/main/java/nextstep/session/service/SessionService.java
+++ src/main/java/nextstep/session/service/SessionService.java
@@ -3,6 +3,7 @@ package nextstep.session.service;
 import nextstep.courses.domain.Course;
 import nextstep.courses.service.CourseService;
 import nextstep.session.domain.Session;
+import nextstep.session.domain.SessionRepository;
 import nextstep.session.ui.CreateSessionDto;
 import nextstep.users.domain.NsUser;
 import org.springframework.stereotype.Service;
@@ -12,14 +13,24 @@ import javax.annotation.Resource;
 @Service("sessionService")
 public class SessionService {
 
+    private SessionRepository sessionRepository;
     @Resource(name = "courseService")
     private CourseService courseService;
 
-
     public Session createSession(NsUser loginUser, CreateSessionDto dto) {
-        Course course = courseService.findCourseById(dto.getCourseId());
+        Course course = courseService.findById(dto.getCourseId());
         Session session = dto.toSession(loginUser);
+        sessionRepository.save(session);
         course.addSession(session);
         return session;
+    }
+
+    public Session findById(Long id) {
+        return sessionRepository.findById(id);
+    }
+
+    public void enrollSession(NsUser loginUser, Long sessionId) {
+        Session session = findById(sessionId);
+        session.enroll(loginUser);
     }
 }
