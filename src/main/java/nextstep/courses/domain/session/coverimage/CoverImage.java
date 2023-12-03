21685ca6ef19a9d51462a67025d5ddfcaa8e302b
jonghunbaek <qor6028@naver.com>
2023-12-03 13:55:22 +0900

    feat : Name, Size를 필드로 가지는 커버이미지 클래스 추가

---
+++ src/main/java/nextstep/courses/domain/session/coverimage/CoverImage.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain.session.coverimage;
+
+import nextstep.courses.exception.ImageSizeException;
+import nextstep.courses.exception.NotExistException;
+
+public class CoverImage {
+
+    private Name name;
+    private Size size;
+
+    public CoverImage(String fileName, int width, int height, long capacitySize) throws ImageSizeException, NotExistException {
+        this.name = new Name(fileName);
+        this.size = new Size(width, height, capacitySize);
+    }
+}
