6650e6d82f77f54563f8400530342b9ad985ad1e
cjsrhd94 <cjsrhd94@gmail.com>
2023-12-03 18:16:44 +0900

    test: SessionRegistrationTest 구현

---
+++ src/test/java/nextstep/courses/domain/SessionRegistrationTest.java
@@ -0,0 +1,45 @@
+package nextstep.courses.domain;
+
+import static nextstep.users.domain.NsUserTest.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.courses.domain.enums.PaidType;
+
+public class SessionRegistrationTest {
+
+	SessionRegistration freeSession;
+	SessionRegistration paidSession;
+
+	@BeforeEach
+	void setUp() {
+		freeSession = new SessionRegistration(
+			PaidType.FREE,
+			new Tuition(0),
+			new SessionCapacity(0),
+			new Students()
+		);
+		paidSession = new SessionRegistration(
+			PaidType.PAID,
+			new Tuition(50000),
+			new SessionCapacity(1),
+			new Students()
+		);
+	}
+
+	@DisplayName("강의 최대 수용인원이 유료일 때만 최대 수용인원 검사를 한다.")
+	@Test
+	void valid_amount() {
+		freeSession.register(JAVAJIGI);
+		freeSession.register(SANJIGI);
+
+		paidSession.register(JAVAJIGI);
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> paidSession.valid(50000))
+			.withMessage("최대 수강 인원을 초과했습니다.");
+	}
+}
