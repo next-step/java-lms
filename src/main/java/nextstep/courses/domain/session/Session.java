7b42963ef01ae1d0290af535c2501887a6aa88d9
qwe5507 <apvmffkdls@gmail.com>
2023-12-03 21:27:13 +0900

    feat: 과정(Course)은 기수 단위로 운영하며, 여러 개의 강의(Session)를 가질 수 있다.

--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -8,12 +8,18 @@ public class Session {
     private final SessionPeriod sessionPeriod;
     private final SessionStatus sessionStatus;
     private final CoverImage coverImage;
+    private final int genesis;
+    private final Sessions sessions;
 
-    public Session(final Long id, final String title, final SessionPeriod sessionPeriod, final SessionStatus sessionStatus, final CoverImage coverImage) {
+    public Session(final Long id, final String title, final SessionPeriod sessionPeriod,
+                   final SessionStatus sessionStatus, final CoverImage coverImage,
+                   final int genesis, final Sessions sessions) {
         this.id = id;
         this.title = title;
         this.sessionPeriod = sessionPeriod;
         this.sessionStatus = sessionStatus;
         this.coverImage = coverImage;
+        this.genesis = genesis;
+        this.sessions = sessions;
     }
 }
---
+++ src/main/java/nextstep/courses/domain/session/Sessions.java
@@ -0,0 +1,11 @@
+package nextstep.courses.domain.session;
+
+import java.util.List;
+
+public class Sessions {
+    private final List<Session> sessions;
+
+    public Sessions(final List<Session> sessions) {
+        this.sessions = sessions;
+    }
+}
