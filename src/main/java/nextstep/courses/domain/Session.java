cb156706f26335fa1cb4df496316485948877d12
ByungChan Lee <lbc2415@gmail.com>
2023-12-03 23:57:39 +0900

    feature: added Constructor for Session class

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -5,12 +5,23 @@ import nextstep.courses.domain.field.SessionStatus;
 import nextstep.courses.domain.field.SessionType;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Session {
+    private static AtomicLong autoGenId = new AtomicLong(1L);
+
     private Long id;
+    private Long courseId;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private CoverImage coverImage;
     private SessionType sessionType;
     private SessionStatus sessionStatus;
+
+    public Session(Long courseId, CoverImage coverImage, SessionType sessionType, SessionStatus sessionStatus) {
+        this.courseId = courseId;
+        this.coverImage = coverImage;
+        this.sessionType = sessionType;
+        this.sessionStatus = sessionStatus;
+    }
 }
---
+++ src/main/java/nextstep/courses/domain/service/CourseService.java
@@ -0,0 +1,4 @@
+package nextstep.courses.domain.service;
+
+public class CourseService {
+}
