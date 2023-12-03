380c1b9a10a75471358391b78591c7702f8757bc
qwe5507 <apvmffkdls@gmail.com>
2023-12-03 20:31:58 +0900

    feat: 이미지 크기는 1MB 이하여야 한다.

---
+++ src/main/java/nextstep/courses/domain/image/CoverImage.java
@@ -0,0 +1,14 @@
+package nextstep.courses.domain.image;
+
+public class CoverImage {
+    private static final Long MAX_SIZE = 1024L * 1024L;
+    public static final String MAX_SIZE_OVER_MSG = "이미지 사이즈는 1MB를 초과 할 수 없습니다.";
+    private final long size;
+
+    public CoverImage(final long size) {
+        if (size > MAX_SIZE) {
+            throw new IllegalArgumentException(MAX_SIZE_OVER_MSG);
+        }
+        this.size = size;
+    }
+}
--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -1,13 +1,19 @@
 package nextstep.courses.domain.session;
 
+import nextstep.courses.domain.image.CoverImage;
+
 public class Session {
     private final Long id;
+    private final String title;
     private final SessionPeriod sessionPeriod;
     private final SessionStatus sessionStatus;
+    private final CoverImage coverImage;
 
-    public Session(final Long id, final SessionPeriod sessionPeriod, final SessionStatus sessionStatus) {
+    public Session(final Long id, final String title, final SessionPeriod sessionPeriod, final SessionStatus sessionStatus, final CoverImage coverImage) {
         this.id = id;
+        this.title = title;
         this.sessionPeriod = sessionPeriod;
         this.sessionStatus = sessionStatus;
+        this.coverImage = coverImage;
     }
 }
