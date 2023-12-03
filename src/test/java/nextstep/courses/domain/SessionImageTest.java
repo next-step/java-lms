252bca780613a86a2ca6284aafa2b0aa41f343a1
이창섭 <ventulus95@gmail.com>
2023-12-03 22:31:24 +0900

    refact: 파일에 종속되지 않는 테스트 구성

--- src/test/java/nextstep/courses/domain/SessionImageTest.java
+++ src/test/java/nextstep/courses/domain/SessionImageTest.java
@@ -12,29 +12,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SessionImageTest {
 
-    private Path workingDir;
-    @BeforeEach
-    void setUp() {
-        this.workingDir = Path.of("", "src/test/resources");
-    }
-
-    private URI getUri(String fileName) {
-        return workingDir.resolve(fileName).toUri();
-    }
-
     @Test
     void 이미지는_1MB이하여야한다() {
-        File file = new File(getUri("test_2mb.jpg"));
         assertThatThrownBy(() -> {
-            new SessionImage(file);
+            new SessionImage(1024*1024*2, "jpg", new ImageSize(200, 200));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 이미지는_이미지형식외의_파일은_불가능하다() {
-        File file = new File(getUri("test.txt"));
         assertThatThrownBy(() -> {
-            new SessionImage(file);
+            new SessionImage(1024, "text", new ImageSize(200, 200));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
