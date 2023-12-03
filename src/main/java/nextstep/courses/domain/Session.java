77d6d744a4392b6579018bb71513c8ae6ef9a1ad
kyunghyun-park <qqqq_o@naver.com>
2023-12-04 01:36:54 +0900

    refactor: 강의 필수값 null 검증 추가

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -15,15 +15,22 @@ public class Session {
     protected final Students students;
 
     public Session(Long id, CoverImage coverImage, LocalDate startDate, LocalDate endDate) {
-        this(id, coverImage, new Period(startDate, endDate), Status.NOT_OPEN, new Students());
+        this(id, coverImage, startDate, endDate, Status.NOT_OPEN, new Students());
     }
 
-    private Session(Long id, CoverImage coverImage, Period period, Status status, Students students) {
+    private Session(Long id, CoverImage coverImage, LocalDate startDate, LocalDate endDate, Status status, Students students) {
+        validateNotNull(id, coverImage, startDate, endDate);
         this.id = id;
         this.coverImage = coverImage;
-        this.period = period;
+        this.period = new Period(startDate, endDate);
         this.status = status;
         this.students = students;
+    }
+
+    private void validateNotNull(Long id, CoverImage coverImage, LocalDate startDate, LocalDate endDate) {
+        if (id == null || coverImage == null || startDate == null || endDate == null) {
+            throw new IllegalArgumentException("id, 커버이미지, 시작일, 종료일은 필수값입니다.");
+        }
     }
 
     public void register(Payment payment) {
