9febea9adfedda0aebfe258555c7a86b8a3bfd32
pilming44 <pilming44@naver.com>
2023-12-05 01:35:26 +0900

    refactor, test, docs: apache commons를 참고해서 getBaseName, getExtension 간소화 구현

--- src/test/java/nextstep/lms/domain/ParserTest.java
+++ src/test/java/nextstep/lms/domain/ParserTest.java
@@ -1,18 +1,41 @@
 package nextstep.lms.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class ParserTest {
-    @DisplayName("파일명 포맷이 아닐경우 예외 발생")
+    @DisplayName("파일명이 없을경우 예외발생")
     @ParameterizedTest
-    @ValueSource(strings = {"next.step.java", "nextstep"})
-    void 파일명_포맷_확인_예외발생(String name) {
-        assertThatThrownBy(() -> Parser.fileNameFormatChecking(name))
+    @NullAndEmptySource
+    void 파일명_null(String name) {
+        assertThatThrownBy(() -> Parser.getBaseName(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("파일명이 올바르지않습니다.");
+    }
+    @DisplayName("정상 파일명")
+    @ParameterizedTest
+    @CsvSource(value = {"next.png:next", "choi.jae.hyeon.svg:choi.jae.hyeon"}, delimiter = ':')
+    void 정상_파일명(String fileName, String expectedFileName) {
+        assertThat(Parser.getBaseName(fileName)).isEqualTo(expectedFileName);
+    }
+
+    @DisplayName("확장자가 없을경우 예외발생")
+    @Test
+    void 파일명_null() {
+        assertThatThrownBy(() -> Parser.getExtension("nextstep"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("확장자가 없습니다.");
+    }
+
+    @DisplayName("정상 확장자")
+    @ParameterizedTest
+    @CsvSource(value = {"next.png:png", "choi.jae.hyeon.svg:svg"}, delimiter = ':')
+    void 정상_확장자(String fileName, String expectedExtension) {
+        assertThat(Parser.getExtension(fileName)).isEqualTo(expectedExtension);
     }
 }
