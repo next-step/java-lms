f09fc9acae91e1d50820889472bfed1e3c8c2be3
KimGwangHoon <khkimykk@naver.com>
2023-12-05 00:38:50 +0900

    fix: 강의 상태 get 수정

--- src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
@@ -3,6 +3,7 @@ package nextstep.courses.infrastructure;
 import nextstep.courses.domain.session.*;
 import nextstep.courses.type.SessionStatus;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -30,6 +31,17 @@ public class SessionRepositoryTest {
         Session session = new Session("title", sessionPeriod, price, SessionStatus.RECRUIT, null, 1L);
         int result = sessionRepository.save(session);
         assertThat(result).isOne();
+    }
+
+    @DisplayName("id로 조회 테스트")
+    @Test
+    void id로_조회_테스트() {
+        SessionPeriod sessionPeriod = new SessionPeriod("2023-12-01", "2023-12-31");
+        Price price = new Price(false, 10000, new ParticipantManager(10));
+        Session session = new Session("title", sessionPeriod, price, SessionStatus.RECRUIT, null, 1L);
+        sessionRepository.save(session);
+        Session result = sessionRepository.findById(1L);
+        assertThat(result.title()).isEqualTo(session.title());
     }
 
 }
