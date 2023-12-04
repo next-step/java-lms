655b660aac599a5568c78fe4784981d3ed0a9867
ByungChan Lee <lbc2415@gmail.com>
2023-12-04 14:59:55 +0900

    feature: added verification for coverImage

--- src/main/java/nextstep/courses/domain/field/CoverImage.java
+++ src/main/java/nextstep/courses/domain/field/CoverImage.java
@@ -8,4 +8,26 @@ public class CoverImage {
     private long height;
 
     private ImageType imageType;
+
+    public CoverImage(long size, long width, long height, ImageType imageType) {
+        verifySize(size);
+        verifyWidthHeight(width, height);
+
+        this.size = size;
+        this.width = width;
+        this.height = height;
+        this.imageType = imageType;
+    }
+
+    private static void verifyWidthHeight(long width, long height) {
+        if (width < 300 && height < 200) {
+            throw new IllegalArgumentException("이미지의 width는 300픽셀, height는 200픽셀 이상만 가능합니다.");
+        }
+    }
+
+    private static void verifySize(long size) {
+        if (size > 1) {
+            throw new IllegalArgumentException("이미지 크기는 1MB 이하만 가능합니다.");
+        }
+    }
 }
