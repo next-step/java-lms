9df8f2bbc73e60f0fb7c85a817ec07dfcd80433a
cjsrhd94 <cjsrhd94@gmail.com>
2023-12-04 17:25:26 +0900

    chore: enum description 부분 삭제

--- src/main/java/nextstep/courses/domain/enums/PaidType.java
+++ src/main/java/nextstep/courses/domain/enums/PaidType.java
@@ -1,15 +1,9 @@
 package nextstep.courses.domain.enums;
 
 public enum PaidType {
-	FREE("무료"),
-	PAID("유료")
+	FREE,
+	PAID
 	;
-
-	private final String description;
-
-	PaidType(String description) {
-		this.description = description;
-	}
 
 	public boolean isFree() {
 		return this == FREE;
--- src/main/java/nextstep/courses/domain/enums/Status.java
+++ src/main/java/nextstep/courses/domain/enums/Status.java
@@ -1,15 +1,9 @@
 package nextstep.courses.domain.enums;
 
 public enum Status {
-	READY("준비중"),
-	APPLYING("모집중"),
-	END("모집 종료");
-
-	private final String description;
-
-	Status(String description) {
-		this.description = description;
-	}
+	READY,
+	APPLYING,
+	END;
 
 	public boolean isApplying() {
 		return this == APPLYING;
