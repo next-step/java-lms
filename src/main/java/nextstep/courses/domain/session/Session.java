96975d95e015f3b43fa66dcda702d6bd46e2881d
jonghunbaek <qor6028@naver.com>
2023-12-04 16:36:13 +0900

    feat : 무료, 유료 강의 부모 추상 클래스 추가

---
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -0,0 +1,23 @@
+package nextstep.courses.domain.session;
+
+import nextstep.courses.domain.session.coverimage.CoverImage;
+import nextstep.users.domain.NsUser;
+
+public abstract class Session {
+
+    protected Long id;
+    protected PayType payType;
+    protected Status status;
+    protected CoverImage coverImage;
+    protected SessionStudents sessionStudents;
+
+    public Session(Long id, PayType payType, Status status, CoverImage coverImage) {
+        this.id = id;
+        this.payType = payType;
+        this.status = status;
+        this.coverImage = coverImage;
+        this.sessionStudents = new SessionStudents();
+    }
+
+    public abstract void register(NsUser nsUser);
+}
