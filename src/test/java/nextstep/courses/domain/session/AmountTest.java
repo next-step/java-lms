cda4ab1e793b2ce048032caee9a6e9e5eb32d787
qwe5507 <apvmffkdls@gmail.com>
2023-12-04 22:23:26 +0900

    test: Amount 포장 객체 테스트 코드 추가

--- src/test/java/nextstep/courses/domain/session/AmountTest.java
+++ src/test/java/nextstep/courses/domain/session/AmountTest.java
@@ -1,7 +1,9 @@
 package nextstep.courses.domain.session;
 
+import nextstep.payments.domain.Payment;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AmountTest {
@@ -10,5 +12,18 @@ class AmountTest {
         assertThatThrownBy(() -> {
             new Amount(-1L);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 금액이_0원일때() {
+        final Amount amount = new Amount(0L);
+        assertThat(amount.isFree()).isTrue();
+    }
+
+    @Test
+    void 금액이_다를떄() {
+        final Amount amount = new Amount(100L);
+        final Payment payment = new Payment("qwe5507", 1L, 1L, 10L);
+        assertThat(amount.isNotSame(payment)).isTrue();
     }
 }
