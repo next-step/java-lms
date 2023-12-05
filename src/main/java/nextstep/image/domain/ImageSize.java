c0a7182e1851d732571e95bbdafdb19020682546
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-12-06 00:25:30 +0900

    feat : ImageSize 이미지의 height는 200픽셀 이상인지 검증하는 기능 추가

--- src/main/java/nextstep/image/domain/ImageSize.java
+++ src/main/java/nextstep/image/domain/ImageSize.java
@@ -1,22 +1,36 @@
 package nextstep.image.domain;
 
+import nextstep.image.exception.HeightValidationException;
 import nextstep.image.exception.WidthValidationException;
 
 public class ImageSize {
 
     public static final String WIDTH_VALIDATION_EXCEPTION = "이미지의 너비는 300픽셀 이상이어야 합니다.";
+    public static final String HEIGHT_VALIATION_EXCEPTION = "이미지의 높이는 300픽셀 이상이어야 합니다.";
 
     private long width;
     private long height;
 
-    public ImageSize(int width) {
+    public ImageSize() {
+
+    }
+
+    public ImageSize(int width, int height) {
         validateWidth(width);
+        validateHeight(height);
         this.width = width;
+        this.height = height;
     }
 
     private void validateWidth(int width) {
         if (width < 300) {
             throw new WidthValidationException(WIDTH_VALIDATION_EXCEPTION);
+        }
+    }
+
+    private void validateHeight(int height) {
+        if (height < 200) {
+            throw new HeightValidationException(HEIGHT_VALIATION_EXCEPTION);
         }
     }
 }
--- src/test/java/nextstep/image/domain/ImageSizeTest.java
+++ src/test/java/nextstep/image/domain/ImageSizeTest.java
@@ -1,8 +1,10 @@
 package nextstep.image.domain;
 
+import static nextstep.image.domain.ImageSize.HEIGHT_VALIATION_EXCEPTION;
 import static nextstep.image.domain.ImageSize.WIDTH_VALIDATION_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import nextstep.image.exception.HeightValidationException;
 import nextstep.image.exception.WidthValidationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,11 +16,24 @@ class ImageSizeTest {
     void width_exception() {
         // given
         int width = 299;
+        int height = 200;
 
         // when // then
-        assertThatThrownBy(() -> new ImageSize(299))
+        assertThatThrownBy(() -> new ImageSize(width, height))
                 .isExactlyInstanceOf(WidthValidationException.class)
                 .hasMessage(WIDTH_VALIDATION_EXCEPTION);
     }
 
+    @Test
+    @DisplayName("이미지의 height는 200픽셀 이상인지 검증한다.")
+    void height_exception() {
+        // given
+        int width = 300;
+        int height = 199;
+
+        // when // then
+        assertThatThrownBy(() -> new ImageSize(width, height))
+                .isExactlyInstanceOf(HeightValidationException.class)
+                .hasMessage(HEIGHT_VALIATION_EXCEPTION);
+    }
 }
