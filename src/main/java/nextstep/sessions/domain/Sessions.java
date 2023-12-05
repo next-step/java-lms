c32d95fc8b37cf02b753fe9e443d589a781aa0e1
qweadzs <qweadzs@naver.com>
2023-12-06 00:12:52 +0900

    feat: Course, Sessions 필드 추가

--- src/main/java/nextstep/sessions/domain/Sessions.java
+++ src/main/java/nextstep/sessions/domain/Sessions.java
@@ -1,4 +1,11 @@
 package nextstep.sessions.domain;
 
+import java.util.List;
+
 public class Sessions {
+    private final List<Session> sessions;
+
+    public Sessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
 }
