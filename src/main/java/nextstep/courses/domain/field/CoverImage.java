b9cb1863e13a04c3f5d345957090be618efa00b8
ByungChan Lee <lbc2415@gmail.com>
2023-12-05 22:09:13 +0900

    refactor: refactored to add constant variable for Minimum width and height

--- src/main/java/nextstep/courses/domain/field/CoverImage.java
+++ src/main/java/nextstep/courses/domain/field/CoverImage.java
@@ -1,6 +1,9 @@
 package nextstep.courses.domain.field;
 
 public class CoverImage {
+    private final long MIN_WIDTH = 300;
+    private final long MIN_HEIGHT = 200;
+
     private double size;
 
     private long width;
@@ -19,13 +22,13 @@ public class CoverImage {
         this.imageType = imageType;
     }
 
-    private static void verifyWidthHeight(long width, long height) {
-        if (width < 300 && height < 200) {
+    private void verifyWidthHeight(long width, long height) {
+        if (width < this.MIN_WIDTH && height < this.MIN_HEIGHT) {
             throw new IllegalArgumentException("이미지의 width는 300픽셀, height는 200픽셀 이상만 가능합니다.");
         }
     }
 
-    private static void verifySize(double size) {
+    private void verifySize(double size) {
         if (size > 1) {
             throw new IllegalArgumentException("이미지 크기는 1MB 이하만 가능합니다.");
         }
