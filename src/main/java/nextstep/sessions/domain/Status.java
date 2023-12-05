aa79344d26775d763e1151058ec13c03c8a35bb2
qweadzs <qweadzs@naver.com>
2023-12-05 22:21:53 +0900

    feat: 강의상태, 강의타입 Test 코드 추가

--- src/main/java/nextstep/sessions/domain/Status.java
+++ src/main/java/nextstep/sessions/domain/Status.java
@@ -1,4 +1,21 @@
 package nextstep.sessions.domain;
 
+import java.util.Arrays;
+
 public enum Status {
+    PREPARE("prepare"),
+    RECRUITING("recruiting"),
+    CLOSED("closed"),
+    ;
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+    public static Status of(String sessionStatus) {
+        return Arrays.stream(values())
+                .filter(status -> status.status.equals(sessionStatus))
+                .findAny()
+                .orElseThrow(()-> new IllegalArgumentException("허용되지 않은 상태 타입입니다."));
+    }
 }
