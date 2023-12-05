79ef19362e3432d1ad6a4e26edb04c6828f59459
KimGwangHoon <khkimykk@naver.com>
2023-12-06 01:05:39 +0900

    feat: 강의목록 코스id로 조회 구현

--- src/main/java/nextstep/courses/infrastructure/JdbcSessionRepository.java
+++ src/main/java/nextstep/courses/infrastructure/JdbcSessionRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository("sessionRepository")
 public class JdbcSessionRepository implements SessionRepository {
@@ -41,6 +42,25 @@ public class JdbcSessionRepository implements SessionRepository {
                     Price.of(isFree, money, ParticipantManager.of(maxParticipants)), SessionStatus.valueOf(status), courseId);
         };
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    @Override
+    public List<Session> findByCourseId(Long findCourseId) {
+        String sql = "select id, title, start_date_time, end_date_time, status, is_free, money, max_participants, course_id from session where course_id = ?";
+        RowMapper<Session> rowMapper = (rs, rowNum) -> {
+            Long sessionId = rs.getLong(1);
+            String title = rs.getString(2);
+            LocalDateTime startDateTime = toLocalDateTime(rs.getTimestamp(3));
+            LocalDateTime endDateTime = toLocalDateTime(rs.getTimestamp(4));
+            String status = rs.getString(5);
+            boolean isFree = rs.getBoolean(6);
+            int money = rs.getInt(7);
+            int maxParticipants = rs.getInt(8);
+            Long courseId = rs.getLong(9);
+            return new Session(sessionId, title, SessionPeriod.of(startDateTime, endDateTime),
+                    Price.of(isFree, money, ParticipantManager.of(maxParticipants)), SessionStatus.valueOf(status), courseId);
+        };
+        return jdbcTemplate.query(sql, rowMapper, findCourseId);
     }
 
     private LocalDateTime toLocalDateTime(Timestamp timestamp) {
