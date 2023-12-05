827b61d14626937c9fd408b9b9aff49f15c99f6f
KimGwangHoon <khkimykk@naver.com>
2023-12-06 01:48:51 +0900

    feat: 결제한 사용자 강의 등록 서비스 추가

--- src/main/java/nextstep/courses/service/SessionService.java
+++ src/main/java/nextstep/courses/service/SessionService.java
@@ -3,6 +3,9 @@ package nextstep.courses.service;
 import nextstep.courses.domain.image.ImageRepository;
 import nextstep.courses.domain.session.Session;
 import nextstep.courses.domain.session.SessionRepository;
+import nextstep.payments.domain.Payment;
+import nextstep.users.domain.NsUser;
+import nextstep.users.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,9 @@ public class SessionService {
     @Autowired
     private ImageRepository imageRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Transactional
     public void saveSession(Session session) {
         sessionRepository.save(session);
@@ -27,5 +33,12 @@ public class SessionService {
     @Transactional(readOnly = true)
     public Session findById(Long id) {
         return sessionRepository.findById(id);
+    }
+
+    @Transactional
+    public void SessionPayment(Payment payment) {
+        Session session = sessionRepository.findById(payment.sessionId());
+        NsUser nsUser = userRepository.findById(payment.nsUserId()).orElseThrow(IllegalArgumentException::new);
+        session.addParticipant(payment.amount(), nsUser);
     }
 }
