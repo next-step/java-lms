a7ee59cfde71d6fa9bf63ab823725c47aabf7971
hoon25 <cchoon95@hanmail.net>
2023-12-07 02:00:42 +0900

    refactor(SessionImageTest): SessionImageFixture 사용

--- src/test/java/nextstep/session/domain/SessionImageTest.java
+++ src/test/java/nextstep/session/domain/SessionImageTest.java
@@ -2,6 +2,10 @@ package nextstep.session.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static nextstep.session.domain.fixture.SessionImageFixture.DEFAULT_HEIGHT;
+import static nextstep.session.domain.fixture.SessionImageFixture.DEFAULT_IMAGE_SIZE;
+import static nextstep.session.domain.fixture.SessionImageFixture.DEFAULT_WIDTH;
+import static nextstep.session.domain.fixture.SessionImageFixture.createSessionImage;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,31 +21,31 @@ class SessionImageTest {
     @Test
     void 생성_유효하지_않은_형식_실패() {
         // expect
-        assertThatThrownBy(() -> SessionImage.of("test.txt", 1024 * 1024, 300, 200))
+        assertThatThrownBy(() -> createSessionImage("test.txt"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 생성_용량초과_실패() {
         // expect
-        assertThatThrownBy(() -> SessionImage.of("S3://test.gif", 1024 * 1024 + 1, 300, 200))
+        assertThatThrownBy(() -> createSessionImage(DEFAULT_IMAGE_SIZE + 1, DEFAULT_WIDTH, DEFAULT_HEIGHT))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 생성_pixel_실패() {
         // expect
-        assertThatThrownBy(() -> SessionImage.of("S3://test.gif", 1024 * 1024, 299, 200))
+        assertThatThrownBy(() -> createSessionImage(DEFAULT_IMAGE_SIZE, DEFAULT_WIDTH - 1, DEFAULT_HEIGHT))
                 .isInstanceOf(IllegalArgumentException.class);
         // expect
-        assertThatThrownBy(() -> SessionImage.of("S3://test.gif", 1024 * 1024, 300, 199))
+        assertThatThrownBy(() -> createSessionImage(DEFAULT_IMAGE_SIZE, DEFAULT_WIDTH, DEFAULT_HEIGHT - 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 생성_비율_실패() {
         // expect
-        assertThatThrownBy(() -> SessionImage.of("S3://test.gif", 1024 * 1024, 400, 200))
+        assertThatThrownBy(() -> createSessionImage(DEFAULT_IMAGE_SIZE, DEFAULT_WIDTH + 100, DEFAULT_HEIGHT))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
--- src/test/java/nextstep/session/domain/fixture/SessionImageFixture.java
+++ src/test/java/nextstep/session/domain/fixture/SessionImageFixture.java
@@ -1,9 +1,19 @@
 package nextstep.session.domain.fixture;
 
-import nextstep.session.domain.ImageType;
 import nextstep.session.domain.SessionImage;
 
 public class SessionImageFixture {
-    public static final SessionImage sessionImageFixture = SessionImage.of("S3://test.gif", 1024, 300, 200);
+    public static final String DEFAULT_IMAGE_URL = "S3://test.gif";
+    public static final int DEFAULT_IMAGE_SIZE = 1024 * 1024;
+    public static final int DEFAULT_WIDTH = 300;
+    public static final int DEFAULT_HEIGHT = 200;
+    public static final SessionImage sessionImageFixture = SessionImage.of(DEFAULT_IMAGE_URL, DEFAULT_IMAGE_SIZE, DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
+    public static SessionImage createSessionImage(String fileURL) {
+        return SessionImage.of(fileURL, DEFAULT_IMAGE_SIZE, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    public static SessionImage createSessionImage(int imageSize, int width, int height) {
+        return SessionImage.of(DEFAULT_IMAGE_URL, imageSize, width, height);
+    }
 }
