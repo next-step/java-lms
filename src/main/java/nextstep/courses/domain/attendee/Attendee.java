331d14f2ef64202abc17d8ecd16ea2db38a431b7
namuhuchutong <namuhuchutong@gmail.com>
2023-12-06 20:07:17 +0900

    refactor: Attendee 객체 id 필드 제거

--- src/main/java/nextstep/courses/domain/attendee/Attendee.java
+++ src/main/java/nextstep/courses/domain/attendee/Attendee.java
@@ -4,20 +4,11 @@ import java.util.Objects;
 
 public class Attendee {
 
-    private final Long id;
-
     private final Long nsUserId;
 
     private final Long sessionId;
 
-    public Attendee(Long id, Long nsUserId, Long sessionId) {
-        this.id = id;
-        this.nsUserId = nsUserId;
-        this.sessionId = sessionId;
-    }
-
     public Attendee(Long nsUserId, Long sessionId) {
-        this.id = 0L;
         this.nsUserId = nsUserId;
         this.sessionId = sessionId;
     }
@@ -27,13 +18,12 @@ public class Attendee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Attendee attendee = (Attendee) o;
-        return Objects.equals(id, attendee.id)
-                && Objects.equals(nsUserId, attendee.nsUserId)
+        return Objects.equals(nsUserId, attendee.nsUserId)
                 && Objects.equals(sessionId, attendee.sessionId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nsUserId, sessionId);
+        return Objects.hash(nsUserId, sessionId);
     }
 }
