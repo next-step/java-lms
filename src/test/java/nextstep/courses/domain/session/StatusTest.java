a4d184dd774906653a2a20fcf0507df62096c511
jonghunbaek <qor6028@naver.com>
2023-12-06 12:41:48 +0900

    refactor : Status테스트 코드 리팩토링

--- src/test/java/nextstep/courses/domain/session/StatusTest.java
+++ src/test/java/nextstep/courses/domain/session/StatusTest.java
@@ -4,15 +4,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static nextstep.courses.domain.session.Status.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StatusTest {
 
     @DisplayName("인자로 강의 상태를 전달 받아 모집중 상태이면 true를 반환 하고 아니면 false를 반환한다.")
     @Test
     void validateStatus() {
-        assertThat(isNotRecruiting(RECRUIT)).isTrue();
-        assertThat(isNotRecruiting(PREPARE)).isFalse();
-        assertThat(isNotRecruiting(FINISH)).isFalse();
+        assertAll(
+            () -> assertFalse(isNotRecruiting(RECRUIT)),
+            () -> assertTrue(isNotRecruiting(PREPARE)),
+            () -> assertTrue(isNotRecruiting(FINISH))
+        );
     }
 }
