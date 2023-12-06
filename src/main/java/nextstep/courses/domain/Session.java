a59e03079412e712a52b0d33635fa4c93b2560ca
이승준 <skp.seungjun@sk.com>
2023-12-06 10:35:56 +0900

    feature: 모집중 상태에서만 회원 등록 가능하도록 변경

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -2,8 +2,6 @@ package nextstep.courses.domain;
 
 import nextstep.users.domain.NsUser;
 
-import java.time.ZonedDateTime;
-
 import static nextstep.courses.domain.SessionState.READY;
 
 /**
@@ -28,6 +26,17 @@ public class Session {
     }
 
     public void registerUser(NsUser user) {
+        if (this.state != READY) {
+            throw new IllegalStateException("사용자 등록은 모집중 상태에서만 가능하지만 모집중 상태가 아닙니다.");
+        }
         registeredUsers.add(user);
+    }
+
+    public void updateStateTo(SessionState state) {
+        if (state == null) {
+            throw new IllegalArgumentException("null로 상태를 업데이트할 수 없습니다.");
+        }
+
+        this.state = state;
     }
 }
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -6,9 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static nextstep.courses.domain.SessionState.READY;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static nextstep.courses.domain.SessionState.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SessionTest {
@@ -30,5 +29,31 @@ class SessionTest {
             session.registerUser(NsUserTest.SANJIGI);
         })
                 .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    @DisplayName("[Session.registerUser()] 강의는 모집중 상태에서만 수강생 등록 가능")
+    public void reigsterStateTest() {
+        SessionImage image = new SessionImage(100, 300, 200, ImageExtension.jpg);
+        SessionDuration duration = SessionDuration.fromIso8601("2023-12-06T10:23:10.000+09:00", "2023-12-07T10:00:00.000+09:00");
+        Session readySession = Session.createNewSession(image, duration);
+        Session recruitSession = Session.createNewSession(image, duration);
+        Session endSession = Session.createNewSession(image, duration);
+
+        readySession.updateStateTo(READY);
+        recruitSession.updateStateTo(RECRUIT);
+        endSession.updateStateTo(END);
+
+        assertThatCode(() -> {
+            readySession.registerUser(NsUserTest.JAVAJIGI);
+        }).doesNotThrowAnyException();
+
+        assertThatThrownBy(() -> {
+            recruitSession.registerUser(NsUserTest.JAVAJIGI);
+        }).isInstanceOf(Exception.class);
+
+        assertThatThrownBy(() -> {
+            endSession.registerUser(NsUserTest.JAVAJIGI);
+        }).isInstanceOf(Exception.class);
     }
 }
