6529be5b0414447589d159c26570a8ff60332019
pilming44 <pilming44@naver.com>
2023-12-08 04:07:06 +0900

    style: 오타수정

--- src/test/java/nextstep/payments/domain/PaymentTest.java
+++ src/test/java/nextstep/payments/domain/PaymentTest.java
@@ -9,7 +9,7 @@ class PaymentTest {
     @DisplayName("금액이 다른지 확인")
     @ParameterizedTest
     @CsvSource(value = {"800_000:false", "800_001:true", "799_999:true", "0:true"}, delimiter = ':')
-    void 같은_금액인지_확인(Long amount, boolean expected) {
+    void 다른_금액인지_확인(Long amount, boolean expected) {
         Payment payment = new Payment("1", 1L, 1L, 800_000L);
         Assertions.assertThat(payment.isDifferentAmount(amount)).isEqualTo(expected);
     }
