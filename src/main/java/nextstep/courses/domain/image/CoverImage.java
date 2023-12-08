5a6cebe61f5f2726c840c05e181b4116a6391bcb
qwe5507 <apvmffkdls@gmail.com>
2023-12-08 12:08:22 +0900

    refactor: [step3 피드백 반영] VO객체 final키워드 다시 추가

--- src/main/java/nextstep/courses/domain/image/CoverImage.java
+++ src/main/java/nextstep/courses/domain/image/CoverImage.java
@@ -7,9 +7,9 @@ import java.time.LocalDateTime;
 public class CoverImage extends BaseEntity {
     private static final Long MAX_SIZE = 1024L * 1024L;
     public static final String MAX_SIZE_OVER_MSG = "이미지 사이즈는 1MB를 초과 할 수 없습니다.";
-    private long size;
-    private ImagePixel imagePixel;
-    private ImageType imageType;
+    private final long size;
+    private final ImagePixel imagePixel;
+    private final ImageType imageType;
 
 
     public CoverImage(final long size, final ImagePixel imagePixel, final ImageType imageType) {
