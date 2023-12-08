ff7bf4e2fc7a9ec96f4d2d40cb1188fb535d9130
geunnseung <bsg1017@naver.com>
2023-12-08 13:24:21 +0900

    :sparkles: 강의 기간 도메인 생성 - 강의 기간 검증 로직

---
+++ src/main/java/nextstep/courses/domain/SessionDuration.java
@@ -0,0 +1,22 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDateTime;
+
+public class SessionDuration {
+
+    private LocalDateTime startDateTime;
+
+    private LocalDateTime endDateTime;
+
+    public SessionDuration(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        validate(startDateTime, endDateTime);
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+
+    private void validate(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        if (startDateTime.isAfter(endDateTime) || startDateTime.isEqual(endDateTime)) {
+            throw new IllegalArgumentException("시작일은 종료일보다 이전이어야 합니다");
+        }
+    }
+}
---
+++ src/test/java/nextstep/courses/domain/SessionDurationTest.java
@@ -0,0 +1,33 @@
+package nextstep.courses.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionDurationTest {
+
+    @DisplayName("시작일과 종료일이 같을 때 예외를 던진다")
+    @Test
+    void durationSameException() {
+        LocalDateTime startTime = LocalDateTime.of(2023, 12, 8, 0, 0);
+        LocalDateTime endTime = LocalDateTime.of(2023, 12, 8, 0, 0);
+        assertThatThrownBy(() -> new SessionDuration(startTime, endTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시작일은 종료일보다 이전이어야 합니다");
+    }
+
+    @DisplayName("종료일이 시작일보다 이를 때 예외를 던진다")
+    @Test
+    void durationException() {
+        LocalDateTime startTime = LocalDateTime.of(2023, 12, 8, 0, 0);
+        LocalDateTime endTime = LocalDateTime.of(2023, 12, 7, 23, 59);
+        assertThatThrownBy(() -> new SessionDuration(startTime, endTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시작일은 종료일보다 이전이어야 합니다");
+    }
+}
