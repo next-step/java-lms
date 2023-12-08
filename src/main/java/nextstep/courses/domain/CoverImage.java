1e5331c4de24ca7fcc197cf12de5252794573c47
geunnseung <bsg1017@naver.com>
2023-12-08 13:13:42 +0900

    :sparkles: CoverImage 도메인 생성 - ImageType 추가

---
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -0,0 +1,23 @@
+package nextstep.courses.domain;
+
+import nextstep.courses.domain.Image.ImageSize;
+import nextstep.courses.domain.Image.ImageType;
+import nextstep.courses.domain.Image.ImgaePixel;
+
+public class CoverImage {
+
+    private Long id;
+
+    private ImageSize imageSize;
+
+    private ImgaePixel imgaePixel;
+
+    private ImageType imageType;
+
+    public CoverImage(Long id, ImageSize imageSize, ImgaePixel imgaePixel, ImageType imageType) {
+        this.id = id;
+        this.imageSize = imageSize;
+        this.imgaePixel = imgaePixel;
+        this.imageType = imageType;
+    }
+}
---
+++ src/main/java/nextstep/courses/domain/Image/ImageType.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain.Image;
+
+public enum ImageType {
+
+    GIF("gif"),
+    JPG("jpg"),
+    JPEG("jpeg"),
+    PNG("png"),
+    SVG("svg");
+
+    private String type;
+
+    ImageType(String type) {
+    }
+}
