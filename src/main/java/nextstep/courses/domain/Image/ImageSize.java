3ebf41231ca191a31a9b5dff9e58349f5650267c
geunnseung <bsg1017@naver.com>
2023-12-08 13:12:07 +0900

    :sparkles: ImageSize - 이미지 사이즈 검증 - 이미지 사이즈 테스트

---
+++ src/main/java/nextstep/courses/domain/Image/ImageSize.java
@@ -0,0 +1,19 @@
+package nextstep.courses.domain.Image;
+
+public class ImageSize {
+
+    private static final int MAX_IMAGE_SIZE = 1024 * 1024;
+
+    private int size;
+
+    public ImageSize(int size) {
+        validate(size);
+        this.size = size;
+    }
+
+    private void validate(int size) {
+        if (size > MAX_IMAGE_SIZE) {
+            throw new IllegalArgumentException("이미지 사이즈는 1MB를 넘을 수 없습니다");
+        }
+    }
+}
