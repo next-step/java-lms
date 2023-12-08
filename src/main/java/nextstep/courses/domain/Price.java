966aea249fc651ff0b273afd829a86a1ce3b4f9b
geunnseung <bsg1017@naver.com>
2023-12-08 14:33:43 +0900

    :sparkles: 강의 수강료 확인 로직

--- src/main/java/nextstep/courses/domain/Price.java
+++ src/main/java/nextstep/courses/domain/Price.java
@@ -16,4 +16,11 @@ public class Price {
             throw new IllegalArgumentException("금액은 음수일 수 없습니다");
         }
     }
+
+    public boolean validatePrice(int userPay) {
+        if(this.price != userPay) {
+            throw new IllegalArgumentException("정확한 수강료를 납부해주세요");
+        }
+        return true;
+    }
 }
--- src/test/java/nextstep/courses/domain/PriceTest.java
+++ src/test/java/nextstep/courses/domain/PriceTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +16,21 @@ class PriceTest {
         assertThatThrownBy(() -> new Price(-5000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("금액은 음수일 수 없습니다");
+    }
+
+    @DisplayName("납부한 금액이 수강료와 같지 않으면 예외를 던진다")
+    @Test
+    void wrongPay() {
+        Price price = new Price(30000);
+        assertThatThrownBy(() -> price.validatePrice(30001))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("정확한 수강료를 납부해주세요");
+    }
+
+    @DisplayName("정확한 수강료가 납부되면 true를 반환한다")
+    @Test
+    void correctpay() {
+        Price price = new Price(30000);
+        assertThat(price.validatePrice(30000));
     }
 }
