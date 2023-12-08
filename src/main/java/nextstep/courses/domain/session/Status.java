a573f8e727a91ba9ef117f7f6ab79ed47f5fc77c
jonghunbaek <qor6028@naver.com>
2023-12-08 17:16:46 +0900

    feat : 문자열 인자와 일치하는 상수를 찾아 반환

--- src/main/java/nextstep/courses/domain/session/Status.java
+++ src/main/java/nextstep/courses/domain/session/Status.java
@@ -1,5 +1,7 @@
 package nextstep.courses.domain.session;
 
+import java.util.Arrays;
+
 public enum Status {
 
     PREPARE("준비중"),
@@ -14,6 +16,13 @@ public enum Status {
 
     public static boolean isNotRecruiting(Status status) {
         return !RECRUIT.equals(status);
+    }
+
+    public static Status status(String status) {
+        return Arrays.stream(values())
+            .filter(val -> val.toString().equals(status))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("일치하는 Status가 없습니다. 인자 값 :: " + status));
     }
 
     public String description() {
