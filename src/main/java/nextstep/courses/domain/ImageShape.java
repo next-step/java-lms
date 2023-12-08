778bf646007978e68ab7e75d3c4ccc03372860c5
perhona <perhona@kakao.com>
2023-12-08 15:26:25 +0900

    docs(step3): 3단계 요구사항 추가, 2단계 피드백 일부 반영

--- src/main/java/nextstep/courses/domain/ImageShape.java
+++ src/main/java/nextstep/courses/domain/ImageShape.java
@@ -16,6 +16,12 @@ public class ImageShape {
 	}
 
 	private void validate(Long width, Long height) {
+		if (width == null || height == null) {
+			throw new IllegalArgumentException("이미지의 가로 세로 값은 빈 값이 올 수 없습니다.");
+		}
+		if (width == 0L || height == 0L) {
+			throw new IllegalArgumentException("이미지의 가로 세로 값은 0이 될 수 없습니다.");
+		}
 		if (width < MIN_WIDTH) {
 			throw new IllegalArgumentException("이미지의 width는 300픽셀 이상이어야 합니다.");
 		}
