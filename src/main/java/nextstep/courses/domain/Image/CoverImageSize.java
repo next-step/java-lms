0b5ee5ebfd3a6891d3c7adca24bc54e4700c1efb
geunnseung <bsg1017@naver.com>
2023-12-10 14:12:18 +0900

    :recycle: coverImageSize 테스트 추가

--- src/main/java/nextstep/courses/domain/Image/CoverImageSize.java
+++ src/main/java/nextstep/courses/domain/Image/CoverImageSize.java
@@ -1,5 +1,7 @@
 package nextstep.courses.domain.Image;
 
+import java.util.Objects;
+
 public class CoverImageSize {
 
     private static final int MAX_IMAGE_SIZE = 1024 * 1024;
@@ -15,5 +17,18 @@ public class CoverImageSize {
         if (size > MAX_IMAGE_SIZE) {
             throw new IllegalArgumentException("이미지 사이즈는 1MB를 넘을 수 없습니다");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CoverImageSize that = (CoverImageSize) o;
+        return size == that.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size);
     }
 }
--- src/test/java/nextstep/courses/domain/Image/CoverImageSizeTest.java
+++ src/test/java/nextstep/courses/domain/Image/CoverImageSizeTest.java
@@ -3,6 +3,7 @@ package nextstep.courses.domain.Image;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CoverImageSizeTest {
@@ -13,5 +14,12 @@ class CoverImageSizeTest {
         assertThatThrownBy(() -> new CoverImageSize(1024 * 1024 + 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미지 사이즈는 1MB를 넘을 수 없습니다");
+    }
+
+    @DisplayName("이미지 사이즈가 1MB 이하일 때 이미지 사이즈 객체가 생성된다")
+    @Test
+    void sizeTest() {
+        CoverImageSize coverImageSize = new CoverImageSize(1024 * 1024);
+        assertThat(coverImageSize).isEqualTo(new CoverImageSize(1024 * 1024));
     }
 }
