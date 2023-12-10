764070a92b2cd7b7fdcc247c1f96f8b90efe839c
chunghyunleeme <chunghyun.dev@gmail.com>
2023-12-10 21:24:44 +0900

    feat: 이미지 타입은 gif, jpg(jpeg 포함),, png, svg만 허용한다.

--- src/main/java/nextstep/courses/domain/Image.java
+++ src/main/java/nextstep/courses/domain/Image.java
@@ -4,16 +4,24 @@ public class Image {
 
     private int size;
 
-    private String type;
+    private ImageType type;
 
     private int width;
 
     private int height;
 
-    public Image(int size) {
-        if (1 <= size) {
+    public Image() {
+    }
+
+    public Image(int size, ImageType type) {
+        if (1 < size) {
             throw new IllegalArgumentException("이미지 크기는 1MB 이하여야 합니다.");
         }
         this.size = size;
+        this.type = type;
+    }
+
+    public ImageType getType() {
+        return this.type;
     }
 }
--- src/test/java/nextstep/courses/domain/ImageTest.java
+++ src/test/java/nextstep/courses/domain/ImageTest.java
@@ -1,5 +1,6 @@
 package nextstep.courses.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -7,11 +8,24 @@ import org.junit.jupiter.api.Test;
 public class ImageTest {
 
     @Test
+    void 이미지_타입은_gif_jpg_jpeg_포함__png_svg만_허용한다() {
+        // given
+        int size = 1;
+
+        // when
+        Image image = new Image(size, ImageType.JPG);
+
+        // then
+        assertThat(image.getType()).isEqualTo(ImageType.JPG);
+    }
+
+    @Test
     void 이미지_크기는_1MB_이하여야_한다() {
         // given
         int size = 2;
 
         // when, then
-        assertThatThrownBy(() -> new Image(size)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Image(size, ImageType.GIF)).isInstanceOf(
+            IllegalArgumentException.class);
     }
 }
