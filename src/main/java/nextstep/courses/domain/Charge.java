4ed2db7af4c933d1585069611092b4428a7b9e06
kimbro97 <rlagudwog@gmail.com>
2023-12-11 23:34:15 +0900

    refactor: 성공/실패 케이스를 분리 작업

--- src/main/java/nextstep/courses/domain/Charge.java
+++ src/main/java/nextstep/courses/domain/Charge.java
@@ -1,5 +1,7 @@
 package nextstep.courses.domain;
 
+import java.util.Objects;
+
 public class Charge {
     private static final int FREE_SESSION_PRICE = 0;
 
@@ -9,6 +11,19 @@ public class Charge {
 
     public Charge(ChargeStatus status) {
         this(status, FREE_SESSION_PRICE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Charge charge = (Charge) o;
+        return price == charge.price && status == charge.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(status, price);
     }
 
     public Charge(ChargeStatus status, int price) {
--- src/test/java/nextstep/courses/domain/ChargeTest.java
+++ src/test/java/nextstep/courses/domain/ChargeTest.java
@@ -8,9 +8,14 @@ public class ChargeTest {
     @Test
     @DisplayName("무료 강의에 가격이 있을 때 예외가 발생한다.")
     void create_exception() {
-        new Charge(ChargeStatus.FREE, 10000);
-        new Charge(ChargeStatus.CHARGE, 500000);
         Assertions.assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Charge(ChargeStatus.FREE, 100000));
+    }
+
+    @Test
+    @DisplayName("강의 생성 할 수 있다 ")
+    void create() {
+        Charge charge = new Charge(ChargeStatus.CHARGE, 100000);
+        Assertions.assertThat(charge).isEqualTo(new Charge(ChargeStatus.CHARGE, 100000));
     }
 }
