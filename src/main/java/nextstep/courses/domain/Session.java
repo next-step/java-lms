aa4fc0c5606bb289689641760fe91a19a6a1c71a
kmj990929 <kmj990929@dgist.ac.kr>
2023-12-12 00:30:26 +0900

    feat: Session에 stratDate, endDate 추가

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,4 +1,18 @@
 package nextstep.courses.domain;
 
+import java.time.LocalDate;
+
 public class Session {
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    public Session() {
+        this(LocalDate.now(), null);
+    }
+
+    public Session(LocalDate startDate, LocalDate endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
