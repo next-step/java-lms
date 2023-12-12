2e70b29781efce7d75f2087cd3d09b51281aa9aa
jonghunbaek <qor6028@naver.com>
2023-12-12 14:26:34 +0900

    refactor : 테스트코드 리팩토링

--- src/test/java/nextstep/courses/infrastructure/JdbcEnrolmentRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/JdbcEnrolmentRepositoryTest.java
@@ -3,6 +3,7 @@ package nextstep.courses.infrastructure;
 import nextstep.courses.domain.session.enroll.Enrolment;
 import nextstep.courses.domain.session.repository.EnrolmentRepository;
 import nextstep.courses.domain.session.repository.StudentRepository;
+import nextstep.courses.domain.session.student.Student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import static nextstep.courses.domain.session.student.SelectionStatus.*;
 import static org.assertj.core.api.Assertions.*;
 
 @JdbcTest
@@ -30,9 +32,13 @@ class JdbcEnrolmentRepositoryTest {
     @DisplayName("강의 아이디에 일치하는 수강 신청 데이터를 찾는다.")
     @Test
     void findBySession() {
+        studentRepository.save(new Student(1L, 1L, WAITING));
+        studentRepository.save(new Student(1L, 2L, WAITING));
+        studentRepository.save(new Student(1L, 3L, WAITING));
+
         Enrolment enrolment = enrolmentRepository.findBySession(1L)
             .orElseThrow();
 
-        assertThat(enrolment.studentsSize()).isZero();
+        assertThat(enrolment.studentsSize()).isEqualTo(3);
     }
 }
