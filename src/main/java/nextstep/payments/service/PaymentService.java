3ea9ab25bfe6b0ee4d5e5f21cc6d1a4f2d6c56ba
parkje0927 <parkje09270@gmail.com>
2023-12-15 21:02:29 +0900

    feat: 수강신청 Step2 - 강의 결제 관련 내용 수정, 강의 id 에 맞는 결제 정보 조회 추가

--- src/main/java/nextstep/payments/service/PaymentService.java
+++ src/main/java/nextstep/payments/service/PaymentService.java
@@ -2,9 +2,21 @@ package nextstep.payments.service;
 
 import nextstep.payments.domain.Payment;
 
+import java.util.List;
+
 public class PaymentService {
     public Payment payment(String id) {
         // PG사 API를 통해 id에 해당하는 결제 정보를 반환
+        return new Payment();
+    }
+
+    //sessionId 에 맞는 결제 정보를 가져왔다고 가정
+    public List<Payment> findBySessionId(Long sessionId) {
+        return List.of(new Payment(), new Payment());
+    }
+
+    public Payment findBySessionIdAndNsUserId(Long sessionId, Long nsUserId) {
+        //sessionId 와 nsUserId 에 맞는 결제 정보를 가져왔다고 가정
         return new Payment();
     }
 }
