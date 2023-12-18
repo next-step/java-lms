b75c21b2492cf347cb4794479c97b9b7b9ed350c
kimbro97 <rlagudwog@gmail.com>
2023-12-18 21:41:00 +0900

    refactor: 매직리터럴 상수 선언 작업

--- src/main/java/nextstep/courses/domain/Image.java
+++ src/main/java/nextstep/courses/domain/Image.java
@@ -1,6 +1,7 @@
 package nextstep.courses.domain;
 
 public class Image {
+    private static final int MAX_SIZE = 1;
     private final int size;
     public Image(int size) {
         validateSize(size);
@@ -8,7 +9,7 @@ public class Image {
     }
 
     private void validateSize(int size) {
-        if (size > 1) {
+        if (size > MAX_SIZE) {
             throw new IllegalArgumentException("이미지 크기는 1MB 이하여야 한다");
         }
     }
