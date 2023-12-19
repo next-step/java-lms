f5c4ce9d79e7e4b031c2db8b317cefabed7cbcc1
kimbro97 <rlagudwog@gmail.com>
2023-12-19 22:08:38 +0900

    feat: session 작업

---
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -0,0 +1,40 @@
+package nextstep.courses.domain;
+
+import nextstep.users.domain.NsUser;
+
+import java.time.LocalDate;
+
+public class Session {
+    private final String title;
+    private final Image image;
+    private final SessionPeriod sessionPeriod;
+    private final Enrollment enrollment;
+    private final SessionType sesstionType;
+    private final int price;
+
+    public Session(String title, int size, String imageType,
+                   int width, int height,
+                   LocalDate startedAt, LocalDate endedAt,
+                   String state, String sessionType,
+                   int capacity, int price) {
+        this(title, new Image(size, imageType, width, height),
+                new SessionPeriod(startedAt, endedAt), new Enrollment(SessionState.of(state), capacity),
+                SessionType.of(sessionType), price);
+    }
+
+    public Session(String title, Image image,
+                   SessionPeriod sessionPeriod, Enrollment enrollment,
+                   SessionType sessionType, int price ) {
+        this.title = title;
+        this.image = image;
+        this.sessionPeriod = sessionPeriod;
+        this.enrollment = enrollment;
+        this.sesstionType = sessionType;
+        this.price = price;
+    }
+
+    public void enroll(NsUser loginUser) {
+        enrollment.enroll(loginUser);
+    }
+}
+
---
+++ src/main/java/nextstep/courses/domain/SessionType.java
@@ -0,0 +1,25 @@
+package nextstep.courses.domain;
+
+import java.util.Arrays;
+
+public enum SessionType {
+    FREE("무료"),
+    CHARGE("유료");
+
+    private String type;
+
+    SessionType(String type) {
+        this.type = type;
+    }
+
+    public static SessionType of(String type) {
+        return Arrays.stream(values())
+                .filter(SessionType -> SessionType.getType().equals(type))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 강의 타입니다."));
+    }
+
+    private String getType() {
+        return type;
+    }
+}
