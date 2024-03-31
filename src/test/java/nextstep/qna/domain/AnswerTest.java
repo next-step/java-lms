1186ed6b1e009ecd1fd050fd06ab765e2ca39851
hellonayeon <hellonykwon@gmail.com>
2024-03-31 13:57:13 +0900

    Test: Question, Answer delete 단위 테스트 작성

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,28 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("[성공] 자신이 작성한 답변을 삭제한다.")
+    void 답변_삭제() throws CannotDeleteException {
+        assertThat(A1.delete(NsUserTest.JAVAJIGI).isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("[실패] 자신이 생성하지 않은 답변을 삭제하려는 경우 CannotDeleteException 예외가 발생한다.")
+    void 답변_삭제_불가능() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> A1.delete(NsUserTest.SANJIGI));
+    }
+
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,28 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("[성공] 자신이 작성한 질문을 삭제한다.")
+    void 답변_삭제() throws CannotDeleteException {
+        assertThat(Q1.delete(NsUserTest.JAVAJIGI).isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("[실패] 자신이 생성하지 않은 질문을 삭제하려는 경우 CannotDeleteException 예외가 발생한다.")
+    void 답변_삭제_불가능() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> Q2.delete(NsUserTest.SANJIGI));
+    }
+
 }
