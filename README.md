8a586308dea7ddbe68e3e19f39a3b6b116250a50
firewoody237 <firewoody237@gmail.com>
2024-03-31 10:38:49 +0900

    feat: DeleteHistory 서비스의 DeleteHistoryTargets에 대한 삭제 구현

--- README.md
+++ README.md
@@ -42,7 +42,7 @@
 
 # 리팩토링 리스트
 - [x] DeleteHistoryTargets 일급 컬렉션 구현
-- [ ] DeleteHistory 서비스의 DeleteHistoryTargets에 대한 삭제 구현
+- [x] DeleteHistory 서비스의 DeleteHistoryTargets에 대한 삭제 구현
 - [ ] Answer 일급 컬렉션 구현
 - [ ] Question 삭제 상태 변경 구현
 - [ ] Question 삭제 리스트 반환 구현
--- src/main/java/nextstep/qna/service/DeleteHistoryService.java
+++ src/main/java/nextstep/qna/service/DeleteHistoryService.java
@@ -2,6 +2,7 @@ package nextstep.qna.service;
 
 import nextstep.qna.domain.DeleteHistory;
 import nextstep.qna.domain.DeleteHistoryRepository;
+import nextstep.qna.domain.DeleteHistoryTargets;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +18,10 @@ public class DeleteHistoryService {
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveAll(List<DeleteHistory> deleteHistories) {
         deleteHistoryRepository.saveAll(deleteHistories);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveAll(DeleteHistoryTargets deleteHistoryTargets) {
+        deleteHistoryRepository.saveAll(deleteHistoryTargets.asList());
     }
 }
