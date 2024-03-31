22d1b1e690c03e5e0dd04e00792769957dc0a83e
DongWooE <ehddn977@naver.com>
2024-03-31 21:00:54 +0900

    feat : 질문자가 본인이며 답변이 없으면 삭제가 가능

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -6,8 +6,9 @@ import nextstep.users.domain.NsUser;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Question {
+public class Question{
     private Long id;
 
     private String title;
@@ -31,11 +32,23 @@ public class Question {
         this(0L, writer, title, contents);
     }
 
+    public Question(NsUser writer, String title, String contents, boolean deleted) {
+        this(0L, title, contents, writer, new ArrayList<>(), deleted, LocalDateTime.now(), LocalDateTime.now());
+    }
+
     public Question(Long id, NsUser writer, String title, String contents) {
+        this(id, title, contents, writer, new ArrayList<>(), false, LocalDateTime.now(), LocalDateTime.now());
+    }
+
+    public Question(Long id, String title, String contents, NsUser writer, List<Answer> answers, boolean deleted, LocalDateTime createdDate, LocalDateTime updatedDate) {
         this.id = id;
-        this.writer = writer;
         this.title = title;
         this.contents = contents;
+        this.writer = writer;
+        this.answers = answers;
+        this.deleted = deleted;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
     }
 
     public Long getId() {
@@ -91,6 +104,12 @@ public class Question {
         return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
     }
 
+    public void delete(NsUser nsUser) throws CannotDeleteException {
+        checkIfOwner(nsUser);
+        checkAnswer(nsUser);
+        this.deleted = true;
+    }
+
     public void checkIfOwner(NsUser nsUser) throws CannotDeleteException {
         if(!isOwner(nsUser)){
             throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
@@ -105,5 +124,18 @@ public class Question {
         if(ifNotUser){
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question)) return false;
+        Question question = (Question) o;
+        return deleted == question.deleted && Objects.equals(id, question.id) && Objects.equals(title, question.title) && Objects.equals(contents, question.contents) && Objects.equals(writer, question.writer) && Objects.equals(answers, question.answers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, contents, writer, answers, deleted);
     }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -2,22 +2,31 @@ package nextstep.qna.domain;
 
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static nextstep.qna.domain.AnswerTest.A1;
 import static nextstep.qna.domain.AnswerTest.A2;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static final Question Q1_DELETED = new Question(NsUserTest.JAVAJIGI, "title1", "contents1", true);
+
+    private Question Q1_CLONE;
+    @BeforeEach
+    void setUp() {
+        Q1_CLONE = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+    }
 
     @Test
     @DisplayName("질문자가 로그인한 본인이 아닐 경우에 삭제가 불가능")
     void is_not_login_user() {
         assertThatThrownBy(() -> {
-            Q1.checkIfOwner(NsUserTest.SANJIGI);
+            Q1_CLONE.checkIfOwner(NsUserTest.SANJIGI);
         }).isInstanceOf(CannotDeleteException.class);
     }
 
@@ -25,9 +34,16 @@ public class QuestionTest {
     @DisplayName("질문자와 다른 사람이 작성한 답변글이 있을 경우 삭제가 불가능")
     void has_other_user_answer() {
         assertThatThrownBy(() -> {
-            Q1.addAnswer(A1);
-            Q1.addAnswer(A2);
-            Q1.checkAnswer(NsUserTest.JAVAJIGI);
+            Q1_CLONE.addAnswer(A1);
+            Q1_CLONE.addAnswer(A2);
+            Q1_CLONE.checkAnswer(NsUserTest.JAVAJIGI);
         }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("질문자가 본인이며 답변이 없으면 삭제가 가능")
+    void owner_and_has_no_answers() throws CannotDeleteException {
+        Q1_CLONE.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1_DELETED).isEqualTo(Q1_CLONE);
     }
 }
