d1d55dac75ccd77a538851e1359fe22e0ffecb75
firewoody237 <firewoody237@gmail.com>
2024-03-31 14:06:33 +0900

    refactor: 사용하지 않는 구현 삭제

--- src/main/java/nextstep/qna/service/DeleteHistoryService.java
+++ src/main/java/nextstep/qna/service/DeleteHistoryService.java
@@ -2,7 +2,6 @@ package nextstep.qna.service;
 
 import nextstep.qna.domain.DeleteHistory;
 import nextstep.qna.domain.DeleteHistoryRepository;
-import nextstep.qna.domain.DeleteHistoryTargets;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,10 +17,5 @@ public class DeleteHistoryService {
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveAll(List<DeleteHistory> deleteHistories) {
         deleteHistoryRepository.saveAll(deleteHistories);
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void saveAll(DeleteHistoryTargets deleteHistoryTargets) {
-        deleteHistoryRepository.saveAll(deleteHistoryTargets.asList());
     }
 }
