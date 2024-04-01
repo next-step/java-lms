16c0ca5ecc176b16510e29353823844ad3019e87
hellonayeon <hellonykwon@gmail.com>
2024-04-01 21:59:26 +0900

    Test: Question.delete 호출 시 삭제 히스토리 반환 검증 단위 테스트 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -5,17 +5,32 @@ import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
 
+
     @Test
     @DisplayName("[성공] 자신이 작성한 질문을 삭제한다.")
-    void 답변_삭제() throws CannotDeleteException {
-        assertThat(Q1.delete(NsUserTest.JAVAJIGI).isDeleted()).isTrue();
+    void 답변_삭제() {
+        assertThatNoException().isThrownBy(() -> {
+            Q1.delete(NsUserTest.JAVAJIGI);
+        });
+    }
+
+    @Test
+    @DisplayName("[성공] 자신이 작성한 질문을 삭제하면 질문과 답변에 대한 히스토리 리스트를 반환한다.")
+    void 답변_삭제_히스토리_반환() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A1);
+        assertThat(Q1.delete(NsUserTest.JAVAJIGI).get())
+                .containsExactly(
+                        new DeleteHistory(ContentType.QUESTION, 0L, NsUserTest.JAVAJIGI, LocalDateTime.now()),
+                        new DeleteHistory(ContentType.ANSWER, null, NsUserTest.JAVAJIGI, LocalDateTime.now())
+                );
     }
 
     @Test
