56d979c27191cfec424707b8a870c92abc6f8467
leeedohyun <ldh19738@gmail.com>
2024-04-01 15:11:31 +0900

    test: 기존에 있는 상수를 사용해서 테스트하도록 리팩토링

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -1,5 +1,8 @@
 package nextstep.qna.domain;
 
+import static nextstep.qna.domain.AnswerTest.A1;
+import static nextstep.qna.domain.AnswerTest.A2;
+import static nextstep.users.domain.NsUserTest.JAVAJIGI;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
@@ -7,26 +10,14 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import nextstep.qna.CannotDeleteException;
-import nextstep.users.domain.NsUser;
 
 class AnswersTest {
 
     @Test
     void 답변을_삭제할_떼_다른_사람이_쓴_답변이_있으면_예외가_발생한다() {
-        // given
-        final NsUser user1 = new NsUser(1L, "user1", "password1", "name1", "abc@gmail.com");
-        final NsUser user2 = new NsUser(2L, "user2", "password2", "name2", "efg@gmail.com");
-        final Question question = new Question(user1, "title", "contents");
-        final Answer answer1 = new Answer(user2, question, "contents1");
-        final Answer answer2 = new Answer(user1, question, "contents2");
-        question.addAnswer(answer1);
-        question.addAnswer(answer2);
+        final Answers answers = new Answers(List.of(A1, A2));
 
-        // when
-        final Answers answers = new Answers(List.of(answer1, answer2));
-
-        // then
-        assertThatThrownBy(() -> answers.validateAnswersOwnership(user1))
+        assertThatThrownBy(() -> answers.validateAnswersOwnership(JAVAJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
@@ -34,16 +25,12 @@ class AnswersTest {
     @Test
     void 답변을_추가한다() {
         // given
-        final NsUser user1 = new NsUser(1L, "user1", "password1", "name1", "abc@gmail.com");
-        final NsUser user2 = new NsUser(2L, "user2", "password2", "name2", "efg@gmail.com");
-        final Question question = new Question(user1, "title", "contents");
-        final Answer answer = new Answer(user2, question, "contents1");
+        final Answers answers = new Answers();
 
         // when
-        final Answers answers = new Answers();
-        answers.add(answer);
+        answers.add(A1);
 
         // then
-        assertThat(answers.getAnswers()).containsExactly(answer);
+        assertThat(answers.getAnswers()).containsExactly(A1);
     }
 }
--- src/test/java/nextstep/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/nextstep/qna/domain/DeleteHistoriesTest.java
@@ -1,29 +1,24 @@
 package nextstep.qna.domain;
 
+import static nextstep.qna.domain.AnswerTest.A1;
+import static nextstep.qna.domain.AnswerTest.A2;
+import static nextstep.qna.domain.QuestionTest.Q1;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-
-import nextstep.users.domain.NsUser;
 
 class DeleteHistoriesTest {
 
     @Test
     void 추가() {
         // given
-        final NsUser user1 = new NsUser(1L, "user1", "password1", "name1", "abc@gmail.com");
-        final NsUser user2 = new NsUser(2L, "user2", "password2", "name2", "efg@gmail.com");
-        final Question question = new Question(user1, "title", "contents");
-        final Answer answer1 = new Answer(user2, question, "contents1");
-        final Answer answer2 = new Answer(user1, question, "contents2");
-        final List<Answer> answers = List.of(answer1, answer2);
-        answers.forEach(question::addAnswer);
+        Q1.addAnswer(A1);
+        Q1.addAnswer(A2);
+
+        final DeleteHistories deleteHistories = new DeleteHistories();
 
         // when
-        final DeleteHistories deleteHistories = new DeleteHistories();
-        deleteHistories.add(question);
+        deleteHistories.add(Q1);
 
         // then
         assertThat(deleteHistories.getDeleteHistories()).hasSize(3);
