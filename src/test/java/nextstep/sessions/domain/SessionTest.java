ed9a099704261932174b703f0e50dbf9d52c2f76
leejinwoo <leejinwoo1126@gmail.com>
2024-04-02 17:32:34 +0900

    test: 관련 Session 테스트를 Nested 로 묶음

--- src/test/java/nextstep/sessions/domain/SessionTest.java
+++ src/test/java/nextstep/sessions/domain/SessionTest.java
@@ -2,6 +2,8 @@ package nextstep.sessions.domain;
 
 import nextstep.money.Money;
 import nextstep.payments.domain.Payment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -26,115 +28,113 @@ class SessionTest {
         assertThat(session).isEqualTo(new Session("TDD, 자바 18기", PREPARING, now));
     }
 
-    @ParameterizedTest
-    @EnumSource(mode = EnumSource.Mode.EXCLUDE, names = {"RECRUITING"})
-    void 강의상태가_모집중이_아닌경우_신청불가하다(SessionState sessionState) {
-        Session session = new Session("TDD, 자바 18기", sessionState);
-        assertThatThrownBy(() -> session.requestJoin(JAVAJIGI, LocalDateTime.now()))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("수강 신청 테스트")
+    @Nested
+    class SessionRequestJoinNestedTest {
+        @ParameterizedTest
+        @EnumSource(mode = EnumSource.Mode.EXCLUDE, names = {"RECRUITING"})
+        void 강의상태가_모집중이_아닌경우_신청불가하다(SessionState sessionState) {
+            Session session = new Session("TDD, 자바 18기", sessionState);
+            assertThatThrownBy(() -> session.requestJoin(JAVAJIGI, LocalDateTime.now()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 모집종료된_무료강의는_신청불가하다() {
+            Session session = new Session("TDD, 자바 18기", FINISHED, 0, new FreeSession());
+            assertThatThrownBy(() -> session.requestJoin(JAVAJIGI, LocalDateTime.now()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 모집중인_유료강의가_수강인원을_초과한_경우_신청할수없다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING, 10, new PaidSession(10, Money.wons(1000L)));
+            assertThatThrownBy(() -> session.requestJoin(JAVAJIGI, LocalDateTime.now()))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 수강신청가능한경우_주문서를생성한다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING);
+
+            LocalDateTime now = LocalDateTime.now();
+            Payment payment = session.requestJoin(JAVAJIGI, now);
+
+            assertThat(payment).isEqualTo(new Payment(0L, 1L, Money.ZERO, now));
+        }
     }
 
-    @Test
-    void 모집종료된_무료강의는_신청불가하다() {
-        Session session = new Session("TDD, 자바 18기", FINISHED, 0, new FreeSession());
-        assertThatThrownBy(() -> session.requestJoin(JAVAJIGI, LocalDateTime.now()))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+    @DisplayName("수강 등록 테스트")
+    @Nested
+    class SessionJoinNestedTest {
+        @Test
+        void 로그인유저가_널인경우_예외를_던진다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING);
+            assertThatThrownBy(() -> session.join(null, null))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @Test
-    void 모집중인_유료강의가_수강인원을_초과한_경우_신청할수없다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 10, new PaidSession(10, Money.wons(1000L)));
-        assertThatThrownBy(() -> session.requestJoin(JAVAJIGI, LocalDateTime.now()))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @Test
+        void 결제정보가_널인경우_예외를_던진다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING);
+            assertThatThrownBy(() -> session.join(JAVAJIGI, null))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @Test
-    void 수강신청가능한경우_주문서를생성한다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING);
+        @Test
+        void 이미_등록된_수강자인경우_예외를_던진다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession());
+            session.addListener(JAVAJIGI);
 
-        LocalDateTime now = LocalDateTime.now();
-        Payment payment = session.requestJoin(JAVAJIGI, now);
+            Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.wons(1000L));
 
-        assertThat(payment).isEqualTo(new Payment(0L, 1L, Money.ZERO, now));
-    }
+            assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    // 수강 등록시 NESTED
-    @Test
-    void 수강등록시_로그인유저가_널인경우_예외를_던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING);
-        assertThatThrownBy(() -> session.join(null, null))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @Test
+        void 결제완료상태가_아닌경우_예외를던진다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession());
+            Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.ZERO);
 
-    @Test
-    void 수강등록시_결제정보가_널인경우_예외를_던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING);
-        assertThatThrownBy(() -> session.join(JAVAJIGI, null))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+            assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @Test
-    void 수강등록시_이미_등록된_수강자인경우_예외를_던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession());
-        session.addListener(JAVAJIGI);
+        @Test
+        void 강의아이디가_일치하지_않는경우_예외를던진다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession()); // 0L
+            Payment payment = new Payment(1L, JAVAJIGI.getId(), Money.ZERO, PAYMENT_COMPLETE);
 
-        Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.wons(1000L));
+            assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-        assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @Test
+        void 유저아이디가_일치하지_않는경우_예외를던진다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession());
+            Payment payment = new Payment(0L, SANJIGI.getId(), Money.ZERO, PAYMENT_COMPLETE);
 
-    @Test
-    void 수강등록시_결제정보가_일치하지않는경우_예외를던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession());
-        Payment payment = new Payment(1L, JAVAJIGI.getId(), Money.ZERO);
+            assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-        assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @Test
+        void 강의금액이_일치하지_않는경우_예외를던진다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new PaidSession(10, Money.wons(1000L)));
+            Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.ZERO, PAYMENT_COMPLETE);
 
-    @Test
-    void 수강등록시_결제완료상태가_아닌경우_예외를던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession());
-        Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.ZERO);
+            assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-        assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @Test
+        void 정상적으로_수강등록시_카운트가_증가한다() {
+            Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new PaidSession(10, Money.wons(1000L)));
+            Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.wons(1000L), PAYMENT_COMPLETE);
+            session.join(JAVAJIGI, payment);
 
-    @Test
-    void 수강등록시_결제정보에서_강의아이디가_일치하지_않는경우_예외를던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession()); // 0L
-        Payment payment = new Payment(1L, JAVAJIGI.getId(), Money.ZERO, PAYMENT_COMPLETE);
-
-        assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 수강등록시_결제정보에서_유저아이디가_일치하지_않는경우_예외를던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new FreeSession());
-        Payment payment = new Payment(0L, SANJIGI.getId(), Money.ZERO, PAYMENT_COMPLETE);
-
-        assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 수강등록시_결제정보에서_결제금액이_일치하지_않는경우_예외를던진다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new PaidSession(10, Money.wons(1000L)));
-        Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.ZERO, PAYMENT_COMPLETE);
-
-        assertThatThrownBy(() -> session.join(JAVAJIGI, payment))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 수강등록하면_카운트가_증가한다() {
-        Session session = new Session("TDD, 자바 18기", RECRUITING, 0, new PaidSession(10, Money.wons(1000L)));
-        Payment payment = new Payment(0L, JAVAJIGI.getId(), Money.wons(1000L), PAYMENT_COMPLETE);
-        session.join(JAVAJIGI, payment);
-
-        assertThat(session.getCount()).isEqualTo(1);
+            assertThat(session.getCount()).isEqualTo(1);
+        }
     }
 }
