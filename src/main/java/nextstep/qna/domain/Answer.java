96f5a9cd810ebc070a7ae56725dfa6172fef1801
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-03 01:19:52 +0900

    Test: 작성자가 아닌 사람이 답변을 삭제하려고 하면 예외를 던진다

--- src/main/java/nextstep/qna/domain/Answer.java
+++ src/main/java/nextstep/qna/domain/Answer.java
@@ -50,7 +50,7 @@ public class Answer {
 
     public void delete(NsUser user) throws CannotDeleteException {
         if (!isOwner(user)) {
-            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+            throw new CannotDeleteException("이 유저는 답변을 삭제할 권한이 없습니다");
         }
         this.deleted = true;
     }
--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,24 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("작성자가 아닌 사람이 답변을 삭제하려고 하면 예외를 던진다")
+    @Test
+    void isNotOwner() {
+        assertThatThrownBy(() -> A1.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("이 유저는 답변을 삭제할 권한이 없습니다");
+    }
+
+
 }
