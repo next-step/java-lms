4cf921829e87ca797661a8dffe8e286f40e05ef3
최지훈 <boy0516@naver.com>
2024-04-03 23:47:34 +0900

    test: answer 삭제 테스트 추가

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,18 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI,QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(NsUserTest.SANJIGI,QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 삭제_테스트() throws CannotDeleteException {
+        A1.delete();
+        assertThat(A1.isDeleted()).isEqualTo(true);
+    }
 }
