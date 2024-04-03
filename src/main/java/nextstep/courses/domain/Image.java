096a335427799483613dfc2b62c240b2e58c364c
NaayoungKwon <kny8092@gmail.com>
2024-04-04 01:21:57 +0900

    feat: 이미지 생성자 작성

--- src/main/java/nextstep/courses/domain/Image.java
+++ src/main/java/nextstep/courses/domain/Image.java
@@ -1,8 +1,29 @@
 package nextstep.courses.domain;
 
 public class Image {
-    Integer size; // TODO : 1MB 이하
+    Integer size;
     ImageType type;
     Integer width;
-    Integer height; // TODO width:height = 3:2
+    Integer height;
+
+    public Image(Integer size, ImageType type, Integer width, Integer height) {
+        validateSize(size);
+        validateRatio(width, height);
+        this.size = size;
+        this.type = type;
+        this.width = width;
+        this.height = height;
+    }
+
+    private void validateSize(Integer size) {
+        if(size > 1024 * 1024){
+            throw new IllegalArgumentException("이미지 크기는 1MB 이하여야합니다.");
+        }
+    }
+
+    private void validateRatio(Integer width, Integer height){
+        if(width * 2 != height * 3){
+            throw new IllegalArgumentException("가로, 세로 비율을 3:2여야 합니다.");
+        }
+    }
 }
