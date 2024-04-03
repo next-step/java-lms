8687125997e29e4329d79c454b310100d92754da
NaayoungKwon <kny8092@gmail.com>
2024-04-04 01:25:10 +0900

    feat: 강의 생성자 작성

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -9,4 +9,18 @@ public class Session {
     private Image coverImage;
     private SessionPayType sessionPayType;
     private Integer maxStudent; // TODO : free : 제한 없음, paid : 제한 있음
+
+    public Session(Course course, LocalDate startDate, LocalDate endDate, Image coverImage, SessionPayType sessionPayType, Integer maxStudent) {
+        validateSessionDate(startDate, endDate);
+        this.course = course;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.coverImage = coverImage;
+        this.sessionPayType = sessionPayType;
+        this.maxStudent = maxStudent;
+    }
+
+    private void validateSessionDate(LocalDate startDate, LocalDate endDate){
+        throw new IllegalArgumentException("시작일보다 종료일이 먼저올 수 없습니다.");
+    }
 }
