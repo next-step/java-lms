50c2f73f3de25d0222037d2c545d32175942634a
이승호 <seung7642@gmail.com>
2024-04-04 23:02:12 +0900

    docs: TODO 목록 정리

--- src/main/java/nextstep/qna/domain/Answers.java
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -3,6 +3,7 @@ package nextstep.qna.domain;
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +12,7 @@ public class Answers {
     private final List<Answer> answers;
 
     public Answers() {
-        this(List.of());
+        this(new ArrayList<>());
     }
 
     public Answers(List<Answer> answers) {
