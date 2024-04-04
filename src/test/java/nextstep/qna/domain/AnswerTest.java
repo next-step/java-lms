b95baa8b27026e4c42a92c52b1ab7fab96b523b0
이승호 <seung7642@gmail.com>
2024-04-04 21:08:11 +0900

    test: 현재 로그인한 사용자와 답변자가 다르면 예외가 발생한다.

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("현재 로그인한 사용자와 답변자가 다르면 예외가 발생한다.")
+    @Test
+    void test01() {
+        assertThatThrownBy(() -> A1.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변은 삭제할 수 없습니다.");
+    }
 }
