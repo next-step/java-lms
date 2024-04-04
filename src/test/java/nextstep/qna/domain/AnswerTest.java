6ff6c2b207f1d9e7d4ff294d3421293eed33fdb9
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-04 23:32:58 +0900

    TEST : Question, Answer, Answers에 대한 단위 테스트 구현

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,22 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void deleteHistory_생성() {
+        assertThat(A1.createDeleteHistory())
+                .isEqualTo(new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), LocalDateTime.now()));
+        assertThat(A1.isDeleted()).isTrue();
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,29 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void delete_실패_권한_없음() {
+        assertThatThrownBy(
+                () -> Q1.delete(NsUserTest.SANJIGI)
+        ).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    void delete_성공() throws CannotDeleteException {
+        assertThat(Q1.delete(NsUserTest.JAVAJIGI))
+                .containsExactly(new DeleteHistory(ContentType.QUESTION, Q1.getId(), Q1.getWriter(), LocalDateTime.now()));
+    }
+
 }
