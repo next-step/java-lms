20033a2609523ef081f8185ded7f9aa5877fb1f7
gutenLee <3bnm1128@naver.com>
2024-04-05 00:45:39 +0900

    refactor : Attendees 예외 추가

--- src/main/java/nextstep/courses/domain/Attendees.java
+++ src/main/java/nextstep/courses/domain/Attendees.java
@@ -2,18 +2,21 @@ package nextstep.courses.domain;
 
 import nextstep.users.domain.NsUser;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Attendees {
 
-    private List<NsUser> attendees;
+    private Set<NsUser> attendees;
 
     public Attendees() {
-        this.attendees = new ArrayList<>();
+        this.attendees = new HashSet<>();
     }
 
     public void add(NsUser user) {
+        if (attendees.contains(user)) {
+            throw new IllegalStateException("이미 등록된 유저 입니다");
+        }
         attendees.add(user);
     }
 }
