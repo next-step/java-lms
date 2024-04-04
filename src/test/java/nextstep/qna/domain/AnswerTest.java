3a613501a2c3e58f9480b57bf10d252e3e9dd950
leeedohyun <ldh19738@gmail.com>
2024-04-04 12:43:02 +0900

    test: 답변을 삭제한 후 deleted 상태 검증 추가

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -4,19 +4,22 @@ import static nextstep.users.domain.NsUserTest.JAVAJIGI;
 import static nextstep.users.domain.NsUserTest.SANJIGI;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.Test;
 
 import nextstep.qna.CannotDeleteException;
-import nextstep.users.domain.NsUserTest;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A1 = new Answer(JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
     @Test
-    void 답변을_삭제한다() throws CannotDeleteException {
-        assertThat(A1.delete(JAVAJIGI)).isEqualTo(new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), A1.getCreatedDate()));
+    void 답변을_삭제한다() {
+        assertAll(
+                () -> assertThat(A1.delete(JAVAJIGI)).isEqualTo(new DeleteHistory(ContentType.ANSWER, A1.getId(), A1.getWriter(), A1.getCreatedDate())),
+                () -> assertThat(A1.isDeleted()).isTrue()
+        );
     }
 
     @Test
