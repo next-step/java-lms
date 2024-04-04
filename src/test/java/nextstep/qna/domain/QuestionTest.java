0232201055520a7e55e4f16feebb4cbd42329810
이승호 <seung7642@gmail.com>
2024-04-04 21:16:43 +0900

    test: 현재 로그인한 사용자와 질문자가 다르면 예외가 발생한다.

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("현재 로그인한 사용자와 질문자가 다르면 예외가 발생한다.")
+    @Test
+    void test01() {
+        assertThatThrownBy(() -> Q1.deleteBy(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessage("현재 로그인 계정과 질문자가 다릅니다.");
+    }
 }
