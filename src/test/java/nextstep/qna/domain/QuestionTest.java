84fc5d2fd8cc92badb8589423878e24d3429fe6a
이승호 <seung7642@gmail.com>
2024-04-04 21:30:34 +0900

    test: 현재 로그인 계정과 질문 작성자가 같다면 질문을 삭제한다.

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -5,17 +5,25 @@ import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
 
-    @DisplayName("현재 로그인한 사용자와 질문자가 다르면 예외가 발생한다.")
+    @DisplayName("현재 로그인 계정과 질문 작성자가 다르면 예외가 발생한다.")
     @Test
     void test01() {
         assertThatThrownBy(() -> Q1.deleteBy(NsUserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("현재 로그인 계정과 질문자가 다릅니다.");
+    }
+
+    @DisplayName("현재 로그인 계정과 질문 작성자가 같다면 질문을 삭제한다.")
+    @Test
+    void test02() throws CannotDeleteException {
+        Q1.deleteBy(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
     }
 }
