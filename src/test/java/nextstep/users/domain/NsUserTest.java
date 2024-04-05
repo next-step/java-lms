237765521f68711913554accbae5831a2a2f71f2
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-05 20:40:51 +0900

    Test: 강의의 상태가 모집중인지 검증한다

--- src/test/java/nextstep/users/domain/NsUserTest.java
+++ src/test/java/nextstep/users/domain/NsUserTest.java
@@ -3,11 +3,13 @@ package nextstep.users.domain;
 import nextstep.courses.domain.Session;
 import nextstep.courses.domain.image.Image;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static nextstep.courses.domain.Status.RECRUITING;
 import static nextstep.courses.domain.UsageType.PAY;
 import static nextstep.courses.domain.image.Type.GIF;
+import static org.assertj.core.api.Assertions.*;
 
 public class NsUserTest {
     public static final NsUser JAVAJIGI = new NsUser(1L, "javajigi", "password", "name", "javajigi@slipp.net");
@@ -17,6 +19,13 @@ public class NsUserTest {
     void 수강신청() {
         Session tddCleanCode = new Session(1L, "tdd클린코드",10000, new Image(5, GIF, 300, 200), RECRUITING, PAY);
         JAVAJIGI.register(tddCleanCode);
-        Assertions.assertThat(JAVAJIGI.numberOfSession()).isEqualTo(1);
+        assertThat(JAVAJIGI.numberOfSession()).isEqualTo(1);
+    }
+
+    @DisplayName("강의의 상태가 모집중인지 검증한다")
+    @Test
+    void isRecruiting() {
+        Session tddCleanCode = new Session(1L, "tdd클린코드",10000, new Image(5, GIF, 300, 200), RECRUITING, PAY);
+        assertThat(tddCleanCode.isRecruiting()).isTrue();
     }
 }
