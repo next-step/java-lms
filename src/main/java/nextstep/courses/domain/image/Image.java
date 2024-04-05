53fb6bcafc4775b0617d2681e6bdb8cd1e2e63e3
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-05 18:18:01 +0900

    Test: 이미지 width가 300픽셀미만이면 예외를 반환한다

--- src/main/java/nextstep/courses/domain/image/Image.java
+++ src/main/java/nextstep/courses/domain/image/Image.java
@@ -3,12 +3,18 @@ package nextstep.courses.domain.image;
 public class Image {
     private final int capacity;
     private final Type type;
+    private final int width;
 
-    public Image(int capacity, Type type) {
-        if (capacity > 1) {
-            throw new IllegalArgumentException("이미지크기는 1MB를 초과할 수 업다");
+    public Image(int capacity, Type type, int width) {
+        if (capacity < 1) {
+            throw new IllegalArgumentException("이미지크기는 1MB를 초과할 수 없다");
+        }
+
+        if (width < 300) {
+            throw new IllegalArgumentException("이미지 width는 300픽셀이상이어야 한다");
         }
         this.capacity = capacity;
         this.type = type;
+        this.width = width;
     }
 }
--- src/test/java/nextstep/courses/domain/ImageTest.java
+++ src/test/java/nextstep/courses/domain/ImageTest.java
@@ -2,17 +2,27 @@ package nextstep.courses.domain;
 
 import nextstep.courses.domain.image.Image;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static nextstep.courses.domain.image.Type.GIF;
 
 public class ImageTest {
 
+    @DisplayName("이미지 크기가 1MB를 초과하면 예외를 반환한다")
     @Test
     void capacityException() {
-        Assertions.assertThatThrownBy(() -> new Image(5, GIF))
+        Assertions.assertThatThrownBy(() -> new Image(0, GIF, 300))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이미지크기는 1MB를 초과할 수 업다");
+                .hasMessage("이미지크기는 1MB를 초과할 수 없다");
+    }
+
+    @DisplayName("이미지 width가 300픽셀미만이면 예외를 반환한다")
+    @Test
+    void widthException() {
+        Assertions.assertThatThrownBy(() -> new Image(5, GIF, 200))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미지 width는 300픽셀이상이어야 한다");
     }
 
 
