7a2a5e3873a15d8977b8ba7c7ff375f2dcb6d8bc
이승호 <seung7642@gmail.com>
2024-04-06 23:48:34 +0900

    test: 이미지의 가로 크기가 300px 보다 작거나, 높이가 200px 보다 작으면 예외가 발생한다.

--- src/test/java/nextstep/courses/domain/SessionCoverImageTest.java
+++ src/test/java/nextstep/courses/domain/SessionCoverImageTest.java
@@ -15,4 +15,12 @@ public class SessionCoverImageTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미지 용량은 1MB 이하여야 합니다.");
     }
+
+    @DisplayName("이미지의 가로 크기가 300px 보다 작거나, 높이가 200px 보다 작으면 예외가 발생한다.")
+    @Test
+    void test02() {
+        assertThatThrownBy(() -> new SessionCoverImage(1000, ImageType.GIF, 90, 60))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("가로 크기는 300px 이상, 높이는 200px 이상이어야 합니다.");
+    }
 }
