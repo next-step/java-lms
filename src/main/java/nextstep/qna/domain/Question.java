1cf98a27d0a7416fdb8d66d0e045ba8b5a5c8a51
박소민 <qkrthals0524@naver.com>
2024-04-06 18:56:29 +0900

    질문 객체 내 삭제 메서드 추가 및 로그인 사용자, 글작성자 체크 테스트코드 작성

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,12 +1,13 @@
 package nextstep.qna.domain;
 
-import nextstep.users.domain.NsUser;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 
 public class Question {
+
     private Long id;
 
     private String title;
@@ -72,21 +73,28 @@ public class Question {
         return writer.equals(loginUser);
     }
 
+    public boolean isDeleted() {
+        return deleted;
+    }
+
     public Question setDeleted(boolean deleted) {
         this.deleted = deleted;
         return this;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
     }
 
     public List<Answer> getAnswers() {
         return answers;
     }
 
+    public void delete(NsUser loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+    }
+
     @Override
     public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents
+            + ", writer=" + writer + "]";
     }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,26 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
+
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    //삭제가 가능한 경우
+    //1. 로그인한 사용자와 글작성자가 같은 가?
+    //1-1. if 답변이 없는가? 삭제o
+    //1-2. elseif 질문자, 답변자가 모두 같은가? 삭제0
+
+    @Test
+    public void 로그인한_사용자와_글작성자가_같아야_질문_삭제_가능() {
+        assertThatThrownBy(() -> {
+            Q2.delete(NsUserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class).hasMessageContaining("질문을 삭제할 권한이 없습니다.");
+    }
+
 }
