d93fa7e1df59b9ad665800a3eb13495b38438553
이승호 <seung7642@gmail.com>
2024-04-06 23:05:14 +0900

    test: 이미지 용량이 1MB 보다 크다면 예외가 발생한다.

---
+++ src/test/java/nextstep/courses/domain/SessionCoverImageTest.java
@@ -0,0 +1,18 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SessionCoverImageTest {
+
+    @DisplayName("이미지 용량이 1MB 보다 크다면 예외가 발생한다.")
+    @Test
+    void test01() {
+        int sizeOfBytes = 1_048_577; // 1MB = 1,048,576 byte
+        assertThatThrownBy(() -> new SessionCoverImage(sizeOfBytes, ImageType.GIF, 300, 200))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미지 용량은 1MB 이하여야 합니다.");
+    }
+}
