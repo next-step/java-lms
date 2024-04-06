1133456b58fa5a865f342fe3b9387bb61045c720
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-06 16:25:27 +0900

    Refactor: Image생성자의 if문들을 리팩터링하라

--- src/main/java/nextstep/courses/domain/image/Image.java
+++ src/main/java/nextstep/courses/domain/image/Image.java
@@ -9,30 +9,44 @@ public class Image {
     private final double ratio;
 
     public Image(int capacity, ImageType imageType, int width, int height) {
-        if (capacity < 1) {
-            throw new IllegalArgumentException("이미지크기는 1MB를 초과할 수 없다");
-        }
-
-        if (width < 300) {
-            throw new IllegalArgumentException("이미지 width는 300픽셀이상이어야 한다");
-        }
-
-        if (height < 200) {
-            throw new IllegalArgumentException("이미지 height는 200픽셀이상이어야 한다");
-        }
+        validateCapacity(capacity);
+        validateWidth(width);
+        validateHeight(height);
+        validateRatio(width, height);
 
         this.capacity = capacity;
         this.imageType = imageType;
         this.width = width;
         this.height = height;
-        this.ratio = calculateRatio();
+        this.ratio = calculateRatio(width, height);
     }
 
-    private double calculateRatio() {
-        if (width / height != 1.5) {
+    private void validateCapacity(int capacity) {
+        if (capacity < 1) {
+            throw new IllegalArgumentException("이미지크기는 1MB를 초과할 수 없다");
+        }
+    }
+
+    private void validateWidth(int width) {
+        if (width < 300) {
+            throw new IllegalArgumentException("이미지 width는 300픽셀이상이어야 한다");
+        }
+    }
+
+    private void validateHeight(int height) {
+        if (height < 200) {
+            throw new IllegalArgumentException("이미지 height는 200픽셀이상이어야 한다");
+        }
+    }
+
+    private void validateRatio(int width, int height) {
+        if (width / (double) height != 1.5) {
             throw new IllegalArgumentException("width와 height의 비율은 3:2여야 한다");
         }
-        return width / height;
+    }
+
+    private double calculateRatio(int width, int height) {
+        return width / (double) height;
     }
 
 }
