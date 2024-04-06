289341330ce4676714067702d70836e6eb7cd537
박소민 <qkrthals0524@naver.com>
2024-04-06 22:28:40 +0900

    QnAService deleteQuestion 메서드 수정

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -12,17 +12,10 @@ public class QuestionTest {
 
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
-
-    //삭제가 가능한 경우
-    //1. 로그인한 사용자와 글작성자가 같은 가?
-    //1-1. if 답변이 없는가? 삭제o
-    //1-2. elseif 질문자, 답변자가 모두 같은가? 삭제0
-
-    //그외 요구사항
-    //answers 변수를 일급컬렉션으로 만들어본다
+    
 
     @Test
-    public void 로그인한_사용자와_글작성자가_같아야_질문_삭제_가능() {
+    public void 로그인한_사용자와_글작성자가_다른경우_질문_삭제_불가능() {
         assertThatThrownBy(() -> {
             Q2.delete(NsUserTest.JAVAJIGI);
         }).isInstanceOf(CannotDeleteException.class).hasMessageContaining("질문을 삭제할 권한이 없습니다.");
