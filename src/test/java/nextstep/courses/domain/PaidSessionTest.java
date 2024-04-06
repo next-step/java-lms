1852189921dada913802fbcfc8f6fbb2837a3562
이승호 <seung7642@gmail.com>
2024-04-07 00:51:25 +0900

    test: 수강 신청은 강의 상태가 모집 중일 때만 가능하다.

--- src/test/java/nextstep/courses/domain/PaidSessionTest.java
+++ src/test/java/nextstep/courses/domain/PaidSessionTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PaidSessionTest {
@@ -18,5 +19,13 @@ public class PaidSessionTest {
         assertThatThrownBy(() -> session.register(List.of(NsUserTest.JAVAJIGI)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("최대 수강인원을 초과하였습니다.");
+    }
+
+    @DisplayName("수강 신청은 강의 상태가 모집 중일 때만 가능하다.")
+    @Test
+    void test02() {
+        Session session = new PaidSession(1, LocalDateTime.now(), LocalDateTime.now().plusDays(1));
+        session.changeStatus(SessionStatus.RECRUITING);
+        assertThatCode(() -> session.register(List.of(NsUserTest.JAVAJIGI))).doesNotThrowAnyException();
     }
 }
