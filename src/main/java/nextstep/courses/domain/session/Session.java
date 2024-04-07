a00e6cc121908f0096d4e03ce3e5809c9ec381cd
hellonayeon <hellonykwon@gmail.com>
2024-04-07 17:22:56 +0900

    Feature: Session 도메인 내부에 Status, Conditions 합성 관계 추가

--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -1,19 +1,21 @@
 package nextstep.courses.domain.session;
 
+import nextstep.courses.exception.SessionException;
+
 public class Session {
 
     private final SessionCapacity capacity;
     private final SessionCoverImage coverImage;
     private final Long fee;
+    private final SessionStatus sessionStatus;
+    private final EnrollmentConditions enrollmentConditions;
 
-    public Session(SessionCapacity capacity, Long fee) {
-        this(capacity, null, fee);
-    }
-
-    public Session(SessionCapacity capacity, SessionCoverImage coverImage, Long fee) {
+    public Session(SessionCapacity capacity, SessionCoverImage coverImage, Long fee, SessionStatus sessionStatus, EnrollmentConditions enrollmentConditions) {
         this.capacity = capacity;
         this.coverImage = coverImage;
         this.fee = fee;
+        this.sessionStatus = sessionStatus;
+        this.enrollmentConditions = enrollmentConditions;
     }
 
     public boolean hasCapacity() {
