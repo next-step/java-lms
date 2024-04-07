c4a02d571cd41abed557e2174b07ec0097b9088b
최지훈 <boy0516@naver.com>
2024-04-07 23:28:49 +0900

    test: 이미지 비율 테스트 코드

---
+++ src/test/java/nextstep/courses/domain/ImageRateTest.java
@@ -0,0 +1,18 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class ImageRateTest {
+    @DisplayName("이미지 비율 검증")
+    @Test
+    void 이미지_비율_검증() {
+        ImageRate imageRate = new ImageRate(300, 200);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new ImageRate(300, 100))
+            .withMessage("이미지 width, height 비율이 3:2를 만족하지 않습니다: 3:1");
+    }
+}
