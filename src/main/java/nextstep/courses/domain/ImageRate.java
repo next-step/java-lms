ab2f1d374a746f0d966101724d420cd4de0acacc
최지훈 <boy0516@naver.com>
2024-04-08 00:06:13 +0900

    feat: 이미지 가로 픽셀 검증 추가

--- src/main/java/nextstep/courses/domain/ImageRate.java
+++ src/main/java/nextstep/courses/domain/ImageRate.java
@@ -2,11 +2,13 @@ package nextstep.courses.domain;
 
 public class ImageRate {
 
+    private final int MIN_WIDTH_SIZE = 300;
     public int width;
     public int height;
 
     public ImageRate(int width, int height) {
         validRate(width, height);
+        validPixelSize(width);
         this.width = width;
         this.height = height;
     }
@@ -16,6 +18,13 @@ public class ImageRate {
             throw new IllegalArgumentException("이미지 width, height 비율이 3:2를 만족하지 않습니다");
         }
     }
+
+    private void validPixelSize(int width) {
+        if (width < MIN_WIDTH_SIZE) {
+            throw new IllegalArgumentException("이미지 width가 300px 보다 작습니다");
+        }
+    }
+
 
 
 }
