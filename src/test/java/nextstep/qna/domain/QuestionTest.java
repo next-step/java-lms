41af4ed494102b20e28d205edbb8a8c809a8f56d
kylekim2123 <kyle2123@naver.com>
2024-04-07 19:49:48 +0900

    [STEP 1] test: Question 객체의 삭제 기능에 대한 정상 테스트 코드 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,35 @@
 package nextstep.qna.domain;
 
-import nextstep.users.domain.NsUserTest;
+import static nextstep.qna.domain.ContentType.ANSWER;
+import static nextstep.qna.domain.ContentType.QUESTION;
+import static nextstep.users.domain.NsUserTest.JAVAJIGI;
+import static nextstep.users.domain.NsUserTest.SANJIGI;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    public static final Question Q1 = new Question(JAVAJIGI, "title1", "contents1");
+    public static final Question Q2 = new Question(SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("모든 답변을 포함한 질문을 삭제하고, 삭제 이력을 남긴다.")
+    void deleteBy_AllQuestionAndAnswers_DeleteHistories() {
+        final Question question = new Question(1L, JAVAJIGI, "title", "contents");
+        final Answer answer = new Answer(10L, JAVAJIGI, Q1, "contents");
+        question.addAnswer(answer);
+
+        assertThat(question.isDeleted()).isFalse();
+
+        final LocalDateTime deleteDateTime = LocalDateTime.of(2024, 4, 1, 0, 0);
+
+        assertThat(question.deleteBy(JAVAJIGI, deleteDateTime)).containsExactly(
+                new DeleteHistory(QUESTION, question.getId(), question.getWriter(), deleteDateTime),
+                new DeleteHistory(ANSWER, answer.getId(), answer.getWriter(), deleteDateTime));
+        assertThat(question.isDeleted()).isTrue();
+    }
 }
