3435ffa47a6ef32e22aaafbb50795fe4fa2eaa5c
DongWooE <ehddn977@naver.com>
2024-04-07 23:21:01 +0900

    feat : Session에 수강신청 기능 추가

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,13 +1,23 @@
 package nextstep.courses.domain;
 
+import nextstep.payments.domain.Payment;
+
 public class Session {
 
     private final CoverImage coverImage;
     private final EnrollmentManager enrollmentManager;
     private final SessionPeriod sessionPeriod;
+
     public Session(CoverImage coverImage, EnrollmentManager enrollmentManager, SessionPeriod sessionPeriod) {
         this.coverImage = coverImage;
         this.enrollmentManager = enrollmentManager;
         this.sessionPeriod = sessionPeriod;
+    }
+
+    public Session enroll(Payment payment){
+        if(this.enrollmentManager.canEnroll(payment)){
+            return new Session(this.coverImage, this.enrollmentManager.decreaseCount(), this.sessionPeriod);
+        }
+        throw new IllegalArgumentException("수강 신청을 할 수 없습니다.");
     }
 }
