07fc9a9210dee43040b972d0c86357ba36ecc3f1
park-cheonho <cheonho08@naver.com>
2024-04-08 01:24:59 +0900

    test: 도메인 객체 테스트 코드 구현
    test: 도메인 객체 테스트 코드 구현

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,20 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    @Test
+    @DisplayName("질문 상태값 삭제로 변경")
+    void changeQuestionStatusDeleteTest() throws CannotDeleteException {
+        Q1.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
 }
