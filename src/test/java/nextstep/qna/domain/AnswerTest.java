df80853e47a746491908b21ba727ccf2536968d9
hong yuseok <jericoh@naver.com>
2024-04-08 00:55:30 +0900

    refactor: 테스트코드 리팩토링

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -3,7 +3,6 @@ package nextstep.qna.domain;
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -21,18 +20,11 @@ public class AnswerTest {
     @Nested
     @DisplayName("deleteByUser() 테스트")
     class DeleteByUserTest {
-        Answer answer;
-
-        @BeforeEach
-        void initAnswerOfDeleteByUserTest() {
-            answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents");
-        }
-
         @Test
         @DisplayName("user와 Answer.writer가 일치하지 않는 경우 CannotDeleteException이 발생한다.")
         void testFailCase() {
             NsUser user = NsUserTest.SANJIGI;
-            assertThatThrownBy(() -> answer.deleteByUser(user))
+            assertThatThrownBy(() -> answerByJAVAJIGI.deleteByUser(user))
                     .isExactlyInstanceOf(CannotDeleteException.class)
                     .hasMessage(NO_AUTHORITY_TO_DELETE_ANSWER.message());
         }
@@ -41,6 +33,7 @@ public class AnswerTest {
         @DisplayName("삭제가 진행되는 경우 Answer.deleted가 true로 변하며 DeleteHistory를 반환한다.")
         void testSuccessCase() throws CannotDeleteException {
             NsUser user = NsUserTest.JAVAJIGI;
+            Answer answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents");
             DeleteHistory expectedDeleteHistory = new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now());
 
             DeleteHistory deleteHistory = answer.deleteByUser(user);
