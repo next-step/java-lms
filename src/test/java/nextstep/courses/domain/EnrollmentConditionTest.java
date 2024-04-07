fe329f786187a6cb60c6565d488494a1a62ecfd5
hellonayeon <hellonykwon@gmail.com>
2024-04-07 17:20:20 +0900

    Test: Payment 인스턴스 Fixture 추가, SessionCapacity 클래스 변수 제거 (현재 인원 공유하는 이슈)

--- src/test/java/nextstep/courses/domain/EnrollmentConditionTest.java
+++ src/test/java/nextstep/courses/domain/EnrollmentConditionTest.java
@@ -3,10 +3,10 @@ package nextstep.courses.domain;
 import nextstep.courses.domain.session.*;
 import nextstep.courses.exception.ExceedSessionCapacityException;
 import nextstep.courses.exception.MismatchSessionFeeException;
-import nextstep.payments.domain.Payment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.courses.domain.fixture.SessionFixture.payment;
 import static nextstep.courses.domain.fixture.SessionFixture.session;
 import static nextstep.courses.exception.SessionExceptionMessage.CAPACITY_EXCEED;
 import static nextstep.courses.exception.SessionExceptionMessage.PAYMENT_MISMATCH;
@@ -43,17 +43,15 @@ public class EnrollmentConditionTest {
     @Test
     @DisplayName("[성공] 수강생이 결제한 금액과 수강료가 일치할 때 수강 신청이 가능하다.")
     void 강의_수강료() {
-        Payment payment = new Payment("paymentId", 0L, 0L, 800_000L);
         assertThatNoException()
-                .isThrownBy(() -> (new FeeCondition(payment)).isSatisfied(session()));
+                .isThrownBy(() -> (new FeeCondition(payment())).isSatisfied(session()));
     }
 
     @Test
     @DisplayName("[실패] 강의의 결제 금액과 수강료가 일치하지 않으면 MismatchSessionFeeException 예외가 발생한다.")
     void 강의_수강료_불일치() {
-        Payment payment = new Payment("paymentId", 0L, 0L, 100_000L);
         assertThatExceptionOfType(MismatchSessionFeeException.class)
-                .isThrownBy(() -> (new FeeCondition(payment)).isSatisfied(session()))
+                .isThrownBy(() -> (new FeeCondition(payment(100_000L))).isSatisfied(session()))
                 .withMessageContaining(PAYMENT_MISMATCH.getMessage());
     }
 
--- src/test/java/nextstep/courses/domain/fixture/SessionFixture.java
+++ src/test/java/nextstep/courses/domain/fixture/SessionFixture.java
@@ -1,28 +1,42 @@
 package nextstep.courses.domain.fixture;
 
-import nextstep.courses.domain.session.EnrollmentConditions;
-import nextstep.courses.domain.session.Session;
-import nextstep.courses.domain.session.SessionCapacity;
-import nextstep.courses.domain.session.SessionCoverImage;
+import nextstep.courses.domain.session.*;
 import nextstep.courses.exception.SessionException;
+import nextstep.payments.domain.Payment;
+
+import static nextstep.courses.domain.session.SessionStatus.*;
 
 public class SessionFixture {
 
     private static final Long FEE = 800_000L;
+    private static final String PAYMENT_ID = "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed";
 
     public static SessionCoverImage coverImage() throws SessionException {
         return new SessionCoverImage(1024*1024, 300, 200, "gif");
     }
 
     public static Session session() throws SessionException {
-        return new Session(new SessionCapacity(100), coverImage(), FEE, new EnrollmentConditions());
+        return new Session(new SessionCapacity(100), coverImage(), FEE, RECRUITING, new EnrollmentConditions());
     }
 
     public static Session session(SessionCapacity capacity) throws SessionException {
-        return new Session(capacity, coverImage(), FEE, new EnrollmentConditions());
+        return new Session(capacity, coverImage(), FEE, RECRUITING, new EnrollmentConditions());
+    }
+
+    public static Session session(EnrollmentConditions enrollmentConditions) throws SessionException {
+        return new Session(new SessionCapacity(100), coverImage(), FEE, RECRUITING, enrollmentConditions);
     }
 
     public static Session session(SessionCapacity capacity, Long fee, EnrollmentConditions enrollmentConditions) throws SessionException {
-        return new Session(capacity, coverImage(), fee, enrollmentConditions);
+        return new Session(capacity, coverImage(), fee, RECRUITING, enrollmentConditions);
     }
+
+    public static Payment payment() {
+        return new Payment(PAYMENT_ID, 0L, 0L, FEE);
+    }
+
+    public static Payment payment(long amount) {
+        return new Payment(PAYMENT_ID, 0L, 0L, amount);
+    }
+
 }
