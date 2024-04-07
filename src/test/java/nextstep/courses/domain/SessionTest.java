83fc5b292074b9ade9c18e8870c14fb836c91999
최지훈 <boy0516@naver.com>
2024-04-08 00:47:03 +0900

    test: Session 수강신청 테스트 수정

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -2,7 +2,6 @@ package nextstep.courses.domain;
 
 import java.util.List;
 
-import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,13 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SessionTest {
 
-    public static final Session L1 = new Session(List.of(NsUserTest.JAVAJIGI));
+    public static final Session L1 = new Session(List.of(JoinUserTest.JAVAJIGI));
 
     @DisplayName("수강신청")
     @Test
     void 수강신청_기본기능() {
         Session session = new Session();
-        session.join(NsUserTest.JAVAJIGI);
+        session.join(JoinUserTest.JAVAJIGI);
         assertThat(session).isEqualTo(L1);
     }
 
