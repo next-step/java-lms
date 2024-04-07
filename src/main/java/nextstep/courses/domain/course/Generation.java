50ae1b5e2176222b8135b14702516d02c65a38ec
yeeji-kwon <yeji.kwon@myrealtrip.com>
2024-04-07 20:52:55 +0900

    feature: 강의 기수(Generation) 도메인 테스트 및 로직 구현

---
+++ src/main/java/nextstep/courses/domain/course/Generation.java
@@ -0,0 +1,24 @@
+package nextstep.courses.domain.course;
+
+public class Generation {
+
+  public static final String NEGATIVE_NUMBER_OR_ZERO_IS_NOT_ALLOWED = "기수 값은 0이나 음수는 허용되지 않습니다. input: %s";
+
+  private Long id;
+
+  public Generation(Long id) {
+    validate(id);
+    this.id = id;
+  }
+
+  public boolean isSame(Long id) {
+    return this.id.equals(id);
+  }
+
+  private boolean validate(Long number) {
+    if (number <= 0) {
+      throw new IllegalArgumentException(String.format(NEGATIVE_NUMBER_OR_ZERO_IS_NOT_ALLOWED, number));
+    }
+    return true;
+  }
+}
