14da1ad3c02bd38b5b289a2e584bd035dce40569
이승호 <seung7642@gmail.com>
2024-04-07 16:52:01 +0900

    test: 서로 다른 기수라면 동일 강의를 추가할 수 있다.

--- src/test/java/nextstep/courses/domain/CourseTest.java
+++ src/test/java/nextstep/courses/domain/CourseTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CourseTest {
@@ -15,9 +16,20 @@ public class CourseTest {
         Course course = new Course("TDD", 1L);
         Session session = new FreeSession(LocalDateTime.now(), LocalDateTime.now().plusDays(1));
         assertThatThrownBy(() -> {
-            course.addSession(session);
-            course.addSession(session);
+            course.addSession(1, session);
+            course.addSession(1, session);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미 동일한 강의가 있습니다.");
+    }
+
+    @DisplayName("서로 다른 기수라면 동일 강의를 추가할 수 있다.")
+    @Test
+    void test02() {
+        Course course = new Course("TDD", 1L);
+        Session session = new FreeSession(LocalDateTime.now(), LocalDateTime.now().plusDays(1));
+        assertThatCode(() -> {
+            course.addSession(1, session);
+            course.addSession(2, session);
+        }).doesNotThrowAnyException();
     }
 }
