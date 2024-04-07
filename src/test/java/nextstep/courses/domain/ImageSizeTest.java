eb7176fb64ec08e4f59c3c5a99676650aaddc73f
최지훈 <boy0516@naver.com>
2024-04-07 23:37:25 +0900

    test: 이미지 사이즈 검증 테스트

---
+++ src/test/java/nextstep/courses/domain/ImageSizeTest.java
@@ -0,0 +1,16 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class ImageSizeTest {
+    @DisplayName("이미지 1MB 초과 검증")
+    @Test
+    void 이미지_용량_검증() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new ImageSize(2))
+            .withMessage("이미지 사이즈가 1MB를 초과했습니다: 2MB");
+    }
+}
