5f292f700530764fedced5a86d517c205025afa2
이승호 <seung7642@gmail.com>
2024-04-07 15:42:21 +0900

    feat: 수강생이 결제한 금액과 수강료가 일치하지 않는 경우 예외가 발생한다.

--- src/main/java/nextstep/courses/domain/PaidSession.java
+++ src/main/java/nextstep/courses/domain/PaidSession.java
@@ -1,5 +1,6 @@
 package nextstep.courses.domain;
 
+import nextstep.payments.domain.Payment;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -7,17 +8,43 @@ import java.util.List;
 
 public class PaidSession extends Session {
 
-    public PaidSession(int maximumNumberOfStudent, LocalDateTime startedAt, LocalDateTime endedAt) {
+    private final int amount;
+
+    public PaidSession(int maximumNumberOfStudent, int amount, LocalDateTime startedAt, LocalDateTime endedAt) {
         super(maximumNumberOfStudent, startedAt, endedAt);
+        this.amount = amount;
     }
 
     @Override
-    public void register(List<NsUser> users) {
-        validateLessEqualThenMaximumNumber(users);
+    public void registers(List<NsUser> users) {
+        users.forEach(this::register);
     }
 
-    private void validateLessEqualThenMaximumNumber(List<NsUser> users) {
-        if (students.size() + users.size() > maximumNumberOfStudent) {
+    @Override
+    public void register(NsUser user) {
+        this.register(user, Payment.findByUserId(user.getId()));
+    }
+
+    public void register(NsUser user, Payment payment) {
+        validateLessEqualThenMaximumNumber();
+        validateRecruiting();
+        validateSameAmount(payment);
+    }
+
+    private void validateSameAmount(Payment payment) {
+        if (!payment.isSameAmount(amount)) {
+            throw new IllegalArgumentException("결제 금액과 수강료가 일치하지 않습니다.");
+        }
+    }
+
+    private void validateRecruiting() {
+        if (!SessionStatus.isRecruiting(status)) {
+            throw new IllegalArgumentException("현재 모집 중인 강의가 아닙니다.");
+        }
+    }
+
+    private void validateLessEqualThenMaximumNumber() {
+        if (students.size() >= maximumNumberOfStudent) {
             throw new IllegalArgumentException("최대 수강인원을 초과하였습니다.");
         }
     }
