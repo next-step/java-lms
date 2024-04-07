c8431010572d686b186ba57bfd562c8d1dcfb5df
DongWooE <ehddn977@naver.com>
2024-04-07 20:52:46 +0900

    feat : 강의 상태 enum 추가 및 상태값에 따라서 강의수강신청 여부 기능 추가

---
+++ src/main/java/nextstep/qna/domain/SessionStatus.java
@@ -0,0 +1,21 @@
+package nextstep.qna.domain;
+
+public enum SessionStatus {
+
+    PREPARING("준비중"),
+    RECRUITING("모집중"),
+    CLOSED("종료");
+
+    private final String desc;
+
+    SessionStatus(String desc) {
+        this.desc = desc;
+    }
+
+    public boolean canEnroll() {
+        if(this.equals(RECRUITING)){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/nextstep/qna/domain/SessionStatusTest.java
@@ -0,0 +1,17 @@
+package nextstep.qna.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionStatusTest {
+
+    @Test
+    @DisplayName("강의 수강신청은 강의 상태가 모집중일 때만 가능")
+    void is_recruiting() {
+        assertTrue(SessionStatus.RECRUITING.canEnroll());
+        assertFalse(SessionStatus.CLOSED.canEnroll());
+        assertFalse(SessionStatus.PREPARING.canEnroll());
+    }
+}
