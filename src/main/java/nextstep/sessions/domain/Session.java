e010e243a53660f1bdf42fafd438b76de0c487de
leeedohyun <ldh19738@gmail.com>
2024-04-08 12:59:20 +0900

    feat: 시작일이 지난 강의 수강 신청하는 경우 예외 처리

--- src/main/java/nextstep/sessions/domain/Session.java
+++ src/main/java/nextstep/sessions/domain/Session.java
@@ -1,5 +1,6 @@
 package nextstep.sessions.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,10 @@ public class Session {
 
     private Status status;
 
+    private LocalDateTime startedAt;
+
+    private LocalDateTime endAt;
+
     private List<NsUser> attendees = new ArrayList<>();
 
     public Session(final int maxNumber, final Status status) {
@@ -20,7 +25,19 @@ public class Session {
         this.status = status;
     }
 
+    public Session(final int maxNumber, final Status status, final LocalDateTime endAt) {
+        this(maxNumber, status, LocalDateTime.now(), endAt);
+    }
+
+    public Session(final int maxNumber, final Status status, final LocalDateTime startedAt, final LocalDateTime endAt) {
+        this.maxNumber = maxNumber;
+        this.status = status;
+        this.startedAt = startedAt;
+        this.endAt = endAt;
+    }
+
     public void addAttendee(final NsUser user) {
+        validateStartedAt();
         validateAttendeeNumber();
         validateRecruitingStatus();
         attendees.add(user);
@@ -35,6 +52,13 @@ public class Session {
     private void validateRecruitingStatus() {
         if (status != Status.RECRUITING) {
             throw new IllegalArgumentException("모집중인 강의가 아닙니다.");
+        }
+    }
+
+    private void validateStartedAt() {
+        if (startedAt.isBefore(LocalDateTime.now())) {
+            status = Status.END;
+            throw new IllegalArgumentException("시작일이 지난 강의는 수강 신청할 수 없습니다.");
         }
     }
 
