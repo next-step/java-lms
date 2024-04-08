4fa5886e457f91530c082e603f6d3b0b141ea152
leeedohyun <ldh19738@gmail.com>
2024-04-08 21:46:40 +0900

    test: 이미지 heigth 예외 테스트

--- src/test/java/nextstep/images/domain/CoverImageTest.java
+++ src/test/java/nextstep/images/domain/CoverImageTest.java
@@ -15,4 +15,9 @@ class CoverImageTest {
     void 이미지의_width는_300픽셀_미만이면_업로드_실패한다() {
         assertThatIllegalArgumentException().isThrownBy(() -> new CoverImage(1.0, 299));
     }
+
+    @Test
+    void 이미지의_height는_200픽셀_미만이면_업로드_실패한다() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new CoverImage(1.0, 300, 199));
+    }
 }
