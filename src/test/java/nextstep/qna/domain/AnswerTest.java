d8b491b80f76a392e23d3a6ecf538dfd00f5b26d
jay <ljg29494@gmail.com>
2024-04-08 19:45:44 +0900

    * refactored QnAService deleteQuestion (#448)
    * refactored Question, Answer
        * used first-class collection
        * generated unit TestCodes
      * modified gradle.properties enable ./gradlew clean build
        * add gradle option to enable access to unnamed module

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,31 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("Answer 삭제 성공 테스트")
+    public void deleteAnswerSuccessTest() throws Exception {
+        A1.delete(NsUserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Answer 삭제 실패 테스트")
+    public void deleteAnswerFailTest() {
+        assertThatThrownBy(() -> {
+            A1.delete(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
 }
---
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -0,0 +1,37 @@
+package nextstep.qna.domain;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class AnswersTest {
+
+    @Test
+    @DisplayName("Answers 삭제 성공 테스트")
+    public void deleteAnswersSuccessTest() throws Exception {
+        Answer answer = AnswerTest.A1;
+        Answers answers = new Answers(List.of(answer));
+
+        List<DeleteHistory> deleteHistoryList = answers.delete(NsUserTest.JAVAJIGI);
+
+        assertThat(answer.isDeleted()).isTrue();
+        assertThat(deleteHistoryList).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("Answers 삭제 실패 테스트")
+    public void deleteAnswersFailTest() {
+        Answers answers = new Answers(List.of(AnswerTest.A2));
+
+        assertThatThrownBy(() -> {
+            answers.delete(NsUserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+    }
+}
