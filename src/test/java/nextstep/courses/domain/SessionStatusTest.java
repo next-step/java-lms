a50edbc0cf6660069cf9f96fc25184da86f680fc
hellonayeon <hellonykwon@gmail.com>
2024-04-08 22:20:15 +0900

    Test: (Feedback) SessionStatus 단위 테스트 - 모집 가능, 모집 불가능 테스트케이스 분리

--- src/test/java/nextstep/courses/domain/SessionStatusTest.java
+++ src/test/java/nextstep/courses/domain/SessionStatusTest.java
@@ -4,6 +4,7 @@ import nextstep.courses.domain.session.SessionStatus;
 import nextstep.courses.exception.CannotEnrollmentSessionStatusException;
 import nextstep.courses.exception.SessionExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,14 +16,16 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class SessionStatusTest {
 
+    @Test
+    @DisplayName("[성공] 강의 상태에 따른 모집 가능을 확인한다.")
+    void 강의_상태_모집_가능() {
+        assertThatNoException().isThrownBy(SessionStatus.RECRUITING::validateCanEnrollment);
+    }
+
     @ParameterizedTest
     @MethodSource("sessionStatusFixture")
-    @DisplayName("[성공] 강의 상태에 따른 모집 가능 여부를 확인한다.")
-    void 강의_상태_모집_가능_여부(SessionStatus status, CannotEnrollmentSessionStatusException exception) {
-        if (exception == null) {
-            assertThatNoException().isThrownBy(status::validateCanEnrollment);
-            return;
-        }
+    @DisplayName("[성공] 강의 상태에 따른 모집 불가능을 확인한다.")
+    void 강의_상태_모집_불가능(SessionStatus status, CannotEnrollmentSessionStatusException exception) {
         assertThatExceptionOfType(exception.getClass())
                 .isThrownBy(status::validateCanEnrollment)
                 .withMessageContaining(SessionExceptionMessage.CANNOT_ENROLLMENT_SESSION_STATUS.getMessage());
@@ -31,7 +34,6 @@ public class SessionStatusTest {
     static Stream<Arguments> sessionStatusFixture() {
         return Stream.of(
                 Arguments.of(SessionStatus.PREPARING, new CannotEnrollmentSessionStatusException(SessionStatus.PREPARING)),
-                Arguments.of(SessionStatus.RECRUITING, null),
                 Arguments.of(SessionStatus.FINISHED, new CannotEnrollmentSessionStatusException(SessionStatus.FINISHED))
         );
     }
