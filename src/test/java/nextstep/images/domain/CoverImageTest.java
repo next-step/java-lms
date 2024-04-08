ff250c443c8fc5d0499c17ac266e26d19289e4cb
leeedohyun <ldh19738@gmail.com>
2024-04-08 21:43:41 +0900

    test: 이미지 width 예외 테스트

--- src/test/java/nextstep/images/domain/CoverImageTest.java
+++ src/test/java/nextstep/images/domain/CoverImageTest.java
@@ -10,4 +10,9 @@ class CoverImageTest {
     void 이미지_크기가_1MB를_초과하는_경우_업로드_실패한다() {
         assertThatIllegalArgumentException().isThrownBy(() -> new CoverImage(1.1));
     }
+
+    @Test
+    void 이미지의_width는_300픽셀_미만이면_업로드_실패한다() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new CoverImage(1.0, 299));
+    }
 }
