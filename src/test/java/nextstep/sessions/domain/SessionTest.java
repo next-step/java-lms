5f7971961ed62ddd3113f0d7422cea3f9ccbf63c
leeedohyun <ldh19738@gmail.com>
2024-04-08 11:38:27 +0900

    test: 모집중이 아닌 강의 수강 신청 예외 테스트

--- src/test/java/nextstep/sessions/domain/SessionTest.java
+++ src/test/java/nextstep/sessions/domain/SessionTest.java
@@ -3,6 +3,8 @@ package nextstep.sessions.domain;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
@@ -19,5 +21,15 @@ class SessionTest {
         assertThatIllegalArgumentException().isThrownBy(
                 () -> session.addAttendee(new NsUser(3L, "lee", "password", "name", "lee@slipp.net"))
         ).withMessage("수강 인원이 초과되었습니다.");
+    }
+    
+    @ParameterizedTest
+    @EnumSource(value = Status.class, names = {"PREPARING", "END"})
+    void 모집중이_아닌_강의인_경우_수강_신청을_하면_실패한다(final Status status) {
+        final Session session = new Session(5, status);
+        
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> session.addAttendee(NsUserTest.JAVAJIGI)
+        ).withMessage("모집중인 강의가 아닙니다.");
     }
 }
