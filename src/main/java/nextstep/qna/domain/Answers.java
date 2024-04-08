0427ff0278a0fcdc1cd65778cb6a1e9aeb6d368a
m5s3 <devmyoungsoo2@gmail.com>
2024-04-08 19:47:27 +0900

    feat(Answers): 답변 삭제 기능 구현

--- src/main/java/nextstep/qna/domain/Answers.java
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -2,6 +2,7 @@ package nextstep.qna.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
@@ -25,20 +26,24 @@ public class Answers {
         return new Answers();
     }
 
-
-
-    public List<DeleteHistory> delete(NsUser nsUser) throws CannotDeleteException {
-        int count = (int) answers.stream()
-                .filter(answer -> !answer.isOwner(nsUser))
-                .count();
-
-        if (count > 0) {
+    public List<DeleteHistory> deleteAll(NsUser nsUser) throws CannotDeleteException {
+        if (!isDeletedAllowed(nsUser)) {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
-        
+
+        return answers.stream()
+                .map(Answer::delete)
+                .collect(Collectors.toList());
     }
 
     public void add(Answer answer) {
         answers.add(answer);
+    }
+
+    private boolean isDeletedAllowed(NsUser nsUser) {
+        int count = (int) answers.stream()
+                .filter(answer -> !answer.isOwner(nsUser))
+                .count();
+        return count == 0;
     }
 }
---
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -0,0 +1,48 @@
+package nextstep.qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+class AnswersTest {
+
+    @Test
+    void 모든답변_삭제() throws CannotDeleteException {
+        Question q = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        Answer answer1 = new Answer(NsUserTest.JAVAJIGI, q, "answer 2");
+        Answer answer2 = new Answer(NsUserTest.JAVAJIGI, q, "answer 3");
+        Answer answer3 = new Answer(NsUserTest.JAVAJIGI, q, "answer 1");
+        Answers answers = Answers.from();
+        answers.add(answer1);
+        answers.add(answer2);
+        answers.add(answer3);
+        int expected = 3;
+
+        assertThat(answers.deleteAll(NsUserTest.JAVAJIGI).size()).isEqualTo(expected);
+        assertThat(answer1.isDeleted()).isTrue();
+        assertThat(answer2.isDeleted()).isTrue();
+        assertThat(answer3.isDeleted()).isTrue();
+    }
+
+    @Test
+    void 답변중에_다른사람이_있으면_예외발생() throws CannotDeleteException {
+        Question q = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        Answer answer1 = new Answer(NsUserTest.JAVAJIGI, q, "answer 2");
+        Answer answer2 = new Answer(NsUserTest.JAVAJIGI, q, "answer 3");
+        Answer answer3 = new Answer(NsUserTest.SANJIGI, q, "answer 1");
+        Answers answers = Answers.from();
+        answers.add(answer1);
+        answers.add(answer2);
+        answers.add(answer3);
+
+        assertThat(answer1.isDeleted()).isFalse();
+        assertThat(answer2.isDeleted()).isFalse();
+        assertThat(answer3.isDeleted()).isFalse();
+
+        assertThatThrownBy(() -> answers.deleteAll(NsUserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+}
