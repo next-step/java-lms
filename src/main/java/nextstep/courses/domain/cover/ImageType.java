2b51fb15b78f5f9c9dbe065e7e9bc5357c389ca9
namhyeop <skaguq@gmail.com>
2024-04-10 02:20:20 +0900

    feat : 이미지타입-cache 적용

--- src/main/java/nextstep/courses/domain/cover/ImageType.java
+++ src/main/java/nextstep/courses/domain/cover/ImageType.java
@@ -1,15 +1,29 @@
 package nextstep.courses.domain.cover;
 
+import java.util.EnumMap;
+
 public enum ImageType {
     GIF("gif"),
     JPG("jpg"),
     JPEG("jpeg"),
     PNG("png"),
     SVG("svg");
-
     private String value;
+
+    private static final EnumMap<ImageType, Boolean> imageTypeCache = new EnumMap<>(ImageType.class);
+
+    static {
+        for (ImageType imageType : values()) {
+            imageTypeCache.put(imageType, true);
+        }
+    }
 
     ImageType(String value) {
         this.value = value;
+    }
+
+    public static boolean
+    findAvailableImagesType(ImageType imageType){
+        return imageTypeCache.getOrDefault(imageType, false);
     }
 }
