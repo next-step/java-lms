ad66700b8f0582fc4dfccbfba04ad9e37cbd76b6
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-09 19:16:42 +0900

    CHORE : 변경사항 없음

--- src/test/java/nextstep/courses/infrastructure/CourseRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/CourseRepositoryTest.java
@@ -2,6 +2,7 @@ package nextstep.courses.infrastructure;
 
 import nextstep.courses.domain.Course;
 import nextstep.courses.domain.CourseRepository;
+import nextstep.courses.JdbcCourseRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
