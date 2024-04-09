1ad6e5e329193ca77e5139e9e7e64034c5efd621
namhyeop <skaguq@gmail.com>
2024-04-10 00:09:45 +0900

    feat : 유료강의 상태값 Money 추가

--- src/main/java/nextstep/courses/domain/lecture/impl/PaidCourse.java
+++ src/main/java/nextstep/courses/domain/lecture/impl/PaidCourse.java
@@ -5,12 +5,16 @@ import nextstep.courses.domain.lecture.Lecture;
 import nextstep.courses.domain.lecture.LectureStatus;
 import nextstep.courses.domain.lecture.MaxRegistrationCount;
 import nextstep.courses.domain.lecture.RegistrationCount;
+import nextstep.payments.domain.Money;
+import nextstep.payments.domain.Payment;
 
 public class PaidCourse implements Lecture {
 
     private final RegistrationCount registrationCount;
 
     private final MaxRegistrationCount maxRegistrationCount;
+
+    private final Money tuitionFee;
 
     private final LocalDateTime startDate;
 
@@ -20,9 +24,11 @@ public class PaidCourse implements Lecture {
 
     public PaidCourse(RegistrationCount registrationCount,
         MaxRegistrationCount maxRegistrationCount,
-        LocalDateTime startDate, LocalDateTime endDate, LectureStatus lectureStatus) {
+        Money tuitionFee, LocalDateTime startDate, LocalDateTime endDate,
+        LectureStatus lectureStatus) {
         this.registrationCount = registrationCount;
         this.maxRegistrationCount = maxRegistrationCount;
+        this.tuitionFee = tuitionFee;
         this.startDate = startDate;
         this.endDate = endDate;
         this.lectureStatus = lectureStatus;
@@ -36,5 +42,10 @@ public class PaidCourse implements Lecture {
     public boolean isRegistrationAvailable() {
         return isRecruitmentOpen(lectureStatus)
             && maxRegistrationCount.isMaxRegistrationCountOver(this.registrationCount);
+    }
+
+    @Override
+    public boolean isPaymentAmountSameTuitionFee(Payment payment){
+        return payment.isSamePaymentAmount(tuitionFee);
     }
 }
--- src/test/java/nextstep/courses/domain/lecture/impl/PaidCourseTest.java
+++ src/test/java/nextstep/courses/domain/lecture/impl/PaidCourseTest.java
@@ -9,6 +9,8 @@ import nextstep.courses.domain.lecture.LectureStatus;
 import nextstep.courses.domain.lecture.MaxRegistrationCount;
 import nextstep.courses.domain.lecture.RegistrationCount;
 import nextstep.courses.error.exception.MaxRegistrationCountNotZero;
+import nextstep.payments.domain.Money;
+import nextstep.payments.domain.Payment;
 import org.junit.jupiter.api.Test;
 
 class PaidCourseTest {
@@ -17,43 +19,82 @@ class PaidCourseTest {
     void 강의_최대_수강_인원은_0일_수_없다() {
         assertThatThrownBy(() -> new PaidCourse(new RegistrationCount(2),
             new MaxRegistrationCount(new RegistrationCount(0)),
-            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.RECRUITING))
+            new Money(1000),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.RECRUITING))
             .isInstanceOf(MaxRegistrationCountNotZero.class)
             .hasMessage("최대 등록수는 0일수 없습니다 입력값: 0");
     }
 
     @Test
     void 강의_최대_수강_인원을_넘지_않을_경우_강의_신청이_가능해야한다() {
-        Lecture paidLecture = new PaidCourse(new RegistrationCount(2), new MaxRegistrationCount(new RegistrationCount(3)),
-            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.RECRUITING);
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(2),
+            new MaxRegistrationCount(new RegistrationCount(3)),
+            new Money(1000),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.RECRUITING);
 
         assertThat(paidLecture.isRegistrationAvailable()).isTrue();
     }
 
     @Test
     void 강의_최대_수강_인원을_넘을_경우_강의_신청이_불가능해야한다() {
-        Lecture paidLecture = new PaidCourse(new RegistrationCount(4), new MaxRegistrationCount(new RegistrationCount(3)),
-            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.RECRUITING);
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(4),
+            new MaxRegistrationCount(new RegistrationCount(3)),
+            new Money(1000),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.RECRUITING);
 
         assertThat(paidLecture.isRegistrationAvailable()).isFalse();
     }
 
     @Test
-    void 수강생이_결제한_금액과_수강료가_일치하지_않은_경우_강의_신청이_불가능하다() {
+    void 유료강의는_수강생이_결제한_금액과_수강료가_일치하는_경우_강의_신청이_가능하다() {
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(1),
+            new MaxRegistrationCount(new RegistrationCount(3)),
+            new Money(1000),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.RECRUITING);
+
+        assertThat(paidLecture.isPaymentAmountSameTuitionFee(new Payment("1",1L,1L,new Money(1000)))).isTrue();
+    }
+
+    @Test
+    void 유료강의는_수강생이_결제한_금액과_수강료가_일치하지_않은_경우_강의_신청이_불가능하다() {
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(1),
+            new MaxRegistrationCount(new RegistrationCount(3)),
+            new Money(1000),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.RECRUITING);
+
+        assertThat(paidLecture.isPaymentAmountSameTuitionFee(new Payment("1",1L,1L,new Money(500)))).isFalse();
     }
 
     @Test
     void 유료_강의_수강신청시_강의_상태가_모집중이라면_수강_신청이_가능해야한다() {
-        Lecture paidLecture = new PaidCourse(new RegistrationCount(1), new MaxRegistrationCount(new RegistrationCount(3)),
-            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.RECRUITING);
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(1),
+            new MaxRegistrationCount(new RegistrationCount(3)),
+            new Money(1000),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.RECRUITING);
 
         assertThat(paidLecture.isRegistrationAvailable()).isTrue();
     }
 
     @Test
     void 유료_강의_수강신청시_강의_상태가_모집중이_아니라면_수강_신청이_불가능해야한다() {
-        Lecture paidLecture = new PaidCourse(new RegistrationCount(1), new MaxRegistrationCount(new RegistrationCount(3)),
-            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.PREPARING);
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(1),
+            new MaxRegistrationCount(new RegistrationCount(3)),
+            new Money(1000),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.PREPARING);
 
         assertThat(paidLecture.isRegistrationAvailable()).isFalse();
     }
