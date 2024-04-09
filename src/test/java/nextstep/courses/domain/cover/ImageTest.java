c75809448d0fb9b7206d92ddd351b7fa645a5703
namhyeop <skaguq@gmail.com>
2024-04-09 22:06:35 +0900

    feat : 이미지 객체 테스트 코드 리스트 추가

---
+++ src/test/java/nextstep/courses/domain/cover/ImageTest.java
@@ -0,0 +1,38 @@
+package nextstep.courses.domain.cover;
+
+import org.junit.jupiter.api.Test;
+
+class ImageTest {
+
+    @Test
+    void 이미지의_크기는_1MB만_가능하다() {
+    }
+
+    @Test
+    void 이미지의_크기가_1MB보다_작거나_클_경우_예외가_발생한다() {
+    }
+
+    @Test
+    void 이미지의_크기가_null_또는_빈문자열일_경우_예외가_발생한다() {
+    }
+
+    @Test
+    void width는_300픽셀_height는_200픽셀_이상이_아닐_경우_예외가_발생해야_한다() {
+    }
+
+    @Test
+    void 이미지의_width는_300픽셀_height는_200픽셀_이어야한다() {
+    }
+
+    @Test
+    void width와_height의_비율이_3_2가_아닐_경우_예외가_발생한다() {
+    }
+
+    @Test
+    void width와_height의_비율이_3_2가_여야_한다() {
+    }
+
+    @Test
+    void width와_height는_null_또는_빈_문자열일_수_없다() {
+    }
+}
