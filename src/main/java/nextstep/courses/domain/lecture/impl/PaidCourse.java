10f6679ae024ca779991529fe77b9ec544bcb37f
namhyeop <skaguq@gmail.com>
2024-04-09 23:17:30 +0900

    feat : 유료강의 강의 등록 가능여부 메소드 구현

--- src/main/java/nextstep/courses/domain/lecture/impl/PaidCourse.java
+++ src/main/java/nextstep/courses/domain/lecture/impl/PaidCourse.java
@@ -1,11 +1,16 @@
 package nextstep.courses.domain.lecture.impl;
 
 import java.time.LocalDateTime;
+import nextstep.courses.domain.lecture.Lecture;
 import nextstep.courses.domain.lecture.LectureStatus;
+import nextstep.courses.domain.lecture.MaxRegistrationCount;
+import nextstep.courses.domain.lecture.RegistrationCount;
 
-public class PaidCourse {
+public class PaidCourse implements Lecture {
 
-    private final int maxEnrollment;
+    private final RegistrationCount registrationCount;
+
+    private final MaxRegistrationCount maxRegistrationCount;
 
     private final LocalDateTime startDate;
 
@@ -13,15 +18,23 @@ public class PaidCourse {
 
     private LectureStatus lectureStatus;
 
-    public PaidCourse(int maxEnrollment, LocalDateTime startDate, LocalDateTime endDate,
-        LectureStatus lectureStatus) {
-        this.maxEnrollment = maxEnrollment;
+    public PaidCourse(RegistrationCount registrationCount,
+        MaxRegistrationCount maxRegistrationCount,
+        LocalDateTime startDate, LocalDateTime endDate, LectureStatus lectureStatus) {
+        this.registrationCount = registrationCount;
+        this.maxRegistrationCount = maxRegistrationCount;
         this.startDate = startDate;
         this.endDate = endDate;
         this.lectureStatus = lectureStatus;
     }
 
-    public boolean isEnrollCourse(){
+    public boolean isEnrollCourse() {
         return true;
+    }
+
+    @Override
+    public boolean isRegistrationAvailable() {
+        return isRecruitmentOpen(lectureStatus)
+            && maxRegistrationCount.isMaxRegistrationCountOver(this.registrationCount);
     }
 }
