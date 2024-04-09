2aff1fb63ac72fc326bee00351fc5501fd1b13bf
namhyeop <skaguq@gmail.com>
2024-04-09 22:05:21 +0900

    feat : 유료강의 테스트 코드 리스트 추가

---
+++ src/test/java/nextstep/courses/domain/lecture/impl/PaidCourseTest.java
@@ -0,0 +1,18 @@
+package nextstep.courses.domain.lecture.impl;
+
+import org.junit.jupiter.api.Test;
+
+class PaidCourseTest {
+
+    @Test
+    void 강의_최대_수강_인원은_0일_수_없다() {
+    }
+
+    @Test
+    void 강의_최대_수강_인원을_넘을_경우_강의_신청이_불가능해야한다() {
+    }
+
+    @Test
+    void 수강생이_결제한_금액과_수강료가_일치하지_않은_경우_강의_신청이_불가능하다() {
+    }
+}
