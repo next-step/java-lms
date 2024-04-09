73e003d060cc1a031a36859a68fda5d5ac34252e
namhyeop <skaguq@gmail.com>
2024-04-09 23:17:14 +0900

    feat : 유료강의 테스트 코드 추가

--- src/test/java/nextstep/courses/domain/lecture/impl/PaidCourseTest.java
+++ src/test/java/nextstep/courses/domain/lecture/impl/PaidCourseTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import nextstep.courses.domain.lecture.Lecture;
 import nextstep.courses.domain.lecture.LectureStatus;
+import nextstep.courses.domain.lecture.MaxRegistrationCount;
+import nextstep.courses.domain.lecture.RegistrationCount;
 import org.junit.jupiter.api.Test;
 
 class PaidCourseTest {
@@ -14,7 +16,19 @@ class PaidCourseTest {
     }
 
     @Test
+    void 강의_최대_수강_인원을_넘지_않을_경우_강의_신청이_가능해야한다() {
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(2), new MaxRegistrationCount(new RegistrationCount(3)),
+            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.RECRUITING);
+
+        assertThat(paidLecture.isRegistrationAvailable()).isTrue();
+    }
+
+    @Test
     void 강의_최대_수강_인원을_넘을_경우_강의_신청이_불가능해야한다() {
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(4), new MaxRegistrationCount(new RegistrationCount(3)),
+            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.RECRUITING);
+
+        assertThat(paidLecture.isRegistrationAvailable()).isFalse();
     }
 
     @Test
@@ -22,11 +36,18 @@ class PaidCourseTest {
     }
 
     @Test
-    void 유료_강의_수강신청시_강의_상태가_모집중이_아니라면_수강_신청이_불가능해야한다(){
-        LectureStatus preparing = LectureStatus.PREPARING;
-
-        Lecture paidLecture = new PaidCourse(1, LocalDateTime.now(), LocalDateTime.now(), LectureStatus.PREPARING);
+    void 유료_강의_수강신청시_강의_상태가_모집중이라면_수강_신청이_가능해야한다() {
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(1), new MaxRegistrationCount(new RegistrationCount(3)),
+            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.RECRUITING);
 
         assertThat(paidLecture.isRegistrationAvailable()).isTrue();
+    }
+
+    @Test
+    void 유료_강의_수강신청시_강의_상태가_모집중이_아니라면_수강_신청이_불가능해야한다() {
+        Lecture paidLecture = new PaidCourse(new RegistrationCount(1), new MaxRegistrationCount(new RegistrationCount(3)),
+            LocalDateTime.now(), LocalDateTime.now(), LectureStatus.PREPARING);
+
+        assertThat(paidLecture.isRegistrationAvailable()).isFalse();
     }
 }
