29aedfa19b286a1cc1c145ca184727b6cbc31435
namhyeop <skaguq@gmail.com>
2024-04-09 22:06:12 +0900

    feat : 이미지타입 테스 코드 리스트 추가

---
+++ src/test/java/nextstep/courses/domain/cover/ImageTypeTest.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain.cover;
+
+import org.junit.jupiter.api.Test;
+
+class ImageTypeTest {
+
+    @Test
+    void 이미지_타입이_gif_jpg_jpeg_png_svg_가_아닌_경우_예외가_발생한다(){
+
+    }
+
+    @Test
+    void 이미지_타입이_null_또는_빈문자열일_경우_예외가_발생한다() {
+    }
+}
