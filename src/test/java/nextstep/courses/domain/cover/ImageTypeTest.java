f1f81d1961084b36a8062dc508a44208f152b579
namhyeop <skaguq@gmail.com>
2024-04-10 02:20:05 +0900

    feat : 이미지타입-이미지타입 테스트 코드 추가

--- src/test/java/nextstep/courses/domain/cover/ImageTypeTest.java
+++ src/test/java/nextstep/courses/domain/cover/ImageTypeTest.java
@@ -1,15 +1,22 @@
 package nextstep.courses.domain.cover;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ImageTypeTest {
 
+    @ParameterizedTest
+    @ValueSource(strings = {"gif", "jpg", "jpeg", "png", "svg"})
+    void 이미지_타입이_gif_jpg_jpeg_png_svg인_경우_True를_반환한다(String input){
+        assertThat(ImageType.findAvailableImagesType(ImageType.JPEG)).isTrue();
+    }
+
     @Test
     void 이미지_타입이_gif_jpg_jpeg_png_svg_가_아닌_경우_예외가_발생한다(){
-
+        assertThat(ImageType.findAvailableImagesType(null)).isFalse();
     }
 
-    @Test
-    void 이미지_타입이_null_또는_빈문자열일_경우_예외가_발생한다() {
-    }
 }
