51592e1498c3361d97e360195df491959a690315
Duyan Kim <devduyan@gmail.com>
2024-04-10 22:18:49 +0900

    fix: ImageType 오류 수정

--- src/main/java/nextstep/courses/domain/ImageType.java
+++ src/main/java/nextstep/courses/domain/ImageType.java
@@ -1,11 +1,15 @@
 package nextstep.courses.domain;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum ImageType {
 
     GIF, JPG, JPEG, PNG, SVG;
 
     public static ImageType of(String type) {
-        if (type == null) {
+        if (Arrays.stream(ImageType.values())
+                .noneMatch(imageType -> imageType.name().equals(type.toUpperCase()))) {
             throw new IllegalArgumentException("이미지 타입은 gif, jpg, jpeg, png, svg만 허용됩니다.");
         }
         return ImageType.valueOf(type.toUpperCase());
