95ef2cb23131bb60a1143cba010ecaf4bc07a392
park-cheonho <cheonho08@naver.com>
2024-04-10 15:47:01 +0900

    test: 실패 테스트 케이스 추가
    test: 실패 테스트 케이스 추가

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -6,15 +6,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
     @Test
-    @DisplayName("질문 상태값 삭제로 변경")
-    void changeQuestionStatusDeleteTest() throws CannotDeleteException {
+    @DisplayName("성공 : 질문 상태값 삭제로 변경")
+    void successChangeQuestionStatusDeleteTest() throws CannotDeleteException {
         Q1.delete(NsUserTest.JAVAJIGI);
         assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("실패 : 질문 상태값 삭제로 변경 - 작성자와 로그인유저 다른 경우 : 질문을 삭제할 권한이 없습니다.")
+    void failChangeQuestionStatusDeleteTest() throws CannotDeleteException {
+        assertThatThrownBy(() -> {
+            Q1.delete(NsUserTest.SANJIGI);;
+        }).isInstanceOf(CannotDeleteException.class);
     }
 
 }
