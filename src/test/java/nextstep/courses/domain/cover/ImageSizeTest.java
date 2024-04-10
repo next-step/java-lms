7d30ca79068ef2ad15b8697054d877aa759461f1
namhyeop <skaguq@gmail.com>
2024-04-11 01:38:36 +0900

    test : ImageSize-누락 테스트 코드 추가

--- src/test/java/nextstep/courses/domain/cover/ImageSizeTest.java
+++ src/test/java/nextstep/courses/domain/cover/ImageSizeTest.java
@@ -1,6 +1,9 @@
 package nextstep.courses.domain.cover;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.courses.error.exception.ImageSizeException;
 import org.junit.jupiter.api.Test;
 
 class ImageSizeTest {
@@ -9,6 +12,13 @@ class ImageSizeTest {
     void 이미지의_크기는_1MB_이하만_가능하다() {
         ImageSize imageSize = new ImageSize(1);
 
-        Assertions.assertThat(imageSize.getValue()).isEqualTo(1);
+        assertThat(imageSize.getValue()).isEqualTo(1);
+    }
+
+    @Test
+    void 이미지의_크기는_1MB_이하가_아닌_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new ImageSize(2))
+            .isInstanceOf(ImageSizeException.class)
+            .hasMessage("이미지 사이즈의 최대 크기는 1입니다 입력값: 2");
     }
 }
