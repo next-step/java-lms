4ddc64e36909cf2b5d6a7edfe15c73493d53d286
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-10 12:45:50 +0900

    Refactor: 매직넘버를 지양하라

--- src/main/java/nextstep/sessions/domain/image/Image.java
+++ src/main/java/nextstep/sessions/domain/image/Image.java
@@ -1,12 +1,16 @@
 package nextstep.sessions.domain.image;
 
 public class Image {
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 200;
+    private static final double RATIO = 1.5;
+    private static final int CAPACITY = 1024 * 1024;
+
     private final int capacity;
     private final ImageType imageType;
     private final double width;
 
     private final double height;
-    private final double ratio;
 
     public Image(int capacity, ImageType imageType, int width, int height) {
         validateCapacity(capacity);
@@ -18,29 +22,28 @@ public class Image {
         this.imageType = imageType;
         this.width = width;
         this.height = height;
-        this.ratio = calculateRatio(width, height);
     }
 
     private void validateCapacity(int capacity) {
-        if (capacity < 1) {
+        if (capacity > CAPACITY) {
             throw new IllegalArgumentException("이미지크기는 1MB를 초과할 수 없다");
         }
     }
 
     private void validateWidth(int width) {
-        if (width < 300) {
+        if (width < WIDTH) {
             throw new IllegalArgumentException("이미지 width는 300픽셀이상이어야 한다");
         }
     }
 
     private void validateHeight(int height) {
-        if (height < 200) {
+        if (height < HEIGHT) {
             throw new IllegalArgumentException("이미지 height는 200픽셀이상이어야 한다");
         }
     }
 
     private void validateRatio(int width, int height) {
-        if (width / (double) height != 1.5) {
+        if (width / (double) height != RATIO) {
             throw new IllegalArgumentException("width와 height의 비율은 3:2여야 한다");
         }
     }
--- src/test/java/nextstep/sessions/domain/image/ImageTest.java
+++ src/test/java/nextstep/sessions/domain/image/ImageTest.java
@@ -12,7 +12,7 @@ public class ImageTest {
     @DisplayName("이미지 크기가 1MB를 초과하면 예외를 반환한다")
     @Test
     void capacityException() {
-        Assertions.assertThatThrownBy(() -> new Image(0, GIF, 300, 200))
+        Assertions.assertThatThrownBy(() -> new Image(1024 * 2000, GIF, 300, 200))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미지크기는 1MB를 초과할 수 없다");
     }
@@ -29,7 +29,7 @@ public class ImageTest {
     @DisplayName("이미지 height가 200픽셀미만이면 예외를 반환한다")
     @Test
     void heightException() {
-        Assertions.assertThatThrownBy(() -> new Image(5, GIF, 300, 100))
+        Assertions.assertThatThrownBy(() -> new Image(1024 * 1024, GIF, 300, 100))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미지 height는 200픽셀이상이어야 한다");
     }
@@ -37,7 +37,7 @@ public class ImageTest {
     @DisplayName("이미지 비율이 3:2가 아니면 예외를 반환한다")
     @Test
     void ratioException() {
-        Assertions.assertThatThrownBy(() -> new Image(5, GIF, 600, 300))
+        Assertions.assertThatThrownBy(() -> new Image(1024 * 1024, GIF, 600, 300))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("width와 height의 비율은 3:2여야 한다");
     }
