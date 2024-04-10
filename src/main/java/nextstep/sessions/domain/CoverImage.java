177c93cd1886a3cafa393ba6b3ba30bdf27b1af5
kwanik-kor <rhksdlr134@naver.com>
2024-04-10 11:46:56 +0900

    feat: 강의 커버 이미지 클래스 설계 및 테스트 코드 작성 완료

---
+++ src/main/java/nextstep/sessions/domain/CoverImage.java
@@ -0,0 +1,106 @@
+package nextstep.sessions.domain;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CoverImage {
+    private static final long MAX_FILE_SIZE = 1024 * 1024;
+    private static final int MIN_WIDTH = 300;
+    private static final int MIN_HEIGHT = 200;
+    private static final double RATIO = 3.0 / 2.0;
+
+    private final Long id;
+    private final String fileName;
+    private final int width;
+    private final int height;
+    private final long size;
+    private final EnableExtension extension;
+
+    public CoverImage(Long id, String fileName, int width, int height, long size) {
+        assertImageUnderMaxSize(size);
+
+        final EnableExtension extension = EnableExtension.from(fileName);
+        assertValidExtension(extension);
+
+        assertValidWidth(width);
+
+        assertValidHeight(height);
+
+        assertValidRatio(width, height);
+
+        this.id = id;
+        this.fileName = fileName;
+        this.width = width;
+        this.height = height;
+        this.size = size;
+        this.extension = extension;
+    }
+
+    private void assertImageUnderMaxSize(long size) {
+        if (MAX_FILE_SIZE < size) {
+            throw new IllegalArgumentException("이미지 크기는 1MB 이히여야 합니다.");
+        }
+    }
+
+    private void assertValidExtension(EnableExtension extension) {
+        if (extension == EnableExtension.INVALID) {
+            throw new IllegalArgumentException("지원하지 않는 확장자 입니다.");
+        }
+    }
+
+    private void assertValidWidth(int width) {
+        if (width < MIN_WIDTH) {
+            throw new IllegalArgumentException("이미지 너비는 " + MIN_WIDTH + "픽셀 이상이어야 합니다.");
+        }
+    }
+
+    private void assertValidHeight(int height) {
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException("이미지 높이는 " + MIN_HEIGHT + "픽셀 이상이어야 합니다.");
+        }
+    }
+
+    private void assertValidRatio(int width, int height) {
+        if ((double) width != height * RATIO) {
+            throw new IllegalArgumentException("이미지 비율이 적합하지 않습니다.");
+        }
+    }
+
+    enum EnableExtension {
+        GIF,
+        JPG,
+        JPEG,
+        PNG,
+        SVG,
+        INVALID
+        ;
+
+        private static final Map<String, EnableExtension> MAPPER;
+        private static final String COMMA = ".";
+
+        static {
+            MAPPER = Arrays.stream(values())
+                    .collect(Collectors.toMap(Enum::name, Function.identity()));
+        }
+
+        public static EnableExtension from(String fileName) {
+            if (isBlankFileName(fileName)) {
+                return INVALID;
+            }
+
+            return MAPPER.getOrDefault(extractExtension(fileName), INVALID);
+        }
+
+        private static boolean isBlankFileName(String fileName) {
+            return fileName == null || fileName.isBlank();
+        }
+
+        private static String extractExtension(String fileName) {
+            return fileName.substring(fileName.lastIndexOf(COMMA) + 1)
+                    .toUpperCase();
+        }
+
+    }
+}
