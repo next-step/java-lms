cab672b2891246e5674fffe04703dbbdca0eb79a
hong yuseok <jericoh@naver.com>
2024-04-10 18:14:31 +0900

    feat: Period 구현

--- src/main/java/nextstep/courses/ExceptionMessage.java
+++ src/main/java/nextstep/courses/ExceptionMessage.java
@@ -1,7 +1,8 @@
 package nextstep.courses;
 
 public enum ExceptionMessage {
-    INVALID_COVER_IMAGE("잘못된 커버 이미지 입니다.");
+    INVALID_COVER_IMAGE("잘못된 커버 이미지 입니다."),
+    INVALID_PERIOD("잘못된 시작일과 종료일입니다.");
 
     private final String message;
 
