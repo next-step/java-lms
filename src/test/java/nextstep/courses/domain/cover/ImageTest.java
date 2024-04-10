7670fc3c979418e56fa3b4e383c95f8a0725a061
namhyeop <skaguq@gmail.com>
2024-04-10 17:03:32 +0900

    feat : ImageTest 코드 추가

--- src/test/java/nextstep/courses/domain/cover/ImageTest.java
+++ src/test/java/nextstep/courses/domain/cover/ImageTest.java
@@ -1,19 +1,18 @@
 package nextstep.courses.domain.cover;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.courses.error.ImageSizeException;
 import org.junit.jupiter.api.Test;
 
 class ImageTest {
 
     @Test
-    void 이미지의_크기는_1MB만_가능하다() {
-    }
+    void 이미지의_크기가_1MB보다_클_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Image(new ImageSize(2), ImageType.JPG, 2, 3))
+            .isInstanceOf(ImageSizeException.class)
+            .hasMessage("이미지 사이즈의 최대 크기는 1입니다 입력값: 2");
 
-    @Test
-    void 이미지의_크기가_1MB보다_작거나_클_경우_예외가_발생한다() {
-    }
-
-    @Test
-    void 이미지의_크기가_null_또는_빈문자열일_경우_예외가_발생한다() {
     }
 
     @Test
