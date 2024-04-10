ca481c25d68f7b7a293743c5b57f4164b7e209f5
namhyeop <skaguq@gmail.com>
2024-04-11 03:13:54 +0900

    test : 무료강의 등록 테스트 코드 추가

--- src/test/java/nextstep/courses/domain/course/CourseTest.java
+++ src/test/java/nextstep/courses/domain/course/CourseTest.java
@@ -1,6 +1,6 @@
 package nextstep.courses.domain.course;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import nextstep.courses.domain.cover.Image;
@@ -13,16 +13,16 @@ import nextstep.courses.domain.lecture.LectureName;
 import nextstep.courses.domain.lecture.LectureStatus;
 import nextstep.courses.domain.lecture.MaxRegistrationCount;
 import nextstep.courses.domain.lecture.RegistrationCount;
+import nextstep.courses.domain.lecture.impl.FreeLecture;
 import nextstep.courses.domain.lecture.impl.PaidCourse;
 import nextstep.payments.domain.Money;
 import nextstep.payments.domain.Payment;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CourseTest {
 
     @Test
-    void 강의등록이_되어야_합니다(){
+    void 유료강의등록이_되어야_한다() {
         Payment payment = new Payment("1", 1L, 1L, new Money(5000));
         Course course = new Course("18기-자바TDD", 1L);
 
@@ -41,5 +41,26 @@ class CourseTest {
         course.addLecture(new LectureName("유료강의1"), paidCourse);
 
         assertThat(course.registerLecture(new LectureName("유료강의1"), payment)).isTrue();
+    }
+
+    @Test
+    void 무료강의등록이_되어야_한다() {
+        Payment payment = new Payment("1", 1L, 1L, new Money(0));
+        Course course = new Course("18기-자바TDD", 1L);
+
+        Lecture freeLecture = new FreeLecture(new LectureName("무료강의1"),
+            new RegistrationCount(0),
+            new Money(0),
+            new Image(new ImageSize(1),
+                ImageType.JPG,
+                new ImageWidth(300),
+                new ImageHeight(200)),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.RECRUITING);
+
+        course.addLecture(new LectureName("무료강의1"), freeLecture);
+
+        assertThat(course.registerLecture(new LectureName("무료강의1"), payment)).isTrue();
     }
 }
