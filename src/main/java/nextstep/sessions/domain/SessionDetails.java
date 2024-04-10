8fbb7a5a25366dd1650c4363c5ab81840bfd641a
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-10 11:40:04 +0900

    Refactor: 예외에 메시지를 추가하라

--- src/main/java/nextstep/sessions/domain/SessionDetails.java
+++ src/main/java/nextstep/sessions/domain/SessionDetails.java
@@ -25,9 +25,11 @@ public class SessionDetails {
         if (this.sessionStatus.isNotRecruiting()) {
             throw new IllegalArgumentException(String.format("현재 강의는 (%s)인 상태입니다.", this.sessionStatus));
         }
+
         if (!this.sessionType.canEnroll(currentCountOfStudents, maxOfStudents)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format("이 강의의 현재 수강 신청 인원: (%s)명, 최대 수강 인원: (%s)명이므로 현재 마감이 된 상태입니다.", this.currentCountOfStudents, this.maxOfStudents));
         }
+
         this.currentCountOfStudents++;
     }
 
--- src/test/java/nextstep/sessions/domain/SessionDetailsTest.java
+++ src/test/java/nextstep/sessions/domain/SessionDetailsTest.java
@@ -1,6 +1,5 @@
 package nextstep.sessions.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +7,6 @@ import static nextstep.sessions.domain.SessionStatus.END;
 import static nextstep.sessions.domain.SessionStatus.RECRUITING;
 import static nextstep.sessions.domain.SessionType.FREE;
 import static nextstep.sessions.domain.SessionType.PAID;
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SessionDetailsTest {
@@ -23,10 +21,14 @@ public class SessionDetailsTest {
     @DisplayName("유료강의는 수강신청을 했을 때, 최대 수강 인원을 초과하면 예외를 반환한다")
     @Test
     void greaterThanMax() {
-        SessionDetails details = new SessionDetails(40, 40, 30000, PAID, RECRUITING);
+        int currentCountOfStudents = 40;
+        int maxOfStudents = 40;
+
+        SessionDetails details = new SessionDetails(currentCountOfStudents, maxOfStudents, 30000, PAID, RECRUITING);
 
         assertThatThrownBy(details::register)
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("이 강의의 현재 수강 신청 인원: (%s)명, 최대 수강 인원: (%s)명이므로 현재 마감이 된 상태입니다.", currentCountOfStudents, maxOfStudents));
     }
 
     @DisplayName("강의가 모집중이 아닐때 수강신청을 하면 예외를 반환한다")
