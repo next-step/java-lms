b43d2ded5c1d606f94a393e23deb921689463e40
namhyeop <skaguq@gmail.com>
2024-04-11 02:00:54 +0900

    test : Lectures-테스트 코드 추가

--- src/test/java/nextstep/courses/domain/lecture/LecturesTest.java
+++ src/test/java/nextstep/courses/domain/lecture/LecturesTest.java
@@ -1,22 +1,38 @@
 package nextstep.courses.domain.lecture;
 
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashSet;
 import nextstep.courses.domain.lecture.impl.FreeLecture;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LecturesTest {
 
-//    @Test
-//    void Lectures는_강의를_추가할_수_있다(){
-//        Lectures lectures = new Lectures(new HashSet<>());
-//        lectures.addLecture(new FreeLecture(LocalDateTime.now(), LocalDateTime.now(), LectureStatus.PREPARING));
-//
-//        assertThat(lectures.)
-//    }
+    @Test
+    void Lectures는_강의를_추가할_수_있다() {
+        Lectures lectures = new Lectures();
+        Lecture lecture = new FreeLecture(new LectureName("강의이름1"),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.PREPARING);
+        lectures.addLecture(new LectureName("강의이름1"), lecture);
+
+        assertThat(lectures.findLecture(new LectureName("강의이름1"))).isEqualTo(lecture);
+    }
+
+    @Test
+    void Lectures는_강의를_삭제할_수_있다() {
+        Lectures lectures = new Lectures();
+        LectureName lectureName = new LectureName("강의이름2");
+        Lecture lecture = new FreeLecture(new LectureName("강의이름2"),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            LectureStatus.PREPARING);
+
+        lectures.addLecture(lectureName, lecture);
+        Lecture deletedLecture= lectures.deleteLecture(lectureName);
+
+        assertThat(deletedLecture).isEqualTo(lecture);
+    }
 }
