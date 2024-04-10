6f6f59ec047865f46bb82fa158f718e1eb0398d1
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-10 14:31:55 +0900

    TEST : 강의가 모집 중이 아닐 때 수강신청 제한하는지 테스트

--- src/test/java/nextstep/courses/domain/SessionApplyTest.java
+++ src/test/java/nextstep/courses/domain/SessionApplyTest.java
@@ -2,9 +2,26 @@ package nextstep.courses.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 public class SessionApplyTest {
 
-
     // 무료 - 수강 인원 제한 X / 유료 - 수강 인원 제한 초과 불가 & 수강생 결제 금액과 수강료 일치 시 수강 신청 가능
-    // 수강신청은 강의 상태가 모집 중일 때 가능
+    @Test
+    void 수강신청_모집_중이_아닐_때() {
+        Session session = new Session(
+                SessionTest.startAt, SessionTest.endAt, SessionTest.image, SessionTest.progress
+        );
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new SessionApply().apply(session))
+                .withMessageMatching("모집 중인 강의가 아닙니다.");
+    }
+
+    @Test
+    void 수강신청_유료강의_수강인원_초과() {
+        Session session = new Session(
+                SessionTest.startAt, SessionTest.endAt, SessionTest.image, SessionTest.progress, false
+        );
+    }
 }
