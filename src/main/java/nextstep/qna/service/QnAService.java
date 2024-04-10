7373b5e439a972aaacf7591c277baa7cd285137a
park-cheonho <cheonho08@naver.com>
2024-04-10 22:42:26 +0900

    feat: 이벤트 적용
    feat: 이벤트 적용

--- src/main/java/nextstep/qna/service/QnAService.java
+++ src/main/java/nextstep/qna/service/QnAService.java
@@ -4,6 +4,8 @@ import nextstep.qna.CannotDeleteException;
 import nextstep.qna.NotFoundException;
 import nextstep.qna.domain.*;
 import nextstep.users.domain.NsUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,12 +25,17 @@ public class QnAService {
     @Resource(name = "deleteHistoryService")
     private DeleteHistoryService deleteHistoryService;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @Transactional
     public void deleteQuestion(NsUser loginUser, long questionId) throws CannotDeleteException {
         Question question = questionRepository.findById(questionId).orElseThrow(NotFoundException::new);
 
         question.delete(loginUser);
 
-        deleteHistoryService.saveAll(question.deletedHistories());
+        //deleteHistoryService.saveAll(question.deletedHistories());
+
+        publisher.publishEvent(new DeleteHistoryEvent(this, question));
     }
 }
