d2ef0fa50c151e865d51dd993d0dd5fa98bf360b
park-cheonho <cheonho08@naver.com>
2024-04-10 16:02:19 +0900

    test: 다양한 예외 케이스 추가
    test: 다양한 예외 케이스 추가

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
@@ -14,16 +15,31 @@ public class QuestionTest {
     @Test
     @DisplayName("성공 : 질문 상태값 삭제로 변경")
     void successChangeQuestionStatusDeleteTest() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A1);
         Q1.delete(NsUserTest.JAVAJIGI);
-        assertThat(Q1.isDeleted()).isTrue();
+        assertAll(
+                () -> assertThat(Q1.isDeleted()).isTrue(),
+                () -> assertThat(AnswerTest.A1.isDeleted()).isTrue());
+
     }
 
     @Test
     @DisplayName("실패 : 질문 상태값 삭제로 변경 - 작성자와 로그인유저 다른 경우 : 질문을 삭제할 권한이 없습니다.")
     void failChangeQuestionStatusDeleteTest() throws CannotDeleteException {
         assertThatThrownBy(() -> {
-            Q1.delete(NsUserTest.SANJIGI);;
-        }).isInstanceOf(CannotDeleteException.class);
+            Q1.delete(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("실패 : 질문 상태값 삭제로 변경 - 질문에 다른사람의 답변이 있는 경우  : 다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.")
+    void failChangeQuestionStatusDeleteTest2() throws CannotDeleteException {
+        Q1.addAnswer(AnswerTest.A2);
+        assertThatThrownBy(() -> {
+            Q1.delete(NsUserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class)
+                .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
 
 }
