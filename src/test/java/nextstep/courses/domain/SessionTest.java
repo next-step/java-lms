60bb1fdaab25f58cb474ca5c4a5e9178cc8f2abe
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-10 14:32:18 +0900

    TEST : SessionTest의 상수를 public으로 전환

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -8,15 +8,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SessionTest {
 
-    private static final LocalDate startedAt = LocalDate.of(2024, 1, 1);
-    private static final LocalDate endedAt = LocalDate.of(2024, 1, 31);
-    private static final SessionImage image = new SessionImage(300, 200, 1_000_000, new SessionImageType("jpg"));
-    private static final SessionProgress progress = new SessionProgress("준비중");
+    public static final LocalDate startAt = LocalDate.of(2024, 1, 1);
+    public static final LocalDate endAt = LocalDate.of(2024, 1, 31);
+    public static final SessionImage image = new SessionImage(300, 200, 1_000_000, new SessionImageType("jpg"));
+    public static final SessionProgress progress = new SessionProgress("준비중");
 
     @Test
     void 강의_생성() {
-        assertThat(new Session(startedAt, endedAt, image, progress, true))
-                .isEqualTo(new Session(startedAt, endedAt, image, progress, true));
+        assertThat(new Session(startAt, endAt, image, progress, true))
+                .isEqualTo(new Session(startAt, endAt, image, progress, true));
     }
 
 }
