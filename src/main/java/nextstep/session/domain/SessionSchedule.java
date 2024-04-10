6607a3ea15665e3f71628fd088c5c66711c59cc2
박소민 <qkrthals0524@naver.com>
2024-04-10 23:29:57 +0900

    강의 스케줄 객체 생성

---
+++ src/main/java/nextstep/session/domain/SessionSchedule.java
@@ -0,0 +1,29 @@
+package nextstep.session.domain;
+
+import java.time.LocalDate;
+
+public class SessionSchedule {
+
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public SessionSchedule(LocalDate startDate, LocalDate endDate) {
+        validateSchedule(startDate, endDate);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private void validateSchedule(LocalDate startDate, LocalDate endDate) {
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("종료 날짜는 시작 날짜보다 빠를 수 없습니다.");
+        }
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+}
---
+++ src/test/java/nextstep/session/domain/SessionScheduleTest.java
@@ -0,0 +1,28 @@
+package nextstep.session.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+class SessionScheduleTest {
+
+    @Test
+    public void 종료_날짜가_시작_날짜보다_빠를_경우_생성_불가능() {
+        assertThatThrownBy(() -> {
+            new SessionSchedule(LocalDate.now(), LocalDate.now().minusDays(1));
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("종료 날짜는 시작 날짜보다 빠를 수 없습니다.");
+    }
+
+    @Test
+    public void 강의_스케줄_생성() {
+        LocalDate startDate = LocalDate.of(2024, 4, 10);
+        LocalDate endDate = LocalDate.of(2024, 6, 30);
+        SessionSchedule sessionSchedule = new SessionSchedule(startDate, endDate);
+
+        assertThat(sessionSchedule.getStartDate()).isEqualTo(startDate);
+        assertThat(sessionSchedule.getEndDate()).isEqualTo(endDate);
+    }
+}
