f46bce4c72fd37ec49617f5964dc4729cd83f23a
hellonayeon <hellonykwon@gmail.com>
2024-04-11 21:23:39 +0900

    Test: SessionEnrollment 테스트케이스 DisplayName 수정

--- src/test/java/nextstep/courses/domain/SessionEnrollmentTest.java
+++ src/test/java/nextstep/courses/domain/SessionEnrollmentTest.java
@@ -1,7 +1,6 @@
 package nextstep.courses.domain;
 
 import nextstep.courses.domain.enrollment.SessionStatus;
-import nextstep.courses.domain.enrollment.Student;
 import nextstep.courses.domain.enrollment.engine.SessionEnrollment;
 import nextstep.payments.domain.Payment;
 import org.junit.jupiter.api.DisplayName;
@@ -10,30 +9,27 @@ import org.junit.jupiter.api.Test;
 import static nextstep.courses.domain.fixture.PaymentFixture.payment;
 import static nextstep.courses.domain.fixture.SessionEnrollmentFixture.costSessionEnrollment;
 import static nextstep.courses.domain.fixture.SessionEnrollmentFixture.freeSessionEnrollment;
-import static nextstep.courses.domain.fixture.StudentFixture.student;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class SessionEnrollmentTest {
 
     @Test
-    @DisplayName("[성공] 무료 강의 조건을 만족한다.")
-    void 무료_강의_수강신청() {
+    @DisplayName("[성공] 무료 강의 신청을 만족한다.")
+    void 무료_강의_신청_만족() {
         SessionStatus status = SessionStatus.RECRUITING;
         SessionEnrollment enrollment = freeSessionEnrollment(status);
 
-        Student student = student();
         Payment payment = payment(0L);
 
         assertThatNoException().isThrownBy(() -> enrollment.satisfyEnrollment(payment));
     }
 
     @Test
-    @DisplayName("[성공] 유료 강의를 수강신청 한다.")
-    void 유료_강의_수강신청() {
+    @DisplayName("[성공] 유료 강의 신청을 만족한다.")
+    void 유료_강의_신청_만족() {
         SessionStatus status = SessionStatus.RECRUITING;
         SessionEnrollment enrollment = costSessionEnrollment(status, 10, 800_000L);
 
-        Student student = student();
         Payment payment = payment(800_000L);
 
         assertThatNoException().isThrownBy(() -> enrollment.satisfyEnrollment(payment));
