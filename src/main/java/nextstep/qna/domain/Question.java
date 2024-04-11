bc649076393fb652f9c3b88505d5fe483dc5e529
동원 <ai06021@gmail.com>
2024-04-11 10:22:48 +0900

    refactor : question loginUser Check

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -68,8 +69,10 @@ public class Question {
         answers.add(answer);
     }
 
-    public boolean isOwner(NsUser loginUser) {
-        return writer.equals(loginUser);
+    public boolean isOwner(NsUser loginUser) throws CannotDeleteException {
+        if (!writer.equals(loginUser))
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        return true;
     }
 
     public Question setDeleted(boolean deleted) {
