21cdb15dd318509d5f29f5ba0f9efca96eb6dc97
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-11 14:09:57 +0900

    FEAT : 수강생 클래스 생성

--- src/main/java/nextstep/courses/domain/Student.java
+++ src/main/java/nextstep/courses/domain/Student.java
@@ -1,2 +1,41 @@
-package nextstep.courses.domain;public class Student {
+package nextstep.courses.domain;
+
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.function.DoublePredicate;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class Student {
+
+    private Long id;
+    private String password;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Student(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (!(object instanceof Student))
+            return false;
+        Student student = (Student) object;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
