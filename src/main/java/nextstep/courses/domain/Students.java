d0486f9058641e729e36c0bf084c54367617411c
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-11 14:10:18 +0900

    FEAT : 수강생들을 관리하는 클래스 생성(일급 콜렉션)

--- src/main/java/nextstep/courses/domain/Students.java
+++ src/main/java/nextstep/courses/domain/Students.java
@@ -1,2 +1,36 @@
-package nextstep.courses.domain;public class Students {
+package nextstep.courses.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Students {
+
+    private List<Student> students;
+
+    public Students(List<Student> students) {
+        this.students = students;
+    }
+
+    public void addStudent(Student student, int maxStudents) {
+        if (this.students.size() >= maxStudents) {
+            throw new IllegalArgumentException("수강 인원 초과 과정입니다.");
+        }
+        this.students.add(student);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (!(object instanceof Students))
+            return false;
+        Students students1 = (Students) object;
+        return Objects.equals(students, students1.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(students);
+    }
+
 }
