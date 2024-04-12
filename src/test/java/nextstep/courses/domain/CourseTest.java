a2616d3a2332d741fff538d1bd706d2ddb669e13
Sangmin <sm9171@nate.com>
2024-04-12 20:04:17 +0900

    test: 과정은 여러 개의 강의를 가지고 생성된다. fail test

--- src/test/java/nextstep/courses/domain/CourseTest.java
+++ src/test/java/nextstep/courses/domain/CourseTest.java
@@ -3,16 +3,31 @@ package nextstep.courses.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CourseTest {
 	@DisplayName("과정은 기수를 가지고 생성된다.")
 	@Test
-	void testMethodNameHere() {
+	void createCourseWithCardinality() {
 		// given & when
 		Course course = new Course(1L, "tdd 클린코드", 1L);
 
 		// then
 		assertThat(course.getId()).isEqualTo(1);
+	}
+
+	@DisplayName("과정은 여러 개의 강의를 가지고 생성 된다.")
+	@Test
+	void CreateCourseWithMultipleSessions() {
+		// given & when
+		List sessions = List.of(new Session(1L, "객체지향 프로그래밍", 1L),
+			new Session(2L, "클래스 디자인", 1L));
+
+		Course course = new Course(1L, "tdd 클린코드", 1L, sessions);
+
+		// then
+		assertThat(course.getSessionsSize()).isEqualTo(2);
 	}
 }
