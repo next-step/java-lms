2c020a11c6eb3eedb0174dbc8952001facf30f05
danalee <eleeje97@gmail.com>
2024-04-13 01:22:08 +0900

    fix: Answers.delete() 수정

--- src/main/java/nextstep/qna/domain/Answers.java
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -22,20 +22,12 @@ public class Answers {
     }
 
     public List<DeleteHistory> delete(NsUser loginUser) throws CannotDeleteException {
-        if (!hasOnlyMyAnswer(loginUser)) {
-            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-        }
-
         List<DeleteHistory> deleteHistories = new ArrayList<>();
         for (Answer answer : answers) {
             deleteHistories.add(answer.delete(loginUser));
         }
-        return deleteHistories;
-    }
 
-    private boolean hasOnlyMyAnswer(NsUser loginUser) {
-        return answers.stream()
-                .allMatch(answer -> answer.isOwner(loginUser));
+        return deleteHistories;
     }
 
 }
