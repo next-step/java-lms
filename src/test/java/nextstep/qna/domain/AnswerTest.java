eb1e37b286c2c351234e92bac9a87452316869bd
jaeyounglee <ljyda214@gmail.com>
2024-04-14 00:44:20 +0900

    TEST : ParameterizedTest 를 통해 중복 테스트 제거

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,15 @@
 package nextstep.qna.domain;
 
 import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -10,14 +17,17 @@ public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
-    @Test
-    void is_owner() {
-        assertThat(A1.isOwner(NsUserTest.JAVAJIGI)).isTrue();
+    static Stream<Arguments> owner_test() {
+        return Stream.of(
+                Arguments.arguments(A1, NsUserTest.JAVAJIGI, true),
+                Arguments.arguments(A2, NsUserTest.JAVAJIGI, false)
+        );
     }
 
-    @Test
-    void is_not_owner() {
-        assertThat(A1.isOwner(NsUserTest.SANJIGI)).isFalse();
+    @ParameterizedTest
+    @MethodSource(value = "owner_test")
+    void is_owner(Answer answer, NsUser user, boolean isOwner) {
+        assertThat(answer.isOwner(user)).isEqualTo(isOwner);
     }
 
     @Test
