60eb89a6264e7db2c7b9cf5c264e4ffcdd206f52
kylekim2123 <kyle2123@naver.com>
2024-04-13 20:00:23 +0900

    [STEP 2] feat: 커버 이미지 종류에 대한 생성 및 검증 로직 추가

---
+++ src/main/java/nextstep/courses/domain/session/image/ImageType.java
@@ -0,0 +1,30 @@
+package nextstep.courses.domain.session.image;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum ImageType {
+
+    GIF("gif"),
+    JPG("jpg"),
+    JPEG("jpeg"),
+    PNG("png"),
+    SVN("svn");
+
+    private static final Map<String, ImageType> imageTypes = Stream.of(values())
+            .collect(Collectors.toUnmodifiableMap(imageType -> imageType.typeName, Function.identity()));
+
+    private final String typeName;
+
+    ImageType(final String typeName) {
+        this.typeName = typeName;
+    }
+
+    public static ImageType from(final String typeName) {
+        return Optional.ofNullable(imageTypes.get(typeName))
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 커버 이미지 종류입니다. 종류: " + typeName));
+    }
+}
