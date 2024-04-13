677ff855af897b2469f8e37ffe45b3a76353f833
kylekim2123 <kyle2123@naver.com>
2024-04-13 19:31:18 +0900

    [STEP 2] feat: 강의 스케줄에 대한 생성 및 검증 로직 추가

---
+++ src/main/java/nextstep/courses/domain/session/Schedule.java
@@ -0,0 +1,44 @@
+package nextstep.courses.domain.session;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Schedule {
+
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Schedule(final LocalDate startDate, final LocalDate endDate) {
+        validateSessionScheduleEndDateIsAfterStartDate(startDate, endDate);
+
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private void validateSessionScheduleEndDateIsAfterStartDate(final LocalDate startDate, final LocalDate endDate) {
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("강의 종료일은 시작일 보다 앞설 수 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object otherSchedule) {
+        if (this == otherSchedule) {
+            return true;
+        }
+
+        if (otherSchedule == null || getClass() != otherSchedule.getClass()) {
+            return false;
+        }
+
+        final Schedule that = (Schedule)otherSchedule;
+
+        return Objects.equals(this.startDate, that.startDate) &&
+                Objects.equals(this.endDate, that.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.startDate, this.endDate);
+    }
+}
