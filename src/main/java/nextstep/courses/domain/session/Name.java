897ee17ed4114aabd50321206e0802dc727ae25f
kylekim2123 <kyle2123@naver.com>
2024-04-13 19:24:26 +0900

    [STEP 2] feat: 강의 이름에 대한 생성 및 검증 로직 추가

---
+++ src/main/java/nextstep/courses/domain/session/Name.java
@@ -0,0 +1,38 @@
+package nextstep.courses.domain.session;
+
+import java.util.Objects;
+
+public class Name {
+
+    private final String value;
+
+    public Name(final String value) {
+        validateSessionNameIsNotNullOrBlank(value);
+
+        this.value = value;
+    }
+
+    private void validateSessionNameIsNotNullOrBlank(final String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("강의 이름은 null이거나 빈 문자열, 공백, 개행일 수 없습니다. 이름: " + value);
+        }
+    }
+
+    @Override
+    public boolean equals(final Object otherName) {
+        if (this == otherName) {
+            return true;
+        }
+
+        if (otherName == null || getClass() != otherName.getClass()) {
+            return false;
+        }
+
+        return Objects.equals(this.value, ((Name)otherName).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.value);
+    }
+}
