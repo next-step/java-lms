6950f1ec3f31fddd4852af647477567808c2b0ef
Duyan Kim <devduyan@gmail.com>
2024-04-13 13:31:34 +0900

    feat: 결제금액과 수강료가 동일한지 확인

--- src/main/java/nextstep/courses/domain/Fee.java
+++ src/main/java/nextstep/courses/domain/Fee.java
@@ -1,15 +1,18 @@
 package nextstep.courses.domain;
 
+import nextstep.payments.domain.Payment;
+import nextstep.users.domain.NsUser;
+
 public class Fee {
 
-    int amount;
+    private Long amount;
 
-    public Fee(int amount, SessionType type) {
+    public Fee(Long amount, SessionType type) {
         validateFee(amount, type);
         this.amount = amount;
     }
 
-    private void validateFee(int amount, SessionType type) {
+    private void validateFee(Long amount, SessionType type) {
         if (type == SessionType.FREE && amount > 0) {
             throw new IllegalArgumentException("무료 강의는 강의 금액이 있을 수 없습니다.");
         }
@@ -19,7 +22,16 @@ public class Fee {
         }
     }
 
-    public int getAmount() {
+    public Long getAmount() {
         return amount;
+    }
+
+    public void enroll(Long userId, Long sessionId) {
+        // 유료 강의 수강 신청 : payment amount가 수강료와 일치할 때 수강 신청이 가능하다.
+        // Payment id, sessionId, nsUserId, amount가 필요함
+        Payment payment = new Payment();
+        if (!payment.isEqualAmount(this)) {
+            throw new IllegalArgumentException("결제 금액이 수강료와 일치하지 않습니다.");
+        }
     }
 }
--- src/test/java/nextstep/courses/domain/FeeTest.java
+++ src/test/java/nextstep/courses/domain/FeeTest.java
@@ -1,5 +1,6 @@
 package nextstep.courses.domain;
 
+import nextstep.users.domain.NsUser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +11,16 @@ class FeeTest {
     @Test
     @DisplayName("무료강의일 경우, 수강료가 없고 유료강의일 경우, 수강료가 있어야 한다.")
     void create_fee_test() {
-        assertThrows(IllegalArgumentException.class, () -> new Fee(1, SessionType.FREE));
-        assertThrows(IllegalArgumentException.class, () -> new Fee(0, SessionType.PAID));
-        assertDoesNotThrow(() -> new Fee(1, SessionType.PAID));
-        assertDoesNotThrow(() -> new Fee(0, SessionType.FREE));
-
+        assertThrows(IllegalArgumentException.class, () -> new Fee(1L, SessionType.FREE));
+        assertThrows(IllegalArgumentException.class, () -> new Fee(0L, SessionType.PAID));
+        assertDoesNotThrow(() -> new Fee(1L, SessionType.PAID));
+        assertDoesNotThrow(() -> new Fee(0L, SessionType.FREE));
     }
 
+    @Test
+    @DisplayName("결제금액과 수강료가 같지 않으면 에러가 발생한다.")
+    void enroll_fee_test() {
+        Fee fee = new Fee(1L, SessionType.PAID);
+        assertThrows(IllegalArgumentException.class, () -> fee.enroll(1L, 1L));
+    }
 }
