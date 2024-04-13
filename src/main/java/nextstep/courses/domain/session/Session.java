040dbd5da9542605076bbf9632e9981342b7be69
hong yuseok <jericoh@naver.com>
2024-04-14 04:32:12 +0900

    refactor: Session, Period, EnrolledUsers - Session 필드 순서 및 생성자 변경에 따른 테스트 및 로직 수정 - Session 상태 변경시 updatedAt도 변경되도록 수정 - Session get 메서드 추가 - Period get 메서드 추가 - EnrolledUsers get 메서드 추가

--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -6,7 +6,9 @@ import nextstep.payments.domain.Payment;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import static java.util.Objects.nonNull;
 import static nextstep.courses.ExceptionMessage.SESSION_ENROLL_FAIL_MESSAGE;
 import static nextstep.courses.domain.session.SessionStatus.PREPARING;
 
@@ -14,32 +16,39 @@ public class Session {
     private Long id;
     private String title;
     private String description;
-    private CoverImage coverImage;
     private SessionType sessionType;
-    private SessionStatus sessionStatus = PREPARING;
+    private SessionStatus sessionStatus;
     private Period periodOfSession;
+    private CoverImage coverImage;
     private Course course;
     private EnrolledUsers enrolledUsers = new EnrolledUsers();
-    private final LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Session(String title, String description, CoverImage coverImage, SessionType sessionType, Period periodOfSession, Course course) {
-        this(null, title, description, coverImage, sessionType, periodOfSession, course);
+    public Session(String title, String description, SessionType sessionType, Period periodOfSession, CoverImage coverImage, Course course) {
+        this(null, title, description, sessionType, PREPARING, periodOfSession, coverImage, course, LocalDateTime.now(), null);
     }
 
-    public Session(Long id, String title, String description, CoverImage coverImage, SessionType sessionType, Period periodOfSession, Course course) {
+    public Session(Long id, String title, String description, SessionType sessionType, Period periodOfSession, CoverImage coverImage, Course course) {
+        this(id, title, description, sessionType, PREPARING, periodOfSession, coverImage, course, LocalDateTime.now(), null);
+    }
+
+    public Session(Long id, String title, String description, SessionType sessionType, SessionStatus sessionStatus, Period periodOfSession, CoverImage coverImage, Course course, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.coverImage = coverImage;
         this.sessionType = sessionType;
+        this.sessionStatus = sessionStatus;
         this.periodOfSession = periodOfSession;
+        this.coverImage = coverImage;
         this.course = course;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public void enroll(NsUser user, Payment payment) {
         validateSessionEnrollment(user, payment);
-        addEnrolledUse(user);
+        addEnrolledUser(user);
     }
 
     private void validateSessionEnrollment(NsUser user, Payment payment) {
@@ -54,15 +63,84 @@ public class Session {
                 sessionType.isEnrollmentPossible(enrolledUsers.numberOfCurrentEnrollment(), payment);
     }
 
-    private void addEnrolledUse(NsUser user) {
+    private void addEnrolledUser(NsUser user) {
         enrolledUsers.add(user);
     }
 
     public void updateStatusAs(SessionStatus sessionStatus) {
         this.sessionStatus = sessionStatus;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateAsSavedSession(Long id) {
+        if (nonNull(this.id)) {
+            return;
+        }
+        this.id = id;
+    }
+
+    public void updateEnrolledUsers(EnrolledUsers enrolledUsers) {
+        this.enrolledUsers = enrolledUsers;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public int getMaxNumberOfEnrollment() {
+        return sessionType.getMaxNumberOfEnrollment();
+    }
+
+    public long getFee() {
+        return sessionType.getFee();
+    }
+
+    public SessionStatus getSessionStatus() {
+        return sessionStatus;
+    }
+
+    public LocalDateTime getStartedAt() {
+        return periodOfSession.getStartedAt();
+    }
+
+    public LocalDateTime getEndedAt() {
+        return periodOfSession.getEndedAt();
+    }
+
+    public CoverImage getCoverImage() {
+        return coverImage;
+    }
+
+    public Long getIdOfCoverImage() {
+        return coverImage.getId();
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Long getIdOfCourse() {
+        return course.getId();
+    }
+
+    public List<NsUser> getEnrolledUsers() {
+        return enrolledUsers.getEnrolledUsers();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
     }
 }
