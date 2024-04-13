db2cd9dfb31fdfbd85d80f51f6a21026cdbfb67d
hong yuseok <jericoh@naver.com>
2024-04-14 04:27:25 +0900

    test: data.sql과 일치하도록 수정

--- src/test/java/nextstep/users/domain/NsUserTest.java
+++ src/test/java/nextstep/users/domain/NsUserTest.java
@@ -1,7 +1,7 @@
 package nextstep.users.domain;
 
 public class NsUserTest {
-    public static final NsUser JAVAJIGI = new NsUser(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-    public static final NsUser SANJIGI = new NsUser(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
-    public static final NsUser ZIPJIGI = new NsUser(3L, "zipjigi", "password", "name", "zipjigi@slipp.net");
+    public static final NsUser JAVAJIGI = new NsUser(1L, "javajigi", "password", "자바지기", "javajigi@slipp.net");
+    public static final NsUser SANJIGI = new NsUser(2L, "sanjigi", "password", "산지기", "sanjigi@slipp.net");
+    public static final NsUser ZIPJIGI = new NsUser(3L, "zipjigi", "password", "집지기", "zipjigi@slipp.net");
 }
