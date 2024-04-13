48c539e85706479eb41afc0d31f2b89e6798b566
박소민 <qkrthals0524@naver.com>
2024-04-14 01:16:08 +0900

    학생 객체 추가

---
+++ src/main/java/nextstep/session/domain/Student.java
@@ -0,0 +1,12 @@
+package nextstep.session.domain;
+
+public class Student {
+
+    private final Long id;
+    private final String name;
+
+    public Student(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
