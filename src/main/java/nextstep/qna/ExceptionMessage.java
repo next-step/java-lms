ad33d6e3679b973b94a48d9aaac945d2a00f78a2
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-04-14 13:54:29 +0900

    fix: 리팩토링

--- src/main/java/nextstep/qna/ExceptionMessage.java
+++ src/main/java/nextstep/qna/ExceptionMessage.java
@@ -1,7 +1,7 @@
 package nextstep.qna;
 
 public enum ExceptionMessage {
-    NO_AUTHORITY_TO_DELETE_ANSWER("답변을 삭제할 권한이 없습니다."),
+    NO_AUTHORITY_TO_DELETE_ANSWER("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다."),
     NO_AUTHORITY_TO_DELETE_QUESTION("질문을 삭제할 권한이 없습니다.");
 
     private final String message;
