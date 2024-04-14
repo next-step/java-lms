bbff957036e43583f58afeb06d805ee27d748e4e
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-14 17:25:51 +0900

    Refactor: 상태가 변경하기 전에 유효성 검사를 진행하도록 리팩터링하라

--- src/main/java/nextstep/sessions/domain/CountOfStudent.java
+++ src/main/java/nextstep/sessions/domain/CountOfStudent.java
@@ -18,10 +18,10 @@ public class CountOfStudent {
     }
 
     public void increaseCountOfStudents() {
-        this.currentCountOfStudents++;
-        if (sessionType.isCapacityExceeded(currentCountOfStudents, maxOfStudents)) {
-            throw new IllegalArgumentException(String.format("이 강의의 현재 수강 신청 인원: (%s)명, 최대 수강 인원: (%s)명이므로 현재 마감이 된 상태입니다.", currentCountOfStudents, maxOfStudents));
+        if (sessionType.isCapacityExceeded(currentCountOfStudents + 1, maxOfStudents)) {
+            throw new IllegalArgumentException(String.format("이 강의의 현재 수강 신청 인원: (%s)명, 최대 수강 인원: (%s)명이므로 현재 마감이 된 상태입니다.", currentCountOfStudents + 1, maxOfStudents));
         }
+        this.currentCountOfStudents++;
     }
 
 }
