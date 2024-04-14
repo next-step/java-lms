96337a3f1112046f9bb53f126b583f965b7f55b1
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-04-14 15:37:21 +0900

    feat: SessionDate

---
+++ src/main/java/nextstep/courses/domain/SessionDate.java
@@ -0,0 +1,21 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDateTime;
+
+public class SessionDate {
+	private static final String START_DATE_CANT_LATE_THEN_END_DATE = "강의 시작일은 종료일 이전이어야 합니다.";
+	private final LocalDateTime startDate;
+	private final LocalDateTime endDate;
+
+	private SessionDate(LocalDateTime startDate, LocalDateTime endDate) {
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public static SessionDate of(LocalDateTime startDate, LocalDateTime endDate) {
+		if (startDate.isAfter(endDate)) {
+			throw new IllegalArgumentException(START_DATE_CANT_LATE_THEN_END_DATE);
+		}
+		return new SessionDate(startDate, endDate);
+	}
+}
