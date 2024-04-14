19e2c38b2d25ae693e4ea9aeb63da5c375724711
Sangmin <sm9171@nate.com>
2024-04-14 19:59:09 +0900

    refactor: ImageHeight, ImageWidth 래퍼클래스 생성 및 테스트 성공 * '이미지의 width는 300픽셀, height는 200픽셀 이상이어야 하며, width와 height의 비율은 3:2여야 한다.' 테스트 성공

---
+++ src/main/java/nextstep/courses/domain/ImageHeight.java
@@ -0,0 +1,20 @@
+package nextstep.courses.domain;
+
+public class ImageHeight {
+    private int height;
+
+    public ImageHeight(int height) {
+        validateHeight(height);
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    private void validateHeight(final int height) {
+        if (height < 200) {
+            throw new IllegalArgumentException("이미지 높이는 200px보다 커야 합니다.");
+        }
+    }
+}
