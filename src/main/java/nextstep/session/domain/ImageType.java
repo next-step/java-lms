e562977ea2de80980b1fabdc02c28201ec8c0382
박소민 <qkrthals0524@naver.com>
2024-04-15 22:46:53 +0900

    ImageType 객체 내 nameConcat, isSame메서드 추가 및 validate 로직 수정

--- src/main/java/nextstep/session/domain/ImageType.java
+++ src/main/java/nextstep/session/domain/ImageType.java
@@ -1,17 +1,22 @@
 package nextstep.session.domain;
 
-import nextstep.session.InvalidImageConditionsException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public enum ImageType {
     gif, jpg, jpeg, png, svg;
 
-    public static boolean validateType(String input) throws InvalidImageConditionsException {
-        try {
-            ImageType.valueOf(input.trim().toLowerCase());
-            return true;
-        } catch (IllegalArgumentException e) {
-            return false;
+    public static boolean validateType(String input) {
+        return Arrays.stream(ImageType.values())
+            .anyMatch(imageType -> imageType.isSame(input.toLowerCase()));
+    }
 
-        }
+    public static String nameConcat(String delimiter) {
+        return Arrays.stream(ImageType.values()).map(Enum::name).collect(
+            Collectors.joining(delimiter));
+    }
+
+    public boolean isSame(String type) {
+        return this.name() == type;
     }
 }
