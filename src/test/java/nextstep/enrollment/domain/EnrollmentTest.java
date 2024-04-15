f0d55377c761c0cbcfab49fac57f543e2be5b180
leeedohyun <ldh19738@gmail.com>
2024-04-15 15:49:56 +0900

    test: 리팩토링으로 인해 실패하는 테스트 성공하도록 로직 수정

--- src/test/java/nextstep/enrollment/domain/EnrollmentTest.java
+++ src/test/java/nextstep/enrollment/domain/EnrollmentTest.java
@@ -1,11 +1,12 @@
 package nextstep.enrollment.domain;
 
-import static nextstep.enrollment.domain.EnrollmentStatus.*;
+import static nextstep.enrollment.domain.EnrollmentStatus.APPROVED;
 import static nextstep.enrollment.domain.EnrollmentStatus.REGISTERED;
 import static nextstep.sessions.domain.SessionProgressStatus.END;
 import static nextstep.sessions.domain.SessionProgressStatus.PREPARING;
 import static nextstep.sessions.domain.SessionRecruitingStatus.NON_RECRUITING;
 import static nextstep.sessions.domain.SessionRecruitingStatus.RECRUITING;
+import static nextstep.users.domain.NsUserTest.JAVAJIGI;
 import static nextstep.users.domain.NsUserType.WOOTECAM;
 import static nextstep.users.domain.NsUserType.WOOTECO;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,17 +20,20 @@ import org.junit.jupiter.api.Test;
 import nextstep.courses.domain.Course;
 import nextstep.sessions.domain.Session;
 import nextstep.users.domain.NsUser;
-import nextstep.users.domain.NsUserTest;
 
 class EnrollmentTest {
 
     private static final long SESSION_PRICE = 10000L;
 
+    private NsUser user1;
+    private NsUser user2;
     private Course course;
     private Session session;
 
     @BeforeEach
     void setUp() {
+        user1 = new NsUser(3L, "lee", "password", "name", "lee@slipp.net", WOOTECO);
+        user2 = new NsUser(4L, "kim", "password", "name", "lee@slipp.net", WOOTECAM);
         course = new Course("TDD, 클린 코드 with Java", 1L);
         session = new Session(2, RECRUITING, PREPARING, SESSION_PRICE, LocalDateTime.now().plusDays(1),
                 LocalDateTime.now().plusDays(2), course);
@@ -37,8 +41,8 @@ class EnrollmentTest {
 
     @Test
     void 강의_최대_수강_인원을_초과한_경우_수강_신청에_실패한다() {
-        new Enrollment(session, NsUserTest.JAVAJIGI).enroll(SESSION_PRICE);
-        new Enrollment(session, NsUserTest.SANJIGI).enroll(SESSION_PRICE);
+        enrollAndApprove(user1);
+        enrollAndApprove(user2);
 
         assertThatIllegalArgumentException().isThrownBy(
                 () -> new Enrollment(session, new NsUser(3L, "lee", "password", "name", "lee@slipp.net", WOOTECO)).enroll(SESSION_PRICE)
@@ -49,7 +53,7 @@ class EnrollmentTest {
     void 모집중이_아닌_강의인_경우_수강_신청을_하면_실패한다() {
         final Session session = new Session(5, NON_RECRUITING, PREPARING, 100000L, LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2), course);
 
-        assertThatIllegalArgumentException().isThrownBy(() -> new Enrollment(session, NsUserTest.JAVAJIGI).enroll(100000L))
+        assertThatIllegalArgumentException().isThrownBy(() -> new Enrollment(session, JAVAJIGI).enroll(100000L))
                 .withMessage("모집중인 강의가 아닙니다.");
     }
 
@@ -57,21 +61,21 @@ class EnrollmentTest {
     void 시작일이_지난_강의를_수강_신청을_하면_실패한다() {
         final Session session = new Session(5, RECRUITING, PREPARING, 100000L, LocalDateTime.now().plusDays(1), course);
 
-        assertThatIllegalArgumentException().isThrownBy(() -> new Enrollment(session, NsUserTest.JAVAJIGI).enroll(100000L))
+        assertThatIllegalArgumentException().isThrownBy(() -> new Enrollment(session, JAVAJIGI).enroll(100000L))
                 .withMessage("시작일이 지난 강의는 수강 신청할 수 없습니다.");
     }
 
     @Test
     void 결제_금액과_강의_금액이_일치하지_않으면_실패한다() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Enrollment(session, NsUserTest.JAVAJIGI).enroll(20000L));
+                .isThrownBy(() -> new Enrollment(session, JAVAJIGI).enroll(20000L));
     }
 
     @Test
     void 무료_강의_수강_신청한다() {
         // given
         final Session freeSession = Session.free(RECRUITING, PREPARING,course);
-        final Enrollment enrollment = new Enrollment(freeSession, NsUserTest.JAVAJIGI);
+        final Enrollment enrollment = new Enrollment(freeSession, JAVAJIGI);
 
         // when
         enrollment.enroll(0L);
@@ -83,7 +87,7 @@ class EnrollmentTest {
     @Test
     void 유료_강의_수강_신청한다() {
         // given
-        final Enrollment enrollment = new Enrollment(session, NsUserTest.JAVAJIGI);
+        final Enrollment enrollment = new Enrollment(session, JAVAJIGI);
 
         // when
         enrollment.enroll(SESSION_PRICE);
@@ -94,10 +98,10 @@ class EnrollmentTest {
 
     @Test
     void 이미_수강_신청한_강의인_경우_수강_신청에_실패한다() {
-        new Enrollment(session, NsUserTest.JAVAJIGI).enroll(SESSION_PRICE);
+        enrollAndApprove(user1);
 
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Enrollment(session, new NsUser(1L, "javajigi", "password", "name", "javajigi@slipp.net", WOOTECAM)).enroll(SESSION_PRICE))
+                .isThrownBy(() -> new Enrollment(session, user1).enroll(SESSION_PRICE))
                 .withMessage("이미 수강 신청한 사용자입니다.");
     }
 
@@ -108,7 +112,7 @@ class EnrollmentTest {
                 LocalDateTime.now().plusDays(2), course);
 
         // when
-        final Enrollment enrollment = new Enrollment(session, NsUserTest.JAVAJIGI);
+        final Enrollment enrollment = new Enrollment(session, JAVAJIGI);
 
         // then
         assertThatIllegalArgumentException().isThrownBy(() -> enrollment.enroll(SESSION_PRICE))
@@ -118,14 +122,19 @@ class EnrollmentTest {
     @Test
     void 선발된_인원인_경우_강사가_승인한다() {
         // given
-        final NsUser user = new NsUser(3L, "lee", "password", "name", "lee@slipp.net", WOOTECO);
-        final Enrollment enrollment = new Enrollment(session, user);
+        final Enrollment enrollment = new Enrollment(session, user1);
         enrollment.enroll(SESSION_PRICE);
 
         // when
-        enrollment.approveBy(NsUserTest.JAVAJIGI);
+        enrollment.approveBy(JAVAJIGI);
 
         // then
         assertThat(enrollment.getStatus()).isEqualTo(APPROVED);
+    }
+
+    private void enrollAndApprove(final NsUser attendee) {
+        final Enrollment enrollment = new Enrollment(session, attendee);
+        enrollment.enroll(SESSION_PRICE);
+        enrollment.approveBy(JAVAJIGI);
     }
 }
--- src/test/java/nextstep/sessions/domain/SessionTest.java
+++ src/test/java/nextstep/sessions/domain/SessionTest.java
@@ -1,18 +1,28 @@
 package nextstep.sessions.domain;
 
 import static nextstep.sessions.domain.SessionProgressStatus.PREPARING;
-import static nextstep.sessions.domain.SessionRecruitingStatus.*;
+import static nextstep.sessions.domain.SessionRecruitingStatus.RECRUITING;
+import static nextstep.users.domain.NsUserType.WOOTECO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import nextstep.courses.domain.Course;
 import nextstep.enrollment.domain.Enrollment;
+import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUserTest;
 
 class SessionTest {
+
+    private NsUser user;
+
+    @BeforeEach
+    void setUp() {
+        user = new NsUser(3L, "lee", "password", "name", "lee@slipp.net", WOOTECO);
+    }
 
     @Test
     void 수강_인원이_가득_찼으면_true를_반환한다() {
@@ -20,7 +30,9 @@ class SessionTest {
         final Course course = new Course("TDD, 클린 코드 with Java", 1L);
         final Session session = new Session(1, RECRUITING, PREPARING, 100000L, LocalDateTime.now().plusDays(1),
                 LocalDateTime.now().plusDays(2), course);
-        new Enrollment(session, NsUserTest.JAVAJIGI).enroll(100000L);
+        final Enrollment enrollment = new Enrollment(session, user);
+        enrollment.enroll(100000L);
+        enrollment.approveBy(NsUserTest.JAVAJIGI);
 
         // when
         final boolean isFull = session.isFull();
@@ -49,8 +61,9 @@ class SessionTest {
         final Course course = new Course("TDD, 클린 코드 with Java", 1L);
         final Session session = new Session(4, RECRUITING, PREPARING, 100000L, LocalDateTime.now().plusDays(1),
                 LocalDateTime.now().plusDays(2), course);
-        final Enrollment enrollment = new Enrollment(session, NsUserTest.JAVAJIGI);
+        final Enrollment enrollment = new Enrollment(session, user);
         enrollment.enroll(100000L);
+        enrollment.approveBy(NsUserTest.JAVAJIGI);
 
         // when
         final boolean canNotEnroll = session.canEnroll(enrollment);
