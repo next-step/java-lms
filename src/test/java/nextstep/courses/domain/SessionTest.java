8bf96441ee2960f46f71a06effbf926c35e83140
jaeyounglee <ljyda214@gmail.com>
2024-04-16 01:01:37 +0900

    FEAT : 테스트 코드 세분화

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SessionTest {
 
-    public static final Session sessionA = new Session(true, 0, 0);
+    public static final Session sessionA = new Session();
     public static final Session sessionB = new Session(true,20, 20000, SessionStatus.RECRUITING);
 
     @Test
@@ -20,20 +20,32 @@ public class SessionTest {
     }
 
     @Test
-    void 유료_강의는_최대_수강_인원을_초과할_수_없다() {
+    void 유료_강의는_수강_인원이_최대_수강_인원_이내이면_들을_수_있다() {
         assertThat(sessionB.isLeft(new Amount(20))).isTrue();
+    }
+
+    @Test
+    void 유료_강의는_최대_수강_인원을_초과할_수_없다() {
         assertThat(sessionB.isLeft(new Amount(21))).isFalse();
     }
 
     @Test
     void 유료_강의는_수강생이_결제한_금액과_수강료가_일치할때_수강신청이_가능하다() {
         assertThat(sessionB.canListen(new Money(20000))).isTrue();
+    }
+
+    @Test
+    void 유료_강의는_수강생이_결제한_금액과_수강료가_일치하지_않으면_수강_신청_불가하다() {
         assertThat(sessionB.canListen(new Money(19000))).isFalse();
     }
 
     @Test
-    void 강의_수강신청은_강의__상태가_모집중일_때만_가능하다() {
-        assertThat(sessionA.canRegister()).isFalse();
+    void 강의_수강신청은_강의_상태가_모집중일_때만_가능하다() {
         assertThat(sessionB.canRegister()).isTrue();
+    }
+
+    @Test
+    void 강의_수강신청은_강의_상태가_대기중_종료일_때_불가하다() {
+        assertThat(sessionA.canRegister()).isFalse();
     }
 }
