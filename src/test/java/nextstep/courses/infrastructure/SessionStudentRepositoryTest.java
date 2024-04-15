9d2cd1337c2f1fa5114b9caa13f73677ae309a9b
hellonayeon <hellonykwon@gmail.com>
2024-04-15 22:11:57 +0900

    Test: SessionStudentRepository 조회된 ROW 가 없는 경우의 학습 테스트 작성

--- src/test/java/nextstep/courses/infrastructure/SessionStudentRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/SessionStudentRepositoryTest.java
@@ -4,12 +4,14 @@ import nextstep.courses.domain.enrollment.SessionStudent;
 import nextstep.courses.infrastructure.engine.SessionStudentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 
@@ -18,6 +20,7 @@ import static nextstep.courses.domain.fixture.SessionStudentFixture.student;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class SessionStudentRepositoryTest {
 
     @Autowired
@@ -50,9 +53,22 @@ public class SessionStudentRepositoryTest {
         SessionStudent student2 = student(SESSION_ID, 2L);
         sessionStudentRepository.save(student2);
 
-        List<SessionStudent> saveEntity = sessionStudentRepository.findAllBySessionId(SESSION_ID);
+        List<SessionStudent> students = sessionStudentRepository.findAllBySessionId(SESSION_ID);
 
-        assertThat(saveEntity.get(index).getNsUserId()).isEqualTo(nsUserId);
+        assertThat(students.get(index).getNsUserId()).isEqualTo(nsUserId);
+    }
+
+    @Nested
+    @DisplayName("SessionStudentRepository 학습 테스트")
+    class SessionStudentRepositoryLearningTest {
+
+        @Test
+        @DisplayName("[성공] 리스트 조회 시 ROW 가 없는 경우 빈 리스트를 반환한다.")
+        void 빈_리스트() {
+            List<SessionStudent> students = sessionStudentRepository.findAllBySessionId(9999L);
+            assertThat(students).isEmpty();
+        }
+
     }
 
 }
