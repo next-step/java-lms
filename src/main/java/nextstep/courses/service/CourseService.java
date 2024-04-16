1c4287a5fdf2761ffd8e34554e6e1c3008f211a3
hongtaek oh <oht1221@gmail.com>
2024-04-16 20:37:55 +0900

    4단계(요구사항 변경)
    - 접근 제어자 추가

--- src/main/java/nextstep/courses/service/CourseService.java
+++ src/main/java/nextstep/courses/service/CourseService.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 public class CourseService {
   @Autowired
-  RegistrationRepository registrationRepository;
+  private RegistrationRepository registrationRepository;
 
   @Transactional
   public void approve(Long userId, Long sessionId) {
