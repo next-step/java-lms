fd94afc24689aef79e1f99fdd341a10af5b7d649
hong yuseok <jericoh@naver.com>
2024-04-16 23:52:58 +0900

    refactor: 선발된 인원만 수강 가능하도록 수정(3) - Enrollments에 대한 테스트 구현

--- src/test/java/nextstep/users/domain/NsUserTest.java
+++ src/test/java/nextstep/users/domain/NsUserTest.java
@@ -4,4 +4,5 @@ public class NsUserTest {
     public static final NsUser JAVAJIGI = new NsUser(1L, "javajigi", "password", "자바지기", "javajigi@slipp.net");
     public static final NsUser SANJIGI = new NsUser(2L, "sanjigi", "password", "산지기", "sanjigi@slipp.net");
     public static final NsUser ZIPJIGI = new NsUser(3L, "zipjigi", "password", "집지기", "zipjigi@slipp.net");
+    public static final NsUser BAPZIGI = new NsUser(4L, "bapjigi", "password", "밥지기", "bapjigi@slipp.net");
 }
