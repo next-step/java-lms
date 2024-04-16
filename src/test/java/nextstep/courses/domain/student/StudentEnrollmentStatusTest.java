51f0f4ee8571b7131f9bd9716eae1ac2f05e525e
hellonayeon <hellonykwon@gmail.com>
2024-04-17 00:52:51 +0900

    Test: 수강생 신청 상태 단위 테스트 작서

--- src/test/java/nextstep/courses/domain/student/StudentEnrollmentStatusTest.java
+++ src/test/java/nextstep/courses/domain/student/StudentEnrollmentStatusTest.java
@@ -1,6 +1,6 @@
-package nextstep.courses.domain.status;
+package nextstep.courses.domain.student;
 
-import nextstep.courses.exception.SessionRecruitmentStatusInvalidException;
+import nextstep.courses.exception.StudentEnrollmentStatusInvalidException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,33 +9,33 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static nextstep.courses.domain.status.RecruitmentStatus.NOT_RECRUITMENT;
-import static nextstep.courses.domain.status.RecruitmentStatus.RECRUITMENT;
+import static nextstep.courses.domain.student.StudentEnrollmentStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class SessionRecruitmentStatusTest {
+public class StudentEnrollmentStatusTest {
 
     @ParameterizedTest(name = "[{index}] {0} -> {1}")
     @MethodSource("statusFixture")
-    @DisplayName("[성공] 문자열을 모집 상태 객체로 변환한다.")
-    void 모집_상태_변환(String statusString, RecruitmentStatus status) {
-        assertThat(RecruitmentStatus.convert(statusString)).isEqualTo(status);
+    @DisplayName("[성공] 문자열을 c 객체로 변환한다.")
+    void 수강생_신청_상태_변환(String statusString, StudentEnrollmentStatus status) {
+        assertThat(StudentEnrollmentStatus.convert(statusString)).isEqualTo(status);
     }
 
     @Test
-    @DisplayName("[실패] 모집 상태에 없는 문자열을 객체로 변환할 경우 SessionRecruitmentStatusInvalidException 예외가 발생한다.")
-    void 모집_상태가_아닌_문자열_변환() {
-        assertThatExceptionOfType(SessionRecruitmentStatusInvalidException.class)
+    @DisplayName("[실패] 수강생 신청 상태에 없는 문자열을 객체로 변환할 경우 StudentEnrollmentStatusInvalidException 예외가 발생한다.")
+    void 수강생_신청_상태가_아닌_문자열_변환() {
+        assertThatExceptionOfType(StudentEnrollmentStatusInvalidException.class)
                 .isThrownBy(() -> {
-                    RecruitmentStatus.convert("모집 상태에 없는 문자열");
+                    StudentEnrollmentStatus.convert("수강생 신청 상태에 없는 문자열");
                 });
     }
 
     static Stream<Arguments> statusFixture() {
         return Stream.of(
-                Arguments.of("모집중", RECRUITMENT),
-                Arguments.of("비모집중", NOT_RECRUITMENT)
+                Arguments.of("대기중", PENDING),
+                Arguments.of("승인", APPROVAL),
+                Arguments.of("취소", CANCEL)
         );
     }
 
