a4364cf9ae07e75137b4e099f02c65515885b7a1
동원 <ai06021@gmail.com>
2024-04-16 15:23:41 +0900

    refactor : SessionStatus Enum

--- src/main/java/nextstep/courses/domain/Status.java
+++ src/main/java/nextstep/courses/domain/Status.java
@@ -6,12 +6,20 @@ public enum Status {
     END("end"),
     RECRUIT("recruit");
 
-    final String status;
+    private final String status;
     Status(String status) {
         this.status = status;
     }
 
     public String getStatus() {
         return status;
+    }
+
+    public static Status fromString(String status) {
+        for (Status state : Status.values()) {
+            if (state.getStatus().equals(status))
+                return state;
+        }
+        throw new IllegalArgumentException(status + "는 잘못된 강의 상태 입니다");
     }
 }
