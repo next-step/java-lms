0bef2ed3e214dff3365d9a6725e9f159c14dde20
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-17 06:23:55 +0900

    Feat: capacity 값 객체 생성

---
+++ src/main/java/nextstep/sessions/domain/image/Capacity.java
@@ -0,0 +1,35 @@
+package nextstep.sessions.domain.image;
+
+import java.util.Objects;
+
+public class Capacity {
+
+    private static final int CAPACITY = 1024 * 1024;
+
+    private final int capacity;
+
+    public Capacity(int capacity) {
+        validateCapacity(capacity);
+        this.capacity = capacity;
+    }
+
+    private void validateCapacity(int capacity) {
+        if (capacity > CAPACITY) {
+            throw new IllegalArgumentException(String.format("이미지크기는 1MB(%s)를 초과할 수 없다", CAPACITY));
+        }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Capacity capacity1 = (Capacity) object;
+        return capacity == capacity1.capacity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(capacity);
+    }
+
+}
--- src/main/java/nextstep/sessions/domain/image/Image.java
+++ src/main/java/nextstep/sessions/domain/image/Image.java
@@ -1,23 +1,19 @@
 package nextstep.sessions.domain.image;
 
 public class Image {
-    private static final int CAPACITY = 1024 * 1024;
 
-    private final int capacity;
+    private final Capacity capacity;
     private final ImageType imageType;
     private final ImageSize size;
 
     public Image(int capacity, ImageType imageType, ImageSize size) {
-        validateCapacity(capacity);
+        this(new Capacity(capacity), imageType, size);
+    }
+
+    public Image(Capacity capacity, ImageType imageType, ImageSize size) {
         this.capacity = capacity;
         this.imageType = imageType;
         this.size = size;
-    }
-
-    private void validateCapacity(int capacity) {
-        if (capacity > CAPACITY) {
-            throw new IllegalArgumentException(String.format("이미지크기는 1MB(%s)를 초과할 수 없다", CAPACITY));
-        }
     }
 
 }
