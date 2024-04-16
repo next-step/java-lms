5bec6919a6a7980766a895668eee103604c21da6
namhyeop <skaguq@gmail.com>
2024-04-17 02:22:20 +0900

    feat : DTO 분리 (피드백 반영)

--- src/main/java/nextstep/courses/service/lmsService.java
+++ src/main/java/nextstep/courses/service/lmsService.java
@@ -2,14 +2,15 @@ package nextstep.courses.service;
 
 import nextstep.courses.domain.course.Course;
 import nextstep.courses.domain.session.SessionName;
-import nextstep.payments.domain.Payment;
+import nextstep.courses.dto.RequestSessionDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class lmsService {
 
-    public boolean registerSession(String courseTitle, String sessionName, Payment payment){
-        Course course = new Course(courseTitle, 1L);
-        return course.registerSession(new SessionName(sessionName), payment);
+    public boolean registerSession(RequestSessionDto requestSessionDto) {
+        Course course = new Course(requestSessionDto.getCourseTitle(), 1L);
+        return course.registerSession(new SessionName(requestSessionDto.getSessionName()),
+            requestSessionDto.getPayment());
     }
 }
