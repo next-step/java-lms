4b01f734b792e4a4487d756a91647226755f34d1
jaeyounglee <ljyda214@gmail.com>
2024-04-18 01:02:30 +0900

    CHG : java 내 Duration 클래스와 구분하기 위해 클래스 이름 변경

--- src/main/java/nextstep/courses/domain/Period.java
+++ src/main/java/nextstep/courses/domain/Period.java
@@ -2,16 +2,16 @@ package nextstep.courses.domain;
 
 import java.time.LocalDate;
 
-public class Duration {
+public class Period {
 
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public Duration() {
+    public Period() {
         this(LocalDate.now(), LocalDate.now().plusDays(60));
     }
 
-    public Duration(LocalDate startDate, LocalDate endDate) {
+    public Period(LocalDate startDate, LocalDate endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
     }
