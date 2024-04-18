4d5afe5d685528c57627889538a8e6e487071889
kylekim2123 <kyle2123@naver.com>
2024-04-18 18:53:01 +0900

    [STEP 3] test: 수강 신청 시 인원 증가에 대한 테스트 코드 작성

--- src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import nextstep.courses.domain.session.Session;
 import nextstep.courses.domain.session.SessionRepository;
 import nextstep.courses.domain.session.SessionTest;
+import nextstep.payments.domain.Payment;
 
 @JdbcTest
+@Transactional
 class SessionRepositoryTest {
 
     @Autowired
@@ -30,8 +33,28 @@ class SessionRepositoryTest {
     @DisplayName("새로운 강의를 생성한다.")
     void Save_NewSession() {
         final Session newSession = SessionTest.session();
-        final int savedCount = sessionRepository.save(newSession);
+        final long savedSessionId = sessionRepository.save(newSession);
+        newSession.assignId(savedSessionId);
 
-        assertThat(savedCount).isOne();
+        assertThat(newSession)
+                .isEqualTo(sessionRepository.findById(savedSessionId).get());
+    }
+
+    @Test
+    @DisplayName("수강 신청을 하면, 현재 수강 신청 인원이 증가한다.")
+    void Enroll_IncreaseEnrollmentCount() {
+        Session session = SessionTest.session();
+        final long savedSessionId = sessionRepository.save(session);
+
+        session = sessionRepository.findById(savedSessionId).get();
+        assertThat(session.currentEnrollmentCount())
+                .isZero();
+
+        session.enroll(new Payment(10000));
+        sessionRepository.updateEnrollmentCount(session);
+
+        session = sessionRepository.findById(savedSessionId).get();
+        assertThat(session.currentEnrollmentCount())
+                .isOne();
     }
 }
