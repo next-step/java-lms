2c0dbc8cd4874a23b171481eccba77401beb23b9
박소민 <qkrthals0524@naver.com>
2024-04-18 23:14:57 +0900

    get 메서드 추가, status convert 메서드 추가

--- src/main/java/nextstep/session/domain/SessionStatus.java
+++ src/main/java/nextstep/session/domain/SessionStatus.java
@@ -1,5 +1,8 @@
 package nextstep.session.domain;
 
+import java.util.Arrays;
+import nextstep.session.SessionStatusInvalidException;
+
 public enum SessionStatus {
     PREPARING("준비중"),
     RECRUITING("모집중"),
@@ -13,5 +16,16 @@ public enum SessionStatus {
 
     public boolean isSessionRecruiting() {
         return this == RECRUITING;
+    }
+
+    public static SessionStatus convert(String status) {
+        return Arrays.stream(values())
+            .filter(sessionType -> sessionType.description.equals(status))
+            .findAny()
+            .orElseThrow(() -> new SessionStatusInvalidException("존재하지 않는 상태입니다."));
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
