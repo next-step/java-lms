ac2980972b20af6ff4e44270319f572652d62edc
namhyeop <skaguq@gmail.com>
2024-04-18 21:07:41 +0900

    feat : MethodSource 적용 (피드백 반영)

--- src/test/java/nextstep/courses/domain/session/impl/PaidSessionTest.java
+++ src/test/java/nextstep/courses/domain/session/impl/PaidSessionTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 import nextstep.courses.domain.cover.Image;
 import nextstep.courses.domain.cover.ImageHeight;
 import nextstep.courses.domain.cover.ImageSize;
@@ -18,6 +19,8 @@ import nextstep.courses.error.exception.MaxRegistrationCountNotZero;
 import nextstep.payments.domain.Money;
 import nextstep.payments.domain.Payment;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PaidSessionTest {
 
@@ -28,47 +31,41 @@ class PaidSessionTest {
             .hasMessage("최대 등록수는 0일수 없습니다 입력값: 0");
     }
 
-    @Test
-    void 강의_최대_수강_인원을_넘지_않을_경우_강의_신청이_가능해야한다() {
-        Session paidSession = createSuccessPaidSession();
-
+    @ParameterizedTest
+    @MethodSource("createSuccessPaidSession")
+    void 강의_최대_수강_인원을_넘지_않을_경우_강의_신청이_가능해야한다(Session paidSession) {
         assertThat(paidSession.isRegistrationAvailable()).isTrue();
     }
 
-    @Test
-    void 강의_최대_수강_인원을_넘을_경우_강의_신청이_불가능해야한다() {
-        Session paidSession = createSessionWithExceededMaxRegistrationCount();
-
+    @ParameterizedTest
+    @MethodSource("createSessionWithExceededMaxRegistrationCount")
+    void 강의_최대_수강_인원을_넘을_경우_강의_신청이_불가능해야한다(Session paidSession) {
         assertThat(paidSession.isRegistrationAvailable()).isFalse();
     }
 
-    @Test
-    void 유료강의는_수강생이_결제한_금액과_수강료가_일치하는_경우_강의_신청이_가능하다() {
-        Session paidSession = createSuccessPaidSession();
-
+    @ParameterizedTest
+    @MethodSource("createSuccessPaidSession")
+    void 유료강의는_수강생이_결제한_금액과_수강료가_일치하는_경우_강의_신청이_가능하다(Session paidSession) {
         assertThat(paidSession.isPaymentAmountSameTuitionFee(
             new Payment("1", 1L, 1L, new Money(1000)))).isTrue();
     }
 
-    @Test
-    void 유료강의는_수강생이_결제한_금액과_수강료가_일치하지_않은_경우_강의_신청이_불가능하다() {
-        Session paidSession = createSuccessPaidSession();
-
+    @ParameterizedTest
+    @MethodSource("createSuccessPaidSession")
+    void 유료강의는_수강생이_결제한_금액과_수강료가_일치하지_않은_경우_강의_신청이_불가능하다(Session paidSession) {
         assertThat(paidSession.isPaymentAmountSameTuitionFee(
             new Payment("1", 1L, 1L, new Money(500)))).isFalse();
     }
 
-    @Test
-    void 유료_강의_수강신청시_강의_상태가_모집중이라면_수강_신청이_가능해야한다() {
-        Session paidSession = createSuccessPaidSession();
-
+    @ParameterizedTest
+    @MethodSource("createSuccessPaidSession")
+    void 유료_강의_수강신청시_강의_상태가_모집중이라면_수강_신청이_가능해야한다(Session paidSession) {
         assertThat(paidSession.isRegistrationAvailable()).isTrue();
     }
 
-    @Test
-    void 유료_강의_수강신청시_강의_상태가_모집중이_아니라면_수강_신청이_불가능해야한다() {
-        Session paidSession = createSessionStatusPreparingSession();
-
+    @ParameterizedTest
+    @MethodSource("createSessionStatusPreparingSession")
+    void 유료_강의_수강신청시_강의_상태가_모집중이_아니라면_수강_신청이_불가능해야한다(Session paidSession) {
         assertThat(paidSession.isRegistrationAvailable()).isFalse();
     }
 
@@ -84,8 +81,8 @@ class PaidSessionTest {
             SessionStatus.RECRUITING);
     }
 
-    private PaidSession createSessionWithExceededMaxRegistrationCount(){
-        return new PaidSession(
+    static Stream<PaidSession> createSessionWithExceededMaxRegistrationCount() {
+        return Stream.of(new PaidSession(
             new SessionName("강의이름"),
             new RegistrationCount(4),
             new MaxRegistrationCount(new RegistrationCount(3)),
@@ -93,11 +90,11 @@ class PaidSessionTest {
             new Image(new ImageSize(1), ImageType.JPG, new ImageWidth(300), new ImageHeight(200)),
             LocalDateTime.now(),
             LocalDateTime.now(),
-            SessionStatus.RECRUITING);
+            SessionStatus.RECRUITING));
     }
 
-    private PaidSession createSuccessPaidSession() {
-        return new PaidSession(
+    static Stream<PaidSession> createSuccessPaidSession() {
+        return Stream.of(new PaidSession(
             new SessionName("강의이름"),
             new RegistrationCount(2),
             new MaxRegistrationCount(new RegistrationCount(3)),
@@ -105,11 +102,11 @@ class PaidSessionTest {
             new Image(new ImageSize(1), ImageType.JPG, new ImageWidth(300), new ImageHeight(200)),
             LocalDateTime.now(),
             LocalDateTime.now(),
-            SessionStatus.RECRUITING);
+            SessionStatus.RECRUITING));
     }
 
-    private PaidSession createSessionStatusPreparingSession() {
-        return new PaidSession(
+    static Stream<PaidSession> createSessionStatusPreparingSession() {
+        return Stream.of(new PaidSession(
             new SessionName("강의이름"),
             new RegistrationCount(1),
             new MaxRegistrationCount(new RegistrationCount(3)),
@@ -117,6 +114,6 @@ class PaidSessionTest {
             new Image(new ImageSize(1), ImageType.JPG, new ImageWidth(300), new ImageHeight(200)),
             LocalDateTime.now(),
             LocalDateTime.now(),
-            SessionStatus.PREPARING);
+            SessionStatus.PREPARING));
     }
 }
