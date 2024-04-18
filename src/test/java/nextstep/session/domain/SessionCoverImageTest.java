b5e55221e37f4aa14c5f867e01b5ac2092f428cb
박소민 <qkrthals0524@naver.com>
2024-04-18 23:13:32 +0900

    id 값 입력없이 커버이미지 생성하도록 수정

--- src/test/java/nextstep/session/domain/SessionCoverImageTest.java
+++ src/test/java/nextstep/session/domain/SessionCoverImageTest.java
@@ -11,7 +11,7 @@ class SessionCoverImageTest {
     @Test
     public void 이미지는_1MB를_초과할_수_없다() {
         assertThatThrownBy(() -> {
-            new SessionCoverImage(900, 600, 10000000, "png");
+            new SessionCoverImage(1L, 900, 600, 10000000, "png");
         }).isInstanceOf(InvalidImageConditionsException.class)
             .hasMessageContaining("이미지 크기는"
                 + "1MB 이하여야합니다. 입력된 이미지 크기 9MB");
@@ -20,14 +20,14 @@ class SessionCoverImageTest {
     @Test
     public void 이미지는_정해진_확장자만_추가_가능하다() {
         assertThatThrownBy(() -> {
-            new SessionCoverImage(900, 600, 10000, "BMP");
+            new SessionCoverImage(1L, 900, 600, 10000, "BMP");
         }).isInstanceOf(InvalidImageConditionsException.class)
             .hasMessageContaining("명시된 확장자만 추가 가능합니다.gif,jpg,jpeg,png,svg");
     }
 
     @Test
     public void 이미지_생성() throws InvalidImageConditionsException {
-        SessionCoverImage coverImage = new SessionCoverImage(900, 600, 10000, "png");
+        SessionCoverImage coverImage = new SessionCoverImage(1L, 900, 600, 10000, "png");
         assertThat(coverImage.isSameImageType(String.valueOf(ImageType.png))).isTrue();
     }
 }
