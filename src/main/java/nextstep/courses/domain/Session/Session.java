a9a177b2a914319f157d85be664e8f03fe85c68a
socar-cloud <cloud@socar.kr>
2024-04-19 17:21:30 +0900

    feat: 피드백 반영

--- src/main/java/nextstep/courses/domain/Session/Session.java
+++ src/main/java/nextstep/courses/domain/Session/Session.java
@@ -3,24 +3,53 @@ package nextstep.courses.domain.Session;
 import nextstep.courses.domain.image.Image;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Session {
-    private final LocalDate startDate;
-    private final LocalDate endDate;
+    private final Duration duration;
     private final Image coverImage;
-    private final Type type;
     private final int fee;
     private final int maxStudents;
-    private Students students;
-
+    private final Students students = new Students();
     private Status status = Status.READY;
 
-    Session(LocalDate startDate, LocalDate endDate, Image coverImage, Type type, int fee, int maxStudents) {
-        this.startDate = startDate;
-        this.endDate = endDate;
+    public Session(LocalDate startDate, LocalDate endDate, Image coverImage) {
+        this.duration = new Duration(startDate, endDate);
         this.coverImage = coverImage;
-        this.type = type;
+        this.fee = 0;
+        this.maxStudents = Integer.MAX_VALUE;
+    }
+
+    public Session(LocalDate startDate, LocalDate endDate, Image coverImage, int fee, int maxStudents) {
+        this.duration = new Duration(startDate, endDate);
+        this.coverImage = coverImage;
         this.fee = fee;
         this.maxStudents = maxStudents;
+    }
+
+    public void apply(List<Student> studentList) {
+        if (!status.isApplying()) {
+            throw new RuntimeException("수강 신청은 강의 상태가 모집중이 아니면 불가능합니다. 강의 상태 : " + status);
+        }
+
+        Students newStudents = new Students(studentList);
+
+        if (isPaySession()) {
+            newStudents.isPaidSessionFee(fee);
+
+            if (students.getCounts() + newStudents.getCounts() > maxStudents) {
+                throw new RuntimeException("유료 강의는 최대 수강인원을 초과할 수 없습니다. 최대수강인원 : " + maxStudents);
+            }
+        }
+
+        this.students.addAll(students);
+    }
+
+    public void changeStatus(Status status) {
+        this.status = status;
+    }
+
+    private boolean isPaySession() {
+        return this.fee > 0;
     }
 }
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,6 +1,6 @@
 package nextstep.courses.domain;
 
-import nextstep.courses.domain.Session.PaySession;
+import nextstep.courses.domain.Session.Session;
 import nextstep.courses.domain.Session.Status;
 import nextstep.courses.domain.Session.Student;
 import nextstep.courses.domain.image.Image;
@@ -21,70 +21,75 @@ public class SessionTest {
     private final int fee = 10000;
     private final int maxStudents = 1;
 
-    private PaySession paySession;
+    private Session session;
 
     @BeforeEach
     public void setUp() {
-        paySession = new PaySession(startDate, endDate, coverImage, fee, maxStudents);
+        session = new Session(startDate, endDate, coverImage, fee, maxStudents);
     }
-
 
     @Test
     public void 무료강의는_최대수강인원_제한이_없다() {
-        // 어떻게 테스트 작성해야할지?
+        List<Student> studentList = createStudentList(2, 0);
+
+        Session freeSession = new Session(startDate, endDate, coverImage, 0, 1);
+
+        freeSession.changeStatus(Status.APPLYING);
+
+        assertDoesNotThrow(() -> freeSession.apply(studentList));
     }
 
     @Test
-    public void 유료강의는_강의최대수강인원을_초과할수없다() {
+    public void 강의는_강의최대수강인원을_초과할수없다() {
         List<Student> studentList = createStudentList(2, 10000);
 
-        paySession.changeStatus(Status.APPLYING);
+        session.changeStatus(Status.APPLYING);
 
-        assertThrows(RuntimeException.class, () -> paySession.apply(studentList));
+        assertThrows(RuntimeException.class, () -> session.apply(studentList));
     }
 
     @Test
-    public void 유료강의는_수강생이_결제한금액과_수강료가_일치할때_수강신청이_가능하다() {
-        PaySession paySession = new PaySession(startDate, endDate, coverImage, fee, maxStudents);
+    public void 강의는_수강생이_결제한금액과_수강료가_일치할때_수강신청이_가능하다() {
+        Session Session = new Session(startDate, endDate, coverImage, fee, maxStudents);
 
         List<Student> studentList = createStudentList(1, 10000);
 
-        paySession.changeStatus(Status.APPLYING);
+        Session.changeStatus(Status.APPLYING);
 
-        assertDoesNotThrow(() -> paySession.apply(studentList));
+        assertDoesNotThrow(() -> Session.apply(studentList));
     }
 
     @Test
-    public void 유료강의는_수강생이_결제한금액과_수강료가_일치하지않으면_수강신청이_불가능하다() {
-        PaySession paySession = new PaySession(startDate, endDate, coverImage, fee, maxStudents);
+    public void 강의는_수강생이_결제한금액과_수강료가_일치하지않으면_수강신청이_불가능하다() {
+        Session Session = new Session(startDate, endDate, coverImage, fee, maxStudents);
 
         List<Student> studentList = createStudentList(1, 20000);
 
-        paySession.changeStatus(Status.APPLYING);
+        Session.changeStatus(Status.APPLYING);
 
-        assertThrows(RuntimeException.class, () -> paySession.apply(studentList));
+        assertThrows(RuntimeException.class, () -> Session.apply(studentList));
     }
 
     @Test
     public void 강의_수강신청은_강의_상태가_모집중일때만_가능하다() {
-        PaySession paySession = new PaySession(startDate, endDate, coverImage, fee, maxStudents);
+        Session Session = new Session(startDate, endDate, coverImage, fee, maxStudents);
 
         List<Student> studentList = createStudentList(1, 10000);
 
-        paySession.changeStatus(Status.APPLYING);
+        Session.changeStatus(Status.APPLYING);
 
-        assertDoesNotThrow(() -> paySession.apply(studentList));
+        assertDoesNotThrow(() -> Session.apply(studentList));
     }
 
     @Test
     public void 강의_수강신청은_강의_상태가_모집중이_아니면_불가능하다() {
-        PaySession paySession = new PaySession(startDate, endDate, coverImage, fee, maxStudents);
+        Session Session = new Session(startDate, endDate, coverImage, fee, maxStudents);
 
         List<Student> studentList = createStudentList(1, 10000);
 
-        paySession.changeStatus(Status.COMPLETE);
+        Session.changeStatus(Status.COMPLETE);
 
-        assertThrows(RuntimeException.class, () -> paySession.apply(studentList));
+        assertThrows(RuntimeException.class, () -> Session.apply(studentList));
     }
 
     private List<Student> createStudentList(int number, int fee) {
