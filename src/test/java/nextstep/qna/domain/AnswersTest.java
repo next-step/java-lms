4fecba0d1afd029abeede0c105127e78319f0d62
kimsunghyun <tjdgus2319@gmail.com>
2024-04-19 15:37:16 +0900

    test: Answers 삭제 테스트

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -2,6 +2,7 @@ package nextstep.qna.domain;
 
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,25 +13,46 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswersTest {
 
+	private static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+	private static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+	private Answers sameOwnerAnswers;
+	private Answers otherOwnerAnswers;
+
+	@BeforeEach
+	public void setUp() {
+		List<Answer> otherAnswerList = new ArrayList<>();
+		otherAnswerList.add(AnswerTest.A1);
+		otherAnswerList.add(AnswerTest.A2);
+		otherOwnerAnswers = new Answers(otherAnswerList);
+
+		List<Answer> sameAnswerList = new ArrayList<>();
+		sameAnswerList.add(AnswerTest.A1);
+		sameAnswerList.add(AnswerTest.A1);
+		sameOwnerAnswers = new Answers(sameAnswerList);
+	}
+
 	@Test
 	public void 다른사람이_쓴글이_존재시_확인_테스트() {
-		List<Answer> answerList = new ArrayList<>();
-		answerList.add(AnswerTest.A1);
-		answerList.add(AnswerTest.A2);
-		Answers answers = new Answers(answerList);
-		assertThatThrownBy(() -> answers.checkOwner(NsUserTest.SANJIGI))
+		assertThatThrownBy(() -> otherOwnerAnswers.checkOwner(NsUserTest.JAVAJIGI))
 				.isInstanceOf(CannotDeleteException.class)
 				.hasMessageContaining("다른 사람");
 	}
 
 	@Test
 	public void 자신만_쓴글이_존재시_테스트() {
-		List<Answer> answerList = new ArrayList<>();
-		answerList.add(AnswerTest.A1);
-		answerList.add(AnswerTest.A1);
-		Answers answers = new Answers(answerList);
-		assertThat(answers.isOwners(NsUserTest.JAVAJIGI)).isEqualTo(true);
+		assertThat(sameOwnerAnswers.isOwners(NsUserTest.JAVAJIGI)).isEqualTo(true);
 	}
 
+	@Test
+	public void 답변_전체삭제_실패_테스트() {
+		assertThatThrownBy(() -> otherOwnerAnswers.setDeleteAnswers(NsUserTest.JAVAJIGI));
+	}
+
+	@Test
+	public void 답변_전체삭제_성공_테스트() throws CannotDeleteException {
+		sameOwnerAnswers.setDeleteAnswers(NsUserTest.JAVAJIGI);
+		assertThat(sameOwnerAnswers.getAnswer(0).isDeleted()).isTrue();
+	}
 
 }
