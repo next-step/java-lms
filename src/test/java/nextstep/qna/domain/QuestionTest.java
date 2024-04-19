95cd41cecb4bbfe42158698834e25459f54a16f6
kimsunghyun <tjdgus2319@gmail.com>
2024-04-19 15:38:00 +0900

    test: question 삭제 테스트

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,19 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    public void 질문삭제_정상동작_확인테스트() throws CannotDeleteException {
+        Question question = Q1.setDeleted(NsUserTest.JAVAJIGI);
+        assertThat(question.isDeleted()).isTrue();
+    }
+
 }
