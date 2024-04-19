70077e5c28d774afb62befcb375e98f4be5b2a75
kimsunghyun <tjdgus2319@gmail.com>
2024-04-19 15:38:57 +0900

    feat: Question에서 Answers에 삭제 메시지 전달

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -7,86 +8,87 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Question {
-    private Long id;
+	private Long id;
 
-    private String title;
+	private String title;
 
-    private String contents;
+	private String contents;
 
-    private NsUser writer;
+	private NsUser writer;
 
-    private List<Answer> answers = new ArrayList<>();
+	private List<Answer> answers = new ArrayList<>();
 
-    private boolean deleted = false;
+	private boolean deleted = false;
 
-    private LocalDateTime createdDate = LocalDateTime.now();
+	private LocalDateTime createdDate = LocalDateTime.now();
 
-    private LocalDateTime updatedDate;
+	private LocalDateTime updatedDate;
 
-    public Question() {
-    }
+	public Question() {
+	}
 
-    public Question(NsUser writer, String title, String contents) {
-        this(0L, writer, title, contents);
-    }
+	public Question(NsUser writer, String title, String contents) {
+		this(0L, writer, title, contents);
+	}
 
-    public Question(Long id, NsUser writer, String title, String contents) {
-        this.id = id;
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
-    }
+	public Question(Long id, NsUser writer, String title, String contents) {
+		this.id = id;
+		this.writer = writer;
+		this.title = title;
+		this.contents = contents;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public NsUser getWriter() {
+		return writer;
+	}
 
-    public Question setTitle(String title) {
-        this.title = title;
-        return this;
-    }
+	public void addAnswer(Answer answer) {
+		answer.toQuestion(this);
+		answers.add(answer);
+	}
 
-    public String getContents() {
-        return contents;
-    }
+	public boolean isOwner(NsUser loginUser) {
+		return writer.equals(loginUser);
+	}
 
-    public Question setContents(String contents) {
-        this.contents = contents;
-        return this;
-    }
+	public Question setDeleted(boolean deleted) {
+		this.deleted = deleted;
+		return this;
+	}
 
-    public NsUser getWriter() {
-        return writer;
-    }
+	public Question setDeleted(NsUser register) throws CannotDeleteException {
+		if (!this.isOwner(register)) {
+			throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+		}
 
-    public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
-        answers.add(answer);
-    }
+		this.deleted = true;
 
-    public boolean isOwner(NsUser loginUser) {
-        return writer.equals(loginUser);
-    }
+		if (!answers.isEmpty()) {
+			deletedAnswers();
+		}
 
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
+		return this;
+	}
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+	private void deletedAnswers() throws CannotDeleteException {
+		Answers answersCollection = new Answers(answers);
+		answersCollection.setDeleteAnswers(writer);
+	}
 
-    public List<Answer> getAnswers() {
-        return answers;
-    }
+	public boolean isDeleted() {
+		return deleted;
+	}
 
-    @Override
-    public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
-    }
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+	}
 }
