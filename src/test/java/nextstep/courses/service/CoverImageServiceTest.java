3a7ede1cefb39ac458f84a7e8a622f61afb25dbd
kylekim2123 <kyle2123@naver.com>
2024-04-21 01:45:18 +0900

    [STEP 3] test: CoverImageService 테스트 코드 작성

---
+++ src/test/java/nextstep/courses/service/CoverImageServiceTest.java
@@ -0,0 +1,30 @@
+package nextstep.courses.service;
+
+import static nextstep.courses.domain.session.image.CoverImageTest.COVER_IMAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import nextstep.courses.domain.session.image.CoverImage;
+
+@SpringBootTest
+class CoverImageServiceTest {
+
+    @Autowired
+    private CoverImageService coverImageService;
+
+    @Test
+    @Transactional
+    @DisplayName("새로운 커버 이미지를 생성한다.")
+    void Save_NewCoverImage() {
+        final Long savedCoverImageId = coverImageService.save(COVER_IMAGE, 1L);
+        final CoverImage savedCoverImage = coverImageService.findById(savedCoverImageId);
+
+        assertThat(savedCoverImage.id())
+                .isEqualTo(savedCoverImageId);
+    }
+}
