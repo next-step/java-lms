b00b41e06f06424e054d84b99cda40d89097c03d
namhyeop <skaguq@gmail.com>
2024-04-21 22:46:49 +0900

    feat : 유효기간 객체 추가

---
+++ src/main/java/nextstep/courses/domain/session/ValidityPeriod.java
@@ -0,0 +1,33 @@
+package nextstep.courses.domain.session;
+
+import java.time.LocalDateTime;
+import nextstep.courses.error.exception.EndDateBeforeStartDateException;
+import nextstep.courses.error.exception.NotExistTime;
+
+public class ValidityPeriod {
+
+    private final LocalDateTime startDate;
+
+    private final LocalDateTime endDate;
+
+    public ValidityPeriod(LocalDateTime startDate, LocalDateTime endDate) {
+        if (startDate == null || endDate == null){
+            throw new NotExistTime(startDate, endDate);
+        }
+
+        if (endDate.isBefore(startDate)) {
+            throw new EndDateBeforeStartDateException(startDate, endDate);
+        }
+
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+}
