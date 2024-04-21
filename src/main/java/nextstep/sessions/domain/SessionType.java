d62232c34fc9a2a392080277ad06da15031d4122
Sangmin <sm9171@nate.com>
2024-04-21 19:14:45 +0900

    refactor: sessionType description 필드 추가

--- src/main/java/nextstep/sessions/domain/SessionType.java
+++ src/main/java/nextstep/sessions/domain/SessionType.java
@@ -1,5 +1,12 @@
 package nextstep.sessions.domain;
 
 public enum SessionType {
-	FREE, PAID
+	FREE("무료강의"),
+	PAID("유료강의");
+
+	private final String description;
+
+	SessionType(String description) {
+		this.description = description;
+	}
 }
