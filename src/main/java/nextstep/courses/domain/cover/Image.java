71e7fd6a30507a1094810eddd50f8735cdb8683a
namhyeop <skaguq@gmail.com>
2024-04-21 22:49:56 +0900

    feat : Image 객체 생성자 추가

--- src/main/java/nextstep/courses/domain/cover/Image.java
+++ src/main/java/nextstep/courses/domain/cover/Image.java
@@ -1,8 +1,11 @@
 package nextstep.courses.domain.cover;
 
 import nextstep.courses.domain.BaseEntity;
+import nextstep.courses.entity.ImageEntity;
 
 public class Image extends BaseEntity {
+
+    private Long id;
 
     private final ImageSize size;
 
@@ -17,11 +20,35 @@ public class Image extends BaseEntity {
         return Math.abs(ratio - 1.5) < 0.001; // 허용 가능한 오차 범위 내에서 비율이 3:2인지 확인
     }
 
+    public Image(ImageEntity imageEntity){
+        this(imageEntity.getId(),
+            new ImageSize(imageEntity.getSize()),
+            ImageType.valueOf(imageEntity.getType()),
+            new ImageWidth(imageEntity.getWidth()),
+            new ImageHeight(imageEntity.getHeight()));
+    }
+
     public Image(ImageSize size, ImageType type, ImageWidth width, ImageHeight height) {
+        this(null, size, type, width, height);
+    }
+    public Image(Long id, ImageSize size, ImageType type, ImageWidth width, ImageHeight height) {
+        this.id = id;
         this.size = size;
         this.type = type;
         this.width = width;
         this.height = height;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getSize() {
+        return size.getValue();
+    }
+
+    public String  getType() {
+        return type.getValue();
     }
 
     public int getWidth() {
