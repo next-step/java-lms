7abe0357d4acacde0a6d5416b5d676d21d9d07ea
astrid-dm <himax_01@naver.com>
2024-04-22 02:01:04 +0900

    chore : Course의 패키지 경로 변경

--- src/test/java/nextstep/courses/domain/course/CourseTest.java
+++ src/test/java/nextstep/courses/domain/course/CourseTest.java
@@ -1,8 +1,7 @@
-package nextstep.courses.domain.session;
+package nextstep.courses.domain.course;
 
-import nextstep.courses.domain.Course;
+import nextstep.courses.domain.session.*;
 import nextstep.courses.domain.session.image.CoverImage;
-import nextstep.users.domain.Users;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -29,10 +28,10 @@ class CourseTest {
         SessionPeriod sessionPeriod = SessionPeriod.of(
                 LocalDateTime.of(2024,1,1,0,0,0),
                 LocalDateTime.of(2024,4,1,0,0,0));
-        CoverImage coverImage = CoverImage.of("jpg", 1024, 300,200);
+        List<CoverImage> coverImages = List.of(CoverImage.of(1L,"jpg", 1024, 300,200));
 
-        return new PaidSession(sessionId, sessionPeriod, coverImage,
-                SessionStatusEnum.OPEN, 1, fee);
+        return new PaidSession(sessionId, sessionPeriod, coverImages,
+                SessionStatusEnum.OPEN, true, 1, 1, fee);
     }
 
     private FreeSession getFreeSession() {
@@ -40,9 +39,9 @@ class CourseTest {
         SessionPeriod sessionPeriod = SessionPeriod.of(
                 LocalDateTime.of(2024,1,1,0,0,0),
                 LocalDateTime.of(2024,4,1,0,0,0));
-        CoverImage coverImage = CoverImage.of("jpg", 1024, 300,200);
+        List<CoverImage> coverImages = List.of(CoverImage.of(1L,"jpg", 1024, 300,200));
 
-        return new FreeSession(sessionId, sessionPeriod, coverImage, SessionStatusEnum.OPEN);
+        return new FreeSession(sessionId, sessionPeriod, coverImages, SessionStatusEnum.OPEN, 0, true);
     }
 
 }
