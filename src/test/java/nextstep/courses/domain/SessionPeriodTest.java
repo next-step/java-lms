91c9de148f2a8b8d8cef77fc773f8a16b1457c13
dongock <okdong23@naver.com>
2024-04-21 18:20:16 +0900

    refactor:[2-12] Exception Test -> assertThatIllegalArgumentException 적용

--- src/test/java/nextstep/courses/domain/SessionPeriodTest.java
+++ src/test/java/nextstep/courses/domain/SessionPeriodTest.java
@@ -6,16 +6,15 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class SessionPeriodTest {
     
     @Test
-    @DisplayName("강의 기간 중 강의 시작일이 8자리 형식이 아닐 때 오류를 발생시킨다.")
+    @DisplayName("강의 종료 날짜가 강의 시작 날짜보다 빠를 시 오류를 발생시킨다.")
     void sessionStartDateValidateTest() {
-        assertThatThrownBy(
-                () -> new SessionPeriod(LocalDate.of(2024, 04, 8), LocalDate.of(2024, 04, 7))
-        ).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("종료 날짜는 시작 날짜보다 빠를 수 없습니다.");
+        assertThatIllegalArgumentException().isThrownBy(
+                        () -> new SessionPeriod(LocalDate.of(2024, 04, 8), LocalDate.of(2024, 04, 7))
+                ).withMessageContaining("종료 날짜는 시작 날짜보다 빠를 수 없습니다.");
     }
 }
