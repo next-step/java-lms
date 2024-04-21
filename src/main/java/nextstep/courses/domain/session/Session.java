9c6ba88454c45407a70f56d95ebe91fd99a76658
namhyeop <skaguq@gmail.com>
2024-04-21 23:01:16 +0900

    feat : Session-freeSession을 위한 default 메소드 추가 및 DB 접근을 위한 Getter 추가

--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -1,5 +1,7 @@
 package nextstep.courses.domain.session;
 
+import java.time.LocalDateTime;
+import nextstep.courses.domain.cover.Image;
 import nextstep.payments.domain.Payment;
 
 public interface Session {
@@ -8,9 +10,31 @@ public interface Session {
         return SessionStatus.RECRUITING == sessionStatus;
     }
 
+    default int getMaxRegistrationCount(){
+        return new MaxRegistrationCount(new RegistrationCount(Integer.MAX_VALUE)).getRegistrationCount();
+    };
+
     void addRegistrationCount();
 
     boolean isRegistrationAvailable();
 
     boolean isPaymentAmountSameTuitionFee(Payment payment);
+
+    Long getId();
+
+    String getSessionName();
+
+    int getRegistrationCount();
+
+    int getTuitionFee();
+
+    Image getImage();
+
+    String getSessionStatus();
+
+    ValidityPeriod getValidityPeriod();
+
+    LocalDateTime getCreatedAt();
+
+    LocalDateTime getUpdatedAt();
 }
