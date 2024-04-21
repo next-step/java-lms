10ff72088dcc4aee6fb356baf21679a9cbc83b96
namhyeop <skaguq@gmail.com>
2024-04-21 22:57:23 +0900

    feat : SessionBuilder 추가

--- src/test/java/nextstep/courses/fixture/builder/PaidSessionBuilder.java
+++ src/test/java/nextstep/courses/fixture/builder/PaidSessionBuilder.java
@@ -1,6 +1,7 @@
 package nextstep.courses.fixture.builder;
 
 import nextstep.courses.domain.cover.Image;
+import nextstep.courses.domain.session.MaxRegistrationCount;
 import nextstep.courses.domain.session.RegistrationCount;
 import nextstep.courses.domain.session.SessionName;
 import nextstep.courses.domain.session.SessionStatus;
@@ -8,10 +9,13 @@ import nextstep.courses.domain.session.ValidityPeriod;
 import nextstep.courses.domain.session.impl.FreeSession;
 import nextstep.payments.domain.Money;
 
-public class FreeSessionBuilder {
+public class PaidSessionBuilder {
+
     private SessionName name;
 
     private RegistrationCount registrationCount;
+
+    private MaxRegistrationCount maxRegistrationCount;
 
     private Money tuitionFee;
 
@@ -21,28 +25,38 @@ public class FreeSessionBuilder {
 
     private ValidityPeriod validityPeriod;
 
-    private FreeSessionBuilder() {}
+    private PaidSessionBuilder() {}
 
-    public static FreeSessionBuilder anFreeSession() {
-        return new FreeSessionBuilder();
+    public static PaidSessionBuilder anFreeSession() {
+        return new PaidSessionBuilder();
     }
 
-    public FreeSessionBuilder withName(String name) {
+    public PaidSessionBuilder withName(String name) {
         this.name = new SessionName(name);
         return this;
     }
 
-    public FreeSessionBuilder withImage(Image image) {
+    public PaidSessionBuilder withMaxRegistrationCount(int maxRegistrationCount) {
+        this.maxRegistrationCount = new MaxRegistrationCount(new RegistrationCount(maxRegistrationCount));
+        return this;
+    }
+
+    public PaidSessionBuilder withTuitionFee(int tuitionFee) {
+        this.tuitionFee = new Money(tuitionFee);
+        return this;
+    }
+
+    public PaidSessionBuilder withImage(Image image) {
         this.image = image;
         return this;
     }
 
-    public FreeSessionBuilder withSessionStatus(SessionStatus status) {
+    public PaidSessionBuilder withSessionStatus(SessionStatus status) {
         this.sessionStatus = status;
         return this;
     }
 
-    public FreeSessionBuilder withValidityPeriod(ValidityPeriod validityPeriod) {
+    public PaidSessionBuilder withValidityPeriod(ValidityPeriod validityPeriod) {
         this.validityPeriod = validityPeriod;
         return this;
     }
