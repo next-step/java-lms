c0783b689a82cd80043b6d6fad534ae2f49760ae
kimsunghyun <tjdgus2319@gmail.com>
2024-04-22 18:20:14 +0900

    feat: 강의 상태 enum 클래스 생성

---
+++ src/main/java/nextstep/courses/domain/SessionStatus.java
@@ -0,0 +1,18 @@
+package nextstep.courses.domain;
+
+public enum SessionStatus {
+	PREPARING(0),
+	PROCEEDING(1),
+	CLOSED(2);
+
+	private int statusValue;
+
+	SessionStatus(int statusValue) {
+		this.statusValue = statusValue;
+	}
+
+	public int getStatusValue() {
+		return statusValue;
+	}
+
+}
