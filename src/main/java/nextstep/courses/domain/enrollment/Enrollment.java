4cdb60810c4ebc39f09562a08624b7f99b9517e6
namhyeop <skaguq@gmail.com>
2024-04-24 03:15:26 +0900

    feat : 불필요한 코드 삭제

--- src/main/java/nextstep/courses/domain/enrollment/Enrollment.java
+++ src/main/java/nextstep/courses/domain/enrollment/Enrollment.java
@@ -1,15 +1,10 @@
 package nextstep.courses.domain.enrollment;
 
 import nextstep.courses.domain.session.Session;
-import nextstep.courses.domain.session.SessionStatus;
 import nextstep.payments.domain.Money;
 import nextstep.payments.domain.Payment;
 
 public interface Enrollment {
-
-    default boolean isRecruitmentOpen(SessionStatus sessionStatus) {
-        return SessionStatus.RECRUITING == sessionStatus;
-    }
 
     void enroll(Session session, Payment payment);
 
