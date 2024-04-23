8ba8cc24a8d44bde35f6226b960a4340cfa3c10b
jaeyounglee <ljyda214@gmail.com>
2024-04-24 00:41:23 +0900

    FEAT : 이미지 확장자를 enum 으로 변경하여 구현

--- src/main/java/nextstep/courses/domain/FileExtension.java
+++ src/main/java/nextstep/courses/domain/FileExtension.java
@@ -1,13 +1,11 @@
 package nextstep.courses.domain;
 
-import java.util.Set;
-
 public class FileExtension {
 
     private final String extension;
 
     public FileExtension(String extension) {
-        if (!ApprovedExtension.contains(extension)) {
+        if (!ImageExtension.contains(extension)) {
             throw new IllegalArgumentException("적절하지 못한 확장자 정보입니다.");
         }
         this.extension = extension;
