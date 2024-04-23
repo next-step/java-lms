46da59ac7dd1667a449bff709841bee8c3602bbc
jaeyounglee <ljyda214@gmail.com>
2024-04-24 00:47:43 +0900

    CHG : 기준값을 상수 처리

--- src/main/java/nextstep/courses/domain/Size.java
+++ src/main/java/nextstep/courses/domain/Size.java
@@ -2,10 +2,11 @@ package nextstep.courses.domain;
 
 public class Size {
 
+    public static final int MAX_SIZE = 1024;
     private final int value;
 
     public Size(int value) {
-        if ( value > 1024) {
+        if ( value > MAX_SIZE) {
             throw new IllegalArgumentException("이미지 파일 크기는 1MB 이하여야 합니다.");
         }
         this.value = value;
