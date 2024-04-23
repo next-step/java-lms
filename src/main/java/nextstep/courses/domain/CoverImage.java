10af78931a1ce8fff7817210cb066190cc3cfb5d
jaeyounglee <ljyda214@gmail.com>
2024-04-24 00:43:18 +0900

    CHG : 클래스 이름 변경

--- src/main/java/nextstep/courses/domain/CoverImage.java
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -1,21 +1,13 @@
 package nextstep.courses.domain;
 
-import java.io.File;
-import java.util.Set;
-
-public class ImageFile {
+public class CoverImage {
 
     private final ImageInfo imageInfo;
     private final FileExtension extension;
 
 
-    public ImageFile(ImageInfo imageInfo, String extension) {
+    public CoverImage(ImageInfo imageInfo, String extension) {
         this.imageInfo = imageInfo;
         this.extension = new FileExtension(extension);
     }
-
-    public boolean hasApprovedExtension() {
-        return false;
-    }
-
 }
