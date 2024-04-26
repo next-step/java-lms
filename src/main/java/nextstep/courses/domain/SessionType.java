4ddcace5518c349e9907aa03caa8525512a9d22b
kimsunghyun <tjdgus2319@gmail.com>
2024-04-26 15:37:05 +0900

    fix: 잘못된 네이밍 삭제

--- src/main/java/nextstep/courses/domain/SessionType.java
+++
@@ -1,7 +0,0 @@
-package nextstep.courses.domain;
-
-public interface SessionType {
-	boolean isMaximumNumberOfParticipantsLimited(int NumberOfParticipants);
-
-	boolean isSamePaymentAndSessionPrice(int price);
-}
