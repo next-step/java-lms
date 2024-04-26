c028a693afb71cb5ecc376cee7c2fa80441a63c3
namhyeop <skaguq@gmail.com>
2024-04-26 22:13:50 +0900

    feat : Image <-> ImageEntity 의존성 제거

--- src/main/java/nextstep/courses/domain/cover/Image.java
+++ src/main/java/nextstep/courses/domain/cover/Image.java
@@ -1,7 +1,6 @@
 package nextstep.courses.domain.cover;
 
 import nextstep.courses.entity.BaseEntity;
-import nextstep.courses.entity.ImageEntity;
 
 public class Image extends BaseEntity {
 
@@ -20,17 +19,10 @@ public class Image extends BaseEntity {
         return Math.abs(ratio - 1.5) < 0.001; // 허용 가능한 오차 범위 내에서 비율이 3:2인지 확인
     }
 
-    public Image(ImageEntity imageEntity){
-        this(imageEntity.getId(),
-            new ImageSize(imageEntity.getSize()),
-            ImageType.valueOf(imageEntity.getType()),
-            new ImageWidth(imageEntity.getWidth()),
-            new ImageHeight(imageEntity.getHeight()));
-    }
-
     public Image(ImageSize size, ImageType type, ImageWidth width, ImageHeight height) {
         this(null, size, type, width, height);
     }
+
     public Image(Long id, ImageSize size, ImageType type, ImageWidth width, ImageHeight height) {
         this.id = id;
         this.size = size;
