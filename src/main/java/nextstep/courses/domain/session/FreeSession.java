25ba2248a11d97dacb205127253f9b38ac375db6
namhyeop <skaguq@gmail.com>
2024-04-26 22:38:22 +0900

    feat : Session <-> SessionEntity Cyclic Dependency제거

--- src/main/java/nextstep/courses/domain/session/FreeSession.java
+++ src/main/java/nextstep/courses/domain/session/FreeSession.java
@@ -3,31 +3,10 @@ package nextstep.courses.domain.session;
 import nextstep.courses.domain.cover.Image;
 import nextstep.courses.domain.session.engine.Session;
 import nextstep.courses.domain.session.enrollment.Enrollment;
-import nextstep.courses.domain.session.enrollment.SessionEnrollment;
-import nextstep.courses.domain.session.enrollment.state.SessionState;
-import nextstep.courses.domain.session.feetype.FeeType;
 import nextstep.courses.domain.session.name.SessionName;
 import nextstep.courses.domain.session.period.Period;
-import nextstep.courses.entity.SessionEntity;
-import nextstep.courses.factory.EnrollmentCountFactory;
-import nextstep.payments.domain.Money;
 
 public class FreeSession extends Session {
-
-    public FreeSession(SessionEntity sessionEntity) {
-        this(sessionEntity.getId(),
-            new SessionName(sessionEntity.getSessionName()),
-            new SessionEnrollment(
-                EnrollmentCountFactory.get(FeeType.valueOf(sessionEntity.getFeeType()),
-                    sessionEntity.getRegistrationCount(),
-                    sessionEntity.getMaxRegistrationCount()),
-                new SessionState(
-                    SessionState.valueOfRecruitmentState(sessionEntity.getRecruitmentState())),
-                new Money(sessionEntity.getTuitionFee()),
-                FeeType.valueOf(sessionEntity.getFeeType())),
-            null,
-            new Period(sessionEntity.getStartDate(), sessionEntity.getEndDate()));
-    }
 
     public FreeSession(Session session, Enrollment enrollment) {
         this(session.getId(), new SessionName(session.getSessionName()), enrollment,
--- src/main/java/nextstep/courses/factory/SessionFactory.java
+++ src/main/java/nextstep/courses/factory/SessionFactory.java
@@ -3,9 +3,15 @@ package nextstep.courses.factory;
 import nextstep.courses.domain.session.FreeSession;
 import nextstep.courses.domain.session.PaidSession;
 import nextstep.courses.domain.session.engine.Session;
+import nextstep.courses.domain.session.enrollment.SessionEnrollment;
+import nextstep.courses.domain.session.enrollment.count.MaxRegistrationCount;
+import nextstep.courses.domain.session.enrollment.state.SessionState;
 import nextstep.courses.domain.session.feetype.FeeType;
+import nextstep.courses.domain.session.name.SessionName;
+import nextstep.courses.domain.session.period.Period;
 import nextstep.courses.entity.SessionEntity;
 import nextstep.courses.error.exception.NotExistSessionType;
+import nextstep.payments.domain.Money;
 
 public class SessionFactory {
 
@@ -14,11 +20,34 @@ public class SessionFactory {
 
     public static Session get(SessionEntity sessionEntity) {
         if (FeeType.PAID == FeeType.valueOf(sessionEntity.getFeeType())) {
-            return new PaidSession(sessionEntity);
+            return new PaidSession(sessionEntity.getId(),
+                new SessionName(sessionEntity.getSessionName()),
+                new SessionEnrollment(
+                    EnrollmentCountFactory.get(FeeType.valueOf(sessionEntity.getFeeType()),
+                        sessionEntity.getRegistrationCount(),
+                        sessionEntity.getMaxRegistrationCount()),
+                    new SessionState(
+                        SessionState.valueOfRecruitmentState(sessionEntity.getRecruitmentState())),
+                    new Money(sessionEntity.getTuitionFee()),
+                    FeeType.valueOf(sessionEntity.getFeeType())),
+                null,
+                new Period(sessionEntity.getStartDate(), sessionEntity.getEndDate()),
+                new MaxRegistrationCount(sessionEntity.getRegistrationCount()));
         }
 
         if (FeeType.FREE == FeeType.valueOf(sessionEntity.getFeeType())) {
-            return new FreeSession(sessionEntity);
+            return new FreeSession(sessionEntity.getId(),
+                new SessionName(sessionEntity.getSessionName()),
+                new SessionEnrollment(
+                    EnrollmentCountFactory.get(FeeType.valueOf(sessionEntity.getFeeType()),
+                        sessionEntity.getRegistrationCount(),
+                        sessionEntity.getMaxRegistrationCount()),
+                    new SessionState(
+                        SessionState.valueOfRecruitmentState(sessionEntity.getRecruitmentState())),
+                    new Money(sessionEntity.getTuitionFee()),
+                    FeeType.valueOf(sessionEntity.getFeeType())),
+                null,
+                new Period(sessionEntity.getStartDate(), sessionEntity.getEndDate()));
         }
 
         throw new NotExistSessionType();
