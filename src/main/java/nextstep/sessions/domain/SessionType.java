c6d5cce1bd3d142963d50bfbce14b0c3587562da
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-27 13:19:03 +0900

    Test: Students#register exception이 발생하는지 테스트하라

--- src/main/java/nextstep/sessions/domain/SessionType.java
+++ src/main/java/nextstep/sessions/domain/SessionType.java
@@ -2,7 +2,7 @@ package nextstep.sessions.domain;
 
 public enum SessionType {
     FREE((currentCountOfStudents, maxOfStudents) -> false),
-    PAID((currentCountOfStudents, maxOfStudents) -> currentCountOfStudents > maxOfStudents);
+    PAID((currentCountOfStudents, maxOfStudents) -> currentCountOfStudents >= maxOfStudents);
 
     private final CapacityExceededCheck capacityExceededCheck;
 
--- src/test/java/nextstep/sessions/domain/StudentsTest.java
+++ src/test/java/nextstep/sessions/domain/StudentsTest.java
@@ -3,6 +3,7 @@ package nextstep.sessions.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StudentsTest {
     @Test
@@ -16,5 +17,18 @@ public class StudentsTest {
 
         // after register(..)
         assertThat(students.countOfStudents()).isEqualTo(1);
+    }
+
+    @Test
+    void registerException() {
+        Students students = new Students(3);
+
+        students.register(new Student(1L, 1L));
+        students.register(new Student(2L, 1L));
+        students.register(new Student(3L, 1L));
+
+        assertThatThrownBy(() -> students.register(new Student(4L, 1L)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이 강의의 현재 수강 신청 인원: (3)명, 최대 수강 인원: (3)명이므로 현재 마감이 된 상태입니다.");
     }
 }
