65fcd33da9f7209f5b1f11222452a7b06eda3762
namhyeop <skaguq@gmail.com>
2024-04-28 23:36:38 +0900

    feat : userRepository 쿼리 수정

--- src/main/java/nextstep/users/infrastructure/JdbcUserRepository.java
+++ src/main/java/nextstep/users/infrastructure/JdbcUserRepository.java
@@ -3,6 +3,8 @@ package nextstep.users.infrastructure;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import nextstep.courses.domain.student.StudentType;
+import nextstep.payments.domain.Money;
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.UserRepository;
 import org.springframework.jdbc.core.JdbcOperations;
@@ -19,15 +21,17 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public Optional<NsUser> findByUserId(String userId) {
-        String sql = "select id, user_id, password, name, email, created_at, updated_at from ns_user where user_id = ?";
+        String sql = "select id, user_id, password, name, email, money, student_type, created_at, updated_at from ns_user where user_id = ?";
         RowMapper<NsUser> rowMapper = (rs, rowNum) -> new NsUser(
                 rs.getLong(1),
                 rs.getString(2),
                 rs.getString(3),
                 rs.getString(4),
                 rs.getString(5),
-                toLocalDateTime(rs.getTimestamp(6)),
-                toLocalDateTime(rs.getTimestamp(7)));
+            (Money) rs.getObject(6),
+            (StudentType) rs.getObject(7),
+                toLocalDateTime(rs.getTimestamp(8)),
+                toLocalDateTime(rs.getTimestamp(9)));
         return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, userId));
     }
 
