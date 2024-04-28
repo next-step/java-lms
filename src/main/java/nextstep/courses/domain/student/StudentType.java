916b8ece1702b669df9eabe59cfee4c80dd3d780
namhyeop <skaguq@gmail.com>
2024-04-28 23:35:24 +0900

    feat : StudentType 추가

---
+++ src/main/java/nextstep/courses/domain/student/StudentType.java
@@ -0,0 +1,18 @@
+package nextstep.courses.domain.student;
+
+public enum StudentType {
+    NO_TYPE("일반"),
+    WOOWAHAN_TECH_COURSE_FREE("우아한테크코스(무료)"),
+    WOOWAHAN_TECH_CAMP_PRO("우아한테크캠프 Pro(유료)");
+
+    private final String value;
+
+    StudentType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
