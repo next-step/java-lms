c95e15f9602fddd16ecd3a31c360a64ef6ed08a2
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-04-29 13:08:02 +0900

    Feat: 모집 상태가 RECRUITING일 때만 수강신청이 가능하다

--- src/main/java/nextstep/sessions/domain/SessionRegisterDetails.java
+++ src/main/java/nextstep/sessions/domain/SessionRegisterDetails.java
@@ -13,23 +13,33 @@ public class SessionRegisterDetails {
 
     private final SessionStatus sessionStatus;
 
+    private final RecruitmentStatus recruitmentStatus;
+
     private int maxOfStudents;
 
     private Students students;
 
     public SessionRegisterDetails(long id, Price price, SessionStatus sessionStatus, int maxOfStudents) {
-        this(id, price, sessionStatus, maxOfStudents, new Students(maxOfStudents));
+        this(id, price, sessionStatus, RecruitmentStatus.RECRUITING, maxOfStudents, new Students(maxOfStudents));
     }
 
-    public SessionRegisterDetails(long id, Price price, SessionStatus sessionStatus, int maxOfStudents, Students students) {
+    public SessionRegisterDetails(long id, Price price, SessionStatus sessionStatus, RecruitmentStatus recruitmentStatus, int maxOfStudents) {
+        this(id, price, sessionStatus, recruitmentStatus, maxOfStudents, new Students(maxOfStudents));
+    }
+
+    public SessionRegisterDetails(long id, Price price, SessionStatus sessionStatus, RecruitmentStatus recruitmentStatus, int maxOfStudents, Students students) {
         this.id = id;
         this.price = price;
         this.sessionStatus = sessionStatus;
+        this.recruitmentStatus = recruitmentStatus;
         this.maxOfStudents = maxOfStudents;
         this.students = students;
     }
 
     public Student enroll(Student student, List<Student> enrolledStudents, Payment payment) {
+        if (this.recruitmentStatus.isNotRecruiting()) {
+            throw new IllegalArgumentException(String.format("현재 강의는 (%s)인 상태입니다.", this.recruitmentStatus));
+        }
         if (this.sessionStatus.isNotInProgress()) {
             throw new IllegalArgumentException(String.format("현재 강의는 (%s)인 상태입니다.", this.sessionStatus));
         }
--- src/test/java/nextstep/sessions/domain/SessionRegisterDetailsTest.java
+++ src/test/java/nextstep/sessions/domain/SessionRegisterDetailsTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static nextstep.sessions.domain.RecruitmentStatus.*;
+import static nextstep.sessions.domain.RecruitmentStatus.RECRUITING;
 import static nextstep.sessions.domain.SessionStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -33,9 +35,21 @@ public class SessionRegisterDetailsTest {
         assertThat(student).isEqualTo(new Student(1L, 1L));
     }
 
-    @DisplayName("강의가 모집중이 아닐때 수강신청을 하면 예외를 반환한다")
+    @DisplayName("수강신청을 한다")
     @Test
-    void statusIsNotRecruiting() {
+    void always1() {
+        SessionRegisterDetails details = new SessionRegisterDetails(1L, new Price(30000), IN_PROGRESS, RECRUITING, 40);
+
+        List<Student> students = new ArrayList<>();
+
+        Student student = details.enroll(new Student(1L, 1L), students, payment);
+
+        assertThat(student).isEqualTo(new Student(1L, 1L));
+    }
+
+    @DisplayName("강의가 진행중이 아닐때 수강신청을 하면 예외를 반환한다")
+    @Test
+    void statusIsNotInProgress() {
         SessionStatus end = END;
         SessionRegisterDetails details = new SessionRegisterDetails(1L, new Price(30000), end, 40);
 
@@ -44,10 +58,21 @@ public class SessionRegisterDetailsTest {
                 .hasMessage(String.format("현재 강의는 (%s)인 상태입니다.", end));
     }
 
+    @DisplayName("강의가 모집중이 아닐때 수강신청을 하면 예외를 반환한다")
+    @Test
+    void statusIsNotRecruiting() {
+        RecruitmentStatus notRecruiting = NOT_RECRUITING;
+        SessionRegisterDetails details = new SessionRegisterDetails(1L, new Price(30000), IN_PROGRESS, notRecruiting, 40);
+
+        assertThatThrownBy(() -> details.enroll(new Student(1L, 1L), new ArrayList<>(), payment))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("현재 강의는 (%s)인 상태입니다.", notRecruiting));
+    }
+
     @DisplayName("강의의 가격과 결제한 금액이 같지 않은지 검증한다")
     @Test
     void isSameAmount() {
-        SessionRegisterDetails details = new SessionRegisterDetails(1L, new Price(30000), RECRUITING, 40);
+        SessionRegisterDetails details = new SessionRegisterDetails(1L, new Price(30000),IN_PROGRESS, RECRUITING, 40);
 
         assertThat(details.isNotSamePrice(20000)).isTrue();
     }
