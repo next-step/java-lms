d92b8636531433710c9301d7ce88d71f9391705d
kimsunghyun <tjdgus2319@gmail.com>
2024-04-29 17:00:57 +0900

    refactor: 부 생성자가 주 생성자 호출하도록 수정

--- src/main/java/nextstep/session/domain/ImageInfo.java
+++ src/main/java/nextstep/session/domain/ImageInfo.java
@@ -5,14 +5,14 @@ public class ImageInfo {
 	private ImageReSolution imageReSolution;
 	private ImageType imageType;
 
+	public ImageInfo(String type) {
+		this(null, null, ImageType.getImageType(type));
+	}
+
 	public ImageInfo(ImageSize imageSize, ImageReSolution imageReSolution, ImageType imageType) {
 		this.imageSize = imageSize;
 		this.imageReSolution = imageReSolution;
 		this.imageType = imageType;
-	}
-
-	public ImageInfo(String type) {
-		this.imageType = ImageType.getImageType(type);
 	}
 
 }
