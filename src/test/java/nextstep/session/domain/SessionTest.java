b0584e8c42118b9169979acda96e204658c6766a
kimsunghyun <tjdgus2319@gmail.com>
2024-04-30 17:32:11 +0900

    refactor: 사용하지 않는 패키지 정리

--- src/test/java/nextstep/session/domain/SessionTest.java
+++ src/test/java/nextstep/session/domain/SessionTest.java
@@ -1,7 +1,6 @@
 package nextstep.session.domain;
 
 import nextstep.courses.domain.Course;
-import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -11,8 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SessionTest {
 	public static final Course C1 = new Course("title1", 1L);
 	public static final Period P = new Period(LocalDateTime.now(), LocalDateTime.now().plusDays(1));
-	public static final ImageInfo imageInfo = new ImageInfo(new ImageSize(1024), new ImageReSolution(300, 200), ImageType.JPG);
-	public static final Session piadSession = new PaidSession(SessionTest.C1, SessionTest.imageInfo, SessionTest.P, 50000,50);
+	public static final ImageInfo imageInfo = new ImageInfo(1L, new ImageSize(1024), new ImageReSolution(300, 200), ImageType.JPG);
+	public static final Session piadSession = new PaidSession(SessionTest.C1, SessionTest.imageInfo, SessionTest.P, 50000, 50);
 
 	@Test
 	void 유료강의_수강생결제금액_수강료일치_성공_실패_테스트() {
