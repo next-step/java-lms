7eef947b1dc42bde620bb7bb73fa7e2a76443b0a
Sangmin <sm9171@nate.com>
2024-05-04 00:42:55 +0900

    test: sessionRepositoryTest 완료

--- src/main/java/nextstep/sessions/infrastructure/JdbcSessionRepository.java
+++ src/main/java/nextstep/sessions/infrastructure/JdbcSessionRepository.java
@@ -1,13 +1,11 @@
 package nextstep.sessions.infrastructure;
 
-import nextstep.image.domain.*;
 import nextstep.sessions.domain.*;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.util.List;
 
 public class JdbcSessionRepository implements SessionRepository {
 	private JdbcOperations jdbcTemplate;
@@ -24,42 +22,21 @@ public class JdbcSessionRepository implements SessionRepository {
 
 	@Override
 	public Session findById(Long id) {
-		String sql = "select session.id, session.title, session.creator_id, image.id, image.session_id, image.url, image.image_type, image.width, image.height, image.size, session.created_at, session.updated_at " +
-				"from session left outer join image on image.session_id = session.id" +
-				" left outer join user on session.id = user.session_id" +
-				" where id = ?";
+		String sql = "select session.id, session.title, session.creator_id, session.charge_status, session.price, session.capacity, session.session_status,session.started_at, session.ended_at " +
+				"from session where id = ?";
+
 		RowMapper<Session> rowMapper = (rs, rowNum) -> new Session(
 				rs.getLong(1),
-				new SessionInfo(
-						rs.getString(2),
-						rs.getLong(3),
-						new Image(
-								rs.getLong(4),
-								rs.getLong(5),
-								rs.getString(6),
-								ImageType.valueOf(rs.getString(7)),
-								new ImageShape(
-										new ImageWidth(rs.getInt(8)),
-										new ImageHeight(rs.getInt(9))
-								),
-								rs.getInt(10)
-						)
-				),
-				new Charge(
-						ChargeStatus.valueOf(rs.getString(11)),
-						rs.getInt(12)
-				),
-				new Enrollment(
-						SessionStatus.valueOf(rs.getString(13)),
-						rs.getInt(14),
-						rs.getObject(15, List.class)
-				),
-				new SessionDate(
-						new StartedAt(toLocalDate(rs.getTimestamp(16))),
-						new EndedAt(toLocalDate(rs.getTimestamp(17)))
-				)
-
+				rs.getString(2),
+				rs.getLong(3),
+				ChargeStatus.valueOf(rs.getString(4)),
+				rs.getInt(5),
+				rs.getInt(6),
+				SessionStatus.valueOf(rs.getString(7)),
+				toLocalDate(rs.getTimestamp(8)),
+				toLocalDate(rs.getTimestamp(9))
 		);
+
 		return jdbcTemplate.queryForObject(sql, rowMapper, id);
 	}
 
