5e02b299b637957c78ec181d879d27472d0cbe4d
hhhhhwi <aab555586@gmail.com>
2024-05-09 21:51:21 +0900

    feat: 강의에 대한 객체 생성 (Session) 및 validation 구현

---
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -0,0 +1,29 @@
+package nextstep.courses.domain.session;
+
+import java.time.LocalDateTime;
+
+public class Session {
+	private Long id;
+
+	private SessionType type;
+
+	private int maxNumberOfStudent;
+
+	private SessionImage sessionImage;
+
+	private LocalDateTime startDate;
+
+	private LocalDateTime endDate;
+
+	public Session(SessionType type, int maxNumberOfStudent, SessionImage sessionImage, LocalDateTime startDate, LocalDateTime endDate) {
+		if(type.equals(SessionType.PAID) && maxNumberOfStudent <= 0) {
+			throw new IllegalArgumentException("유료 강의의 최대 수강 인원은 1 이상이여야 합니다.");
+		}
+
+		this.type = type;
+		this.maxNumberOfStudent = maxNumberOfStudent;
+		this.sessionImage = sessionImage;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+}
---
+++ src/main/java/nextstep/courses/domain/session/SessionType.java
@@ -0,0 +1,6 @@
+package nextstep.courses.domain.session;
+
+public enum SessionType {
+	FREE,
+	PAID;
+}
