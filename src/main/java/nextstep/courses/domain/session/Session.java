80b91d5700b55de954fa669e9fe47572e17a5e75
유지웅 <poujis0@gmail.com>
2024-05-13 21:39:27 +0900

    [feat] payment 구현

--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -4,22 +4,28 @@ import nextstep.courses.CanNotApplyException;
 import nextstep.courses.domain.Course;
 import nextstep.courses.domain.Image.Image;
 import nextstep.courses.domain.user.NsUsers;
+import nextstep.payments.domain.Payment;
+import nextstep.payments.domain.Payments;
 import nextstep.users.domain.NsUser;
 
 public abstract class Session {
 
+    private final long id;
     private final Image image;
     private final Period period;
     private final SessionStatus status;
     private final NsUsers users;
     private final Course course;
+    private final Payments payments;
 
     public Session(Image image, Period period, SessionStatus status, Course course) {
+        this.id = 0L;
         this.image = image;
         this.period = period;
         this.status = status;
         this.course = course;
         this.users = new NsUsers();
+        this.payments = new Payments();
     }
 
     public NsUsers getUsers() {
@@ -32,10 +38,15 @@ public abstract class Session {
         validSessionStatus();
         isAppliable(pay, users.numberOfUsers() + 1);
         users.add(user);
+        payments.add(toPayment(pay, user));
     }
 
-    public void validSessionStatus() throws Exception {
-        if(!status.equals(SessionStatus.RECRUIT)){
+    public Payment toPayment(int pay, NsUser user) {
+        return new Payment("0", id, user.getId(), (long) pay);
+    }
+
+    private void validSessionStatus() throws Exception {
+        if (!status.equals(SessionStatus.RECRUIT)) {
             throw new CanNotApplyException("모집 중인 강의가 아닙니다.");
         }
     }
