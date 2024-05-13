d34226cfd08899d1dc5b7424ad39998db72c0a58
유지웅 <poujis0@gmail.com>
2024-05-13 20:02:02 +0900

    [feat] 무료 강의는 돈과 수강생 제한이 없다.

---
+++ src/main/java/nextstep/courses/domain/session/FreeSession.java
@@ -0,0 +1,24 @@
+package nextstep.courses.domain.session;
+
+import nextstep.courses.CanNotApplyException;
+import nextstep.courses.domain.Image.Image;
+
+public class FreeSession extends Session{
+    private static final int FREE_COST = 0;
+
+    public FreeSession(Image image, Period period, SessionStatus status) {
+        super(image, period, status);
+    }
+
+    @Override
+    public boolean isAppliable(int pay, int order) throws CanNotApplyException {
+        validPay(pay);
+        return true;
+    }
+
+    private static void validPay(int pay) throws CanNotApplyException {
+        if(pay != FREE_COST){
+            throw new CanNotApplyException("무료 강의입니다.");
+        }
+    }
+}
