0979cac883cfe4f6ecd114aa64f255c368f01ddd
박소민 <qkrthals0524@naver.com>
2024-05-17 00:17:41 +0900

    findAny -> findFirst 변경

--- src/main/java/nextstep/session/domain/SessionProgressStatus.java
+++ src/main/java/nextstep/session/domain/SessionProgressStatus.java
@@ -17,7 +17,7 @@ public enum SessionProgressStatus {
     public static SessionProgressStatus convert(String status) {
         return Arrays.stream(values())
             .filter(sessionType -> sessionType.description.equals(status))
-            .findAny()
+            .findFirst()
             .orElseThrow(() -> new SessionStatusInvalidException("존재하지 않는 상태입니다."));
     }
 
