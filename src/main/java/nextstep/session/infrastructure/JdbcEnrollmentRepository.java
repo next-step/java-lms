f0e800aa007bd779dafdd76408d09f7b0ef83677
kimsunghyun <tjdgus2319@gmail.com>
2024-05-22 21:25:25 +0900

    fix: enrollment에 User 리스트 생성하는 부분 추가

--- src/main/java/nextstep/session/infrastructure/JdbcEnrollmentRepository.java
+++ src/main/java/nextstep/session/infrastructure/JdbcEnrollmentRepository.java
@@ -3,6 +3,7 @@ package nextstep.session.infrastructure;
 import nextstep.session.domain.Enrollment;
 import nextstep.session.domain.EnrollmentRepository;
 import nextstep.session.domain.SessionStatus;
+import nextstep.session.domain.StudentEnrollmentInfomationRepository;
 import nextstep.users.domain.NsUser;
 import nextstep.users.domain.NsUsers;
 import nextstep.users.domain.UserRepository;
@@ -12,14 +13,17 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository("EnrollmentRepository")
 public class JdbcEnrollmentRepository implements EnrollmentRepository {
 	private final JdbcOperations jdbcTemplate;
+	private final StudentEnrollmentInfomationRepository studentEnrollmentInfomationRepository;
 	private final UserRepository userRepository;
 
-	public JdbcEnrollmentRepository(JdbcOperations jdbcTemplate, UserRepository userRepository) {
+	public JdbcEnrollmentRepository(JdbcOperations jdbcTemplate, StudentEnrollmentInfomationRepository studentEnrollmentInfomationRepository, UserRepository userRepository) {
 		this.jdbcTemplate = jdbcTemplate;
+		this.studentEnrollmentInfomationRepository = studentEnrollmentInfomationRepository;
 		this.userRepository = userRepository;
 	}
 
@@ -31,7 +35,7 @@ public class JdbcEnrollmentRepository implements EnrollmentRepository {
 
 	@Override
 	public Optional<Enrollment> findById(long id) {
-		List<NsUser> allBySessionId = userRepository.findAllBySessionId(id);
+		List<NsUser> allStudentsByStudentId = getNsUsers(id);
 		String sql = "select id, maximum_number_of_participants, session_price, session_status, session_id from enrollment where id = ?";
 		RowMapper<Enrollment> rowMapper = (rs, rowNum) -> new Enrollment(
 				rs.getLong(1),
@@ -39,13 +43,20 @@ public class JdbcEnrollmentRepository implements EnrollmentRepository {
 				rs.getLong(3),
 				rs.getLong(4),
 				SessionStatus.getSessionStatus(rs.getInt(5)),
-				new NsUsers(allBySessionId));
+				new NsUsers(allStudentsByStudentId));
 		return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, id));
+	}
+
+	private List<NsUser> getNsUsers(long id) {
+		List<Long> allStudentIdByEnrollmentId = studentEnrollmentInfomationRepository.findAllByEnrollmentId(id).stream()
+				.map(s -> s.getStudentId()).collect(Collectors.toList());
+		List<NsUser> allStudentsByStudentId = userRepository.findAllByStudentId(allStudentIdByEnrollmentId);
+		return allStudentsByStudentId;
 	}
 
 	@Override
 	public Optional<Enrollment> findBySessionId(long id) {
-		List<NsUser> allBySessionId = userRepository.findAllBySessionId(id);
+		List<NsUser> allStudentsByStudentId = getNsUsers(id);
 		String sql = "select id, maximum_number_of_participants, session_price, session_status, session_id from enrollment where session_id = ?";
 		RowMapper<Enrollment> rowMapper = (rs, rowNum) -> new Enrollment(
 				rs.getLong(1),
@@ -53,7 +64,7 @@ public class JdbcEnrollmentRepository implements EnrollmentRepository {
 				rs.getLong(3),
 				rs.getLong(4),
 				SessionStatus.getSessionStatus(rs.getInt(5)),
-				new NsUsers(allBySessionId));
+				new NsUsers(allStudentsByStudentId));
 		return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, id));
 	}
 
