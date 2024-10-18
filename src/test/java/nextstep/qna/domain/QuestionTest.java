ad8be8f1b79bfaa0674041acd160fd0c4490801c
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-18 17:18:33 +0900

    refactor: 질문 작성자 확인 Question에 위임
    질문 작성자 확인 Question에 위임

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,20 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void 질문작성자가_아니면_CannotDeleteException이_발생한다() {
+        assertThatThrownBy(() -> Q1.verifyOwner(NsUserTest.SANJIGI))
+            .isInstanceOf(CannotDeleteException.class)
+            .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
 }
