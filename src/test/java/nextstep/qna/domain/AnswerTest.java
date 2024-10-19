51f045100a3eec7f81274024da1d188e639f81fe
Byungil Sohn <skylord418@gmail.com>
2024-10-19 11:30:28 +0900

    feat : add delete feature in Answer domain class

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,30 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void delete() throws CannotDeleteException {
+        DeleteHistory deleteHistory = A1.delete(NsUserTest.JAVAJIGI);
+
+        assertThat(A1.isDeleted()).isTrue();
+        assertThat(deleteHistory).isEqualTo(
+                new DeleteHistory(ContentType.ANSWER, A1.getId(), NsUserTest.JAVAJIGI, LocalDateTime.now()));
+    }
+
+    @Test
+    void throw_exception_if_login_user_not_match() {
+        assertThatThrownBy(() -> A2.delete(NsUserTest.JAVAJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
 }
