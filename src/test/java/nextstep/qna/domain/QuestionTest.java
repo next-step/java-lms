5b0abcbf2f93d79f456d0150a10ebcaa4cdf6fda
SangHyun Kim <ksh9891@gmail.com>
2024-10-20 15:54:26 +0900

    refactor(lms): question 도메인 모델에 delete 메서드 생성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("글 작성자가 아닌 유저가 글을 삭제하려고 하는 경우 예외가 발생한다.")
+    void shouldThrowExceptionWhenUserIsNotAuthorAndAttemptsToDeletePost() {
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANGHYUN))
+            .hasMessage("질문을 삭제할 권한이 없습니다.")
+            .isExactlyInstanceOf(CannotDeleteException.class);
+    }
 }
