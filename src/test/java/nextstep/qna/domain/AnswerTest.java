00cbfe3acdf5b1aad749592ab2f6b4f001b66430
JIHO CHOI <wlghsp@gmail.com>
2024-10-20 13:24:23 +0900

    test: 답변글 작성자 확인 예외 테스트 추가

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,24 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.exception.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static nextstep.qna.domain.Answer.ANOTHER_OWNER_EXISTS_EXCEPTION_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("답변글의 소유자가 다르면 예외를 던진다.")
+    @Test
+    void checkIfAnotherOwnerExistsTest() {
+        assertThatThrownBy(
+                () -> A1.checkIfAnotherOwnerExists(NsUserTest.SANJIGI)
+        )
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining(ANOTHER_OWNER_EXISTS_EXCEPTION_MESSAGE);
+    }
 }
