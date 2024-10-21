51c57580cccc752f41960c29bde8d181118686d9
skdev0619 <skdev0619@naver.com>
2024-10-21 15:41:32 +0900

    질문자와 로그인 사용자가 다르면 삭제가 불가능하다

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -68,9 +69,21 @@ public class Question {
         answers.add(answer);
     }
 
-    public boolean isOwner(NsUser loginUser) {
+    //region [checkDeletePermission]
+    public void checkDeletePermission(NsUser loginUser) throws CannotDeleteException {
+        if (isNotOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+    }
+
+    private boolean isOwner(NsUser loginUser) {
         return writer.equals(loginUser);
     }
+
+    private boolean isNotOwner(NsUser loginUser){
+        return !isOwner(loginUser);
+    }
+    //endregion
 
     public Question setDeleted(boolean deleted) {
         this.deleted = deleted;
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,23 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("질문자와 로그인 사용자가 다르면 삭제가 불가능하다")
+    @Test
+    void checkDeletePermission() throws CannotDeleteException {
+        assertThatThrownBy(() -> Q1.checkDeletePermission(NsUserTest.SANJIGI))
+                        .isInstanceOf(CannotDeleteException.class)
+                        .hasMessage("질문을 삭제할 권한이 없습니다.");
+
+    }
+
 }
