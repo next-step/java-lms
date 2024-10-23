7eec2f452b6d5b70204109e161c5d41610e9efeb
ming <devel.moon17@gmail.com>
2024-10-23 21:30:50 +0900

    test: 1단계 - 답변 작성자 본인인지 확인 테스트 작성

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,20 +1,18 @@
 package nextstep.qna.domain;
 
-import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
     @Test
-    void 다른_사람_답변이_있어_삭제불가(){
-        assertThatThrownBy(() -> {
-            A1.isOwner(NsUserTest.JAVAJIGI);
-        }).isInstanceOf(CannotDeleteException.class);
+    void 작성자_본인인지_확인(){
+        assertThat(A1.isOwner(NsUserTest.JAVAJIGI)).isTrue();
+        assertThat(A1.isOwner(NsUserTest.SANJIGI)).isFalse();
     }
 
 }
