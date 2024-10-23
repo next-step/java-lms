9838f6a144b8917ae7041da72fd7996f8b5cad38
이윤선 <yoonseon3@gmail.com>
2024-10-23 22:32:29 +0900

    test: add 메서드가 삭제내역을 추가한다.

---
+++ src/test/java/nextstep/qna/domain/DeleteHistoriesTest.java
@@ -0,0 +1,24 @@
+package nextstep.qna.domain;
+
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DeleteHistoriesTest {
+    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+
+    @Test
+    @DisplayName("성공 - add 메서드가 삭제내역을 추가한다.")
+    void addTest() {
+        DeleteHistory deleteHistory = new DeleteHistory(ContentType.QUESTION, 1L, Q1.getWriter(), LocalDateTime.now());
+        DeleteHistories deleteHistories = new DeleteHistories();
+        deleteHistories.add(deleteHistory);
+
+        assertThat(deleteHistories.getDeleteHistories()).hasSize(1);
+        assertThat(deleteHistories.getDeleteHistories()).containsExactly(deleteHistory);
+    }
+}
