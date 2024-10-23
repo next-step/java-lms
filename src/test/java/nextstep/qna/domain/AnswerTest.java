76300f1bad410c05795ba18214457a98f7e746ea
SeongJunWi <dnltjdwns235@gmail.com>
2024-10-23 21:50:30 +0900

    feat QuestionTest, AnswerTest 테스트 작성

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,33 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+    @Test
+    @DisplayName("삭제가 deleted를 true로 만드는 것인지 체크하는 테스트.")
+    void stateDeleteTest(){
+        A1.delete();
+        assertThat(A1.isDeleted()).isTrue();
+    }
+    @Test
+    @DisplayName("답변자가 질문자와 같은경우 삭제 할 수 있다.")
+    void ownerDeleteTest(){
+        A1.delete();
+        assertThat(A1.isDeleted()).isTrue();
+    }
+    @Test
+    @DisplayName("답변자가 질문자와 다른경우 삭제 할 수 없다.")
+    void anotherDeleteTest(){
+        assertThatThrownBy(()->{
+            A2.delete();
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,36 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("삭제가 deleted를 true로 만드는 것인지 체크하는 테스트.")
+    void stateDeleteTest() throws CannotDeleteException {
+        Q1.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("로그인 사용자와 질문한 사람이 같은 경우 삭제할 수 있다.")
+    void ownerDeleteTest() throws CannotDeleteException {
+        Q1.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("로그인 사용자와 질문한 사람이 다른 경우 삭제할 수 없다.")
+    void anotherDeleteTest(){
+        assertThatThrownBy(()->{
+            Q2.delete(NsUserTest.JAVAJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
