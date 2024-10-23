bf2403b406e6f1a5f830095003e4bcbf3b63dd8a
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-23 15:28:24 +0900

    refactor: DeleteHistory 생성 메소드

--- src/main/java/nextstep/qna/domain/DeleteHistory.java
+++ src/main/java/nextstep/qna/domain/DeleteHistory.java
@@ -3,6 +3,8 @@ package nextstep.qna.domain;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class DeleteHistory {
@@ -46,5 +48,20 @@ public class DeleteHistory {
     public String toString() {
         return "DeleteHistory [id=" + id + ", contentType=" + contentType + ", contentId=" + contentId + ", deletedBy="
                 + deletedBy + ", createdDate=" + createdDate + "]";
+    }
+
+    public static List<DeleteHistory> createDeleteHistoryList(Question question) {
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
+        deleteHistories.add(createQuestion(question));
+        question.getAnswers().forEach(answer -> deleteHistories.add(createAnswer(answer)));
+        return deleteHistories;
+    }
+
+    private static DeleteHistory createQuestion(Question question) {
+        return new DeleteHistory(ContentType.QUESTION, question.getId(), question.getWriter(), LocalDateTime.now());
+    }
+
+    private static DeleteHistory createAnswer(Answer answer) {
+        return new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now());
     }
 }
