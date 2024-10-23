830d0da1168666d18b25ee8dcc29063fc7bcd2cd
이윤선 <yoonseon3@gmail.com>
2024-10-23 22:51:29 +0900

    test: getDeleteHistories 메서드의 반환된 List를 수정했을 때 예외가 발생한다.

--- src/test/java/nextstep/qna/domain/DeleteHistoriesTest.java
+++ src/test/java/nextstep/qna/domain/DeleteHistoriesTest.java
@@ -4,9 +4,8 @@ import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DeleteHistoriesTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
@@ -20,5 +19,14 @@ class DeleteHistoriesTest {
 
         assertThat(deleteHistories.getDeleteHistories()).hasSize(1);
         assertThat(deleteHistories.getDeleteHistories()).containsExactly(deleteHistory);
+    }
+
+    @Test
+    @DisplayName("실패 - getDeleteHistories 메서드의 반환된 List를 수정했을 때 예외가 발생한다.")
+    void throwExceptionWhenModifyingReturnedDeleteHistoryList() {
+        DeleteHistories deleteHistories = new DeleteHistories();
+        assertThatThrownBy(() -> deleteHistories.getDeleteHistories().add(
+                new DeleteHistory(ContentType.QUESTION, 1L, Q1.getWriter())))
+                .isInstanceOf(UnsupportedOperationException.class);
     }
 }
