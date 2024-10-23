132532239f844024c10faad6b4441eb3739d9d06
mitsui3point <rladbsgh0331@gmail.com>
2024-10-23 22:39:31 +0900

    docs(lms): modify README_LMS
    feat(lms): add Answer delete method
    
    feat(lms): add AnswerTest delete_성공, delete_답변작성자가_아닐_경우

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,32 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    private Answer answer;
+
+    @BeforeEach
+    void setUp() {
+        answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    }
+
+    @Test
+    void delete_성공() throws CannotDeleteException {
+        Answer delete = answer.delete(NsUserTest.JAVAJIGI);
+        Assertions.assertThat(delete.isDeleted()).isTrue();
+    }
+
+    @Test
+    void delete_답변작성자가_아닐_경우() {
+        Assertions.assertThatThrownBy(() -> {
+            Answer delete = answer.delete(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
