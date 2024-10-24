1022a7bbb1d4d9268db76465f182a43f4f684a0e
이윤선 <yoonseon3@gmail.com>
2024-10-24 21:03:31 +0900

    test: delete 메서드가 다른 사람이 쓴 답변이 없을 때 정상 처리된다.

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -10,7 +10,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
     @Test
     @DisplayName("실패 - delete 메서드가 질문을 삭제할 권한이 없을 때 예외가 발생한다.")
@@ -30,15 +31,20 @@ public class QuestionTest {
     @Test
     @DisplayName("실패 - delete 메서드가 다른 사람이 쓴 답변이 있을 때 예외가 발생한다.")
     void throwExceptionWhen2() {
-        Answer answer1 = new Answer(NsUserTest.JAVAJIGI, Q1, "답변1");
-        Answer answer2 = new Answer(NsUserTest.JAVAJIGI, Q1, "답변2");
-        Answer answer3 = new Answer(NsUserTest.SANJIGI, Q1, "답변3");
-        Q1.getAnswers().add(answer1);
-        Q1.getAnswers().add(answer2);
-        Q1.getAnswers().add(answer3);
+        Q1.getAnswers().add(A1);
+        Q1.getAnswers().add(A2);
 
         assertThatThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
+
+    @Test
+    @DisplayName("성공 - delete 메서드가 다른 사람이 쓴 답변이 없을 때 정상 처리된다.")
+    void deleteTest2() throws Exception{
+        Q1.addAnswer(A1);
+        Q1.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
 }
