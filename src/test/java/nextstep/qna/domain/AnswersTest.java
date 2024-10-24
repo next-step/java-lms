ad70c7db8fcb0e9172b6a8dba6f6e8bb7378baa3
이윤선 <yoonseon3@gmail.com>
2024-10-24 22:13:30 +0900

    fix: 불필요 테스트 삭제

--- src/test/java/nextstep/qna/domain/AnswersTest.java
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -9,13 +9,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AnswersTest {
-    private static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-
     @Test
     @DisplayName("성공 - add 메서드가 답변을 추가한다.")
     void addTest() {
-        Answer answer = new Answer(Q1.getWriter(), Q1, "답변1");
         Answers answers = new Answers();
+        Answer answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.createQuestion(NsUserTest.JAVAJIGI), "답변1");
         answers.add(answer);
 
         assertThat(answers.getAnswers()).hasSize(1);
@@ -25,8 +23,8 @@ class AnswersTest {
     @Test
     @DisplayName("실패 - validateExistOtherUser 메서드가 작성자가 아닌 다른 사람의 답변이 존재할 때 예외가 발생한다.")
     void validateExistOtherUserTest() {
-        Answer answer1 = new Answer(Q1.getWriter(), Q1, "답변1");
-        Answer answer2 = new Answer(NsUserTest.SANJIGI, Q1, "답변2");
+        Answer answer1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.createQuestion(NsUserTest.JAVAJIGI), "답변1");
+        Answer answer2 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.createQuestion(NsUserTest.JAVAJIGI), "답변1");
         Answers answers = new Answers();
         answers.add(answer1);
         answers.add(answer2);
@@ -34,14 +32,5 @@ class AnswersTest {
         assertThatThrownBy(() -> answers.validateExistOtherUser(NsUserTest.JAVAJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessage("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-    }
-
-    @Test
-    @DisplayName("실패 - getAnswers 메서드의 반환된 List를 수정했을 때 예외가 발생한다.")
-    void throwExceptionWhenModifyingReturnedDeleteAnswerList() {
-        Answers answers = new Answers();
-        assertThatThrownBy(() -> answers.getAnswers().add(
-                new Answer(Q1.getWriter(), Q1, "답변1")))
-                .isInstanceOf(UnsupportedOperationException.class);
     }
 }
