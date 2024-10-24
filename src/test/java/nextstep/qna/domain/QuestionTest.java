0587eb62d0f0a34a2a90d9d193a7c7af5472f572
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-10-24 16:42:28 +0900

    feat: 질문자와 답변글의 모든 답변자가 같은 경우 정상 삭제

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -15,22 +15,34 @@ public class QuestionTest {
     @DisplayName("질문 삭제 시 질문한 사람과 로그인한 사람이 같지 않은 경우 예외가 발생하는지")
     @Test
     void deleteTest_WhenUserIsNotOwner() {
-        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        assertThatThrownBy(() -> question.delete(NsUserTest.SANJIGI))
             .isInstanceOf(CannotDeleteException.class);
     }
 
     @DisplayName("질문 삭제 시 답변이 없는 경우 삭제가 가능한지")
     @Test
     void deleteTest_WhenAnswersEmpty() {
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
         assertThatNoException()
-                .isThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI));
+                .isThrownBy(() -> question.delete(NsUserTest.JAVAJIGI));
+    }
+
+    @DisplayName("질문 삭제 시 답변글의 모든 답변자가 질문자인 경우 삭제가 가능한지")
+    @Test
+    void deleteTest_WhenAnswersAllOwner() {
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        question.addAnswer(new Answer(NsUserTest.JAVAJIGI, Q1, "Answers Contents1"));
+        assertThatNoException()
+            .isThrownBy(() -> question.delete(NsUserTest.JAVAJIGI));
     }
 
     @DisplayName("질문 삭제 시 다른 사람의 답변이 있는 경우 예외가 발생하는지")
     @Test
     void deleteTest_WhenAnswersNotEmpty() {
-        Q1.addAnswer(new Answer(NsUserTest.SANJIGI, Q1, "Answers Contents1"));
-        assertThatThrownBy(() -> Q1.delete(NsUserTest.JAVAJIGI))
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        question.addAnswer(new Answer(NsUserTest.SANJIGI, Q1, "Answers Contents1"));
+        assertThatThrownBy(() -> question.delete(NsUserTest.JAVAJIGI))
             .isInstanceOf(CannotDeleteException.class);
     }
 }
