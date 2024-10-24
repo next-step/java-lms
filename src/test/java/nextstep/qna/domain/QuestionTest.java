2f263993c2178d6ce8b2708d0cf7f7aad3c2f215
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-10-24 12:02:15 +0900

    test: 실패하는 테스트 작성 (질문 삭제 시 질문한 사람과 로그인한 사람이 같지 않으면 예외 발생)

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,20 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("질문 삭제 시 질문한 사람과 로그인한 사람이 같지 않은 경우 예외가 발생하는지")
+    @Test
+    void deleteTest_WhenUserIsNotOwner() {
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
+            .isInstanceOf(CannotDeleteException.class);
+    }
 }
