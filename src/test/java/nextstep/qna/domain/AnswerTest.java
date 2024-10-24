94a25cbe241a958211a9e71048137c79576e9ce9
sinyebin <dpqls0625@naver.com>
2024-10-24 11:51:47 +0900

    feat: answer 삭제 권한 없음, 질문 삭제 테스트

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,30 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @DisplayName("삭제 권한 없음")
+    @Test
+    void 삭제_권한x(){
+        assertThatThrownBy(() -> {
+            A1.validateDeleteQuestionAnswer(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
+    @DisplayName("질문 삭제")
+    @Test
+    void 질문_삭제(){
+        A1.deleteAnswer();
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
 }
