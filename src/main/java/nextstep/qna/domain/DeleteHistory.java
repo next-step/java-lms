6c8e9b9f848867ac46a83571e7776dd6f3408167
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-10-24 17:09:36 +0900

    feat: Delete history가 삭제된 Answer 기반으로 생성

--- src/main/java/nextstep/qna/domain/DeleteHistory.java
+++ src/main/java/nextstep/qna/domain/DeleteHistory.java
@@ -26,6 +26,10 @@ public class DeleteHistory {
         this.createdDate = createdDate;
     }
 
+    public DeleteHistory(Answer answer) {
+        this(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now());
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/nextstep/qna/domain/DeleteHistoryTest.java
+++ src/test/java/nextstep/qna/domain/DeleteHistoryTest.java
@@ -1,7 +1,10 @@
 package nextstep.qna.domain;
 
+import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,9 +13,8 @@ class DeleteHistoryTest {
     @DisplayName("삭제된 Answer 객체를 기반으로 생성이 잘 되는지")
     @Test
     void createTest_byAnswer() {
-        Answer answer = new Answer(1L, "contents", "writer");
+        Answer answer = new Answer(1L, NsUserTest.JAVAJIGI, QuestionTest.Q1, "writer");
         DeleteHistory deleteHistory = new DeleteHistory(answer);
-
-        assertThat(deleteHistory).isEqualTo(new DeleteHistory(1L, ContentType.ANSWER, "writer"));
+        assertThat(deleteHistory).isEqualTo(new DeleteHistory(ContentType.ANSWER, 1L, NsUserTest.JAVAJIGI, LocalDateTime.now()));
     }
 }
