2ad9dd27f32e4e2f64ffc4266afce6f3d0f9e064
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-26 02:10:39 +0900

    test: Payment 결재내역 생성 테스트코드

---
+++ src/test/java/nextstep/payment/PaymentTest.java
@@ -0,0 +1,25 @@
+package nextstep.payment;
+
+import nextstep.payments.domain.Payment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+- 결제 내역을 생성한다.
+*/
+public class PaymentTest {
+
+    @DisplayName("결재 내역을 생성한다.")
+    @Test
+    void createPaymentTest() {
+        Payment payment = new Payment(1L, 1L, 1L, 800000);
+
+        assertThat(payment)
+                .extracting("id", "sessionId", "nsUserId", "amount")
+                .contains(1L, 1L, 1L, 800000);
+    }
+}
