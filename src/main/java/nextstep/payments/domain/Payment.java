2b21067d314a5670689f57e28f799cd45ff42eb2
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-26 02:10:27 +0900

    fix: Payment 도메인수정

--- src/main/java/nextstep/payments/domain/Payment.java
+++ src/main/java/nextstep/payments/domain/Payment.java
@@ -3,7 +3,7 @@ package nextstep.payments.domain;
 import java.time.LocalDateTime;
 
 public class Payment {
-    private String id;
+    private Long id;
 
     // 결제한 강의 아이디
     private Long sessionId;
@@ -12,14 +12,11 @@ public class Payment {
     private Long nsUserId;
 
     // 결제 금액
-    private Long amount;
+    private int amount;
 
     private LocalDateTime createdAt;
 
-    public Payment() {
-    }
-
-    public Payment(String id, Long sessionId, Long nsUserId, Long amount) {
+    public Payment(Long id, Long sessionId, Long nsUserId, int amount) {
         this.id = id;
         this.sessionId = sessionId;
         this.nsUserId = nsUserId;
