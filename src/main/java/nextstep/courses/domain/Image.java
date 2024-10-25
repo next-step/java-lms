c4364d28b707023c5e700fee73e197de5a705cb7
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-26 01:39:17 +0900

    feat: Image 도메인 생성

---
+++ src/main/java/nextstep/courses/domain/Image.java
@@ -0,0 +1,58 @@
+package nextstep.courses.domain;
+
+public class Image {
+
+    private final static int MAXIMUM_IMAGE_SIZE = 1;
+    private final static int MAXIMUM_WIDTH = 300;
+    private final static int MAXIMUM_HEIGHT = 200;
+    private final static String OVER_SIZE_MESSAGE = "이미지 사이즈는 1MB를 초과하면 안됩니다.";
+    private final static String OVER_WIDTH_HEIGHT = "이미지의 너비가 300px, 높이가 200px을 초과하면 안됩니다.";
+    private final static String NOT_CORRECT_RATE = "이미지의 너비 높이가 3:2 비율이여야 합니다.";
+
+    private final String name;
+    private final int width;
+    private final int height;
+    private final int size;
+
+    public Image(String name, int width, int height, int size) {
+        confirmRate(width, height);
+        confirmWidthHeight(width, height);
+        confirmImageSize(size);
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.size = size;
+    }
+
+    private void confirmImageSize(int size) {
+        if(size > MAXIMUM_IMAGE_SIZE) {
+            throw new IllegalArgumentException(OVER_SIZE_MESSAGE);
+        }
+    }
+
+    private void confirmWidthHeight(int width, int height) {
+        if(width > MAXIMUM_WIDTH || height > MAXIMUM_HEIGHT) {
+            throw new IllegalArgumentException(OVER_WIDTH_HEIGHT);
+        }
+    }
+
+    private void confirmRate(int width, int height) {
+        int greatestCommonDenominator = getGreatestCommonDenominator(width, height);
+        int ratioWidth = width / greatestCommonDenominator;
+        int ratioHeight = height / greatestCommonDenominator;
+
+        if (ratioWidth != 3 || ratioHeight != 2) {
+            throw new IllegalArgumentException(NOT_CORRECT_RATE);
+        }
+    }
+
+    // 최대 공약수를 계산하는 메소드
+    private int getGreatestCommonDenominator(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
