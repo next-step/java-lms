32931f08c48cd7dfccbf0fa793ddac78efbcfe45
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-26 02:42:52 +0900

    feat: 강의 도메인 생성

---
+++ src/main/java/nextstep/lecture/Lecture.java
@@ -0,0 +1,128 @@
+package nextstep.lecture;
+
+import nextstep.courses.domain.Image;
+import nextstep.payments.domain.Payment;
+
+import java.util.Date;
+
+public class Lecture {
+
+    private final static String DATE_MESSAGE = "종료일이 시작일보다 빠릅니다.";
+    private final static String PAID_SUBSCRIBE_MESSAGE = "유료강의는 결제내역이 필수입니다.";
+    private final static String SUBSCRIBE_STATUS_NOT_WAIT_MESSAGE = "현재 강의가 모집중이 아닙니다.";
+    private final static String SUBSCRIBE_COUNT_MAX_MESSAGE = "강의가 이미 만석입니다.";
+
+    private final Long id;
+    private final String title;
+    private final Image image;
+    private final PaymentType paymentType;
+    private SubscribeStatus subscribeStatus;
+    private int subscribeMax;
+    private int price;
+    private int subscribeCount = 0;
+    private final Date startDate;
+    private final Date endDate;
+
+    private Lecture(Long id, String title, Image image, PaymentType paymentType, int subscribeMax, int price, Date startDate, Date endDate) {
+        confirmDate(startDate, endDate);
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.paymentType = paymentType;
+        this.subscribeStatus = SubscribeStatus.READY;
+        this.subscribeMax = subscribeMax;
+        this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private Lecture(Long id, String title, Image image, PaymentType paymentType, Date startDate, Date endDate) {
+        confirmDate(startDate, endDate);
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.paymentType = paymentType;
+        this.subscribeStatus = SubscribeStatus.READY;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    //무료강의를 생성한다.
+    public static Lecture createFree(Long id, String title, Image image, Date startDate, Date endDate) {
+        return new Lecture(id, title, image, PaymentType.FREE, startDate, endDate);
+    }
+
+    //유료강의를 생성한다.
+    public static Lecture createPaid(Long id, String title, Image image, int subscribeMax, int price, Date startDate, Date endDate) {
+        return new Lecture(id, title, image, PaymentType.PAID, subscribeMax, price, startDate, endDate);
+    }
+
+    public int getSubscribeCount() {
+        return subscribeCount;
+    }
+
+    public SubscribeStatus getSubscribeStatus() {
+        return subscribeStatus;
+    }
+
+    //수강신청을 한다(무료)
+    public void subsribe() {
+        confirmSubscribeStatus();
+        if (paymentType != PaymentType.FREE) {
+            throw new IllegalArgumentException(PAID_SUBSCRIBE_MESSAGE);
+        }
+        addSubscribeCount();
+    }
+
+    //수강신청을 한다(유료)
+    public void subsribe(Payment payment) {
+        confirmSubscribeStatus();
+        if (paymentType == PaymentType.FREE) {
+            addSubscribeCount();
+            return;
+        }
+        payment.checkMatchAmount(this.price);
+        addSubscribeCount();
+    }
+
+    //모집중으로 상태를 변경한다.
+    public void waitLecture() {
+        changeSubscribeStatus(SubscribeStatus.WAIT);
+    }
+
+    //종료로 상태를 변경한다.
+    public void closedLecture() {
+        changeSubscribeStatus(SubscribeStatus.CLOSED);
+    }
+
+    //수강신청 상태를 변경한다.
+    private void changeSubscribeStatus(SubscribeStatus subscribeStatus) {
+        this.subscribeStatus = subscribeStatus;
+    }
+
+    //수강신청하면 인원을 증가시킨다.
+    private void addSubscribeCount() {
+        confirmSubscribeMax(this.subscribeCount + 1);
+        this.subscribeCount++;
+    }
+
+    //시작일 종료일 날짜를 점검한다.
+    private void confirmDate(Date startDate, Date endDate) {
+        if (startDate.after(endDate)) {
+            throw new IllegalArgumentException(DATE_MESSAGE);
+        }
+    }
+
+    //강의가 모집중인지 확인한다.
+    private void confirmSubscribeStatus() {
+        if (this.subscribeStatus != SubscribeStatus.WAIT) {
+            throw new IllegalArgumentException(SUBSCRIBE_STATUS_NOT_WAIT_MESSAGE);
+        }
+    }
+
+    private void confirmSubscribeMax(int subscribeCount) {
+        if (this.subscribeMax < subscribeCount) {
+            throw new IllegalArgumentException(SUBSCRIBE_COUNT_MAX_MESSAGE);
+        }
+    }
+}
