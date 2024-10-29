6773330330a4f1cd3e060a82cb0ec0ddf6a342e6
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-29 21:24:18 +0900

    test: 정확한 테스트를 위해 각 메소드 실행전 테이블 새로생성

---
+++ src/main/resources/drop.sql
@@ -0,0 +1,7 @@
+drop table subscriber;
+drop table course;
+drop table session;
+drop table ns_user;
+drop table question;
+drop table answer;
+drop table delete_history;
--- src/test/java/nextstep/support/TestSupport.java
+++ src/test/java/nextstep/support/TestSupport.java
@@ -1,15 +1,44 @@
 package nextstep.support;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 @Transactional
 public abstract class TestSupport {
 
     @Autowired
-    protected JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
+
+    @BeforeEach
+    void setUpDatabase() {
+        runDropSchemaScript();
+        runSchemaScript();
+        runDataSchemaScript();
+    }
+
+    private void runSchemaScript() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("schema.sql"));
+        populator.execute(dataSource);
+    }
+
+    private void runDataSchemaScript() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("data.sql"));
+        populator.execute(dataSource);
+    }
+
+    private void runDropSchemaScript() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("drop.sql"));
+        populator.execute(dataSource);
+    }
 
 }
