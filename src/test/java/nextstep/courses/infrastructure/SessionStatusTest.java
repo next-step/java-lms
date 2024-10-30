e379e60bece7cafbbefede73f40e24884590deb0
ming <devel.moon17@gmail.com>
2024-10-31 02:07:53 +0900

    feat: 2단계 - 수강신청 강의 상태 도메인 구현

---
+++ src/test/java/nextstep/courses/infrastructure/SessionStatusTest.java
@@ -0,0 +1,17 @@
+package nextstep.courses.infrastructure;
+
+import nextstep.courses.domain.SessionStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SessionStatusTest {
+
+    @Test
+    @DisplayName("강의 상태가 모집중일 때만 신청이 가능하다.")
+    void 강의_상태_모집중(){
+        SessionStatus sessionStatus = SessionStatus.RECRUITING;
+        assertTrue(sessionStatus.isRecruiting());
+    }
+}
