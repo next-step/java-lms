a77b21c57f68ace0b72069e9411b446941930e70
JIHO CHOI <jh.choi2@okestro.com>
2024-11-01 16:58:43 +0900

    refactor: 클래스명 오타 수정

--- src/main/java/nextstep/courses/domain/course/BaseTime.java
+++ src/main/java/nextstep/courses/domain/course/BaseTime.java
@@ -2,8 +2,14 @@ package nextstep.courses.domain.course;
 
 import java.time.LocalDateTime;
 
-public abstract class BaseTIme {
+public abstract class BaseTime {
+
     protected LocalDateTime createdAt;
 
     protected LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
 }
