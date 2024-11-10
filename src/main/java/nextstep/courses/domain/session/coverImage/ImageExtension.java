950b4a83c15bcf4bc54ff3d98496e9dca140bb2e
ming <devel.moon17@gmail.com>
2024-11-10 17:17:48 +0900

    refeactor: 2단계 피드백 반영 - 유효한 확장자 상수로 분리

--- src/main/java/nextstep/courses/domain/session/coverImage/ImageExtension.java
+++ src/main/java/nextstep/courses/domain/session/coverImage/ImageExtension.java
@@ -1,6 +1,7 @@
 package nextstep.courses.domain.session.coverImage;
 
 import java.util.Arrays;
+import java.util.List;
 
 public enum ImageExtension {
     GIF,
@@ -10,8 +11,10 @@ public enum ImageExtension {
     SVG,
     BMP;
 
+    private static final List<ImageExtension> VALID_EXTENSION = Arrays.asList(GIF, JPG, JPEG, PNG, SVG);
+
     public static boolean validExtension(ImageExtension extension) {
-        if (!Arrays.asList(GIF, JPG, JPEG, PNG, SVG).contains(extension)) {
+        if (!VALID_EXTENSION.contains(extension)) {
             throw new IllegalArgumentException("허용되지 않은 확장자 입니다.");
         }
         return true;
