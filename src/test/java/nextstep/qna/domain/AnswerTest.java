a3056074c44dfbcb55844b1c4340abb92832d01a
kass.fresh <kass.fresh@kakaocorp.com>
2025-04-07 15:52:44 +0900

    test: add test case for `Answer`

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,19 +1,27 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
 
     @Test
-    void temp(){
+    void shouldNotAllowDelete_WhenUserIsNotOwner() {
+        assertThatExceptionOfType(CannotDeleteException.class)
+                .isThrownBy(() -> A1.delete(NsUserTest.SANJIGI))
+                .withMessage("답변을 삭제할 권한이 없습니다.");
     }
 
     @Test
-    void temp2(){
-
+    void shouldDelete_WhenUserIsOwner() throws CannotDeleteException {
+        A1.delete(NsUserTest.JAVAJIGI);
+        assertThat(A1.isDeleted()).isTrue();
     }
 
 }
