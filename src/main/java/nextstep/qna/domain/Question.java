e4a5586c437ee6d39586bba6b321c04274848c86
melodist <melt.ep00@gmail.com>
2025-04-07 16:25:22 +0900

    Refactor: Question.delete 추가

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -35,6 +36,15 @@ public class Question {
         this.writer = writer;
         this.title = title;
         this.contents = contents;
+    }
+
+    public List<DeleteHistory> delete(NsUser loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+
+        this.deleted = true;
+        return null;
     }
 
     public Long getId() {
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,31 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @DisplayName("질문 삭제 시 삭제 상태 변경")
+    @Test
+    void deleteStatusTest() throws CannotDeleteException {
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        question.delete(NsUserTest.JAVAJIGI);
+
+        assertThat(question.isDeleted()).isTrue();
+    }
+
+    @DisplayName("로그인 사용자와 질문 작성자가 다르면 예외 발생")
+    @Test
+    void delete_다른_사람이_쓴_글() {
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+        assertThatThrownBy(() -> question.delete(NsUserTest.SANJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
 }
