0d97b796e5ca461af58bd4608170753dd260a691
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-07 15:55:36 +0900

    [feat] Question Test 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,29 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("로그인 사용자가 질문한 사람과 다른 경우 CannotDeleteException 예외가 발생한다.")
+    void question_isNotOwner() {
+        assertThatThrownBy(() -> Q1.validateOwnerOrThrow(NsUserTest.SANJIGI))
+            .isInstanceOf(CannotDeleteException.class)
+            .hasMessage("질문을 삭제할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("로그인 사용자가 질문한 사람이 같은 경우 true를 반환한다.")
+    void question_isOwner() {
+        assertThat(Q2.validateOwnerOrThrow(NsUserTest.SANJIGI)).isTrue();
+    }
 }
+
