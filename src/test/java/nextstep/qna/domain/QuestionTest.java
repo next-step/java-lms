2f8b009b27cfba2f67e3eb2e39742a1a445027d4
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-07 16:00:58 +0900

    [feat] Question Test 작성

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -15,15 +15,16 @@ public class QuestionTest {
     @Test
     @DisplayName("로그인 사용자가 질문한 사람과 다른 경우 CannotDeleteException 예외가 발생한다.")
     void question_isNotOwner() {
-        assertThatThrownBy(() -> Q1.validateOwnerOrThrow(NsUserTest.SANJIGI))
+        assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
             .isInstanceOf(CannotDeleteException.class)
             .hasMessage("질문을 삭제할 수 없습니다.");
     }
 
     @Test
-    @DisplayName("로그인 사용자가 질문한 사람이 같은 경우 true를 반환한다.")
-    void question_isOwner() {
-        assertThat(Q2.validateOwnerOrThrow(NsUserTest.SANJIGI)).isTrue();
+    @DisplayName("로그인 사용자가 질문한 사람이 같은 경우 질문을 삭제한다.")
+    void question_isOwner() throws CannotDeleteException {
+        Q2.delete(NsUserTest.SANJIGI);
+        assertThat(Q2.isDeleted()).isTrue();
     }
 }
 
