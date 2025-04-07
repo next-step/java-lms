07d010991f33accb7f151c76078f3b3a4f2b1783
zoolake <junhomoon224@gmail.com>
2025-04-08 00:15:28 +0900

    feat : 삭제 로직을 도메인 객체로 옮긴다.(Question)

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,26 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    public void 작성자가_답변이없는질문을_삭제한다() throws Exception {
+        Q1.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void 작성자가_아니면_답변이없는질문을_삭제시_예외가_발생한다() throws Exception {
+        assertThatThrownBy(() -> Q2.delete(NsUserTest.JAVAJIGI)).isInstanceOf(CannotDeleteException.class);
+    }
+
 }
