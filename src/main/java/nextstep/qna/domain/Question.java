7bc74cad52dcf2daaa8bdf0b3c338433be6de360
JiEung2 <181757@jnu.ac.kr>
2025-04-09 21:17:29 +0900

    refactor 질문의 작성자가 본인인지 검사하는 기능 질문 클래스로 이동

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,5 +1,6 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
 import java.time.LocalDateTime;
@@ -37,6 +38,12 @@ public class Question {
         this.contents = contents;
     }
 
+    public void validateOwnership(NsUser loginUser) throws CannotDeleteException {
+        if (!isOwner(loginUser)) {
+            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
+        }
+    }
+
     public Long getId() {
         return id;
     }
@@ -68,7 +75,7 @@ public class Question {
         answers.add(answer);
     }
 
-    public boolean isOwner(NsUser loginUser) {
+    private boolean isOwner(NsUser loginUser) {
         return writer.equals(loginUser);
     }
 
