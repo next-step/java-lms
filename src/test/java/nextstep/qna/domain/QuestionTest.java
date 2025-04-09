163d9030acdd3eb4b6ac10098625d5e5f2a68976
JiEung2 <181757@jnu.ac.kr>
2025-04-09 22:38:10 +0900

    test 질문 삭제의 모든 과정을 delete 메서드에 추가하기 위해 테스트 수정

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -11,12 +11,12 @@ public class QuestionTest {
 
     @Test
     void 질문_작성자가_자신일_경우_예외가_발생하지_않는다() throws CannotDeleteException {
-        Q1.validateOwnership(NsUserTest.JAVAJIGI);
+        Q1.delete(NsUserTest.JAVAJIGI);
     }
 
     @Test
     void 질문_작성자가_타인일_경우_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Q1.validateOwnership(NsUserTest.SANJIGI))
+        Assertions.assertThatThrownBy(() -> Q1.delete(NsUserTest.SANJIGI))
                 .isInstanceOf(CannotDeleteException.class)
                 .hasMessageContaining("질문을 삭제할 권한이 없습니다.");
     }
