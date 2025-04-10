5ba98081b999861708bb059e3f6c4296c5bb039c
melodist <melt.ep00@gmail.com>
2025-04-10 18:42:59 +0900

    Refactor: ImageFileName 객체 생성

--- src/test/java/nextstep/courses/domain/SessionThumbnailTest.java
+++ src/test/java/nextstep/courses/domain/SessionThumbnailTest.java
@@ -2,8 +2,6 @@ package nextstep.courses.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -39,67 +37,6 @@ class SessionThumbnailTest {
         assertThatThrownBy(() -> new SessionThumbnail(fullFileName, fileSize, width, height))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("파일 크기는 1MB를 초과할 수 없습니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"test.txt", "test.doc", "test.exe"})
-    @DisplayName("허용되지 않는 확장자로 썸네일을 생성하면 예외가 발생한다")
-    void validateExtension(String fullFileName) {
-        // given
-        long fileSize = 500 * 1024;
-        int width = 300;
-        int height = 200;
-
-        // when & then
-        assertThatThrownBy(() -> new SessionThumbnail(fullFileName, fileSize, width, height))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("허용되지 않는 파일 확장자입니다.");
-    }
-
-    @Test
-    @DisplayName("이미지 크기가 최소 크기보다 작으면 예외가 발생한다")
-    void validateDimensions() {
-        // given
-        String fullFileName = "test.jpg";
-        long fileSize = 500 * 1024;
-        int width = 299;
-        int height = 199;
-
-        // when & then
-        assertThatThrownBy(() -> new SessionThumbnail(fullFileName, fileSize, width, height))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이미지는 최소 300x200 픽셀 이상이어야 합니다.");
-    }
-
-    @Test
-    @DisplayName("이미지 비율이 3:2가 아니면 예외가 발생한다")
-    void validateAspectRatio() {
-        // given
-        String fullFileName = "test.jpg";
-        long fileSize = 500 * 1024;
-        int width = 300;
-        int height = 201; // 300:201는 3:2 비율이 아님
-
-        // when & then
-        assertThatThrownBy(() -> new SessionThumbnail(fullFileName, fileSize, width, height))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이미지는 3:2 비율이어야 합니다.");
-    }
-
-    @Test
-    @DisplayName("3:2 비율의 이미지를 생성할 수 있다")
-    void createWithValidAspectRatio() {
-        // given
-        String fullFileName = "test.jpg";
-        long fileSize = 500 * 1024;
-        int width = 600;
-        int height = 400; // 600:400은 3:2 비율
-
-        // when
-        SessionThumbnail thumbnail = new SessionThumbnail(fullFileName, fileSize, width, height);
-
-        // then
-        assertThat(thumbnail).isNotNull();
     }
 
     @Test
