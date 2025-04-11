1641e2c94bf253fa3f5d3baffc6f9fd01e33055f
zoolake <junhomoon224@gmail.com>
2025-04-11 13:33:32 +0900

    feat : 지원하는 확장자가 아닌 경우 예외 발생

---
+++ src/test/java/nextstep/courses/domain/ImageExtensionTest.java
@@ -0,0 +1,16 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class ImageExtensionTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pdf", ".png", "1#2"})
+    public void 지원하는_확장자가_아닌_경우_예외가_발생한다(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ImageExtension.from(input));
+    }
+}
