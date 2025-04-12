d06010e46fa278cdecee6ece9f53c2fa4be0b7dd
baki <hgs6424@naver.com>
2025-04-12 23:16:47 +0900

    feat: add CoverImage class and corresponding test

---
+++ src/test/java/nextstep/courses/domain/CoverImageTest.java
@@ -0,0 +1,13 @@
+package nextstep.courses.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CoverImageTest {
+
+    @Test
+    @DisplayName("커버 이미지 생성 테스트")
+    void coverImageSizeTest() {
+        new CoverImage();
+    }
+}
