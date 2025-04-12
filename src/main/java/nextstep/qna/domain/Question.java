d8f480f6b6b527c09ce1de5b2d14a19373c062a1
yang Jiyoung <0112jyoung@naver.com>
2025-04-13 08:31:55 +0900

    [refactor] 미사용 함수 제거

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -42,13 +42,6 @@ public class Question {
         return id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContents() {
-        return contents;
-    }
 
     public NsUser getWriter() {
         return writer;
@@ -90,4 +83,6 @@ public class Question {
             throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
         }
     }
+
+
 }
