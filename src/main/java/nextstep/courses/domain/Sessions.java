a420b2ef9abce0e034e60523bbc2e0fbec68e819
zoolake <junhomoon224@gmail.com>
2025-04-12 22:50:50 +0900

    feat : Sessions (일급컬렉션) 생성

---
+++ src/main/java/nextstep/courses/domain/Sessions.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain;
+
+import java.util.List;
+
+public class Sessions {
+    private final List<Session> sessions;
+
+    public Sessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public int count() {
+        return sessions.size();
+    }
+}
