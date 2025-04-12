96ff8e43ff8f7f0ce0f53d42863b691a8007ca75
baki <hgs6424@naver.com>
2025-04-12 23:23:35 +0900

    feat: add size validation to CoverImage class and corresponding test

--- src/main/java/nextstep/courses/domain/CoverImage.java
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -1,5 +1,17 @@
 package nextstep.courses.domain;
 
 public class CoverImage {
+    private final Long size;
 
+    public CoverImage() {
+        this(0L);
+    }
+
+    public CoverImage(Long size) {
+        if (size > 1_048_576) {
+            throw new IllegalArgumentException("커버 이미지의 크기는 1MB 이하여야 합니다.");
+        }
+
+        this.size = size;
+    }
 }
--- src/test/java/nextstep/courses/domain/CoverImageTest.java
+++ src/test/java/nextstep/courses/domain/CoverImageTest.java
@@ -1,5 +1,6 @@
 package nextstep.courses.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +10,14 @@ public class CoverImageTest {
     @DisplayName("커버 이미지 생성 테스트")
     void coverImageSizeTest() {
         new CoverImage();
+    }
+
+    @Test
+    @DisplayName("커버 이미지 크기는 1MB 이하여야 한다.")
+    void coverImageSizeOverTest() {
+        new CoverImage(1_048_576L);
+        Assertions.assertThatThrownBy(() -> new CoverImage(1_048_577L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("커버 이미지의 크기는 1MB 이하여야 합니다.");;
     }
 }
