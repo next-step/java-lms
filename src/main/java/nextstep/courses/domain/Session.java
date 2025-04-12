08782baddb68f1609ab0a49c47246a6bb5665888
baki <hgs6424@naver.com>
2025-04-12 23:18:41 +0900

    feat: add cover image support to Session class and corresponding test

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -6,10 +6,16 @@ import java.time.LocalDateTime;
 public class Session {
     private final LocalDate startAt;
     private final LocalDate endAt;
+    private final CoverImage coverImage;
 
-    public Session(LocalDate startAt, LocalDate endAt) {
+    public Session(LocalDate startAt, LocalDate endAt, CoverImage coverImage) {
         this.startAt = startAt;
         this.endAt = endAt;
+        this.coverImage = coverImage;
+    }
+
+    public Session(LocalDate startAt, LocalDate endAt) {
+        this(startAt, endAt, new CoverImage());
     }
 
     public Session() {
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -10,7 +10,13 @@ class SessionTest {
     @Test
     @DisplayName("강의는 시작일과 종료일을 가진다.")
     void sessionHasStartAtAndEndAtTest() {
-        var session = new Session(LocalDate.now(), LocalDate.now());
+        new Session(LocalDate.now(), LocalDate.now());
+    }
+
+    @Test
+    @DisplayName("강의는 강의 커버 이미지 정보를 가진다.")
+    void sessionHasCoverImageTest() {
+        new Session(LocalDate.now(), LocalDate.now(), new CoverImage());
     }
 
 }
