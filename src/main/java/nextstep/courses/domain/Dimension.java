3255c2f2579d2090cf10f70d5c2a52bdbe34de7e
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-13 18:06:31 +0900

    [feat] Dimension

--- src/main/java/nextstep/courses/domain/Dimension.java
+++ src/main/java/nextstep/courses/domain/Dimension.java
@@ -1,5 +1,22 @@
 package nextstep.courses.domain;
 
 public class Dimension {
+  private static final int MIN_WIDTH = 300;
+  private static final int MIN_HEIGHT = 200;
+  private static final int WIDTH_RATIO = 3;
+  private static final int HEIGHT_RATIO = 2;
 
+  private final int width;
+  private final int height;
+
+  public Dimension(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public boolean isValid() {
+    return width >= MIN_WIDTH
+        && height >= MIN_HEIGHT
+        && width * HEIGHT_RATIO == height * WIDTH_RATIO;
+  }
 }
