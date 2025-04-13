98e940ba50579eee2792962de54df70be2033ee7
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-13 19:41:57 +0900

    [feat] Session

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,5 +1,23 @@
 package nextstep.courses.domain;
 
 public class Session {
+  private final SessionInformation info;
+  private final EnrollmentManager enrollment;
 
+  public Session(SessionInformation info, EnrollmentManager enrollment) {
+    this.info = info;
+    this.enrollment = enrollment;
+  }
+
+  public void register(nextstep.payments.domain.Payment payment) {
+    enrollment.register(payment);
+  }
+
+  public void updateStatus(SessionStatus status) {
+    enrollment.updateStatus(status);
+  }
+
+  public int currentEnrollment() {
+    return enrollment.count();
+  }
 }
