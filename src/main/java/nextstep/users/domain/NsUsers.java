6bb6d4a6d3b65272f6438f3095498fcfa2a84a44
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-04-13 18:02:06 +0900

    [feat] NsUsers

--- src/main/java/nextstep/users/domain/NsUsers.java
+++ src/main/java/nextstep/users/domain/NsUsers.java
@@ -1,5 +1,25 @@
 package nextstep.users.domain;
 
-public class NsUsers {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class NsUsers {
+  private final List<NsUser> values = new ArrayList<>();
+
+  public void add(NsUser user) {
+    values.add(user);
+  }
+
+  public int count() {
+    return values.size();
+  }
+
+  public List<NsUser> asList() {
+    return Collections.unmodifiableList(values);
+  }
+
+  public boolean contains(NsUser user) {
+    return values.contains(user);
+  }
 }
