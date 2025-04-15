6cf007b38380231429b7b3835fd07796cc732309
choincnp <whdudwns7777@gmail.com>
2025-04-15 17:28:30 +0900

    test: Add test method for invalid/valid writer in Answers class

---
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -0,0 +1,21 @@
+package nextstep.qna.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUser;
+
+public class Answers {
+    private final List<Answer> answers = new ArrayList<>();
+
+    public void add(Answer answer) {
+        answers.add(answer);
+    }
+
+    public void deleteAllBy(NsUser user) throws CannotDeleteException {
+        for (Answer answer : answers) {
+            answer.deleteBy(user);
+        }
+    }
+}
---
+++ src/test/java/nextstep/qna/domain/AnswersTest.java
@@ -0,0 +1,48 @@
+package nextstep.qna.domain;
+
+import static nextstep.qna.domain.AnswerTest.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
+import nextstep.users.domain.NsUserTest;
+
+public class AnswersTest {
+    public Answers differentWriterSet = new Answers();
+    public Answers sameWriterSet = new Answers();
+
+    public static final Answer A3 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents3");
+
+    @BeforeEach
+    void setUp() {
+        differentWriterSet.add(A1);
+        differentWriterSet.add(A2);
+        differentWriterSet.add(A3);
+        sameWriterSet.add(A1);
+        sameWriterSet.add(A3);
+    }
+
+    @Test
+    void 답변자가_전부_같지_않을경우_예외() {
+        assertAll(
+            () -> assertDoesNotThrow(
+                () -> sameWriterSet.deleteAllBy(NsUserTest.JAVAJIGI)
+            ),
+            () -> assertThatThrownBy(
+                () -> differentWriterSet.deleteAllBy(NsUserTest.JAVAJIGI)
+            ).isInstanceOf(CannotDeleteException.class)
+        );
+    }
+
+    @Test
+    void 답변자가_모두_같을경우_삭제() {
+        sameWriterSet.deleteAllBy(NsUserTest.JAVAJIGI);
+        assertAll(
+            () -> assertTrue(A1.isDeleted()),
+            () -> assertTrue(A3.isDeleted())
+        );
+    }
+}
