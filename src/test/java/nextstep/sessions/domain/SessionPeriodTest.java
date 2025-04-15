e3f5f4d1d79e1db41038f1db8c46d09e39e46b8a
JiEung2 <181757@jnu.ac.kr>
2025-04-15 20:20:06 +0900

    test SessionPeriod 테스트 추가

---
+++ src/test/java/nextstep/sessions/domain/SessionPeriodTest.java
@@ -0,0 +1,34 @@
+package nextstep.sessions.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class SessionPeriodTest {
+    private Date earlier;
+    private Date later;
+
+    @BeforeEach
+    void setUp() {
+        this.earlier = new Date(2025, 1, 1);
+        this.later = new Date(2025, 3, 1);
+    }
+
+    @Test
+    @DisplayName("시작일이 종료일보다 이전이면 기간을 정상 생성한다.")
+    void 기간_정상_생성() {
+        SessionPeriod sessionPeriod = new SessionPeriod(earlier, later);
+    }
+
+    @Test
+    @DisplayName("시작일이 종료일보다 늦으면 IllegalArgumentException을 던진다.")
+    void 시작일이_종료일보다_늦으면_예외발생() {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(()
+                -> new SessionPeriod(later, earlier))
+                .withMessage("시작일은 종료일보다 이전이어야 합니다.");
+
+    }
+}
