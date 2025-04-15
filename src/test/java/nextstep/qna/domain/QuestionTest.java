e99878e103391baac6950375816dcf6dc307c9eb
choincnp <whdudwns7777@gmail.com>
2025-04-15 16:14:13 +0900

    test: Add test method for invalid writer

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,26 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    public void 질문자_불일치() {
+        assertNotEquals(Q1.getWriter(), Q2.getWriter());
+    }
+
+    @Test
+    public void 질문자_불일치시_예외_발생() {
+        assertThatThrownBy(
+            () -> Q1.deleteBy(Q2.getWriter())
+        ).isInstanceOf(CannotDeleteException.class);
+    }
 }
