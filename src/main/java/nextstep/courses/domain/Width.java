06e52d32bbe36f548f0b24ea93de484136bfbaf1
baki <hgs6424@naver.com>
2025-04-17 22:08:23 +0900

    feat: add Width class with validation and corresponding test

---
+++ src/main/java/nextstep/courses/domain/Width.java
@@ -0,0 +1,14 @@
+package nextstep.courses.domain;
+
+public class Width {
+    private final Integer value;
+
+    public Width(Integer width) {
+        if (width < 300) {
+            throw new IllegalArgumentException("너비는 300px 이상이어야 합니다.");
+        }
+
+        this.value = width;
+
+    }
+}
---
+++ src/test/java/nextstep/courses/domain/WidthTest.java
@@ -0,0 +1,20 @@
+package nextstep.courses.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WidthTest {
+
+    @Test
+    @DisplayName("너비 생성 테스트")
+    void widthTest() {
+        Assertions.assertThat(new Width(300)).isInstanceOf(Width.class);
+        Assertions.assertThatThrownBy(() -> new Width(299))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("너비는 300px 이상이어야 합니다.");
+    }
+
+}
