00243b5c42c70c8004536b3c390f64aab500815d
baki <hgs6424@naver.com>
2025-04-18 23:04:18 +0900

    feat: refactor Course class to use final fields and simplify constructors

--- src/main/java/nextstep/courses/domain/Course.java
+++ src/main/java/nextstep/courses/domain/Course.java
@@ -3,39 +3,32 @@ package nextstep.courses.domain;
 import java.time.LocalDateTime;
 
 public class Course {
-    private Long id;
+    private final Long id;
 
-    private String title;
+    private final String title;
 
-    private Generation generation;
+    private final Generation generation;
 
-    private Sessions sessions;
+    private final Sessions sessions;
 
-    private Long creatorId;
+    private final Long creatorId;
 
-    private LocalDateTime createdAt;
+    private final LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
 
-    public Course(Integer generation) {
-        this.generation = new Generation(generation);
-    }
-
     public Course(Integer generation, Sessions sessions) {
-        this.generation = new Generation(generation);
-        this.sessions = sessions;
+        this(0L, "", generation, sessions, 0L);
     }
 
-    public Course(String title, Long creatorId) {
-        this(0L, title, creatorId, LocalDateTime.now(), null);
-    }
-
-    public Course(Long id, String title, Long creatorId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    private Course(Long id, String title, Integer generation, Sessions sessions, Long creatorId) {
         this.id = id;
         this.title = title;
+        this.generation = new Generation(generation);
+        this.sessions = sessions;
         this.creatorId = creatorId;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
     public String getTitle() {
