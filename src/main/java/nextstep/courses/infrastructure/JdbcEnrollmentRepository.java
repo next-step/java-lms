d60c9a07d6d6e8f7bbde66caa9f26b047ebcfa51
zoolake <junhomoon224@gmail.com>
2025-04-18 16:40:47 +0900

    refactor : 미사용 import 구문 삭제

--- src/main/java/nextstep/courses/infrastructure/JdbcEnrollmentRepository.java
+++ src/main/java/nextstep/courses/infrastructure/JdbcEnrollmentRepository.java
@@ -1,7 +1,6 @@
 package nextstep.courses.infrastructure;
 
 import nextstep.courses.domain.*;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
