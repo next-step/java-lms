2c4f700c06d56612849894941b94756ab7173fe0
choincnp <whdudwns7777@gmail.com>
2025-04-22 01:02:32 +0900

    test: Add session price to divide session into free or paid

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -3,15 +3,31 @@ package nextstep.courses.domain;
 import java.time.LocalDate;
 
 /**
- * 강의 엔터티
+ * 강의 엔터티 - 가변 객체
  */
 public class Session {
     private final Long id;
     private final Period period;
+    private SessionStatus status;
+    private final Integer price;
 
     public Session(Long id, Period period) {
+        this(id, period, SessionStatus.PREPARING);
+    }
+
+    public Session(Long id, Period period, SessionStatus status) {
+        this(id, period, status, 0);
+    }
+
+    public Session(Long id, Period period, SessionStatus status, int price) {
         this.id = id;
         this.period = period;
+        this.status = status;
+        this.price = price;
+    }
+
+    public boolean canRegister() {
+        return status.equals(SessionStatus.OPEN);
     }
 
     public LocalDate startAt() {
@@ -20,5 +36,9 @@ public class Session {
 
     public LocalDate endAt() {
         return period.endAt();
+    }
+
+    public boolean isFree() {
+        return price == 0;
     }
 }
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -4,19 +4,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SessionTest {
+
+    Period period;
+
+    @BeforeEach
+    public void setUp() {
+        period = new Period(LocalDate.now(), LocalDate.now().plusDays(1));
+    }
+
     @Test
     @DisplayName("강의는 시작일과 종료일을 가진다.")
     void createSession() {
-        Period period = new Period(LocalDate.now(), LocalDate.now().plusDays(1));
         Session session = new Session(1L, period);
-        Assertions.assertAll(
-            () -> assertNotNull(session.startAt()),
-            () -> assertNotNull(session.endAt())
+        assertAll(
+            () -> assertEquals(session.startAt(), LocalDate.now()),
+            () -> assertEquals(session.endAt(), LocalDate.now().plusDays(1))
+        );
+    }
+
+    @Test
+    @DisplayName("강의는 모집중 상태일때만 수강신청이 가능하다")
+    void unableToRegister() {
+        Session preparingSession = new Session(1L, period);
+        Session openSession = new Session(1L, period, SessionStatus.OPEN);
+        Session closedSession = new Session(1L, period, SessionStatus.CLOSED);
+        assertAll(
+            () -> assertFalse(preparingSession.canRegister()),
+            () -> assertTrue(openSession.canRegister()),
+            () -> assertFalse(closedSession.canRegister())
+        );
+    }
+
+    @Test
+    @DisplayName("강의는 유료 강의와 무료 강의로 나뉜다")
+    void paidSessionTest() {
+        Session paidSession = new Session(1L, period, SessionStatus.OPEN, 10000);
+        Session freeSession = new Session(2L, period, SessionStatus.OPEN);
+        assertAll(
+            () -> assertFalse(paidSession.isFree()),
+            () -> assertTrue(freeSession.isFree())
         );
     }
 }
