e7d9a5d4c840a632a89b661198c007deac1cf3a2
zoolake <junhomoon224@gmail.com>
2025-04-21 23:58:30 +0900

    refactor : 미사용 import 구문 제거

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
