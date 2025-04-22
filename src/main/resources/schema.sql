987d58b4429a6f7a3c3b8188ad606c7bcec7cc2e
kass.fresh <kass.fresh@kakaocorp.com>
2025-04-22 13:28:55 +0900

    db: add Member, Enrollment table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,6 +27,28 @@ CREATE TABLE session
     FOREIGN KEY (course_id) REFERENCES course (id)
 );
 
+CREATE TABLE member
+(
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE enrollment
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    session_id BIGINT      NOT NULL,
+    member_id  BIGINT      NOT NULL,
+    status     VARCHAR(50) NOT NULL, -- PENDING, APPROVED, REJECTED
+
+    -- FK 설정
+    FOREIGN KEY (session_id) REFERENCES session (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+
+    UNIQUE (session_id, member_id)   -- 중복 수강 신청 방지
+);
+
+
 CREATE TABLE image
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
