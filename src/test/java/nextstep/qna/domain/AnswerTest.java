3cdb2ed6525d56a8ceb65695cdfc027169fec86e
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-11-25 15:12:52 +0900

    refactor : Question 객체에 질문 삭제 권한 체크 메서드 생성 및 테스트 코드 작성, Answer 객체에 삭제 권한 체크 메서드 생성 및 테스트 코드 작성

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,18 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void 다른_사용자가_작성한_답변이_존재하면_에러발생(){
+        Assertions.assertThrows(CannotDeleteException.class, () -> {
+            A1.validateAnswerOwner(NsUserTest.SANJIGI);
+        });
+    }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,21 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    void 글쓴이가_로그인한_유저와_같지않으면_에러발생(){
+        Assertions.assertThrows(CannotDeleteException.class, () -> {
+            Q1.validateOwner(NsUserTest.SANJIGI);
+        });
+    }
+
+
+
 }
