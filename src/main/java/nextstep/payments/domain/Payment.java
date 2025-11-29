b21ad439db6b1c71cf27b93d6c9d5c6446203993
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-11-29 22:21:55 +0900

    feat : Payment 클래스 테스트코드 작성

--- src/main/java/nextstep/payments/domain/Payment.java
+++ src/main/java/nextstep/payments/domain/Payment.java
@@ -19,11 +19,48 @@ public class Payment {
     public Payment() {
     }
 
+    public Payment(Long sessionId, Long nsUserId, Long amount) {
+        this(null, sessionId, nsUserId, amount);
+    }
+
     public Payment(String id, Long sessionId, Long nsUserId, Long amount) {
+        validateSessionId(sessionId);
+        validateNsUserId(nsUserId);
+        validateAmount(amount);
         this.id = id;
         this.sessionId = sessionId;
         this.nsUserId = nsUserId;
         this.amount = amount;
         this.createdAt = LocalDateTime.now();
+    }
+
+    private void validateSessionId(Long sessionId) {
+        if (sessionId == null) {
+            throw new IllegalArgumentException("결제한 강의 아이디는 필수입니다.");
+        }
+    }
+
+    private void validateNsUserId(Long nsUserId) {
+        if (nsUserId == null) {
+            throw new IllegalArgumentException("사용자 아이디는 필수입니다.");
+        }
+    }
+
+    private void validateAmount(Long amount) {
+        if (amount == null) {
+            throw new IllegalArgumentException("결제 금액은 필수입니다.");
+        }
+    }
+
+    public Long getSessionId() {
+        return sessionId;
+    }
+
+    public Long getNsUserId() {
+        return nsUserId;
+    }
+
+    public Long getAmount() {
+        return amount;
     }
 }
---
+++ src/test/java/nextstep/payments/domain/PaymentTest.java
@@ -0,0 +1,37 @@
+package nextstep.payments.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PaymentTest {
+    public static final Payment P1 = new Payment(1L, 2L, 30_000L);
+
+    @Test
+    void 결제_정상_생성() {
+        assertThat(P1.getSessionId()).isEqualTo(1L);
+        assertThat(P1.getNsUserId()).isEqualTo(2L);
+        assertThat(P1.getAmount()).isEqualTo(30_000L);
+    }
+
+    @Test
+    void 강의아이디_null_예외발생() {
+        assertThatThrownBy(() -> new Payment(null, 1L, 30_000L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("결제한 강의 아이디는 필수입니다.");
+    }
+
+    @Test
+    void 사용자아이디_null_예외발생() {
+        assertThatThrownBy(() -> new Payment(1L, null, 30_000L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("사용자 아이디는 필수입니다.");
+    }
+
+    @Test
+    void 결제금액_null_예외발생() {
+        assertThatThrownBy(() -> new Payment(1L, 2L, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("결제 금액은 필수입니다.");
+    }
+}
