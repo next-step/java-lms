0746509085931964f8e8a0894787fdb6c8acf66a
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-12-01 10:46:09 +0900

    refactor : SessionType from 메서드 StreamAPI로 변경

--- src/main/java/nextstep/courses/domain/session/constant/SessionType.java
+++ src/main/java/nextstep/courses/domain/session/constant/SessionType.java
@@ -1,14 +1,18 @@
 package nextstep.courses.domain.session.constant;
 
+import java.util.Arrays;
+
 public enum SessionType {
     FREE, PAID;
 
     public static SessionType from(String value) {
-        for (SessionType status : SessionType.values()) {
-            if (value.equals(status.name())) {
-                return status;
-            }
-        }
-        throw new IllegalArgumentException("강의는 무료 강의와 유료 강의만 개설 가능합니다.");
+        return Arrays.stream(SessionType.values())
+                .filter(type -> type.matchType(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("강의는 무료 강의와 유료 강의만 개설 가능합니다."));
+    }
+
+    private boolean matchType(String value) {
+        return this.name().equals(value);
     }
 }
