5295a285b54243e5a642df325d8c074df2ab7b04
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-12-01 10:43:45 +0900

    refactor : SessionStatus from 메서드 StreamAPI로 변경

--- src/main/java/nextstep/courses/domain/session/constant/SessionStatus.java
+++ src/main/java/nextstep/courses/domain/session/constant/SessionStatus.java
@@ -1,17 +1,21 @@
 package nextstep.courses.domain.session.constant;
 
+import java.util.Arrays;
+
 public enum SessionStatus {
     PENDING,
     ACTIVE,
     FINISHED;
 
     public static SessionStatus from(String value) {
-        for (SessionStatus status : SessionStatus.values()) {
-            if (value.equals(status.name())) {
-                return status;
-            }
-        }
-        throw new IllegalArgumentException("준비중, 모집중, 종료 3가지 상태로만 생성 가능합니다.");
+        return Arrays.stream(SessionStatus.values())
+                .filter(status -> status.matchStatus(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("준비중, 모집중, 종료 3가지 상태로만 생성 가능합니다."));
+    }
+
+    private boolean matchStatus(String value) {
+        return this.name().equals(value);
     }
 
 }
