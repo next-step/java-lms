0b3116b17508cf891d716660cd0d9c11b1c561ff
julee-0430 <yeonju.lee21@gmail.com>
2025-12-03 23:40:43 +0900

    [refactor] Question 클래스에 delete 메서드 추가

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -1,92 +1,64 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Question {
-    private Long id;
-
-    private String title;
-
-    private String contents;
-
-    private NsUser writer;
-
-    private List<Answer> answers = new ArrayList<>();
-
-    private boolean deleted = false;
-
-    private LocalDateTime createdDate = LocalDateTime.now();
-
-    private LocalDateTime updatedDate;
-
-    public Question() {
-    }
+public class Question extends SoftDeletable{
+    private QuestionBody content;
+    private Answers answers = new Answers();
+    private TimeStamp timestamp = new TimeStamp();
 
     public Question(NsUser writer, String title, String contents) {
         this(0L, writer, title, contents);
     }
 
     public Question(Long id, NsUser writer, String title, String contents) {
-        this.id = id;
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
+        this(new QuestionBody(id, writer, title, contents));
+    }
+
+    public Question(QuestionBody questionBody) {
+        this.content = questionBody;
     }
 
     public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Question setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public Question setContents(String contents) {
-        this.contents = contents;
-        return this;
-    }
-
-    public NsUser getWriter() {
-        return writer;
+        return content.getId();
     }
 
     public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
         answers.add(answer);
     }
 
-    public boolean isOwner(NsUser loginUser) {
-        return writer.equals(loginUser);
+    private boolean isOwner(NsUser loginUser) {
+        return content.isWrittenBy(loginUser);
     }
 
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
+    @Override
+    public void deleteBy(NsUser requestUser) throws CannotDeleteException {
+        if (!isOwner(requestUser)) {
+            throw new CannotDeleteException("작성자 외에는 질문을 삭제할 수 없습니다.");
+        }
+
+        delete();
+        answers.deleteBy(requestUser);
     }
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+    public List<DeleteHistory> toDeleteHistories() throws CannotDeleteException {
+        if (!isDeleted()) {
+            return Collections.emptyList();
+        }
+        List<DeleteHistory> deleteHistories = new ArrayList<>();
 
-    public List<Answer> getAnswers() {
-        return answers;
+        deleteHistories.add(content.toDeleteHistory());
+        deleteHistories.addAll(answers.toDeleteHistories());
+        return deleteHistories;
     }
 
     @Override
     public String toString() {
-        return "Question [id=" + getId() + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+        return "Question [" + content.toString() + "]";
     }
 }
--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,57 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
-    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    private Question q1;
+    private Answer a1;
+    private Answer a2;
+
+    @BeforeEach
+    void setUp() {
+        q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+
+        a1 = new Answer(NsUserTest.JAVAJIGI, q1, "answer content1");
+        a2 = new Answer(NsUserTest.SANJIGI, q1, "answer content2");
+    }
+
+    @Test
+    @DisplayName("답변이 없는 질문은 요청자와 작성자가 같은 경우 삭제한다.")
+    void deleteQuestion() throws Exception {
+        q1.deleteBy(NsUserTest.JAVAJIGI);
+        assertThat(q1.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("질문과 답변의 작성자가 같고 요청자도 경우, 질문과 답변 모두 삭제한다.")
+    void deleteQuestionAnswers() throws Exception {
+        q1.addAnswer(a1);
+        q1.deleteBy(NsUserTest.JAVAJIGI);
+        assertThat(q1.isDeleted()).isTrue();
+        assertThat(a1.isDeleted()).isTrue();
+    }
+
+
+    @Test
+    @DisplayName("질문과 답변의 작성자가 다른 경우, 질문과 답변 모두 삭제되지 않는다.")
+    void invalidDeleteQuestionAnswers() {
+        q1.addAnswer(a2);
+        assertThatThrownBy(() -> q1.deleteBy(NsUserTest.JAVAJIGI))
+                .isInstanceOf(CannotDeleteException.class);
+    }
+
+    @Test
+    @DisplayName("삭제 히스토리로 생성 시 정상적으로 질문과 답변 모두 삭제 히스토리로 변환된다.")
+    void toDeleteHistory() throws Exception {
+        q1.addAnswer(a1);
+        q1.deleteBy(NsUserTest.JAVAJIGI);
+        assertThat(q1.toDeleteHistories()).hasSize(2);
+    }
 }
