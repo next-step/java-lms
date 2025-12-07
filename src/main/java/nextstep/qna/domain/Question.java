a686182e9a41fbbcc568156da3af8d97b6e7cefb
username0w <163955522+username0w@users.noreply.github.com>
2025-12-08 03:55:58 +0900

    refactor: Question 구조 개선 및 BaseTimeEntity/BaseEntity 적용

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -6,67 +6,31 @@ import java.util.List;
 import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUser;
 
-public class Question {
-    private Long id;
-
+public class Question extends BaseEntity {
     private QuestionContent content;
 
-    private NsUser writer;
-
     private Answers answers = new Answers();
-
-    private boolean deleted = false;
-
-    private LocalDateTime createdDate = LocalDateTime.now();
-
-    private LocalDateTime updatedDate;
-
-    public Question() {
-    }
 
     public Question(NsUser writer, QuestionContent content) {
         this(0L, writer, content);
     }
 
     public Question(Long id, NsUser writer, QuestionContent content) {
-        this.id = id;
-        this.writer = writer;
+        super(id, writer);
         this.content = content;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getTitle() {
         return content.title();
     }
 
-
     public String getContents() {
         return content.contents();
-    }
-
-    public NsUser getWriter() {
-        return writer;
     }
 
     public void addAnswer(Answer answer) {
         answer.toQuestion(this);
         answers.add(answer);
-    }
-
-    public boolean isOwner(NsUser loginUser) {
-        return writer.equals(loginUser);
-    }
-
-    public Question setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
     }
 
     public Answers getAnswers() {
@@ -80,13 +44,13 @@ public class Question {
         List<DeleteHistory> deleteHistories = new ArrayList<>();
         setDeleted(true);
         deleteHistories.add(
-                new DeleteHistory(ContentType.QUESTION, id, writer, LocalDateTime.now()));
+                new DeleteHistory(ContentType.QUESTION, getId(), getWriter(), LocalDateTime.now()));
         deleteHistories.addAll(answers.deleteAll(user));
         return deleteHistories;
     }
 
     @Override
     public String toString() {
-        return "Question [id=" + getId() + ", content=" + content + ", writer=" + writer + "]";
+        return "Question [id=" + getId() + ", content=" + content + ", writer=" + getWriter() + "]";
     }
 }
