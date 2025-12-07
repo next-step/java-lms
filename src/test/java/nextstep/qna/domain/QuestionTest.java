d52ed7e72ba94317beaf1a6a498e5098328b26a3
username0w <163955522+username0w@users.noreply.github.com>
2025-12-07 17:48:26 +0900

    feat: 질문 삭제 권한 체크 로직을 도메인으로 이동

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,31 @@
 package nextstep.qna.domain;
 
+import static nextstep.users.domain.NsUserTest.SANJIGI;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
-    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+    public static final Question Q2 = new Question(SANJIGI, "title2", "contents2");
+
+    @DisplayName("질문 작성자는 자신의 질문을 삭제할 수 있다")
+    @Test
+    void shouldNotThrow_whenUserIsOwnerAndNoOtherAnswers() {
+        assertThatCode(() -> Q2.delete(SANJIGI))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("질문자와 로그인 사용자가 다른 경우 삭제 불가하다")
+    @Test
+    void shouldThrow_whenUserAndWriterDifferent() {
+        assertThatThrownBy(() -> Q1.delete(SANJIGI))
+                .isInstanceOf(CannotDeleteException.class)
+                .hasMessageContaining("질문을 삭제할 권한이 없습니다");
+    }
+
 }
