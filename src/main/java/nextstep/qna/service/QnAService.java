d7b11baa006f0269f6c8b84044f474eb93335f4b
qwer920414-ctrl <qwer920414@gmail.com>
2025-12-11 04:08:12 +0900

    feat: Question - validateOwner()

--- src/main/java/nextstep/qna/service/QnAService.java
+++ src/main/java/nextstep/qna/service/QnAService.java
@@ -26,10 +26,7 @@ public class QnAService {
     @Transactional
     public void deleteQuestion(NsUser loginUser, long questionId) throws CannotDeleteException {
         Question question = questionRepository.findById(questionId).orElseThrow(NotFoundException::new);
-
-        if (!question.isOwner(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
+        question.validateOwner(loginUser);
 
         Answers answers = question.getAnswers();
         answers.validateDeletableUser(loginUser);
