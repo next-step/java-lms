0e4b58a0c72a643441f27dd44d547b5f9b8c5b2c
ho-jun97 <hojun9707@naver.com>
2025-12-14 02:30:38 +0900

    feat: 강의 상태(준비중,모집중,종료)

---
+++ src/main/java/nextstep/courses/domain/SessionStatus.java
@@ -0,0 +1,18 @@
+package nextstep.courses.domain;
+
+public enum SessionStatus {
+    PREPARING("준비중"),
+    RECRUITING("모집중"),
+    END("종료"),
+    ;
+
+    private final String status;
+
+    SessionStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean enableRecruiting() {
+        return this == SessionStatus.RECRUITING;
+    }
+}
