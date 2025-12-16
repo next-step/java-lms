3612e77e6078ff6cee9d5d3adaf3f5818c4fd958
qwer920414-ctrl <qwer920414@gmail.com>
2025-12-16 22:58:16 +0900

    feat: Session -> CoverImage 클래스 생성자 추가 (Session 구현 전, 빠진 부분 점검..)

--- src/main/java/nextstep/courses/domain/CoverImage.java
+++ src/main/java/nextstep/courses/domain/CoverImage.java
@@ -4,4 +4,14 @@ public class CoverImage {
     private ImageSize imageSize;
     private ImageType imageType;
     private ImageDimension imageDimension;
+
+    public CoverImage(int size, ImageType imageType, int width, int height) {
+        this(new ImageSize(size), imageType, new ImageDimension(width, height));
+    }
+
+    public CoverImage(ImageSize imageSize, ImageType imageType, ImageDimension imageDimension) {
+        this.imageSize = imageSize;
+        this.imageType = imageType;
+        this.imageDimension = imageDimension;
+    }
 }
