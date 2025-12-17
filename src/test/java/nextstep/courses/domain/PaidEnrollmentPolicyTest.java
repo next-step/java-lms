7dd2e8c62e48ac5d1e521cdc10a68ea0b8c093a9
qwer920414-ctrl <qwer920414@gmail.com>
2025-12-18 02:05:03 +0900

    test: PaidEnrollmentPolicy 테스트코드 추가

---
+++ src/test/java/nextstep/courses/domain/PaidEnrollmentPolicyTest.java
@@ -0,0 +1,50 @@
+package nextstep.courses.domain;
+
+import nextstep.payments.domain.Payment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PaidEnrollmentPolicyTest {
+
+    @Test
+    @DisplayName("유료강의 - 성공")
+    void validateEnrollment_success() {
+        Money price = new Money(5000);
+        Capacity capacity = new Capacity(30, 10);
+        PaidEnrollmentPolicy policy = new PaidEnrollmentPolicy(price, capacity);
+
+        Payment payment = new Payment("p1", 1L, 1L, 5_000L);
+
+        assertThatCode(() -> policy.validateEnrollment(payment))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("유료강의 - 수강생 초과 실패")
+    void validateEnrollment_fail_capacity() {
+        Money price = new Money(5000);
+        Capacity capacity = new Capacity(30, 31);
+        PaidEnrollmentPolicy policy = new PaidEnrollmentPolicy(price, capacity);
+
+        Payment payment = new Payment("p1", 1L, 1L, 5_000L);
+
+        assertThatThrownBy(() -> policy.validateEnrollment(payment))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("유료강의 - 금액 불일치 실패")
+    void validateEnrollment_fail_amount() {
+        Money price = new Money(5000);
+        Capacity capacity = new Capacity(30, 20);
+        PaidEnrollmentPolicy policy = new PaidEnrollmentPolicy(price, capacity);
+
+        Payment payment = new Payment("p1", 1L, 1L, 4_000L);
+
+        assertThatThrownBy(() -> policy.validateEnrollment(payment))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
