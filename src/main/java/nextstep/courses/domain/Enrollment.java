029ed1c955a8a38d77e840c49d9fa7bdd4b5497e
JoonKyoLee <joonkyolee@gmail.com>
2025-12-21 17:05:20 +0900

    feat: 수강 신청을 표현하는 Enrollment 추가

---
+++ src/main/java/nextstep/courses/domain/Enrollment.java
@@ -0,0 +1,31 @@
+package nextstep.courses.domain;
+
+import java.util.Objects;
+
+public class Enrollment {
+
+    private final Long studentId;
+    private final Long sessionId;
+
+    public Enrollment(Long studentId, Long sessionId) {
+        this.studentId = studentId;
+        this.sessionId = sessionId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Enrollment that = (Enrollment) o;
+        return Objects.equals(studentId, that.studentId) && Objects.equals(sessionId, that.sessionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, sessionId);
+    }
+}
