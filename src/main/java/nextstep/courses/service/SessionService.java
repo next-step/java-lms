b4b1f5bab6bfb101c57bb2ca950c5b6561742276
qwer920414-ctrl <qwer920414@gmail.com>
2025-12-25 18:13:12 +0900

    feat: SessionService - SessionService 구현(repository 묶음)

---
+++ src/main/java/nextstep/courses/service/SessionService.java
@@ -0,0 +1,28 @@
+package nextstep.courses.service;
+
+import nextstep.courses.domain.Enrollment;
+import nextstep.courses.domain.Session;
+import nextstep.courses.repository.EnrollmentRepository;
+import nextstep.courses.repository.SessionRepository;
+import nextstep.payments.domain.Payment;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SessionService {
+
+    private final SessionRepository sessionRepository;
+    private final EnrollmentRepository enrollmentRepository;
+
+    public SessionService(SessionRepository sessionRepository, EnrollmentRepository enrollmentRepository) {
+        this.sessionRepository = sessionRepository;
+        this.enrollmentRepository = enrollmentRepository;
+    }
+
+    public void enroll(Long sessionId, Long userId, Payment payment) {
+        Session session = sessionRepository.findById(sessionId);
+        Enrollment enrollment = session.enroll(userId, payment);
+        enrollmentRepository.save(enrollment);
+    }
+}
