79b2516760d55b14017f3b151eee5f154d8e2102
이국신 <nw4611@naver.com>
2025-12-31 08:43:24 +0900

    [chore] id 수정

--- src/test/java/nextstep/qna/domain/DeleteHistoryTest.java
+++ src/test/java/nextstep/qna/domain/DeleteHistoryTest.java
@@ -7,4 +7,5 @@ import java.time.LocalDateTime;
 public class DeleteHistoryTest {
     public static final DeleteHistory a1DeleteHistory = new DeleteHistory(ContentType.ANSWER, null, NsUserTest.JAVAJIGI, LocalDateTime.now());
     public static final DeleteHistory a2DeleteHistory = new DeleteHistory(ContentType.ANSWER, null, NsUserTest.SANJIGI, LocalDateTime.now());
+    public static final DeleteHistory q1DeleteHistory = new DeleteHistory(ContentType.QUESTION, 0L, NsUserTest.JAVAJIGI, LocalDateTime.now());
 }
