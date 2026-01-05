55bc61665629d62422e5ae94f564969138108a14
이국신 <nw4611@naver.com>
2026-01-05 20:52:45 +0900

    [refactor] private final 적용

--- src/main/java/nextstep/qna/domain/Answers.java
+++ src/main/java/nextstep/qna/domain/Answers.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class Answers {
-    List<Answer> answers;
+    private final List<Answer> answers;
 
     public Answers() {
         this(new ArrayList<>());
