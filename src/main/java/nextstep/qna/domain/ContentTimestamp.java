17518d428660e38bc5481cfc2ae824761f0cdd40
이국신 <nw4611@naver.com>
2026-01-05 21:23:25 +0900

    [refactor] 타임스탬프와 관련된 클래스 분리

---
+++ src/main/java/nextstep/qna/domain/ContentTimestamp.java
@@ -0,0 +1,21 @@
+package nextstep.qna.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class ContentTimestamp {
+    private final LocalDateTime createdDate = LocalDateTime.now();;
+    private LocalDateTime updatedDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ContentTimestamp that = (ContentTimestamp) o;
+        return Objects.equals(createdDate, that.createdDate) && Objects.equals(updatedDate, that.updatedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(createdDate, updatedDate);
+    }
+}
