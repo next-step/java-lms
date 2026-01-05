8b12d17486059439a63e0ba334d8d73898eebc50
이국신 <nw4611@naver.com>
2026-01-05 21:23:49 +0900

    [refactor] 컨텐트 내용과 관련된 클래스 분리

---
+++ src/main/java/nextstep/qna/domain/ContentBody.java
@@ -0,0 +1,36 @@
+package nextstep.qna.domain;
+
+import nextstep.users.domain.NsUser;
+
+import java.util.Objects;
+
+public class ContentBody {
+    private NsUser writer;
+    private String contents;
+
+    public ContentBody(NsUser writer, String contents) {
+        this.writer = writer;
+        this.contents = contents;
+    }
+
+    public boolean isOwner(NsUser loginUser) {
+        return writer.equals(loginUser);
+    }
+
+    public NsUser getWriter() {
+        return writer;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ContentBody that = (ContentBody) o;
+        return Objects.equals(writer, that.writer) && Objects.equals(contents, that.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(writer, contents);
+    }
+}
