de7a4e856748aaaf3e91d181328e4e668a626fca
이국신 <nw4611@naver.com>
2026-01-05 21:23:58 +0900

    [refactor] 컨텐트 상태와 관련된 클래스 분리

---
+++ src/main/java/nextstep/qna/domain/ContentStatus.java
@@ -0,0 +1,33 @@
+package nextstep.qna.domain;
+
+import java.util.Objects;
+
+public class ContentStatus {
+    private boolean deleted;
+    private final ContentTimestamp timestamp;
+
+    public ContentStatus() {
+        this.deleted = false;
+        this.timestamp = new ContentTimestamp();
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void delete() {
+        deleted = true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ContentStatus that = (ContentStatus) o;
+        return deleted == that.deleted && Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(deleted, timestamp);
+    }
+}
