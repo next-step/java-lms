29a491eb81530a2c72b3ea76e6b1d00f83b58927
이국신 <nw4611@naver.com>
2026-01-12 23:03:05 +0900

    [feat] 이미지 가로, 세로 비율 유효성 검사

--- src/main/java/nextstep/courses/domain/image/ImageSize.java
+++ src/main/java/nextstep/courses/domain/image/ImageSize.java
@@ -5,6 +5,9 @@ import nextstep.courses.ImageDimensionException;
 public class ImageSize {
     public static final int IMAGE_MIN_WIDTH = 300;
     public static final int IMAGE_MIN_HEIGHT = 200;
+    public static final int WIDTH_RATIO = 3;
+    public static final int HEIGHT_RATIO = 2;
+
     private final int width;
     private final int height;
 
@@ -18,6 +21,7 @@ public class ImageSize {
     public void validateDimension(int width, int height) {
         validateWidth(width);
         validateHeight(height);
+        validateRatio(width, height);
     }
 
     private void validateWidth(int width) {
@@ -29,6 +33,12 @@ public class ImageSize {
     private void validateHeight(int height) {
         if (height < IMAGE_MIN_HEIGHT) {
             throw new ImageDimensionException("이미지의 세로는 200을 넘어야합니다.");
+        }
+    }
+
+    private void validateRatio(int width, int height) {
+        if (width * HEIGHT_RATIO != height * WIDTH_RATIO) {
+            throw new ImageDimensionException("이미지의 가로, 세로 비율은 3:2여야합니다.");
         }
     }
 }
--- src/test/java/nextstep/courses/domain/image/ImageSizeTest.java
+++ src/test/java/nextstep/courses/domain/image/ImageSizeTest.java
@@ -1,6 +1,7 @@
 package nextstep.courses.domain.image;
 
 import nextstep.courses.ImageDimensionException;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -14,5 +15,15 @@ public class ImageSizeTest {
     @Test
     void exceedHeight() {
         assertThatThrownBy(() -> new ImageSize(300, 199)).isInstanceOf(ImageDimensionException.class);
+    }
+
+    @Test
+    void invalidImageRatio() {
+        assertThatThrownBy(() -> new ImageSize(400, 300)).isInstanceOf(ImageDimensionException.class);
+    }
+
+    @Test
+    void validImageRatio() {
+        assertDoesNotThrow(() -> new ImageSize(300, 200));
     }
 }
