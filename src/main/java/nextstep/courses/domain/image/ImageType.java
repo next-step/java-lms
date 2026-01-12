b444ebcb0674d21968ebb262daf914380b3a5069
이국신 <nw4611@naver.com>
2026-01-13 03:22:52 +0900

    [feat] ImageType 구현

---
+++ src/main/java/nextstep/courses/domain/image/ImageType.java
@@ -0,0 +1,26 @@
+package nextstep.courses.domain.image;
+
+public enum ImageType {
+    PNG, JPG, JPEG, SVG, UNKNOWN;
+
+    public static ImageType extract(String fileName) {
+        if (!isValidImageFileName(fileName)) {
+            return UNKNOWN;
+        }
+
+        try {
+            String extensionPart = parseExtension(fileName);
+            return ImageType.valueOf(extensionPart);
+        } catch(IllegalArgumentException e) {
+            return UNKNOWN;
+        }
+    }
+
+    private static boolean isValidImageFileName(String fileName) {
+        return fileName != null && !fileName.isBlank() && fileName.contains(".");
+    }
+
+    public static String parseExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1).toUpperCase();
+    }
+}
---
+++ src/test/java/nextstep/courses/domain/image/ImageTypeTest.java
@@ -0,0 +1,34 @@
+package nextstep.courses.domain.image;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ImageTypeTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"  "})
+    public void NullAndEmptyFileName(String fileName) {
+        assertThat(ImageType.extract(fileName)).isEqualTo(ImageType.UNKNOWN);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "a.png, PNG",
+            "b.jpg, JPG",
+            "c.jpeg, JPEG",
+            "d.svg, SVG"
+    })
+    public void extractType(String fileName, ImageType imageType) {
+        assertThat(ImageType.extract(fileName)).isEqualTo(imageType);
+    }
+
+    @Test
+    public void notAllowedFileExtension() {
+        assertThat(ImageType.extract("a.md")).isEqualTo(ImageType.UNKNOWN);
+    }
+}
