c7f5780deff9a25c35709a3faaa8a310cfc6a12e
이국신 <nw4611@naver.com>
2026-01-14 08:11:35 +0900

    [feat] Money 금액 불일치 테스트

--- src/test/java/nextstep/courses/domain/money/MoneyTest.java
+++ src/test/java/nextstep/courses/domain/money/MoneyTest.java
@@ -15,4 +15,9 @@ public class MoneyTest {
     public void equal() {
         assertThat(new Money("1000").isEqualTo(new Money(1000))).isTrue();
     }
+
+    @Test
+    public void notEqual() {
+        assertThat(new Money("1000").isEqualTo(new Money(1))).isFalse();
+    }
 }
