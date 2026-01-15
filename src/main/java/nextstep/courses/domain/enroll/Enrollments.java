daf71a61e651299a86cd1e577b9f37e24985eac3
이국신 <nw4611@naver.com>
2026-01-15 09:01:52 +0900

    [feat] enrollmentㄴ 구현

---
+++ src/main/java/nextstep/courses/domain/enroll/Enrollments.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain.enroll;
+
+import java.util.List;
+
+public class Enrollments {
+    private final List<Enrollment> enrollments;
+
+    public Enrollments(Enrollment ...enrollments) {
+            this.enrollments = List.of(enrollments);
+    }
+
+    public void addEnrollment(Enrollment enrollment) {
+        this.enrollments.add(enrollment);
+    }
+}
