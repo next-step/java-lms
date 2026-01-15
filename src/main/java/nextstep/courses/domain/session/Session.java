d77ecaa508e5043bf4e63b34323b34fc676b93a3
이국신 <nw4611@naver.com>
2026-01-15 09:02:10 +0900

    [feat] session에 등록시 enrollments 추가하도록 수정

--- src/main/java/nextstep/courses/domain/session/Session.java
+++ src/main/java/nextstep/courses/domain/session/Session.java
@@ -1,6 +1,8 @@
 package nextstep.courses.domain.session;
 
 import nextstep.courses.domain.capacity.Capacity;
+import nextstep.courses.domain.enroll.Enrollment;
+import nextstep.courses.domain.enroll.Enrollments;
 import nextstep.courses.domain.image.CoverImage;
 import nextstep.courses.domain.money.Money;
 import nextstep.courses.domain.policy.SessionPolicy;
@@ -13,19 +15,23 @@ public class Session {
     private Long id;
     private final SessionInfo sessionInfo;
     private final CoverImage coverImage;
+    private final Enrollments enrollments;
 
-    public Session(Long id, SessionStatus sessionStatus, SessionPolicy sessionPolicy, LocalDateTime startDateTime, LocalDateTime endDateTime, int capacity, CoverImage coverImage) {
-        this(id, new SessionInfo(sessionStatus, sessionPolicy, new SessionPeriod(startDateTime, endDateTime), new Capacity(capacity)), coverImage);
+    public Session(Long id, SessionStatus sessionStatus, SessionPolicy sessionPolicy, LocalDateTime startDateTime, LocalDateTime endDateTime, int capacity, CoverImage coverImage, Enrollments enrollments) {
+        this(id, new SessionInfo(sessionStatus, sessionPolicy, new SessionPeriod(startDateTime, endDateTime), new Capacity(capacity)), coverImage, enrollments);
     }
 
-    public Session(Long id, SessionInfo sessionInfo, CoverImage coverImage) {
+    public Session(Long id, SessionInfo sessionInfo, CoverImage coverImage, Enrollments enrollments) {
         this.id = id;
         this.sessionInfo = sessionInfo;
         this.coverImage = coverImage;
+        this.enrollments = enrollments;
     }
 
     public Payment enroll(String paymentId, NsUser nsUser, Long amount, LocalDateTime enrolledDateTime) {
         sessionInfo.validateEnrollment(new Money(amount), enrolledDateTime);
+        enrollments.addEnrollment(new Enrollment(nsUser.getId(), id));
+
         return new Payment(paymentId, id, nsUser.getId(), amount);
     }
 }
